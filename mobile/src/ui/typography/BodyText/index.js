213ba74bd12f808d7d2bc8a06593ddf534ec0bde
Burke Shartsis <burke@burkeshartsis.com>
2018-06-01 17:58:37 -0400

    setting up intial font stack for serif fonts

---
+++ mobile/src/ui/typography/BodyText/index.js
@@ -0,0 +1,51 @@
+import { Platform, Text } from 'react-native';
+import PropTypes from 'prop-types';
+import { compose, pure, setPropTypes, defaultProps } from 'recompose';
+import styled from 'ui/styled';
+import { withPlaceholder, Typography } from 'ui/Placeholder';
+
+const styles = styled(({ theme, bold, italic }) => {
+  let fontStack = theme.typography.serif.regular.default;
+
+  if (bold && italic) {
+    fontStack = theme.typography.serif.bold.italic;
+  } else if (bold) {
+    fontStack = theme.typography.serif.bold.default;
+  } else if (italic) {
+    fontStack = theme.typography.serif.regular.italic;
+  }
+
+  return {
+    fontSize: theme.helpers.rem(1.112),
+    fontFamily: fontStack,
+    color: theme.colors.text.primary,
+    ...Platform.select({
+      ios: {
+        paddingTop: theme.helpers.rem(0.1),
+        paddingBottom: theme.helpers.rem(0.125),
+        lineHeight: theme.helpers.verticalRhythm(1.112, 1.4889),
+      },
+      android: {
+        lineHeight: theme.helpers.verticalRhythm(1.112, 1.625),
+      },
+    }),
+  };
+}, 'BodyText');
+
+const BodyText = compose(
+  setPropTypes({
+    bold: PropTypes.bool,
+    italic: PropTypes.bool,
+    isLoading: PropTypes.bool, // display loading placeholder
+    ...Text.propTypes,
+  }),
+  defaultProps({
+    bold: false,
+    italic: false,
+  }),
+  styles,
+  withPlaceholder(Typography),
+  pure
+)(Text);
+
+export default BodyText;
