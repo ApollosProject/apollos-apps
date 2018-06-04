b6fdb8cbf7a4ef4fa9279d1e9cb53f50d20cd084
richarddubay <richardtdubayjr@gmail.com>
2018-06-04 16:07:43 -0400

    added links to the sections and created some files to link to

--- mobile/src/tabs/sections/index.js
+++ mobile/src/tabs/sections/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
+import PropTypes from 'prop-types';
+import Articles from '../../../src/articles';
+import Devotionals from '../../../src/devotionals';
+import News from '../../../src/news';
 
 const styles = StyleSheet.create({
   container: {
@@ -19,14 +23,35 @@ export class SectionsScreen extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Sections Screen</Text>
+        <Button
+          title="Articles"
+          onPress={() => this.props.navigation.navigate('Articles')}
+        />
+        <Button
+          title="Devotionals"
+          onPress={() => this.props.navigation.navigate('Devotionals')}
+        />
+        <Button
+          title="News"
+          onPress={() => this.props.navigation.navigate('News')}
+        />
       </View>
     );
   }
 }
 
+SectionsScreen.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }),
+};
+
 export const SectionsStack = createStackNavigator(
   {
     Sections: SectionsScreen,
+    Articles,
+    Devotionals,
+    News,
   },
   {
     initialRouteName: 'Sections',
