689d19a53b69e471d011b08c9a44dbbd1eac9983
Burke Shartsis <burke@burkeshartsis.com>
2018-06-06 11:20:47 -0400

    migrating Paragraph component. minor refactor ala #20

---
+++ mobile/src/ui/typography/Paragraph/index.js
@@ -0,0 +1,22 @@
+import { compose } from 'recompose';
+import PropTypes from 'prop-types';
+import { View } from 'react-native';
+
+import {
+  withPlaceholder,
+  Paragraph as ParagraphPlaceholder,
+} from 'ui/Placeholder';
+import styled from 'ui/styled';
+
+const Paragraph = compose(
+  styled(({ theme }) => ({
+    paddingVertical: theme.helpers.verticalRhythm(0.5, 0.5),
+  })),
+  withPlaceholder(ParagraphPlaceholder)
+)(View);
+
+Paragraph.propTypes = {
+  isLoading: PropTypes.bool, // display loading placeholder
+};
+
+export default Paragraph;
