e94fcac1c06c7b9d08782c0db6571014041f1335
Burke Shartsis <burke@burkeshartsis.com>
2018-06-07 14:23:56 -0400

    adding Chip

---
+++ mobile/src/ui/Chip/Chip.stories.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react-native';
+
+import FlexedView from 'ui/FlexedView';
+
+import Chip from './';
+
+storiesOf('Chip', module).add('default', () => (
+  <FlexedView style={{ flexWrap: 'wrap', flexDirection: 'row' }}>
+    <Chip onPress={() => {}} title="I'm just a poor chip" />
+    <Chip onPress={() => {}} icon="close" title="I need no sympathy" />
+    <Chip onPress={() => {}} selected title="Easy come" />
+    <Chip onPress={() => {}} icon="close" selected title="Easy go" />
+    <Chip onPress={() => {}} icon="arrow-up" title="Litte high" />
+    <Chip
+      onPress={() => {}}
+      type="secondary"
+      icon="arrow-down"
+      title="Litte low"
+    />
+    <Chip title="📍💨?" />
+    <Chip selected title="¯\_(ツ)_/¯" />
+  </FlexedView>
+));
