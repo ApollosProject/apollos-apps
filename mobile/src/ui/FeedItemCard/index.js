28de77ae6fdc6210a93dfff334c9cdd520be374a
richarddubay <richardtdubayjr@gmail.com>
2018-06-08 16:04:21 -0400

    added GradientOverlayImage back to the FeedItemCard

--- mobile/src/ui/FeedItemCard/index.js
+++ mobile/src/ui/FeedItemCard/index.js
@@ -4,9 +4,9 @@ import { compose, pure, setPropTypes, defaultProps } from 'recompose';
 import { startCase, toLower } from 'lodash';
 
 import { withThemeMixin, withTheme } from 'ui/theme';
-import Icon from 'ui/Icon';
+// import Icon from 'ui/Icon';
 import CategoryLabel from 'ui/CategoryLabel';
-// import GradientOverlayImage from 'ui/GradientOverlayImage'; // #TO-DO: Bring this over
+import GradientOverlayImage from 'ui/GradientOverlayImage';
 import Card, { CardContent, CardActions } from 'ui/Card';
 import { H4 } from 'ui/typography';
 import styled from 'ui/styled';
@@ -79,14 +79,14 @@ const FeedItemCard = enhance(
           isLoading={isLoading}
           withFlex
         />
-        <LikeButton id={id}>
+        {/* <LikeButton id={id}>
           <Icon
             name={isLiked ? 'like-solid' : 'like'}
             size={theme.helpers.rem(1.2)}
             fill={theme.colors.text.primary}
             isLoading={isLoading}
           />
-        </LikeButton>
+        </LikeButton> */}
       </CardActions>
     </Card>
   )
