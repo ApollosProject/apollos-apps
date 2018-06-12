790a9d4dcaa2dce53478b898ad3c07942c73ac29
Conrad VanLandingham <hi@convan.me>
2018-06-12 17:28:37 -0500

    add <ThemeProvider> to root App

--- mobile/App.js
+++ mobile/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { ApolloProvider } from 'react-apollo';
 import { createStackNavigator } from 'react-navigation';
 import ArticleSingle from 'articles/Single';
+import { ThemeProvider } from 'ui/theme';
 import { TabStack } from 'tabs';
 import client from 'client';
 
@@ -21,7 +22,9 @@ export default class App extends React.Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <RootStack />
+        <ThemeProvider>
+          <RootStack />
+        </ThemeProvider>
       </ApolloProvider>
     );
   }
