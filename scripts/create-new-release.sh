630392a96558f054aaa446f9908d73759a232eef
Isaac Hardy <isaac@isaachardy.io>
2018-06-13 16:38:31 -0400

    script to release

--- scripts/create-new-release.sh
+++ scripts/create-new-release.sh
@@ -4,16 +4,25 @@ set -e
 
 DIR="$(pwd)"
 MOBILE_DIR="$DIR/mobile"
+TAG=$1
+MSG=$2
 
-# Move Master to Release Branch to trigger App Builds on App Center
+# Get current branch
 current_branch() {
   git symbolic-ref --short HEAD
 }
-
 saved_branch=$(current_branch)
 
+# If current branch isn't master, switch to it
 if [ "${saved_branch}" != "master" ]; then
    git checkout "master"
 fi
 
+# Merge master into mobile-release branch
 git pull origin master
+git checkout mobile-release
+git merge "master"
+
+# Create Tag and Push
+git tag -a $TAG -m "${MSG}"
+git push --tags
