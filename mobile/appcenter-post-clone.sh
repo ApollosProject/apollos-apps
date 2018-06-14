94ef9a525f34a4d5e5976d7d8c3bee0280e807f2
Isaac Hardy <isaac@isaachardy.io>
2018-06-14 16:06:11 -0400

    correct scirpt

--- mobile/appcenter-post-clone.sh
+++ mobile/appcenter-post-clone.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-touch $APPCENTER_SOURCE_DIRECTORY/mobile/.env
+touch $APPCENTER_SOURCE_DIRECTORY/.env
 
 echo "Is it working?"
 echo $APPCENTER_SOURCE_DIRECTORY
 
-echo "APP_DATA_URL=apollos-church-api.now.sh" > $APPCENTER_SOURCE_DIRECTORY/mobile/.env
+echo "APP_DATA_URL=apollos-church-api.now.sh" > $APPCENTER_SOURCE_DIRECTORY/.env
 
-echo $APPCENTER_SOURCE_DIRECTORY/mobile/.env
+echo $APPCENTER_SOURCE_DIRECTORY/.env
