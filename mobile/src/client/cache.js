125008f4669f3b08e111484dd0627c08e3f886f8
richarddubay <richardtdubayjr@gmail.com>
2018-06-18 14:35:28 -0400

    removed dataIdFromObject and cacheRedirects because we don't need them right now.

--- mobile/src/client/cache.js
+++ mobile/src/client/cache.js
@@ -1,26 +1,14 @@
 import {
   InMemoryCache,
   IntrospectionFragmentMatcher,
-  defaultDataIdFromObject,
 } from 'apollo-cache-inmemory';
 
 const cache = new InMemoryCache({
-  dataIdFromObject(obj) {
-    // eslint-disable-next-line no-underscore-dangle
-    if (obj.entryId) return `${obj.__typename}:${obj.entryId}`;
-    return defaultDataIdFromObject(obj);
-  },
   fragmentMatcher: new IntrospectionFragmentMatcher({
     introspectionQueryResultData: JSON.parse(
       '{"__schema":{"types":[{"kind":"INTERFACE","name":"Node","possibleTypes":[{"name":"ContentChannel"},{"name":"UniversalContentItem"},{"name":"Person"}]},{"kind":"INTERFACE","name":"ContentItem","possibleTypes":[{"name":"UniversalContentItem"}]}]}}'
     ),
   }),
-  cacheRedirects: {
-    Query: {
-      node: (root, { id }, { getCacheKey }) =>
-        getCacheKey({ __typename: 'Content', id }),
-    },
-  },
 });
 
 export default cache;
