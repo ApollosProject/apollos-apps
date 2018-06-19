f0771b6a5371a03e7c8221787049d401b9adf6d7
Conrad VanLandingham <hi@convan.me>
2018-06-19 11:25:46 -0500

    fix android env var to make using localhost work

--- mobile/src/client/httpLink.js
+++ mobile/src/client/httpLink.js
@@ -1,4 +1,8 @@
+import { Platform } from 'react-native';
 import { createHttpLink } from 'apollo-link-http';
 import { APP_DATA_URL } from 'react-native-dotenv';
 
-export default createHttpLink({ uri: APP_DATA_URL });
+let uri = APP_DATA_URL;
+if (Platform.OS === 'android') uri = uri.replace('localhost', '10.0.2.2');
+
+export default createHttpLink({ uri });
