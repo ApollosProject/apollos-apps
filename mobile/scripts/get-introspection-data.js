64635fba0449eb3a8e00bb6f08539edf36a0a5c9
Isaac Hardy <isaac@isaachardy.io>
2018-06-19 17:11:17 -0400

    updated script

--- mobile/scripts/get-introspection-data.js
+++ mobile/scripts/get-introspection-data.js
@@ -2,43 +2,40 @@ const fetch = require('node-fetch');
 const fs = require('fs');
 const Path = require('path');
 
-fetch(`https://apollos-church-api.now.sh/graphql`, {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({
-    query: `
-      {
-        __schema {
-          types {
-            kind
-            name
-            possibleTypes {
-              name
+(async () => {
+  try {
+    const query = await fetch(`https://apollos-church-api.now.sh/graphql`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        query: `
+          {
+            __schema {
+              types {
+                kind
+                name
+                possibleTypes {
+                  name
+                }
+              }
             }
           }
-        }
-      }
-    `,
-  }),
-})
-  .then((result) => result.json())
-  .then((result) => {
-    const { data } = result;
-    // here we're filtering out any type information unrelated to unions or interfaces
-    const filteredData = data.__schema.types.filter(
+        `,
+      }),
+    });
+
+    const { data } = await query.json();
+
+    /* eslint no-underscore-dangle: 0 */
+    data.__schema.types = await data.__schema.types.filter(
       (type) => type.possibleTypes !== null
     );
-    /* eslint no-underscore-dangle: 0 */
-    data.__schema.types = filteredData;
-    fs.writeFileSync(
+
+    await fs.writeFileSync(
       Path.resolve(__dirname, '../src/client/fragmentTypes.json'),
-      JSON.stringify(result.data),
-      (err) => {
-        if (err) {
-          console.error('Error writing fragmentTypes file', err);
-        } else {
-          console.log('Fragment types successfully extracted!');
-        }
-      }
+      JSON.stringify(data)
     );
-  });
+  } catch (e) {
+    throw new Error('Error writing fragmentTypes file', e);
+  }
+})();
