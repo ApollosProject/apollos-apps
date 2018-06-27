073530548bc515150407ca45a5809ac599e7757c
richarddubay <richardtdubayjr@gmail.com>
2018-06-27 14:59:10 -0400

    updated CardTile tests

--- mobile/src/ui/CardTile/CardTile.tests.js
+++ mobile/src/ui/CardTile/CardTile.tests.js
@@ -4,7 +4,7 @@ import renderer from 'react-test-renderer';
 import Providers from 'TestProviders';
 import CardTile from './';
 
-describe('the Card component', () => {
+describe('the CardTile component', () => {
   it('should render', () => {
     const tree = renderer.create(
       <Providers>
