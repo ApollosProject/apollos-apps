54d04b95aabaab9280eb11c25de347ecde4bdee3
Isaac Hardy <isaac@isaachardy.io>
2018-07-02 10:42:16 -0400

    updated snapshots for right and left componentes

--- mobile/src/ui/SideBySideView/SideBySideView.tests.js
+++ mobile/src/ui/SideBySideView/SideBySideView.tests.js
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { View } from 'react-native';
 
-import SideBySide, { ResponsiveSideBySideView } from './';
+import SideBySide, { ResponsiveSideBySideView, Left, Right } from './';
 
 describe('The SideBySide component', () => {
   it('should render correctly', () => {
@@ -23,6 +23,30 @@ describe('The ResponsiveSideBySideView component', () => {
         <View />
         <View />
       </ResponsiveSideBySideView>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('The Right component', () => {
+  it('should render correctly', () => {
+    const tree = renderer.create(
+      <Right>
+        <View />
+        <View />
+      </Right>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('The Left component', () => {
+  it('should render correctly', () => {
+    const tree = renderer.create(
+      <Left>
+        <View />
+        <View />
+      </Left>
     );
     expect(tree).toMatchSnapshot();
   });
