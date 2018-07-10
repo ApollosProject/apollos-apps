faf021e3815827abf32b68c3707e0d6d374ef534
Burke Shartsis <burke@differential.com>
2018-07-10 17:03:08 -0400

    centered Icon stories

--- mobile/src/ui/Icon/Icon.stories.js
+++ mobile/src/ui/Icon/Icon.stories.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { storiesOf } from '@storybook/react-native';
 import { kebabCase } from 'lodash';
 
+import CenterView from 'storybook/decorators/CenterView';
+
 import * as icons from './icons';
 
 import Icon from './';
 
-const stories = storiesOf('Icon', module);
+const stories = storiesOf('Icon', module).addDecorator((getStory) => (
+  <CenterView>{getStory()}</CenterView>
+));
 
 Object.keys(icons).forEach((iconName) => {
   stories.add(iconName, () => <Icon name={kebabCase(iconName)} />);
