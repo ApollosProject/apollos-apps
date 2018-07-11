86dd113dd9397ff9590b27cdc402b0e868602248
Burke Shartsis <burke@differential.com>
2018-07-11 11:47:30 -0400

    update storybook decorator to use absolute path

--- mobile/storybook/decorators/CenterView/index.js
+++ mobile/storybook/decorators/CenterView/index.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import { View } from 'react-native';
 
-import styled from '../../../src/ui/styled';
+import styled from 'ui/styled';
 
 // import style from './style';
 
