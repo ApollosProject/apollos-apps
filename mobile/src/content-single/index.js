3572ab31c890575c5ac9e91f75c1bd279e404613
Conrad VanLandingham <hi@convan.me>
2018-07-11 11:48:37 -0500

    fix child item loading

--- mobile/src/content-single/index.js
+++ mobile/src/content-single/index.js
@@ -44,35 +44,32 @@ class ContentSingle extends PureComponent {
 
   handleOnPressItem(item) {
     this.props.navigation.push('ContentSingle', {
-      itemId: item.node.id,
-      itemTitle: item.node.title,
+      itemId: item.id,
+      itemTitle: item.title,
     });
   }
 
-  renderItem(item, index) {
-    return (
-      <TouchableWithoutFeedback onPress={() => this.handleOnPressItem(item)}>
-        <CardTile
-          number={index + 1}
-          title={get(item, 'node.title', '')}
-          /*
+  renderItem = ({ item, index }) => (
+    <TouchableWithoutFeedback onPress={() => this.handleOnPressItem(item)}>
+      <CardTile
+        number={index + 1}
+        title={get(item, 'title', '')}
+        /*
           * These are props that are not yet being passed in the data.
           * We will need to make sure they get added back when that data is available.
           * byLine={item.content.speaker}
           * date={item.meta.date}
           */
-          isLoading={item.isLoading}
-        />
-      </TouchableWithoutFeedback>
-    );
-  }
+        isLoading={item.isLoading}
+      />
+    </TouchableWithoutFeedback>
+  );
 
   render() {
     return (
       <Query query={getContentItem} variables={this.itemId}>
         {({ loading, error, data }) => {
           if (error) return <ErrorCard error={error} />;
-
           return (
             <ScrollView>
               <GradientOverlayImage
@@ -90,7 +87,7 @@ class ContentSingle extends PureComponent {
                   data,
                   'node.childContentItemsConnection.edges',
                   []
-                )}
+                ).map((edge) => edge.node)}
                 isLoading={loading}
                 loadingStateObject={this.loadingStateObject}
                 renderItem={this.renderItem}
