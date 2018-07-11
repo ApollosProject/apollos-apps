e3674830e4e089818531e0b64cf3af6ff2a658cf
richarddubay <richardtdubayjr@gmail.com>
2018-07-11 14:46:45 -0400

    linked up sentry

--- mobile/App.js
+++ mobile/App.js
@@ -9,6 +9,11 @@ import { TabStack } from 'tabs';
 import client from 'client';
 import LiveNowModal from 'live/liveModal';
 
+import { Sentry } from 'react-native-sentry';
+
+Sentry.config('https://5908fa19ed37447f86b2717423cadec5:45dd3b58792b413cb67109c5e63a0bb7@sentry.io/1241658').install();
+
+
 export const RootStack = createStackNavigator(
   {
     Tab: TabStack,
