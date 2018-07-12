7e660ecf425e8a7ba3b2a765e6bedfd88e6b9ade
Burke Shartsis <burke@differential.com>
2018-07-12 12:39:12 -0400

    added tests and stories for PaddedView

---
+++ mobile/src/ui/PaddedView/PaddedView.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react-native';
+import { H1 } from 'ui/typography';
+
+import PaddedView from '.';
+
+storiesOf('PaddedView', module).add('Default', () => (
+  <PaddedView>
+    <H1>This text is in a PaddedView</H1>
+  </PaddedView>
+));
