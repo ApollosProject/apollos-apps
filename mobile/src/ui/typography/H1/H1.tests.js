ef6d5bc03fe0b1642f6c791e4c498b115d1d42e3
Burke Shartsis <burke@differential.com>
2018-07-16 11:56:25 -0400

    updated H1 typographic rhythm (padding)

--- mobile/src/ui/typography/H1/H1.tests.js
+++ mobile/src/ui/typography/H1/H1.tests.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-
 import Providers from 'Providers';
 
 import H1 from './';
@@ -23,10 +22,28 @@ describe('the H1 component', () => {
     );
     expect(tree).toMatchSnapshot();
   });
+  it('should render with correct padding', () => {
+    const tree = renderer.create(
+      <Providers>
+        <H1 padded>Padded H1 text</H1>
+      </Providers>
+    );
+    expect(tree).toMatchSnapshot();
+  });
   it('should render a loading state', () => {
     const tree = renderer.create(
       <Providers>
         <H1 isLoading>Default H1 text</H1>
+      </Providers>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  it('should render a loading state with correct positioning (margins)', () => {
+    const tree = renderer.create(
+      <Providers>
+        <H1 padded isLoading>
+          Padded H1 text
+        </H1>
       </Providers>
     );
     expect(tree).toMatchSnapshot();
--- mobile/src/ui/typography/H1/index.js
+++ mobile/src/ui/typography/H1/index.js
@@ -1,16 +1,20 @@
 import { Text } from 'react-native';
 import PropTypes from 'prop-types';
 import { compose, setDisplayName, pure } from 'recompose';
-
 import styled from 'ui/styled';
 import { withPlaceholder, Typography } from 'ui/Placeholder';
 
 const styles = styled(
-  ({ theme }) => ({
+  ({ theme, padded }) => ({
     fontSize: theme.helpers.rem(2.6875),
     lineHeight: theme.helpers.verticalRhythm(2.6875, 1.15),
     fontFamily: theme.typography.sans.black.default,
     color: theme.colors.text.primary,
+    ...(padded
+      ? {
+          paddingVertical: theme.helpers.verticalRhythm(2.015625),
+        }
+      : {}),
   }),
   'H1'
 );
@@ -23,6 +27,7 @@ const H1 = compose(
 )(Text);
 
 H1.propTypes = {
+  padded: PropTypes.bool,
   isLoading: PropTypes.bool, // display loading placeholder
   ...Text.propTypes,
 };
