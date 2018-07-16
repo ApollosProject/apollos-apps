a21d856029c13ad84aa109e18a577fed9f9972b4
Burke Shartsis <burke@differential.com>
2018-07-16 12:05:18 -0400

    updated H5 typographic rhythm (padding)

--- mobile/src/ui/typography/H5/index.js
+++ mobile/src/ui/typography/H5/index.js
@@ -1,16 +1,21 @@
 import { Text } from 'react-native';
 import PropTypes from 'prop-types';
 import { compose, setDisplayName, pure } from 'recompose';
-
 import styled from 'ui/styled';
 import { withPlaceholder, Typography } from 'ui/Placeholder';
 
 const styles = styled(
-  ({ theme }) => ({
+  ({ theme, padded }) => ({
     fontSize: theme.helpers.rem(0.875),
     lineHeight: theme.helpers.verticalRhythm(0.875),
     fontFamily: theme.typography.sans.medium.default,
     color: theme.colors.text.primary,
+    ...(padded
+      ? {
+          paddingTop: theme.helpers.verticalRhythm(0.5775),
+          paddingBottom: theme.helpers.verticalRhythm(0.4375),
+        }
+      : {}),
   }),
   'H5'
 );
@@ -23,6 +28,7 @@ const H5 = compose(
 )(Text);
 
 H5.propTypes = {
+  padded: PropTypes.bool,
   isLoading: PropTypes.bool, // display loading placeholder
   ...Text.propTypes,
 };
