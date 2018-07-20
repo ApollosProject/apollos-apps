30fea8c91214abb7053a36f44af22136804c782c
Daniel Edwards <dan.edwards@newspring.cc>
2018-07-20 16:54:56 -0400

    #110 - Added Query tests to the content-single, content-feed, discover, and home components & also created snapshots.

--- mobile/src/tabs/home/home.tests.js
+++ mobile/src/tabs/home/home.tests.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import wait from 'waait';
 
 import Providers from 'Providers';
 
@@ -12,35 +11,67 @@ describe('User Home Feed Query', () => {
     const mock = {
       request: {
         query: getUserFeed,
-        variables: {
-          id: 'UniversalContentItem:678b4a38968fc6004dd8b23e586c923e',
-        },
       },
       result: {
         data: {
-          id: 'UniversalContentItem:678b4a38968fc6004dd8b23e586c923e',
-          coverImage: {
-            name: 'Square Image',
-            sources: [
+          userFeed: {
+            edges: [
               {
-                uri:
-                  'https://apollosrock.newspring.cc/GetImage.ashx?guid=ed857076-2623-4fdc-8476-50b3a60e0b68',
+                node: {
+                  __typename: 'UniversalContentItem',
+                  id: 'UniversalContentItem:678b4a38968fc6004dd8b23e586c923e',
+                  coverImage: {
+                    name: 'Square Image',
+                    sources: [
+                      {
+                        uri:
+                          'https://apollosrock.newspring.cc/GetImage.ashx?guid=ed857076-2623-4fdc-8476-50b3a60e0b68',
+                      },
+                    ],
+                  },
+                  parentChannel: {
+                    id: 'ContentChannel:7fba1b1ee253e0fd5f0795b4b8b1175e',
+                    name: 'Devotion Series',
+                    iconName: 'text',
+                  },
+                  title: 'Psalms: A 28-Day Devotional',
+                },
+              },
+              {
+                node: {
+                  __typename: 'UniversalContentItem',
+                  id: 'UniversalContentItem:0f361c619b7e5dd511da181069498250',
+                  coverImage: {
+                    name: 'Square Image',
+                    sources: [
+                      {
+                        uri:
+                          'https://apollosrock.newspring.cc/GetImage.ashx?guid=aa68b674-9cf4-4f8a-8702-9564a4a9fa7b',
+                      },
+                    ],
+                  },
+                  parentChannel: {
+                    id: 'ContentChannel:7fba1b1ee253e0fd5f0795b4b8b1175e',
+                    name: 'Devotion Series',
+                    iconName: 'text',
+                  },
+                  title: '2 Samuel: A 5-Week Devotional',
+                },
               },
             ],
           },
-          parentChannel: {
-            id: 'ContentChannel:7fba1b1ee253e0fd5f0795b4b8b1175e',
-            name: 'Devotion Series',
-            iconName: 'text',
-          },
-          title: 'Psalms: A 28-Day Devotional',
         },
       },
     };
 
+    const navigation = {
+      getParam: jest.fn(),
+      setParams: jest.fn(),
+      navigate: jest.fn(),
+    };
     const tree = renderer.create(
       <Providers mocks={[mock]} addTypename={false}>
-        <Home id="UniversalContentItem:678b4a38968fc6004dd8b23e586c923e" />
+        <Home navigation={navigation} />
       </Providers>
     );
     expect(tree).toMatchSnapshot();
