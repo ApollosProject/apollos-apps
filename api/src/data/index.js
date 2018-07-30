cbcfeea21862f6b2ff877444e8724589ddad6654
Conrad VanLandingham <hi@convan.me>
2018-07-30 11:13:31 -0500

    remove livestream tests

--- api/src/data/index.js
+++ api/src/data/index.js
@@ -36,7 +36,7 @@ export const schema = [
       registerPerson(email: String!, password: String!): Authentication
     }
 
-    ${process.env.NODE_ENV === 'test' ? `scalar Upload` : null}
+    ${process.env.NODE_ENV === 'test' ? `scalar Upload` : ``}
   `,
 ];
 
