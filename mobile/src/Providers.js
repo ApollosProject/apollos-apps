5b988a0fffaf7fc14f4efc97a9cb8f39e051af09
Vincent Wilson <vince@classyh.at>
2018-08-02 11:33:35 -0500

    Fix potential packager issues

--- mobile/src/Providers.js
+++ mobile/src/Providers.js
@@ -1,7 +1,7 @@
 import { nest } from 'recompose';
 
-import { ThemeProvider } from '/mobile/ui/theme';
 import ClientProvider from '/mobile/client';
+import { ThemeProvider } from '/mobile/ui/theme';
 import { WebBrowserProvider } from '/mobile/ui/WebBrowser';
 
 export default nest(ClientProvider, ThemeProvider, WebBrowserProvider);
