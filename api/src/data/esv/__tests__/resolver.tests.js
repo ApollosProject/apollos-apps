1d57731bfb0d986973484a9858d5adc8d00685e8
Isaac Hardy <isaac@isaachardy.io>
2018-08-03 10:35:58 -0400

    updated resolver test

--- api/src/data/esv/__tests__/resolver.tests.js
+++ api/src/data/esv/__tests__/resolver.tests.js
@@ -1,6 +1,34 @@
 // TODO
+// import { graphql } from 'graphql';
+// import fetch from 'isomorphic-fetch';
+// import { makeExecutableSchema } from 'apollo-server';
+//
+// import getContext from '../../../getContext';
+// // we import the root-level schema and resolver so we test the entire integration:
+// import { schema as typeDefs, resolvers } from '../../';
+
 describe('ESVScripture', () => {
+  // let schema;
+  // let context;
+  // beforeEach(() => {
+  //   fetch.resetMocks();
+  //   fetch.mockEsvAPI();
+  //   schema = makeExecutableSchema({ typeDefs, resolvers });
+  //   context = getContext();
+  // });
+
   it('returns', async () => {
+    // const query = `
+    //   query {
+    //     scripture (query: "John1.1") {
+    //       html
+    //     }
+    //   }
+    // `;
+    // const rootValue = {};
+    //
+    // const result = await graphql(schema, query, rootValue, context);
+    // expect(result).toMatchSnapshot();
     expect(true).toBeTruthy();
   });
 });
