4cef34647565f21d5ae30c5d22d3f72a232d3d27
richarddubay <richardtdubayjr@gmail.com>
2018-08-03 16:51:10 -0400

    cruft removal

--- mobile/src/ui/Share/index.js
+++ mobile/src/ui/Share/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
-// import { Share } from 'react-native';
 import PropTypes from 'prop-types';
 import Icon from '/mobile/ui/Icon';
 import Touchable from '/mobile/ui/Touchable';
 import share from '/mobile/utils/content/share';
-
-// const shareSomething = (content) => {
-//   Share.share({
-//     message: content.message,
-//     title: content.title,
-//     url: content.url,
-//   });
-// };
 
 const Share = ({ content }) => (
   <Touchable onPress={() => share(content)}>
