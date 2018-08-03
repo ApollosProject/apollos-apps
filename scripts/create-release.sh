2ae0912229a84640007917cd7a19546081196f26
Isaac Hardy <isaac@isaachardy.io>
2018-08-03 11:51:23 -0400

    feat: updated api, mobile, android, and ios to v0.4.0

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -3,6 +3,8 @@
 set -e
 
 cd ..
+
+# Declare variables
 currentVersion=$(cd mobile && node -p "require('./package.json').version")
 version=$1
 dir=$(pwd)
@@ -11,14 +13,24 @@ apiDIR=$dir/api
 androidDIR=$dir/mobile/android
 iosDIR=$dir/mobile/ios
 
+# Update android build.gradle to new version
 sed -i "" -e "s/versionName \"$currentVersion\"/versionName \"$version\"/g" $androidDIR/app/build.gradle
 
+# Update ios to new version
 cd $iosDIR && xcrun agvtool new-marketing-version $1
 
+# Mobile and API package.json update with changelog generation
 cd $mobileDIR && yarn run release
 cd $apiDIR && yarn run release
 
 cd $dir
 
+# Merge Mobile and API changelogs into one
+awk '{if (!a[$0]++) print}' $mobileDIR/CHANGELOG.md $apiDIR/CHANGELOG.md > CHANGELOG.md
+
+# Remove Mobile and API changelogs
+rm -f $mobileDIR/CHANGELOG.md $apiDIR/CHANGELOG.md
+
+# Commit
 git add .
-git commit -m "updated api, mobile, android, and ios to v${version}"
+git commit -m "feat: updated api, mobile, android, and ios to v${version}"
