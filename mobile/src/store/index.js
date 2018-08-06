cf8559b7e19ff92b6e8d8f04b68f882263c4cb83
Conrad VanLandingham <hi@convan.me>
2018-08-06 15:44:31 -0500

    add initial media player client-side state

--- mobile/src/store/index.js
+++ mobile/src/store/index.js
@@ -1,3 +1,5 @@
+import { merge } from 'lodash';
+import gql from 'graphql-tag';
 import { client } from 'client'; // eslint-disable-line
 // TODO: this will require more organization...ie...not keeping everything in one file.
 // But this is simple while our needs our small.
@@ -5,15 +7,41 @@ import { client } from 'client'; // eslint-disable-line
 export const schema = `
   type Query {
     authToken: String
+    mediaPlayer: MediaPlayerState
   }
 
   type Mutation {
     logout
+    mediaPlayerUpdatePlayer(isPlaying: Bool, isShuffling: Bool, isRepeating: Bool)
+    mediaPlayerNext(skip: Int)
+    mediaPlayerEnqueue(name: String)
+  }
+
+  type MediaPlayerState {
+    nowPlaying: MediaPlayerTrack
+    nowPlayingIndex: Int
+    queue: [MediaPlayerTrack]
+    isPlaying: Bool
+    isShuffling: Bool
+    isRepeating: Bool
+  }
+
+  type MediaPlayerTrack {
+    # TODO: what data do we _need_ to store client side for tracks?
+    name: String
   }
 `;
 
 export const defaults = {
   authToken: null,
+  mediaPlayer: {
+    __typename: 'MediaPlayerState',
+    nowPlayingIndex: 0,
+    queue: [],
+    isPlaying: false,
+    isShuffling: false,
+    isRepeating: false,
+  },
 };
 
 export const resolvers = {
@@ -22,6 +50,79 @@ export const resolvers = {
       client.resetStore();
       cache.writeData({ data: { authToken: null } });
       return null;
+    },
+    mediaPlayerEnqueue: (root, { name }, { cache }) => {
+      const query = gql`
+        query {
+          mediaPlayer @client {
+            queue
+          }
+        }
+      `;
+      const { mediaPlayer } = cache.readQuery({ query });
+      cache.writeQuery({
+        query,
+        data: {
+          mediaPlayer: {
+            __typename: 'MediaPlayerState',
+            queue: [...mediaPlayer.queue, { name }],
+          },
+        },
+      });
+    },
+    mediaPlayerNext: (root, { skip = 1 }, { cache }) => {
+      const query = gql`
+        query {
+          mediaPlayer @client {
+            nowPlayingIndex
+            queue
+          }
+        }
+      `;
+      const { mediaPlayer } = cache.readQuery({ query });
+      console.log({ mediaPlayer });
+
+      let nowPlayingIndex = mediaPlayer.nowPlayingIndex + skip;
+      if (nowPlayingIndex < 0) nowPlayingIndex = mediaPlayer.queue.length - 1;
+      if (nowPlayingIndex >= mediaPlayer.queue.length) nowPlayingIndex = 0;
+
+      cache.writeQuery({
+        query,
+        data: {
+          mediaPlayer: {
+            __typename: 'MediaPlayerState',
+            nowPlayingIndex,
+            queue: query.queue,
+          },
+        },
+      });
+    },
+    mediaPlayerUpdatePlayer: (
+      root,
+      { isPlaying, isShuffling, isRepeating },
+      { cache }
+    ) => {
+      const query = gql`
+        query {
+          mediaPlayer @client {
+            isPlaying
+            isShuffling
+            isRepeating
+          }
+        }
+      `;
+      const { mediaPlayer } = cache.readQuery({ query });
+      cache.writeQuery({
+        query,
+        data: {
+          mediaPlayer: merge(mediaPlayer, {
+            isPlaying,
+            isShuffling,
+            isRepeating,
+            __typename: 'MediaPlayerState',
+          }),
+        },
+      });
     },
   },
 };
