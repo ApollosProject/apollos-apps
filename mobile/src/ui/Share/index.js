4de7b550016861a850b89cc1ce5b8410e0c02c20
richarddubay <richardtdubayjr@gmail.com>
2018-08-07 16:50:49 -0400

    add theme colors for styling

--- mobile/src/ui/Share/index.js
+++ mobile/src/ui/Share/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { compose, pure } from 'recompose';
 import Icon from '/mobile/ui/Icon';
 import Touchable from '/mobile/ui/Touchable';
 import share from '/mobile/utils/content/share';
+import { withTheme } from '/mobile/ui/theme';
 
-const Share = ({ content }) => (
+const enhance = compose(pure, withTheme());
+
+const Share = enhance(({ content, theme }) => (
   <Touchable onPress={() => share(content)}>
-    <Icon name={'share'} />
+    <Icon name={'share'} fill={theme.colors.background.primary} />
   </Touchable>
-);
+));
 
 Share.propTypes = {
   content: PropTypes.shape({
