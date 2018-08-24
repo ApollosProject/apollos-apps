30b4cc3dc63a63ff82c571fe2020f6b658e430e1
Conrad VanLandingham <hi@convan.me>
2018-08-24 10:03:43 -0500

    fix linter

--- packages/apolloschurchapp/src/ui/MediaPlayer/index.js
+++ packages/apolloschurchapp/src/ui/MediaPlayer/index.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
+import PropTypes from 'prop-types';
 
 import FlexedView from '../FlexedView';
 import styled from '../styled';
@@ -20,6 +21,10 @@ const getMediaPlayerVisibility = gql`
 `;
 
 class MediaPlayer extends PureComponent {
+  static propTypes = {
+    children: PropTypes.node,
+  };
+
   renderPlayer = ({ data = {} }) => {
     if (!data.mediaPlayer || !data.mediaPlayer.isVisible) return null;
     return <CoverPlayer />;
