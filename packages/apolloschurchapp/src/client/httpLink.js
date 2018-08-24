c8fbe09d2a9bb04c97ab66145aeefecb9b6a3032
Conrad VanLandingham <hi@convan.me>
2018-08-24 08:17:39 -0500

    lets see if we can get travis to wake up

--- packages/apolloschurchapp/src/client/httpLink.js
+++ packages/apolloschurchapp/src/client/httpLink.js
@@ -2,7 +2,6 @@ import { Platform } from 'react-native';
 import { createHttpLink } from 'apollo-link-http';
 import { APP_DATA_URL } from 'react-native-dotenv';
 
-// blah 3
 let uri = APP_DATA_URL;
 // Android's emulator requires localhost network traffic to go through 10.0.2.2
 if (Platform.OS === 'android') uri = uri.replace('localhost', '10.0.2.2');
