5f684004b9c890cb6c0b4269c2482126f23f7894
isaachardy <isaac@isaachardy.io>
2018-08-30 11:53:23 -0400

    updated snapshots, tests, and fixed lint errors

--- packages/apolloschurchapp/src/ui/Like/Like.tests.js
+++ packages/apolloschurchapp/src/ui/Like/Like.tests.js
@@ -6,11 +6,31 @@ import { ThemeProvider } from 'apolloschurchapp/src/ui/theme';
 import Like from '.';
 
 describe('the Share component', () => {
-  it('should render', () => {
-    const sessionId = 'adsfasdfasdfasdf';
+  it('should render a Like', () => {
     const tree = renderer.create(
       <ThemeProvider>
-        <Like id={sessionId} />
+        <Like
+          itemId={'abc'}
+          sessionId={'123'}
+          isLiked={false}
+          operation={'Like'}
+          toggleLike={(data) => data}
+        />
+      </ThemeProvider>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render a UnLike', () => {
+    const tree = renderer.create(
+      <ThemeProvider>
+        <Like
+          itemId={'abc'}
+          sessionId={'123'}
+          isLiked
+          operation={'Unlike'}
+          toggleLike={(data) => data}
+        />
       </ThemeProvider>
     );
     expect(tree).toMatchSnapshot();
