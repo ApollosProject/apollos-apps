cd9f307fad1055ed341ed12e593ca7ee4886e972
richarddubay <richardtdubayjr@gmail.com>
2018-08-31 13:26:45 -0400

    updated the tests for the new api format

--- packages/apolloschurchapp/src/ui/Scripture/Scripture.tests.js
+++ packages/apolloschurchapp/src/ui/Scripture/Scripture.tests.js
@@ -5,19 +5,15 @@ import Providers from 'apolloschurchapp/src/Providers';
 import Item from './Item';
 
 const songOfSolomon1 = {
-  scripture: {
-    query: 'Song of Solomon 1:1-4',
-    html:
-      '<div id="v22001000-22001004" class="basic eng esv passage text" reference="Song of Solomon 1:1–4" start="22001000" end="22001004"><p id="p22001001_01-1" class="starts-chapter"><b class="chapter-num" id="v22001001-1">1:1&nbsp;</b>The Song of Songs, which is Solomon\'s.</p>\n<p class="block-indent"><h4 id="p22001001_06-1" class="speaker">She</h4>\n<span class="begin-line-group"></span>\n<span id="p22001002_06-1" class="line"><b class="verse-num inline" id="v22001002-1">2&nbsp;</b>&nbsp;&nbsp;Let him kiss me with the kisses of his mouth!</span><br /><span id="p22001002_06-1" class="line">&nbsp;&nbsp;For your love is better than wine;</span><br /><span id="p22001003_06-1" class="indent line"><b class="verse-num inline" id="v22001003-1">3&nbsp;</b>&nbsp;&nbsp;&nbsp;&nbsp;your anointing oils are fragrant;</span><br /><span id="p22001003_06-1" class="line">&nbsp;&nbsp;your name is oil poured out;</span><br /><span id="p22001003_06-1" class="indent line">&nbsp;&nbsp;&nbsp;&nbsp;therefore virgins love you.</span><br /><span id="p22001004_06-1" class="line"><b class="verse-num inline" id="v22001004-1">4&nbsp;</b>&nbsp;&nbsp;Draw me after you; let us run.</span><br /><span id="p22001004_06-1" class="indent line">&nbsp;&nbsp;&nbsp;&nbsp;The king has brought me into his chambers.</span><br /><span class="end-line-group"></span>\n<h4 id="p22001004_06-1" class="speaker">Others</h4>\n<span class="begin-line-group"></span>\n<span id="p22001004_06-1" class="line">&nbsp;&nbsp;We will exult and rejoice in you;</span><br /><span id="p22001004_06-1" class="indent line">&nbsp;&nbsp;&nbsp;&nbsp;we will extol your love more than wine;</span><br /><span id="p22001004_06-1" class="indent line">&nbsp;&nbsp;&nbsp;&nbsp;rightly do they love you.</span><br /></p><span class="end-line-group"></span>\n</div>',
-  },
+  reference: 'Song of Solomon 1:1-4',
+  content:
+    '<p class="p"><span data-number="1" class="v">1</span>The Song of songs, which is Solomon’s.</p><p class="sp">Beloved</p><p class="q1"><span data-number="2" class="v">2</span>Let him kiss me with the kisses of his mouth;</p><p class="q2">for your love is better than wine.</p><p class="q1"><span data-number="3" class="v">3</span>Your oils have a pleasing fragrance.</p><p class="q2">Your name is oil poured out,</p><p class="q2">therefore the virgins love you.</p><p class="q1"><span data-number="4" class="v">4</span>Take me away with you.</p><p class="q2">Let’s hurry.</p><p class="q2">The king has brought me into his rooms.</p><p class="sp">Friends</p><p class="q1">We will be glad and rejoice in you.</p><p class="q2">We will praise your love more than wine!</p><p class="sp">Beloved</p><p class="q1">They are right to love you.</p>',
 };
 
-const john = {
-  scripture: {
-    query: 'John 11:35-36',
-    html:
-      '<p id="p43011035_01-1" class="virtual"><b class="verse-num" id="v43011035-1">35&nbsp;</b>Jesus wept. <b class="verse-num" id="v43011036-1">36&nbsp;</b>So the Jews said, “See how he loved him!”</p>\n',
-  },
+const john3 = {
+  reference: 'John 3:16-17',
+  content:
+    '<p class="p"><span data-number="16" class="v">16</span><span class="wj">For God so loved the world, that he gave his one and only Son, that whoever believes in him should not perish, but have eternal life. </span><span data-number="17" class="v">17</span><span class="wj">For God didn’t send his Son into the world to judge the world, but that the world should be saved through him. </span></p>',
 };
 
 describe('the ScriptureItem component', () => {
@@ -25,20 +21,20 @@ describe('the ScriptureItem component', () => {
     const tree = renderer.create(
       <Providers>
         <Item
-          query={songOfSolomon1.scripture.query}
-          html={songOfSolomon1.scripture.html}
+          reference={songOfSolomon1.reference}
+          content={songOfSolomon1.content}
           isLoading={false}
         />
       </Providers>
     );
     expect(tree).toMatchSnapshot();
   });
-  it('renders John 11:35-36', () => {
+  it('renders John 3:16-17', () => {
     const tree = renderer.create(
       <Providers>
         <Item
-          query={john.scripture.query}
-          html={john.scripture.html}
+          reference={john3.reference}
+          content={john3.content}
           isLoading={false}
         />
       </Providers>
