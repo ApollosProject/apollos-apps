1ebd6d3549c9c4160d5dfc9aec44d5506af3fccf
Vincent Wilson <vince@classyh.at>
2018-09-10 13:26:03 -0500

    Stop using experimental export feature

--- packages/apollos-church-api/src/data/analytics/index.js
+++ packages/apollos-church-api/src/data/analytics/index.js
@@ -1,3 +1,5 @@
-export resolver from './resolver';
-export schema from './schema';
-export dataSource from './data-source';
+import resolver from './resolver';
+import schema from './schema';
+import dataSource from './data-source';
+
+export { resolver, schema, dataSource };
