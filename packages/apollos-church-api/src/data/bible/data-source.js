3d6db95d9c4b68c99fe1d76e64ddeb45c9b475b2
Vincent Wilson <vince@classyh.at>
2018-09-10 13:22:58 -0500

    Updated API to use Bible.API

--- packages/apollos-church-api/src/data/bible/data-source.js
+++ packages/apollos-church-api/src/data/bible/data-source.js
@@ -13,6 +13,6 @@ export default class Scripture extends RESTDataSource {
 
   async getScripture(query) {
     const bibleId = process.env.BIBLE_ID;
-    return this.get(`${bibleId}/passages/${query}`);
+    return this.get(`${bibleId}/search?query=${query}`);
   }
 }
