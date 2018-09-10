442457685816e51443c95a8a83c73afb47dedb77
Conrad VanLandingham <hi@convan.me>
2018-09-10 13:15:39 -0500

    fix linter

--- packages/apolloschurchapp/src/auth/LoginButton.js
+++ packages/apolloschurchapp/src/auth/LoginButton.js
@@ -1,7 +1,6 @@
 import React, { PureComponent } from 'react';
 import { withNavigation } from 'react-navigation';
 import { Query, Mutation } from 'react-apollo';
-import gql from 'graphql-tag';
 import PropTypes from 'prop-types';
 
 import { ButtonLink } from 'apolloschurchapp/src/ui/Button';
