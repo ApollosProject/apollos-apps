8c9d9d664f917d0cea29d7ad206e4766a885988d
Daniel Edwards <dan.edwards@newspring.cc>
2018-09-19 10:20:49 -0400

    Started Testing

--- packages/apolloschurchapp/src/client/httpLink.js
+++ packages/apolloschurchapp/src/client/httpLink.js
@@ -5,5 +5,6 @@ import { APP_DATA_URL } from 'react-native-dotenv';
 let uri = APP_DATA_URL;
 // Android's emulator requires localhost network traffic to go through 10.0.2.2
 if (Platform.OS === 'android') uri = uri.replace('localhost', '10.0.2.2');
+// if (Platform.OS === 'ios') uri = uri.replace('localhost', '10.0.201.75');
 
 export default createHttpLink({ uri });
--- packages/apolloschurchapp/src/index.js
+++ packages/apolloschurchapp/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { createStackNavigator } from 'react-navigation';
 import { OneSignal } from 'react-native-onesignal';
 // import { Sentry } from 'react-native-sentry';
@@ -36,7 +36,7 @@ const AppModalNavigator = createStackNavigator(
 
 // OneSignal Key below should eventually be in the the env.
 
-const OneSignalInit = {
+class OneSignalInit extends Component {
   componentWillMount() {
     OneSignal.init('b6e75a77-003f-4466-95ca-82cc5cdc407b', {
       kOSSettingsKeyAutoPrompt: true,
@@ -44,29 +44,29 @@ const OneSignalInit = {
     OneSignal.addEventListener('received', this.onReceived);
     OneSignal.addEventListener('opened', this.onOpened);
     OneSignal.addEventListener('ids', this.onIds);
-  },
+  }
 
   componentWillUnmount() {
     OneSignal.removeEventListener('received', this.onReceived);
     OneSignal.removeEventListener('opened', this.onOpened);
     OneSignal.removeEventListener('ids', this.onIds);
-  },
+  }
 
   onReceived(notification) {
     console.log('Notification received: ', notification);
-  },
+  }
 
   onOpened(openResult) {
     console.log('Message: ', openResult.notification.payload.body);
     console.log('Data: ', openResult.notification.payload.additionalData);
     console.log('isActive: ', openResult.notification.isAppInFocus);
     console.log('openResult: ', openResult);
-  },
+  }
 
   onIds(device) {
     console.log('Device info: ', device);
-  },
-};
+  }
+}
 
 const App = () => (
   <Providers>
