900a6e22235b77458c43b4d909b43536e326a3bd
Daniel Edwards <dan.edwards@newspring.cc>
2018-09-20 11:27:35 -0400

    Successful Android Test

--- packages/apolloschurchapp/src/client/httpLink.js
+++ packages/apolloschurchapp/src/client/httpLink.js
@@ -4,7 +4,9 @@ import { APP_DATA_URL } from 'react-native-dotenv';
 
 let uri = APP_DATA_URL;
 // Android's emulator requires localhost network traffic to go through 10.0.2.2
+// If testing on an Android hardware device, you will need to replace 10.0.2.2 with your IP address.
 if (Platform.OS === 'android') uri = uri.replace('localhost', '10.0.2.2');
+// Running the app on a hardware iOS device requires localhost network traffic to go to your specific IP address.
 // if (Platform.OS === 'ios') uri = uri.replace('localhost', '10.0.201.75');
 
 export default createHttpLink({ uri });
