81957788c418c568c7f4c2383b270b93860b54db
Daniel Edwards <dan.edwards@newspring.cc>
2018-09-25 17:06:57 -0400

    Removed Comment

--- packages/apolloschurchapp/src/client/httpLink.js
+++ packages/apolloschurchapp/src/client/httpLink.js
@@ -5,7 +5,5 @@ import { APP_DATA_URL } from 'react-native-dotenv';
 let uri = APP_DATA_URL;
 
 if (Platform.OS === 'android') uri = uri.replace('localhost', '10.0.2.2');
-// Running the app on a hardware iOS device requires localhost network traffic to go to your specific IP address.
-if (Platform.OS === 'ios') uri = uri.replace('localhost', '10.0.201.75');
 
 export default createUploadLink({ uri });
