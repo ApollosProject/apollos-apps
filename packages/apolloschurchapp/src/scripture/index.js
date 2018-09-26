96d14a7dfc796433a9c2ab6f16ce6eb5973978da
richarddubay <richardtdubayjr@gmail.com>
2018-09-26 15:49:22 -0400

    fixed up the connected version of the scripture component

--- packages/apolloschurchapp/src/scripture/index.js
+++ packages/apolloschurchapp/src/scripture/index.js
@@ -1,30 +1,40 @@
-// This is the data-bound "connected" scripture component.
-// This should pull from a query.
 import React from 'react';
 import { Query } from 'react-apollo';
 import { get } from 'lodash';
 import PropTypes from 'prop-types';
-import { View } from 'react-native';
 import ScriptureItem from 'apolloschurchapp/src/ui/Scripture/ScriptureItem';
 import getScripture from './getScripture';
 
-const ScriptureConnected = ({ references }) => (
-  <View>
-    {references.map((query) => (
-      <Query query={getScripture} variables={{ query }} key={query}>
-        {({ loading, data }) => (
-          <ScriptureItem
-            reference={get(data, 'scripture.reference', '')}
-            html={get(data, 'scripture.html', '')}
-            isLoading={loading}
-          />
-        )}
-      </Query>
-    ))}
-  </View>
-);
+/**
+ * This is the data-bound "connected" scripture component.
+ * Maps over the array of references, does a query to the API for each reference,
+ * then passes that returned data to the ScriptureComponent for rendering.
+ */
+const ScriptureConnected = ({
+  ScriptureComponent = ScriptureItem,
+  references,
+}) => {
+  references.map((query) => (
+    <Query query={getScripture} variables={{ query }} key={query}>
+      {({ loading, data }) => (
+        <ScriptureComponent
+          reference={get(data, 'scripture.reference', '')}
+          html={get(data, 'scripture.html', '')}
+          isLoading={loading}
+        />
+      )}
+    </Query>
+  ));
+};
 
+/**
+ * Props passed to this connected component:
+ * ScriptureComponent: An optional component that would allow you to display
+ * scripture your way. Defaults to ScriptureItem.
+ * references: An array of strings of Bible verses (i.e. ['Genesis 1:1'])
+ */
 ScriptureConnected.propTypes = {
+  ScriptureComponent: PropTypes.element,
   references: PropTypes.arrayOf(PropTypes.string),
 };
 
