50e07e8a6c0f2e1c75daf7a49993ee1d7b51076e
Isaac Hardy <isaac.hardy@newspring.cc>
2018-10-09 09:33:00 -0400

    loading for loginbutton

--- packages/apolloschurchapp/src/auth/LoginButton.js
+++ packages/apolloschurchapp/src/auth/LoginButton.js
@@ -4,7 +4,6 @@ import { Query } from 'react-apollo';
 import PropTypes from 'prop-types';
 
 import Button from 'apolloschurchapp/src/ui/Button';
-import ActivityIndicator from 'apolloschurchapp/src/ui/ActivityIndicator';
 
 import getLoginState from './getLoginState';
 
@@ -23,12 +22,12 @@ class LoginButton extends PureComponent {
       <Query query={getLoginState}>
         {({ data }) => {
           const { isLoggedIn, loading } = data;
-          if (loading) return <ActivityIndicator />;
           if (isLoggedIn) return null;
           return (
             <Button
               onPress={this.handleLoginPress}
               title="Get Connected"
+              loading={loading}
               {...otherProps}
             />
           );
