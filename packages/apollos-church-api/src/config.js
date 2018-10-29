f99e97346155e4c3c48b957504861a58dc70ed24
Vincent Wilson <vince@classyh.at>
2018-10-29 10:44:18 -0500

    Remove unneeded comments.

--- packages/apollos-church-api/src/config.js
+++ packages/apollos-church-api/src/config.js
@@ -4,27 +4,3 @@ import ApollosConfig from '@apolloschurch/config';
 ApollosConfig.loadYaml({
   configPath: path.join(__dirname, '..', 'config.yml'),
 });
-
-// setRockVariables({
-//   url: config.ROCK.API_URL,
-//   token: config.ROCK.API_TOKEN,
-// });
-
-// const {
-//   CLOUDINARY,
-//   ROCK,
-//   ANALYTICS,
-//   BIBLE_API,
-//   ROCK_MAPPINGS,
-//   ROCK_CONSTANTS,
-// } = config;
-
-// export default config;
-// export {
-//   CLOUDINARY,
-//   ROCK,
-//   ANALYTICS,
-//   BIBLE_API,
-//   ROCK_MAPPINGS,
-//   ROCK_CONSTANTS,
-// };
