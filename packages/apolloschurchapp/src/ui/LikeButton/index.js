5c4e113cc93abf07d766208f8108a3e4e51ba871
Vincent Wilson <vince@classyh.at>
2018-10-30 10:22:04 -0500

    Update cache perfomantly when liking/unliking content items

--- packages/apolloschurchapp/src/ui/LikeButton/index.js
+++ packages/apolloschurchapp/src/ui/LikeButton/index.js
@@ -3,9 +3,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { get } from 'lodash';
 import { Query, Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
 
 import Like from 'apolloschurchapp/src/ui/Like';
 import { track, events } from 'apolloschurchapp/src/analytics';
+import getAllLikedContent from 'apolloschurchapp/src/tabs/connect/getLikedContent';
+import { contentItemFragment } from 'apolloschurchapp/src/content-single/getContentItem';
 
 import updateLikeEntity from './updateLikeEntity';
 import getLikedContentItem from './getLikedContentItem';
@@ -24,6 +27,67 @@ GetLikeData.propTypes = {
   children: PropTypes.func.isRequired,
 };
 
+const addItemToLikedContentList = ({ cache, item }) => {
+  try {
+    const data = cache.readQuery({ query: getAllLikedContent });
+    const fullItem = cache.readFragment({
+      id: `${item.__typename}:${item.id}`,
+      fragmentName: 'LikedContentItemParts',
+      fragment: gql`
+        fragment LikedContentItemParts on ContentItem {
+          ...contentItemFragment
+          __typename
+          id
+          coverImage {
+            name
+            sources {
+              uri
+            }
+          }
+          isLiked
+          parentChannel {
+            id
+            name
+            iconName
+          }
+          title
+        }
+        ${contentItemFragment}
+      `,
+    });
+    cache.writeQuery({
+      query: getAllLikedContent,
+      data: {
+        ...data,
+        getAllLikedContent: [fullItem, ...data.getAllLikedContent],
+      },
+    });
+  } catch (e) {
+    console.log(e);
+    // Most likely we haven't ran the `getAllLikedContent` query yet.
+    // We can safely exit.
+  }
+};
+
+const removeItemFromLikedContentList = ({ cache, item }) => {
+  try {
+    const data = cache.readQuery({ query: getAllLikedContent });
+    cache.writeQuery({
+      query: getAllLikedContent,
+      data: {
+        ...data,
+        getAllLikedContent: data.getAllLikedContent.filter(
+          (content) => content.id !== item.id
+        ),
+      },
+    });
+  } catch (e) {
+    console.log(e);
+    // Most likely we haven't ran the `getAllLikedContent` query yet.
+    // We can safely exit.
+  }
+};
+
 const UpdateLikeStatus = ({ itemId, item, isLiked, children }) => (
   <Mutation
     mutation={updateLikeEntity}
@@ -35,7 +99,6 @@ const UpdateLikeStatus = ({ itemId, item, isLiked, children }) => (
         __typename: 'Interaction',
       },
     }}
-    refetchQueries={['getAllLikedContent']}
     update={(
       cache,
       {
@@ -44,6 +107,9 @@ const UpdateLikeStatus = ({ itemId, item, isLiked, children }) => (
         },
       }
     ) => {
+      operation === 'Like'
+        ? addItemToLikedContentList({ cache, item })
+        : removeItemFromLikedContentList({ cache, item });
       cache.writeQuery({
         query: getLikedContentItem,
         data: {
