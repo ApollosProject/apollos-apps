304e43f0605da24231bf59f4d94e364742fafc7d
Burke Shartsis <burke@differential.com>
2018-11-08 16:41:28 -0500

    Fixes: - updates Touchable with support for round prop. Fixes native ripple on Android. - removes ios specific prop for TouchableOpacity off of the react primitave

--- packages/apolloschurchapp/src/ui/Touchable/index.js
+++ packages/apolloschurchapp/src/ui/Touchable/index.js
@@ -1,16 +1,44 @@
+import React from 'react';
 import {
   Platform,
   TouchableNativeFeedback,
   TouchableOpacity,
+  View,
 } from 'react-native';
+import PropTypes from 'prop-types';
 
-const IOSTouchable = TouchableOpacity;
+import styled from 'apolloschurchapp/src/ui/styled';
 
-IOSTouchable.defaultProps = {
-  activeOpacity: 0.5,
+const RoundWrapper = styled(({ size }) => ({
+  borderRadius: size,
+  overflow: 'hidden',
+  backgroundColor: 'salmon',
+}))(View);
+
+const RoundAndroidWrapper = ({ round, size, children }) =>
+  Platform.OS === 'android' && round ? (
+    <RoundWrapper size={size}>{children}</RoundWrapper>
+  ) : (
+    children
+  );
+
+const PlatformTouchable =
+  Platform.OS === 'android' ? TouchableNativeFeedback : TouchableOpacity;
+
+const Touchable = ({ round, children, size, ...otherProps }) => (
+  <RoundAndroidWrapper round={round} size={size}>
+    <PlatformTouchable {...otherProps}>{children}</PlatformTouchable>
+  </RoundAndroidWrapper>
+);
+
+Touchable.propTypes = {
+  round: PropTypes.bool,
+  size: PropTypes.number,
+  children: PropTypes.node,
 };
 
-const Touchable =
-  Platform.OS === 'android' ? TouchableNativeFeedback : IOSTouchable;
+Touchable.defaultProps = {
+  activeOpacity: 0.5,
+};
 
 export default Touchable;
