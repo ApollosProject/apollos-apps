a7d91c0abaa0a4d82cda7c5165af4333d7283f42
Burke Shartsis <burke@differential.com>
2018-11-09 12:42:22 -0500

    Fixes: - eslint proptypes warning - moves default props to platform specific component definitions - adds dynamic Ripple color that responds with theme type (dark/light)

--- packages/apolloschurchapp/src/ui/Touchable/index.js
+++ packages/apolloschurchapp/src/ui/Touchable/index.js
@@ -6,15 +6,19 @@ import {
   View,
 } from 'react-native';
 import PropTypes from 'prop-types';
+import { withProps } from 'recompose';
 
 import styled from 'apolloschurchapp/src/ui/styled';
+import { withTheme } from 'apolloschurchapp/src/ui/theme';
 
 const AndroidRippleWrapper = styled(({ borderRadius }) => ({
   borderRadius,
   overflow: 'hidden',
 }))(View);
 
-const AndroidWrapper = ({ borderRadius, children }) =>
+const AndroidWrapper = (
+  { borderRadius, children } // eslint-disable-line react/prop-types
+) =>
   Platform.OS === 'android' ? (
     <AndroidRippleWrapper borderRadius={borderRadius} round>
       {children}
@@ -23,23 +27,30 @@ const AndroidWrapper = ({ borderRadius, children }) =>
     children
   );
 
+// Set a platform specific component
 const PlatformTouchable =
-  Platform.OS === 'android' ? TouchableNativeFeedback : TouchableOpacity;
+  Platform.OS === 'ios' ? TouchableOpacity : TouchableNativeFeedback;
+
+// Set some platform specific default props
+const PlatformTouchableWithDefaultProps =
+  Platform.OS === 'ios'
+    ? withProps({ activeOpacity: 0.5 })(PlatformTouchable)
+    : withTheme(({ theme }) => ({
+        borderRadius: 0,
+        background: TouchableNativeFeedback.Ripple(theme.colors.text.primary),
+      }))(PlatformTouchable);
 
 const Touchable = ({ borderRadius, children, ...otherProps }) => (
   <AndroidWrapper borderRadius={borderRadius}>
-    <PlatformTouchable {...otherProps}>{children}</PlatformTouchable>
+    <PlatformTouchableWithDefaultProps {...otherProps}>
+      {children}
+    </PlatformTouchableWithDefaultProps>
   </AndroidWrapper>
 );
 
 Touchable.propTypes = {
   borderRadius: PropTypes.number,
   children: PropTypes.element,
-};
-
-Touchable.defaultProps = {
-  activeOpacity: 0.5,
-  borderRadius: 0,
 };
 
 export default Touchable;
