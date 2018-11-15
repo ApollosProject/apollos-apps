198e15e893690befe3d9384f5aa512293740a1f7
Burke Shartsis <burke@differential.com>
2018-11-14 23:23:31 -0500

    Adding tests for ButtonIcon

---
+++ packages/apolloschurchapp/src/ui/Button/ButtonIcon/ButtonIcon.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View } from 'react-native';
+import renderer from 'react-test-renderer';
+
+import Providers from 'apolloschurchapp/src/Providers';
+
+import ButtonIcon from '.';
+
+describe('The ButtonIcon component', () => {
+  it('should render', () => {
+    const tree = renderer.create(
+      <Providers>
+        <ButtonIcon name={'umbrella'} onPress={jest.fn()} />
+      </Providers>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  it('should render with custom padding', () => {
+    const tree = renderer.create(
+      <Providers>
+        <ButtonIcon name={'umbrella'} onPress={jest.fn()} iconPadding={50} />
+      </Providers>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  it('should render disabled', () => {
+    const tree = renderer.create(
+      <Providers>
+        <ButtonIcon name={'umbrella'} onPress={jest.fn()} disabled />
+      </Providers>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
