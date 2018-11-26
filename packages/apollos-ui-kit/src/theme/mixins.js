a68f754fcfbf3740536fb376d6d23e159ca1afb6
Conrad VanLandingham <hi@convan.me>
2018-11-26 09:58:02 -0600

    fix theme mixins

--- packages/apollos-ui-kit/src/theme/mixins.js
+++ packages/apollos-ui-kit/src/theme/mixins.js
@@ -19,7 +19,7 @@ const withThemeMixin = (themeInput) =>
         if (typeof themeInput === 'function') {
           themeInputAsObject = themeInput({ ...ownProps, theme });
         }
-        themeInputAsObject = merge(originalThemeInput, themeInputAsObject);
+        themeInputAsObject = merge({}, originalThemeInput, themeInputAsObject);
 
         const themeWithMixin = createTheme(themeInputAsObject);
         return {
