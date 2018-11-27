316b2ce2f8436ce643fb8c3078860ed4909e1f33
Conrad VanLandingham <conrad@differential.com>
2018-11-27 11:00:56 -0600

    Fix theme mixins (#449)
    * fix theme mixins
    
    * Fix `ThemeInput` Passing

--- packages/apollos-ui-kit/src/theme/mixins.js
+++ packages/apollos-ui-kit/src/theme/mixins.js
@@ -1,14 +1,16 @@
-import { compose, mapProps, withContext } from 'recompose';
+import { getContext, compose, mapProps, withContext } from 'recompose';
 import PropTypes from 'prop-types';
 import { merge } from 'lodash';
 
-import withTheme from './withTheme';
 import createTheme, { THEME_PROPS } from './createTheme';
 
 const withThemeMixin = (themeInput) =>
   compose(
     mapProps((props) => ({ ownProps: props })),
-    withTheme(),
+    getContext({
+      theme: PropTypes.shape(THEME_PROPS),
+      themeInput: PropTypes.shape(THEME_PROPS),
+    }),
     withContext(
       {
         theme: PropTypes.shape(THEME_PROPS),
@@ -19,9 +21,10 @@ const withThemeMixin = (themeInput) =>
         if (typeof themeInput === 'function') {
           themeInputAsObject = themeInput({ ...ownProps, theme });
         }
-        themeInputAsObject = merge(originalThemeInput, themeInputAsObject);
+        themeInputAsObject = merge({}, originalThemeInput, themeInputAsObject);
 
         const themeWithMixin = createTheme(themeInputAsObject);
+
         return {
           theme: themeWithMixin,
           themeInput: themeInputAsObject,
