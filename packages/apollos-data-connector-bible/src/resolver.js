589e9fcc7c170a11942fedaa6a232d7b5b1e745e
Vincent Wilson <vince@classyh.at>
2018-12-13 15:37:16 -0600

    Adds Scripture API Multiple Verse Support (#466)
    * Enable multiple verse support in scripture API
    
    * Increased coverage of bible module
    
    * Updated files

--- packages/apollos-data-connector-bible/src/resolver.js
+++ packages/apollos-data-connector-bible/src/resolver.js
@@ -1,14 +1,11 @@
-import { get } from 'lodash';
-
 export default {
   Query: {
     scripture: (root, { query }, { dataSources }) =>
       dataSources.Scripture.getScripture(query),
+    scriptures: (root, { query }, { dataSources }) =>
+      dataSources.Scripture.getScriptures(query),
   },
   Scripture: {
-    id: ({ data: { passages } = {} }) => get(passages, '[0].id'),
-    html: ({ data: { passages } = {} }) => get(passages, '[0].content'),
-    reference: ({ data: { passages } = {} }) => get(passages, '[0].reference'),
-    copyright: ({ data: { passages } = {} }) => get(passages, '[0].copyright'),
+    html: ({ content }) => content,
   },
 };
