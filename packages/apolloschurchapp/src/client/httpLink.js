86731ea4cec16c4f21bec8fb187a27c60830d816
Michael Neeley <micneeley14@gmail.com>
2019-02-04 13:04:56 -0500

    Adds Fastly CDN and Data Caching (#502)
    * added cache control headers
    
    * added new maxage
    
    * adjusted
    
    * fixed syntax
    
    * fixed syntax
    
    * updated APP_URL
    
    * used https

--- packages/apolloschurchapp/src/client/httpLink.js
+++ packages/apolloschurchapp/src/client/httpLink.js
@@ -1,6 +1,9 @@
 import { Platform } from 'react-native';
 import { createUploadLink } from 'apollo-upload-client';
 import Config from 'react-native-config';
+import { split } from 'apollo-link';
+import { createHttpLink } from 'apollo-link-http';
+import { getMainDefinition } from 'apollo-utilities';
 
 let uri = Config.APP_DATA_URL;
 const androidUri = Config.ANDROID_URL || '10.0.2.2';
@@ -8,4 +11,14 @@ const androidUri = Config.ANDROID_URL || '10.0.2.2';
 // Android's emulator requires localhost network traffic to go through 10.0.2.2
 if (Platform.OS === 'android') uri = uri.replace('localhost', androidUri);
 
-export default createUploadLink({ uri });
+export default split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'mutation';
+  },
+  createUploadLink({ uri }),
+  createHttpLink({
+    uri,
+    useGETForQueries: true,
+  })
+);
