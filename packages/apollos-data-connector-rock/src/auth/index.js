5c0a812f160214558539e883463a4816120183b7
Vincent Wilson <vince@classyh.at>
2019-02-05 09:03:36 -0600

    Moved all resolvers to their own file

--- packages/apollos-data-connector-rock/src/auth/index.js
+++ packages/apollos-data-connector-rock/src/auth/index.js
@@ -1,4 +1,3 @@
-import { createGlobalId } from '@apollosproject/server-core';
 import { get } from 'lodash';
 import { registerToken } from './token';
 
@@ -21,27 +20,4 @@ export const contextMiddleware = ({ req, context: ctx }) => {
     }
   }
   return ctx;
-};
-
-export const resolver = {
-  Query: {
-    currentUser: (root, args, { dataSources }) =>
-      dataSources.Auth.getCurrentPerson(),
-  },
-  AuthenticatedUser: {
-    id: ({ id }, args, context, { parentType }) =>
-      createGlobalId(id, parentType.name),
-    profile: (authUser) => authUser,
-  },
-  Authentication: {
-    user: (root, args, { dataSources }) => dataSources.Auth.getCurrentPerson(),
-  },
-  Mutation: {
-    authenticate: (root, { identity, password }, { dataSources }) =>
-      dataSources.Auth.authenticate({ identity, password }),
-    changePassword: (root, { password }, { dataSources }) =>
-      dataSources.Auth.changePassword({ password }),
-    registerPerson: (root, args, { dataSources }) =>
-      dataSources.Auth.registerPerson(args),
-  },
 };
