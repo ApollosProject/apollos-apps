5fd95ea087fa60bd94aebe53f069a73e5489b780
Vincent Wilson <vince@classyh.at>
2019-02-11 16:44:49 -0600

    Added documentation for terraform

---
+++ api_heroku.tf
@@ -0,0 +1,54 @@
+# Configure the Heroku provider
+
+variable "heroku_email" {
+  type = "string"
+  description = "Your Heroku Email"
+}
+
+variable "heroku_api_key" {
+  type = "string"
+  description = "Your Heroku API key (find it in your Heroku account settings)"
+}
+
+variable "app_name" {
+  type = "string"
+  description = "Your app's name. Name must start with a letter, end with a letter or digit and can only contain lowercase letters, digits, and dashes."
+}
+
+# variable "heroku_container_size" {
+#   type = "string"
+#   default = "free"
+#   description = "Heroku Container Size - The default is the only free container size. Other options are hobby, standard-1x, etc."
+# }
+
+
+provider "heroku" {
+  email   = "${var.heroku_email}"
+  api_key = "${var.heroku_api_key}"
+}
+
+# Create a new application
+resource "heroku_app" "default" {
+  region = "us"
+  name = "${var.app_name}"
+
+  config_vars {
+    BIBLE_API_KEY = "your bible.api key"
+    ENGINE_API_KEY = "your engine api key (optional)"
+    ROCK_TOKEN = "the token for your rock api user"
+    ONE_SIGNAL_REST_KEY = "your one signal REST API key (different than the API key included on the client)"
+  }
+
+  stack = "container"
+}
+
+resource "heroku_addon" "cloudinary" {
+  app  = "${heroku_app.default.name}"
+  plan = "cloudinary"
+}
+
+# Uncomment the following lines to enable CDN creation
+resource "heroku_addon" "fastly" {
+  app  = "${heroku_app.default.name}"
+  plan = "fastly:quick"
+}
--- example.terraform.tfvars
+++ example.terraform.tfvars
@@ -1,3 +1,4 @@
+# Rename this file to terraform.tfvars and include your own values.
 heroku_email = "your heroku email"
 
 heroku_api_key = "your heroku API key"
