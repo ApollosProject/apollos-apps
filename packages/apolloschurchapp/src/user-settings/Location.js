7c4e9b1bb1765df9ef8904d986c5e36a2544d7ec
Daniel Edwards <dan.edwards@newspring.cc>
2019-02-13 11:26:57 -0500

    Initial pass

---
+++ packages/apolloschurchapp/src/user-settings/Location.js
@@ -0,0 +1,86 @@
+import React, { PureComponent } from 'react';
+import { Dimensions, Text, StyleSheet } from 'react-native';
+import PropTypes from 'prop-types';
+import MapView, { Marker } from 'react-native-maps';
+
+import {
+  // TextInput,
+  PaddedView,
+  // FlexedView,
+  // Button,
+  ButtonLink,
+  // TableView,
+} from '@apollosproject/ui-kit';
+
+const { width, height } = Dimensions.get('window');
+
+const ASPECT_RATIO = width / height;
+const LATITUDE = 34.595334;
+const LONGITUDE = -82.621761;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const SPACE = 0.01;
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  marker: {
+    marginLeft: 46,
+    marginTop: 33,
+    fontWeight: 'bold',
+  },
+});
+
+class Location extends PureComponent {
+  static navigationOptions = ({ navigation }) => ({
+    title: 'Location',
+    headerLeft: null,
+    headerRight: (
+      <PaddedView vertical={false}>
+        <ButtonLink onPress={() => navigation.goBack()}>Cancel</ButtonLink>
+      </PaddedView>
+    ),
+  });
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+      navigate: PropTypes.func,
+      goBack: PropTypes.func,
+    }),
+  };
+
+  render() {
+    return (
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: LATITUDE,
+          longitude: LONGITUDE,
+          latitudeDelta: LATITUDE_DELTA,
+          longitudeDelta: LONGITUDE_DELTA,
+        }}
+        showsUserLocation
+      >
+        <Marker
+          coordinate={{
+            latitude: LATITUDE + SPACE,
+            longitude: LONGITUDE + SPACE,
+          }}
+          centerOffset={{ x: -18, y: -60 }}
+          anchor={{ x: 0.69, y: 1 }}
+        >
+          <Text style={styles.marker}>X</Text>
+        </Marker>
+      </MapView>
+    );
+  }
+}
+
+export default Location;
