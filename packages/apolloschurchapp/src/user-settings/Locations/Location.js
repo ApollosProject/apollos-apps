bee4f5ee87d0302a117e5a9d23b211046fc291b0
Daniel Edwards <dan.edwards@newspring.cc>
2019-02-19 15:04:06 -0500

    Added scroller with cards. Need to get animateToRegion working

--- packages/apolloschurchapp/src/user-settings/Locations/Location.js
+++ packages/apolloschurchapp/src/user-settings/Locations/Location.js
@@ -1,25 +1,93 @@
-import React, { PureComponent } from 'react';
-import { Dimensions, StyleSheet } from 'react-native';
+import React from 'react';
+import {
+  Dimensions,
+  StyleSheet,
+  View,
+  Animated,
+  Image,
+  Text,
+} from 'react-native';
 import PropTypes from 'prop-types';
-import MapView, { Marker } from 'react-native-maps';
+import MapView from 'react-native-maps';
 
 import { styled, PaddedView, ButtonLink } from '@apollosproject/ui-kit';
-import campuses from './getCampusLocations';
-import markerDot from './markerDot.png';
 
 const { width, height } = Dimensions.get('window');
-const aspectRatio = width / height;
 
-const LATITUDE = 34.001150;
-const LONGITUDE = -81.032393;
-const LATITUDE_DELTA = 7.85;
-const LONGITUDE_DELTA = LATITUDE_DELTA * aspectRatio;
+const CARD_HEIGHT = height / 5;
+const CARD_WIDTH = CARD_HEIGHT + 80;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollView: {
+    position: 'absolute',
+    bottom: 30,
+    left: 0,
+    right: 0,
+    paddingVertical: 10,
+  },
+  endPadding: {
+    paddingRight: width - CARD_WIDTH,
+  },
+  card: {
+    padding: 10,
+    elevation: 2,
+    backgroundColor: '#FFF',
+    marginHorizontal: 10,
+    shadowColor: '#000',
+    shadowRadius: 5,
+    shadowOpacity: 0.3,
+    shadowOffset: { x: 2, y: -2 },
+    height: CARD_HEIGHT,
+    width: CARD_WIDTH,
+    overflow: 'hidden',
+  },
+  cardImage: {
+    flex: 3,
+    width: '100%',
+    height: '100%',
+    alignSelf: 'center',
+  },
+  textContent: {
+    flex: 1,
+  },
+  cardtitle: {
+    fontSize: 12,
+    marginTop: 5,
+    fontWeight: 'bold',
+  },
+  cardDescription: {
+    fontSize: 12,
+    color: '#444',
+  },
+  markerWrap: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  marker: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: 'rgba(107,172,67, 0.9)',
+  },
+  ring: {
+    width: 24,
+    height: 24,
+    borderRadius: 12,
+    backgroundColor: 'rgba(107,172,67, 0.3)',
+    position: 'absolute',
+    borderWidth: 1,
+    borderColor: 'rgba(107,172,67, 0.5)',
+  },
+});
 
 const StyledMapView = styled({
   ...StyleSheet.absoluteFillObject,
 })(MapView);
 
-class Location extends PureComponent {
+class Location extends React.Component {
   static navigationOptions = ({ navigation }) => ({
     title: 'Location',
     headerLeft: null,
@@ -29,6 +97,143 @@ class Location extends PureComponent {
       </PaddedView>
     ),
   });
+
+  state = {
+    markers: [
+      {
+        title: 'Aiken',
+        coordinate: {
+          latitude: 33.5664789,
+          longitude: -81.7465594,
+        },
+        id: 0,
+        image: { uri: 'https://i.imgur.com/sNam9iJ.jpg' },
+      },
+      {
+        title: 'Anderson',
+        coordinate: {
+          latitude: 34.595434,
+          longitude: -82.6244131,
+        },
+        id: 1,
+        image: { uri: 'https://i.imgur.com/sNam9iJ.jpg' },
+      },
+      {
+        title: 'Charleston',
+        coordinate: {
+          latitude: 32.914992,
+          longitude: -80.1035458,
+        },
+        id: 2,
+        image: { uri: 'https://i.imgur.com/sNam9iJ.jpg' },
+      },
+      {
+        title: 'Clemson',
+        coordinate: {
+          latitude: 34.68901,
+          longitude: -82.8589537,
+        },
+        id: 3,
+        image: { uri: 'https://i.imgur.com/sNam9iJ.jpg' },
+      },
+      {
+        title: 'Columbia',
+        coordinate: {
+          latitude: 34.030309,
+          longitude: -81.098607,
+        },
+        id: 4,
+        image: { uri: 'https://i.imgur.com/sNam9iJ.jpg' },
+      },
+      {
+        title: 'Florence',
+        coordinate: {
+          latitude: 34.2121752,
+          longitude: -79.7992434,
+        },
+        id: 5,
+        image: { uri: 'https://i.imgur.com/sNam9iJ.jpg' },
+      },
+      {
+        title: 'Greenville',
+        coordinate: {
+          latitude: 34.852042,
+          longitude: -82.3567597,
+        },
+        id: 6,
+        image: { uri: 'https://i.imgur.com/sNam9iJ.jpg' },
+      },
+      {
+        title: 'Greenwood',
+        coordinate: {
+          latitude: 34.2140468,
+          longitude: -82.1496785,
+        },
+        id: 7,
+        image: { uri: 'https://i.imgur.com/sNam9iJ.jpg' },
+      },
+      {
+        title: 'Hilton Head',
+        coordinate: {
+          latitude: 32.2722634,
+          longitude: -80.9448838,
+        },
+        id: 8,
+        image: { uri: 'https://i.imgur.com/sNam9iJ.jpg' },
+      },
+      {
+        title: 'Myrtle Beach',
+        coordinate: {
+          latitude: 33.7159127,
+          longitude: -78.9272232,
+        },
+        id: 9,
+        image: { uri: 'https://i.imgur.com/sNam9iJ.jpg' },
+      },
+      {
+        title: 'Northeast Columbia',
+        coordinate: {
+          latitude: 34.1109545,
+          longitude: -80.883067,
+        },
+        id: 10,
+        image: { uri: 'https://i.imgur.com/sNam9iJ.jpg' },
+      },
+      {
+        title: 'Powdersville',
+        coordinate: {
+          latitude: 34.7877695,
+          longitude: -82.4856855,
+        },
+        id: 11,
+        image: { uri: 'https://i.imgur.com/sNam9iJ.jpg' },
+      },
+      {
+        title: 'Rock Hill',
+        coordinate: {
+          latitude: 34.950421,
+          longitude: -81.0898858,
+        },
+        id: 12,
+        image: { uri: 'https://i.imgur.com/sNam9iJ.jpg' },
+      },
+      {
+        title: 'Spartanburg',
+        coordinate: {
+          latitude: 34.9331235,
+          longitude: -81.9966683,
+        },
+        id: 13,
+        image: { uri: 'https://i.imgur.com/sNam9iJ.jpg' },
+      },
+    ],
+    region: {
+      latitude: 34.00115,
+      longitude: -81.032393,
+      latitudeDelta: 0.7,
+      longitudeDelta: 0.07,
+    },
+  };
 
   static propTypes = {
     navigation: PropTypes.shape({
@@ -49,29 +254,129 @@ class Location extends PureComponent {
     }),
   };
 
+  componentWillMount() {
+    this.index = 0;
+    this.animation = new Animated.Value(0);
+  }
+
+  componentDidMount() {
+    // We should detect when scrolling has stopped then animate
+    // We should just debounce the event listener here
+    this.animation.addListener(({ value }) => {
+      let index = Math.floor(value / CARD_WIDTH + 0.3); // animate 30% away from landing on the next item
+      if (index >= this.state.markers.length) {
+        index = this.state.markers.length - 1;
+      }
+      if (index <= 0) {
+        index = 0;
+      }
+
+      clearTimeout(this.regionTimeout);
+      this.regionTimeout = setTimeout(() => {
+        if (this.index !== index) {
+          this.index = index;
+          const { coordinate } = this.state.markers[index];
+          this.map.animateToRegion(
+            {
+              ...coordinate,
+              latitudeDelta: this.state.region.latitudeDelta,
+              longitudeDelta: this.state.region.longitudeDelta,
+            },
+            350
+          );
+        }
+      }, 10);
+    });
+  }
+
   render() {
+    const interpolations = this.state.markers.map((marker, index) => {
+      const inputRange = [
+        (index - 1) * CARD_WIDTH,
+        index * CARD_WIDTH,
+        (index + 1) * CARD_WIDTH,
+      ];
+      const scale = this.animation.interpolate({
+        inputRange,
+        outputRange: [1, 2.5, 1],
+        extrapolate: 'clamp',
+      });
+      const opacity = this.animation.interpolate({
+        inputRange,
+        outputRange: [0.35, 1, 0.35],
+        extrapolate: 'clamp',
+      });
+      return { scale, opacity };
+    });
     return (
-      <StyledMapView
-        initialRegion={{
-          latitude: LATITUDE,
-          longitude: LONGITUDE,
-          latitudeDelta: LATITUDE_DELTA,
-          longitudeDelta: LONGITUDE_DELTA,
-        }}
-        showsUserLocation
-      >
-        {campuses.map((campus) => (
-          <Marker
-            coordinate={{
-              latitude: campus.latitude,
-              longitude: campus.longitude,
-            }}
-            title={campus.Name}
-            key={campus.id}
-            image={markerDot}
-          />
-        ))}
-      </StyledMapView>
+      <View style={styles.container}>
+        <StyledMapView
+          ref={(map) => {
+            this.map = map;
+          }}
+          initialRegion={this.state.region}
+          showsUserLocation
+        >
+          {this.state.markers.map((marker, index) => {
+            const scaleStyle = {
+              transform: [
+                {
+                  scale: interpolations[index].scale,
+                },
+              ],
+            };
+            const opacityStyle = {
+              opacity: interpolations[index].opacity,
+            };
+            return (
+              <MapView.Marker key={index} coordinate={marker.coordinate}>
+                <Animated.View style={[styles.markerWrap, opacityStyle]}>
+                  <Animated.View style={[styles.ring, scaleStyle]} />
+                  <View style={styles.marker} />
+                </Animated.View>
+              </MapView.Marker>
+            );
+          })}
+        </StyledMapView>
+        <Animated.ScrollView
+          horizontal
+          scrollEventThrottle={1}
+          showsHorizontalScrollIndicator={false}
+          snapToInterval={CARD_WIDTH}
+          onScroll={Animated.event(
+            [
+              {
+                nativeEvent: {
+                  contentOffset: {
+                    x: this.animation,
+                  },
+                },
+              },
+            ],
+            { useNativeDriver: true }
+          )}
+          style={styles.scrollView}
+          contentContainerStyle={styles.endPadding}
+        >
+          {this.state.markers.map((marker, index) => (
+            <View style={styles.card} key={index}>
+              <Image
+                source={marker.image}
+                style={styles.cardImage}
+                resizeMode="cover"
+              />
+              <View style={styles.textContent}>
+                <Text numberOfLines={1} style={styles.cardtitle}>
+                  {marker.title}
+                </Text>
+                <Text numberOfLines={1} style={styles.cardDescription}>
+                  {marker.description}
+                </Text>
+              </View>
+            </View>
+          ))}
+        </Animated.ScrollView>
+      </View>
     );
   }
 }
--- packages/apolloschurchapp/src/user-settings/Locations/getCampusLocations.js
+++ packages/apolloschurchapp/src/user-settings/Locations/getCampusLocations.js
@@ -1,87 +1,160 @@
+const Images = [
+  {
+    uri:
+      '//s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/locations_chs.2x1.jpg',
+  },
+  {
+    uri:
+      '//s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/Anderson.jpg',
+  },
+  {
+    uri:
+      '//s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/Greenville2x1.jpg',
+  },
+  {
+    uri:
+      '//s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/florence/FLO2x1_-100.jpg',
+  },
+  {
+    uri:
+      '//s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/columbia/2x1lexcola.png',
+  },
+  {
+    uri:
+      '//s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/Greenwood2x1.jpg',
+  },
+  {
+    uri:
+      '//s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/campus.spartanburg.2x1.jpg',
+  },
+  {
+    uri:
+      '//s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/locations_myr.2x1.jpg',
+  },
+  {
+    uri:
+      '//s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/clemson/CLE_22x1_-100.jpg',
+  },
+  {
+    uri:
+      '//s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/pow_launch_8.7.16_web-2.jpg',
+  },
+  {
+    uri:
+      '//s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/HHdoors.jpeg',
+  },
+  {
+    uri:
+      '//s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/columbia/hero.jpg',
+  },
+  {
+    uri:
+      '//s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/rock_hill/RKH_22x1_-100.jpg',
+  },
+  {
+    uri:
+      '//s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/aiken/NSC23471.jpg',
+  },
+];
+
 const campuses = [
   {
     Name: 'Charleston',
     latitude: 32.914992,
     longitude: -80.1035458,
     id: 6,
+    image: Images[0],
   },
   {
     Name: 'Anderson',
     latitude: 34.595434,
     longitude: -82.6244131,
     id: 3,
+    image: Images[1],
   },
   {
     Name: 'Greenville',
     latitude: 34.852042,
     longitude: -82.3567597,
     id: 10,
+    image: Images[2],
   },
   {
     Name: 'Florence',
     latitude: 34.2121752,
     longitude: -79.7992434,
     id: 9,
+    image: Images[3],
   },
   {
     Name: 'Columbia',
     latitude: 34.030309,
     longitude: -81.098607,
     id: 8,
+    image: Images[4],
   },
   {
     Name: 'Greenwood',
     latitude: 34.2140468,
     longitude: -82.1496785,
     id: 11,
+    image: Images[5],
   },
   {
     Name: 'Spartanburg',
     latitude: 34.9331235,
     longitude: -81.9966683,
     id: 18,
+    image: Images[6],
   },
   {
     Name: 'Myrtle Beach',
     latitude: 33.7159127,
     longitude: -78.9272232,
     id: 14,
+    image: Images[7],
   },
   {
     Name: 'Clemson',
     latitude: 34.68901,
     longitude: -82.8589537,
     id: 7,
+    image: Images[8],
   },
   {
     Name: 'Powdersville',
     latitude: 34.7877695,
     longitude: -82.4856855,
     id: 16,
+    image: Images[9],
   },
   {
     Name: 'Hilton Head',
     latitude: 32.2722634,
     longitude: -80.9448838,
     id: 12,
+    image: Images[10],
   },
   {
     Name: 'Northeast Columbia',
     latitude: 34.1109545,
     longitude: -80.883067,
     id: 15,
+    image: Images[11],
   },
   {
     Name: 'Rock Hill',
     latitude: 34.950421,
     longitude: -81.0898858,
     id: 17,
+    image: Images[12],
   },
   {
     Name: 'Aiken',
     latitude: 33.5664789,
     longitude: -81.7465594,
     id: 2,
+    image: Images[13],
   },
 ];
 
