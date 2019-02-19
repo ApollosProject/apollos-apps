0e217eaf0843e2dd0b267f45726eb60b13ab2be4
Burke Shartsis <burke@differential.com>
2019-02-19 15:07:59 -0500

    Remove theme overides condition and add some documentation 💥

--- packages/apollos-ui-kit/src/BackgroundView/index.js
+++ packages/apollos-ui-kit/src/BackgroundView/index.js
@@ -5,12 +5,13 @@ import styled from '../styled';
 import { withTheme } from '../theme';
 
 const BackgroundView = compose(
+  // Accepts all props available to View and LinearGradient.
+  // https://github.com/react-native-community/react-native-linear-gradient#additional-props
   withTheme(({ theme, colors, ...props }) => ({
-    colors: colors ||
-      theme.overrides.backgroundGradient || [
-        theme.colors.background.paper,
-        theme.colors.background.screen,
-      ],
+    colors: colors || [
+      theme.colors.background.paper,
+      theme.colors.background.screen,
+    ],
     ...props,
   })),
   styled({ flex: 1 })
