fb5821b4e6dbfd1ea282ffd1880f3108cbe841ad
calebpanza <45076058+calebpanza@users.noreply.github.com>
2019-02-21 09:34:14 -0500

    Adds props support to BackgroundView (#524)
    * Update BackgroundView Component
    
    Added an override tree that first looks for colors passed into the component, then the local theme file for an override variable, and then used the default background gradient.
    
    * Added tests and snapshots
    
    * update component to pass down additional props.
    
    * updated tests
    
    * Remove theme overides condition and add some documentation 💥

--- packages/apollos-ui-kit/src/BackgroundView/BackgroundView.tests.js
+++ packages/apollos-ui-kit/src/BackgroundView/BackgroundView.tests.js
@@ -14,4 +14,28 @@ describe('The BackgroundView component', () => {
     );
     expect(tree).toMatchSnapshot();
   });
+  it('should render with custom colors', () => {
+    const tree = renderer.create(
+      <Providers>
+        <BackgroundView colors={['#FF0000', '#0000FF']} />
+      </Providers>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  it('should render as a solid color', () => {
+    const tree = renderer.create(
+      <Providers>
+        <BackgroundView colors={['#fa8072', '#fa8072']} />
+      </Providers>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  it('should accept extra props', () => {
+    const tree = renderer.create(
+      <Providers>
+        <BackgroundView locations={[0, 0.5, 0.6]} />
+      </Providers>
+    );
+    expect(tree).toMatchSnapshot();
+  });
 });
--- packages/apollos-ui-kit/src/BackgroundView/index.js
+++ packages/apollos-ui-kit/src/BackgroundView/index.js
@@ -5,8 +5,14 @@ import styled from '../styled';
 import { withTheme } from '../theme';
 
 const BackgroundView = compose(
-  withTheme(({ theme }) => ({
-    colors: [theme.colors.background.paper, theme.colors.background.screen],
+  // Accepts all props available to View and LinearGradient.
+  // https://github.com/react-native-community/react-native-linear-gradient#additional-props
+  withTheme(({ theme, colors, ...props }) => ({
+    colors: colors || [
+      theme.colors.background.paper,
+      theme.colors.background.screen,
+    ],
+    ...props,
   })),
   styled({ flex: 1 })
 )(LinearGradient);
