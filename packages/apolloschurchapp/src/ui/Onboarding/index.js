bb8ca5d2a5bc19c6e09d2a0f89c1170352116d8b
Burke Shartsis <burke@differential.com>
2019-02-21 13:40:02 -0500

    Hide default controls and add ref for custom button controls.

--- packages/apolloschurchapp/src/ui/Onboarding/index.js
+++ packages/apolloschurchapp/src/ui/Onboarding/index.js
@@ -6,6 +6,8 @@ class Onboarding extends Component {
   constructor() {
     super();
 
+    this.swiper = null;
+
     this.state = { pagination: true };
   }
 
@@ -27,26 +29,31 @@ class Onboarding extends Component {
 
   render() {
     return (
-      <Swiper
-        showsPagination={this.state.pagination}
-        onIndexChanged={this.handleOnIndexChanged}
-        loop={false}
-        scrollEnabled={false}
-        showsButtons
-      >
-        <View style={{ flex: 1, backgroundColor: 'salmon' }}>
-          <Text>Hello World 1</Text>
-        </View>
-        <View style={{ flex: 1, backgroundColor: 'cyan' }}>
-          <Text>Hello World 2</Text>
-        </View>
-        <View style={{ flex: 1, backgroundColor: 'lightyellow' }}>
-          <Text>No pager!</Text>
-        </View>
-        <View style={{ flex: 1, backgroundColor: 'lightgreen' }}>
-          <Text>Hello World 3</Text>
-        </View>
-      </Swiper>
+      <>
+        <Swiper
+          showsPagination={this.state.pagination}
+          onIndexChanged={this.handleOnIndexChanged}
+          loop={false}
+          scrollEnabled={false}
+          showsButtons={false}
+          ref={(ref) => (this.swiper = ref)}
+        >
+          <View style={{ flex: 1, backgroundColor: 'salmon' }}>
+            <Text>Hello World 1</Text>
+          </View>
+          <View style={{ flex: 1, backgroundColor: 'cyan' }}>
+            <Text>Hello World 2</Text>
+          </View>
+          <View style={{ flex: 1, backgroundColor: 'lightyellow' }}>
+            <Text>No pager!</Text>
+          </View>
+          <View style={{ flex: 1, backgroundColor: 'lightgreen' }}>
+            <Text>Hello World 3</Text>
+          </View>
+        </Swiper>
+        <Text onPress={() => this.swiper.scrollBy(1)}>Next!</Text>
+        <Text onPress={() => this.swiper.scrollBy(-1)}>Previous!</Text>
+      </>
     );
   }
 }
