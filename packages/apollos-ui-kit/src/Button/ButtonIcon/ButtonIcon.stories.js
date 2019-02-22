ff4bdd3a82235399c72ab1ec35e2e1eab489f363
Burke Shartsis <burke@differential.com>
2019-02-21 20:49:12 -0500

    update ButtonIcon to support a background color.

--- packages/apollos-ui-kit/src/Button/ButtonIcon/ButtonIcon.stories.js
+++ packages/apollos-ui-kit/src/Button/ButtonIcon/ButtonIcon.stories.js
@@ -6,5 +6,8 @@ import ButtonIcon from '.';
 storiesOf('Buttons/Icon', module)
   .add('default', () => <ButtonIcon name={'umbrella'} />)
   .add('iconPadding', () => <ButtonIcon name={'umbrella'} iconPadding={50} />)
+  .add('backgroundColor', () => (
+    <ButtonIcon name={'umbrella'} backgroundColor={'salmon'} />
+  ))
   .add('disabled', () => <ButtonIcon name={'umbrella'} disabled />)
   .add('isLoading', () => <ButtonIcon name={'umbrella'} disabled isLoading />);
