78a921ebec05fc2ca92fff4d55105f62c829397b
Burke Shartsis <burke@differential.com>
2019-02-21 20:59:51 -0500

    continued wip for Onboarding

--- packages/apolloschurchapp/src/ui/Onboarding/index.js
+++ packages/apolloschurchapp/src/ui/Onboarding/index.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+// import PropTypes from 'prop-types';
 import Swiper from 'react-native-swiper';
+
+import AskName from './AskName';
 
 class Onboarding extends Component {
   constructor() {
     super();
 
     this.swiper = null;
-
     this.state = { pagination: true };
   }
 
@@ -29,31 +31,34 @@ class Onboarding extends Component {
 
   render() {
     return (
-      <>
-        <Swiper
-          showsPagination={this.state.pagination}
-          onIndexChanged={this.handleOnIndexChanged}
-          loop={false}
-          scrollEnabled={false}
-          showsButtons={false}
-          ref={(ref) => (this.swiper = ref)}
-        >
-          <View style={{ flex: 1, backgroundColor: 'salmon' }}>
-            <Text>Hello World 1</Text>
-          </View>
-          <View style={{ flex: 1, backgroundColor: 'cyan' }}>
-            <Text>Hello World 2</Text>
-          </View>
-          <View style={{ flex: 1, backgroundColor: 'lightyellow' }}>
-            <Text>No pager!</Text>
-          </View>
-          <View style={{ flex: 1, backgroundColor: 'lightgreen' }}>
-            <Text>Hello World 3</Text>
-          </View>
-        </Swiper>
-        <Text onPress={() => this.swiper.scrollBy(1)}>Next!</Text>
-        <Text onPress={() => this.swiper.scrollBy(-1)}>Previous!</Text>
-      </>
+      <Swiper
+        showsPagination={this.state.pagination}
+        onIndexChanged={this.handleOnIndexChanged}
+        loop={false}
+        scrollEnabled={false}
+        showsButtons={false}
+        ref={(ref) => (this.swiper = ref)}
+      >
+        <AskName />
+        <View style={{ flex: 1, backgroundColor: 'salmon' }}>
+          <Text>Hello World 1</Text>
+          <Text onPress={() => this.swiper.scrollBy(1)}>Next!</Text>
+        </View>
+        <View style={{ flex: 1, backgroundColor: 'cyan' }}>
+          <Text>Hello World 2</Text>
+          <Text onPress={() => this.swiper.scrollBy(1)}>Next!</Text>
+          <Text onPress={() => this.swiper.scrollBy(-1)}>Previous!</Text>
+        </View>
+        <View style={{ flex: 1, backgroundColor: 'lightyellow' }}>
+          <Text>No pager!</Text>
+          <Text onPress={() => this.swiper.scrollBy(1)}>Next!</Text>
+          <Text onPress={() => this.swiper.scrollBy(-1)}>Previous!</Text>
+        </View>
+        <View style={{ flex: 1, backgroundColor: 'lightgreen' }}>
+          <Text>Hello World 3</Text>
+          <Text onPress={() => this.swiper.scrollBy(-1)}>Previous!</Text>
+        </View>
+      </Swiper>
     );
   }
 }
