51e329bd3859752b1bd42eaef61a58d27c6b009b
Burke Shartsis <burke@differential.com>
2019-02-25 13:51:28 -0500

    adding Screen next button test

--- packages/apolloschurchapp/src/ui/Onboarding/Screen/Screen.tests.js
+++ packages/apolloschurchapp/src/ui/Onboarding/Screen/Screen.tests.js
@@ -7,11 +7,21 @@ import Providers from 'apolloschurchapp/src/Providers';
 
 import Screen from '.';
 
-describe('The Onboarding Screen component', () => {
+describe('The onboarding Screen component', () => {
   it('should render children', () => {
     const tree = renderer.create(
       <Providers>
         <Screen>
+          <H1>Boom</H1>
+        </Screen>
+      </Providers>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  it('should render next button navigation', () => {
+    const tree = renderer.create(
+      <Providers>
+        <Screen onboardingScrollBy={jest.fn()}>
           <H1>Boom</H1>
         </Screen>
       </Providers>
