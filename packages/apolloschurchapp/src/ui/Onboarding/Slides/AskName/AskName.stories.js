961e681d067eec617bcaa8adc319a9a10a4420c5
Burke Shartsis <burke@differential.com>
2019-02-28 11:32:54 -0500

    add default AskName story

---
+++ packages/apolloschurchapp/src/ui/Onboarding/Slides/AskName/AskName.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react-native';
+
+import AskName from '.';
+
+storiesOf('Onboarding/Slides/AskName', module).add('default', () => (
+  <AskName />
+));
