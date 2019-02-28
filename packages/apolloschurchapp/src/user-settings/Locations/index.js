3025b26548dc03825f922abf708544279973f645
Daniel Edwards <dan.edwards@newspring.cc>
2019-02-28 09:15:29 -0500

    Refactored the timeout to a debounce

--- packages/apolloschurchapp/src/user-settings/Locations/index.js
+++ packages/apolloschurchapp/src/user-settings/Locations/index.js
@@ -2,6 +2,8 @@ import React, { PureComponent } from 'react';
 import { Dimensions, StyleSheet, View, Animated } from 'react-native';
 import PropTypes from 'prop-types';
 import MapView, { Marker } from 'react-native-maps';
+import Color from 'color';
+import { debounce } from 'lodash';
 
 import {
   PaddedView,
@@ -20,6 +22,10 @@ const { width } = Dimensions.get('window');
 
 const CARD_WIDTH = width * 0.94;
 
+const endPadding = {
+  paddingRight: width - CARD_WIDTH,
+};
+
 const ContainerView = styled({
   flex: 1,
 })(View);
@@ -36,19 +42,8 @@ const MarkerView = styled(({ theme }) => ({
   width: 8,
   height: 8,
   borderRadius: 4,
-  backgroundColor: theme.colors.primary.fade(theme.alpha.medium),
+  backgroundColor: Color(theme.colors.primary).fade(theme.alpha.medium),
 }))(View);
-
-const scaleStyle = {
-  transform: [
-    {
-      scale: interpolations[index].scale,
-    },
-  ],
-};
-const opacityStyle = {
-  opacity: interpolations[index].opacity,
-};
 
 const styles = StyleSheet.create({
   container: {
@@ -76,210 +71,6 @@ const styles = StyleSheet.create({
 });
 
 class Location extends PureComponent {
-  static navigationOptions = ({ navigation }) => ({
-    title: 'Location',
-    headerLeft: null,
-    headerRight: (
-      <PaddedView vertical={false}>
-        <ButtonLink onPress={() => navigation.goBack()}>Back</ButtonLink>
-      </PaddedView>
-    ),
-  });
-
-  state = {
-    markers: [
-      {
-        title: 'Aiken',
-        coordinate: {
-          latitude: 33.5664789,
-          longitude: -81.7465594,
-        },
-        distance: 12,
-        id: 0,
-        image: {
-          uri:
-            'https://s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/aiken/NSC23471.jpg',
-        },
-        description: 'This is a NewSpring Campus',
-      },
-      {
-        title: 'Anderson',
-        coordinate: {
-          latitude: 34.595434,
-          longitude: -82.6244131,
-        },
-        id: 1,
-        image: {
-          uri:
-            'https://s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/Anderson.jpg',
-        },
-        description: 'This is a NewSpring Campus',
-      },
-      {
-        title: 'Charleston',
-        coordinate: {
-          latitude: 32.914992,
-          longitude: -80.1035458,
-        },
-        id: 2,
-        image: {
-          uri:
-            'https://s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/locations_chs.2x1.jpg',
-        },
-        description: 'This is a NewSpring Campus',
-      },
-      {
-        title: 'Clemson',
-        coordinate: {
-          latitude: 34.68901,
-          longitude: -82.8589537,
-        },
-        id: 3,
-        image: {
-          uri:
-            'https://s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/clemson/CLE_22x1_-100.jpg',
-        },
-        description: 'This is a NewSpring Campus',
-      },
-      {
-        title: 'Columbia',
-        coordinate: {
-          latitude: 34.030309,
-          longitude: -81.098607,
-        },
-        id: 4,
-        image: {
-          uri:
-            'https://s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/columbia/2x1lexcola.png',
-        },
-        description: 'This is a NewSpring Campus',
-      },
-      {
-        title: 'Florence',
-        coordinate: {
-          latitude: 34.2121752,
-          longitude: -79.7992434,
-        },
-        id: 5,
-        image: {
-          uri:
-            'https://s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/florence/FLO2x1_-100.jpg',
-        },
-        description: 'This is a NewSpring Campus',
-      },
-      {
-        title: 'Greenville',
-        coordinate: {
-          latitude: 34.852042,
-          longitude: -82.3567597,
-        },
-        id: 6,
-        image: {
-          uri:
-            'https://s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/Greenville2x1.jpg',
-        },
-        description: 'This is a NewSpring Campus',
-      },
-      {
-        title: 'Greenwood',
-        coordinate: {
-          latitude: 34.2140468,
-          longitude: -82.1496785,
-        },
-        id: 7,
-        image: {
-          uri:
-            'https://s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/Greenwood2x1.jpg',
-        },
-        description: 'This is a NewSpring Campus',
-      },
-      {
-        title: 'Hilton Head',
-        coordinate: {
-          latitude: 32.2722634,
-          longitude: -80.9448838,
-        },
-        id: 8,
-        image: {
-          uri:
-            'https://s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/HHdoors.jpeg',
-        },
-        description: 'This is a NewSpring Campus',
-      },
-      {
-        title: 'Myrtle Beach',
-        coordinate: {
-          latitude: 33.7159127,
-          longitude: -78.9272232,
-        },
-        id: 9,
-        image: {
-          uri:
-            'https://s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/locations_myr.2x1.jpg',
-        },
-        description: 'This is a NewSpring Campus',
-      },
-      {
-        title: 'Northeast Columbia',
-        coordinate: {
-          latitude: 34.1109545,
-          longitude: -80.883067,
-        },
-        id: 10,
-        image: {
-          uri:
-            'https://s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/columbia/hero.jpg',
-        },
-        description: 'This is a NewSpring Campus',
-      },
-      {
-        title: 'Powdersville',
-        coordinate: {
-          latitude: 34.7877695,
-          longitude: -82.4856855,
-        },
-        id: 11,
-        image: {
-          uri:
-            'https://s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/pow_launch_8.7.16_web-2.jpg',
-        },
-        description: 'This is a NewSpring Campus',
-      },
-      {
-        title: 'Rock Hill',
-        coordinate: {
-          latitude: 34.950421,
-          longitude: -81.0898858,
-        },
-        id: 12,
-        image: {
-          uri:
-            'https://s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/rock_hill/RKH_22x1_-100.jpg',
-        },
-        description: 'This is a NewSpring Campus',
-      },
-      {
-        title: 'Spartanburg',
-        coordinate: {
-          latitude: 34.9331235,
-          longitude: -81.9966683,
-        },
-        id: 13,
-        image: {
-          uri:
-            'https://s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/campus.spartanburg.2x1.jpg',
-        },
-        description: 'This is a NewSpring Campus',
-      },
-    ],
-    region: {
-      latitude: 34.00115,
-      longitude: -81.032393,
-      latitudeDelta: 4,
-      longitudeDelta: 0.4,
-    },
-  };
-
   static propTypes = {
     navigation: PropTypes.shape({
       getParam: PropTypes.func,
@@ -299,40 +90,250 @@ class Location extends PureComponent {
     }),
   };
 
-  componentWillMount() {
+  static navigationOptions = ({ navigation }) => ({
+    title: 'Location',
+    headerLeft: null,
+    headerRight: (
+      <PaddedView vertical={false}>
+        <ButtonLink onPress={() => navigation.goBack()}>Back</ButtonLink>
+      </PaddedView>
+    ),
+  });
+
+  constructor() {
+    super();
     this.index = 0;
     this.animation = new Animated.Value(0);
+    this.state = {
+      markers: [
+        {
+          title: 'Aiken',
+          coordinate: {
+            latitude: 33.5664789,
+            longitude: -81.7465594,
+          },
+          distance: 12,
+          id: 0,
+          image: {
+            uri:
+              'https://s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/aiken/NSC23471.jpg',
+          },
+          description: 'This is a NewSpring Campus',
+        },
+        {
+          title: 'Anderson',
+          coordinate: {
+            latitude: 34.595434,
+            longitude: -82.6244131,
+          },
+          id: 1,
+          image: {
+            uri:
+              'https://s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/Anderson.jpg',
+          },
+          description: 'This is a NewSpring Campus',
+        },
+        {
+          title: 'Charleston',
+          coordinate: {
+            latitude: 32.914992,
+            longitude: -80.1035458,
+          },
+          id: 2,
+          image: {
+            uri:
+              'https://s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/locations_chs.2x1.jpg',
+          },
+          description: 'This is a NewSpring Campus',
+        },
+        {
+          title: 'Clemson',
+          coordinate: {
+            latitude: 34.68901,
+            longitude: -82.8589537,
+          },
+          id: 3,
+          image: {
+            uri:
+              'https://s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/clemson/CLE_22x1_-100.jpg',
+          },
+          description: 'This is a NewSpring Campus',
+        },
+        {
+          title: 'Columbia',
+          coordinate: {
+            latitude: 34.030309,
+            longitude: -81.098607,
+          },
+          id: 4,
+          image: {
+            uri:
+              'https://s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/columbia/2x1lexcola.png',
+          },
+          description: 'This is a NewSpring Campus',
+        },
+        {
+          title: 'Florence',
+          coordinate: {
+            latitude: 34.2121752,
+            longitude: -79.7992434,
+          },
+          id: 5,
+          image: {
+            uri:
+              'https://s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/florence/FLO2x1_-100.jpg',
+          },
+          description: 'This is a NewSpring Campus',
+        },
+        {
+          title: 'Greenville',
+          coordinate: {
+            latitude: 34.852042,
+            longitude: -82.3567597,
+          },
+          id: 6,
+          image: {
+            uri:
+              'https://s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/Greenville2x1.jpg',
+          },
+          description: 'This is a NewSpring Campus',
+        },
+        {
+          title: 'Greenwood',
+          coordinate: {
+            latitude: 34.2140468,
+            longitude: -82.1496785,
+          },
+          id: 7,
+          image: {
+            uri:
+              'https://s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/Greenwood2x1.jpg',
+          },
+          description: 'This is a NewSpring Campus',
+        },
+        {
+          title: 'Hilton Head',
+          coordinate: {
+            latitude: 32.2722634,
+            longitude: -80.9448838,
+          },
+          id: 8,
+          image: {
+            uri:
+              'https://s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/HHdoors.jpeg',
+          },
+          description: 'This is a NewSpring Campus',
+        },
+        {
+          title: 'Myrtle Beach',
+          coordinate: {
+            latitude: 33.7159127,
+            longitude: -78.9272232,
+          },
+          id: 9,
+          image: {
+            uri:
+              'https://s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/locations_myr.2x1.jpg',
+          },
+          description: 'This is a NewSpring Campus',
+        },
+        {
+          title: 'Northeast Columbia',
+          coordinate: {
+            latitude: 34.1109545,
+            longitude: -80.883067,
+          },
+          id: 10,
+          image: {
+            uri:
+              'https://s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/columbia/hero.jpg',
+          },
+          description: 'This is a NewSpring Campus',
+        },
+        {
+          title: 'Powdersville',
+          coordinate: {
+            latitude: 34.7877695,
+            longitude: -82.4856855,
+          },
+          id: 11,
+          image: {
+            uri:
+              'https://s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/pow_launch_8.7.16_web-2.jpg',
+          },
+          description: 'This is a NewSpring Campus',
+        },
+        {
+          title: 'Rock Hill',
+          coordinate: {
+            latitude: 34.950421,
+            longitude: -81.0898858,
+          },
+          id: 12,
+          image: {
+            uri:
+              'https://s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/rock_hill/RKH_22x1_-100.jpg',
+          },
+          description: 'This is a NewSpring Campus',
+        },
+        {
+          title: 'Spartanburg',
+          coordinate: {
+            latitude: 34.9331235,
+            longitude: -81.9966683,
+          },
+          id: 13,
+          image: {
+            uri:
+              'https://s3.amazonaws.com/ns.images/all/heroes/newspring/campuses/campus.spartanburg.2x1.jpg',
+          },
+          description: 'This is a NewSpring Campus',
+        },
+      ],
+      region: {
+        latitude: 34.00115,
+        longitude: -81.032393,
+        latitudeDelta: 4,
+        longitudeDelta: 0.4,
+      },
+    };
   }
 
-  componentDidMount = () => {
+  componentDidMount() {
     // We should detect when scrolling has stopped then animate
-    // We should just debounce the event listener here
-    this.animation.addListener(({ value }) => {
-      let index = Math.floor(value / CARD_WIDTH); // animate 30% away from landing on the next item
-      if (index >= this.state.markers.length) {
-        index = this.state.markers.length - 1;
-      }
-      if (index <= 0) {
-        index = 0;
-      }
 
-      clearTimeout(this.regionTimeout);
-      this.regionTimeout = setTimeout(() => {
-        if (this.index !== index) {
-          this.index = index;
-          const { coordinate } = this.state.markers[index];
-          this.map.animateToRegion(
-            {
-              ...coordinate,
-              latitudeDelta: this.state.region.latitudeDelta,
-              longitudeDelta: this.state.region.longitudeDelta,
-            },
-            350
-          );
-        }
-      }, 10);
-    });
+    this.animation.addListener(debounce(this.updateCoordinates));
+  }
+
+  // Look into all this index logic and figure out why there's possibly a negative index.
+  // This might have to do with the scroolview index and how that's calculated.
+  // Figure out how value gets passed in. Animation event listeners. how to normalize value.
+
+  updateIndex = ({ value }) => {
+    let index = Math.floor(value / CARD_WIDTH + 0.3); // animate 30% away from landing on the next item
+    if (index >= this.state.markers.length) {
+      index = this.state.markers.length - 1;
+    }
+    if (index <= 0) {
+      index = 0;
+    }
+    if (this.index !== index) {
+      this.index = index;
+      this.updateCoordinates();
+    }
   };
+
+  updateCoordinates() {
+    const { coordinate } = this.state.markers[this.index];
+    this.map.animateToRegion(
+      {
+        ...coordinate,
+        latitudeDelta: this.state.region.latitudeDelta,
+        longitudeDelta: this.state.region.longitudeDelta,
+      },
+      350
+    );
+  }
 
   render() {
     const interpolations = this.state.markers.map((marker, index) => {
@@ -354,7 +355,7 @@ class Location extends PureComponent {
       return { scale, opacity };
     });
     return (
-      <ContainerView style={styles.container}>
+      <ContainerView>
         <MapView
           ref={(map) => {
             this.map = map;
@@ -403,10 +404,10 @@ class Location extends PureComponent {
               ],
               { useNativeDriver: true }
             )}
-            contentContainerStyle={styles.endPadding}
+            contentContainerStyle={endPadding}
           >
             {this.state.markers.map((marker) => (
-              <ContainerView style={styles.container}>
+              <ContainerView>
                 <CampusCard
                   key={marker.id}
                   distance={marker.distance}
