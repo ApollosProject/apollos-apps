456ce69bb3975a5cdba30c82bc78197ea909034c
Burke Shartsis <burke@differential.com>
2019-02-28 11:35:56 -0500

    add description story for AskName

--- packages/apolloschurchapp/src/ui/Onboarding/Slides/AskName/AskName.stories.js
+++ packages/apolloschurchapp/src/ui/Onboarding/Slides/AskName/AskName.stories.js
@@ -5,4 +5,7 @@ import AskName from '.';
 
 storiesOf('Onboarding/Slides/AskName', module)
   .add('default', () => <AskName />)
-  .add('slideTitle', () => <AskName slideTitle={'Custom title text'} />);
+  .add('slideTitle', () => <AskName slideTitle={'Custom title text'} />)
+  .add('description', () => (
+    <AskName description={'Custom description text'} />
+  ));
