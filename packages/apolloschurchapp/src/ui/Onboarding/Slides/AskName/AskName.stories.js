877e295edb593b3e2fdc951c25f5f77aa2e435bf
Burke Shartsis <burke@differential.com>
2019-02-28 11:38:09 -0500

    add SliderWrapper props (aka: additional props) story for AskName

--- packages/apolloschurchapp/src/ui/Onboarding/Slides/AskName/AskName.stories.js
+++ packages/apolloschurchapp/src/ui/Onboarding/Slides/AskName/AskName.stories.js
@@ -6,6 +6,5 @@ import AskName from '.';
 storiesOf('Onboarding/Slides/AskName', module)
   .add('default', () => <AskName />)
   .add('slideTitle', () => <AskName slideTitle={'Custom title text'} />)
-  .add('description', () => (
-    <AskName description={'Custom description text'} />
-  ));
+  .add('description', () => <AskName description={'Custom description text'} />)
+  .add('SlideWrapper props', () => <AskName onboardingScrollBy={() => {}} />);
