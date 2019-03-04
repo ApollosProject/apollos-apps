4dcbbdd920dfb4ece2003e01b1d029ba5292599b
Michael Neeley <micneeley14@gmail.com>
2019-03-04 13:45:33 -0500

    added checkboxes

--- packages/apolloschurchapp/src/ui/Onboarding/slides/Features/index.js
+++ packages/apolloschurchapp/src/ui/Onboarding/slides/Features/index.js
@@ -1,5 +1,5 @@
 import React, { memo } from 'react';
-import { Image } from 'react-native';
+import { Image, View } from 'react-native';
 import PropTypes from 'prop-types';
 
 import { styled, H2, H5 } from '@apollosproject/ui-kit';
@@ -16,15 +16,45 @@ const StyledH5 = styled(({ theme }) => ({
 
 const CoverImage = styled(({ theme }) => ({
   height: '75%',
+  width: '100%',
   marginBottom: theme.sizing.baseUnit * 2,
-  marginVertical: theme.sizing.baseUnit,
+  marginVertical: theme.sizing.baseUnit * 0.5,
+  alignSelf: 'center',
 }))(Image);
+
+const CheckIcon = styled(() => ({
+  height: 20,
+  width: 20,
+  marginRight: 5,
+}))(Image);
+
+const defaults = [
+  'Grow in scripture',
+  'Childcare check-in',
+  'Keep up with your campus',
+  'Find a community group',
+  'Watch services',
+  'Receive prayer',
+  'Manage giving',
+];
+
+const FeatureList = memo(({ featureDescriptions }) => (
+  <View style={{ position: 'absolute', top: 180, alignSelf: 'center' }}>
+    {featureDescriptions.map((feature, key) => (
+      <View style={{ flexDirection: 'row', margin: 5 }}>
+        <CheckIcon source={require('./check.png')} />
+        <StyledH5 key={key}>{feature}</StyledH5>
+      </View>
+    ))}
+  </View>
+));
 
 const Features = memo(({ firstName, description, ...props }) => {
   const titleWithName = `Hey ${firstName}!`;
   return (
     <Slide {...props}>
       <CoverImage source={require('./mountain.jpeg')} />
+      <FeatureList featureDescriptions={defaults} />
       <Title>{titleWithName}</Title>
       <StyledH5>{description}</StyledH5>
     </Slide>
