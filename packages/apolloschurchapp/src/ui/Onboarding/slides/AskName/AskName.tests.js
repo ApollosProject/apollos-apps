5d0bf7377c8c5f878195c4cb5c2fed61bd79c069
Burke Shartsis <burke@differential.com>
2019-03-05 11:01:41 -0500

    fix AskName test naming

--- packages/apolloschurchapp/src/ui/Onboarding/slides/AskName/AskName.tests.js
+++ packages/apolloschurchapp/src/ui/Onboarding/slides/AskName/AskName.tests.js
@@ -5,7 +5,7 @@ import Providers from 'apolloschurchapp/src/Providers';
 
 import AskName from '.';
 
-describe('The Onboarding SlideWrapper component', () => {
+describe('The Onboarding AskName component', () => {
   it('should render', () => {
     const tree = renderer.create(
       <Providers>
