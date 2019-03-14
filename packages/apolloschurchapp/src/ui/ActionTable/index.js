71292af7085991b9aabad9597eb1ddb555e5ded8
Isaac Hardy <isaac.hardy@newspring.cc>
2019-03-14 16:35:39 -0400

    updated api to accept first X number of items

--- packages/apolloschurchapp/src/ui/ActionTable/index.js
+++ packages/apolloschurchapp/src/ui/ActionTable/index.js
@@ -5,9 +5,18 @@ import { Text } from 'react-native';
 import {
   TableView,
   Divider,
-  // styled,
+  ContentCard,
+  CardContent,
+  H5,
+  H3,
+  styled,
 } from '@apollosproject/ui-kit';
+
 import ActionItem from './ActionItem';
+
+const Header = styled(({ theme }) => ({
+  backgroundColor: theme.colors.background.paper,
+}))(CardContent);
 
 class ActionTable extends PureComponent {
   static propTypes = {
@@ -22,20 +31,33 @@ class ActionTable extends PureComponent {
       return <Text>Hi</Text>;
     }
     return (
-      <TableView>
-        {content.map((item) => (
-          <>
-            <ActionItem
-              key={item.id}
-              onPress={onPress}
-              label={item.parentChannel ? item.parentChannel.name : ''}
-              title={item.title || ''}
-              imageSource={item.coverImage ? item.coverImage.sources : ''}
-            />
-            <Divider />
-          </>
-        ))}
-      </TableView>
+      <ContentCard
+        isLoading={isLoading}
+        header={
+          <Header>
+            <H5>FOR YOU</H5>
+            <H3 numberOfLines={3} ellipsizeMode="tail">
+              Some random text that encourages you
+            </H3>
+          </Header>
+        }
+        content={
+          <TableView>
+            {content.map((item) => (
+              <>
+                <ActionItem
+                  key={item.id}
+                  onPress={onPress}
+                  label={item.parentChannel ? item.parentChannel.name : ''}
+                  title={item.title || ''}
+                  imageSource={item.coverImage ? item.coverImage.sources : ''}
+                />
+                <Divider />
+              </>
+            ))}
+          </TableView>
+        }
+      />
     );
   }
 }
