5f2e08967620ff6ae80e0ff66c3559e5b34d4215
Michael Neeley <micneeley14@gmail.com>
2019-03-18 16:35:34 -0400

    updated some tests

--- packages/apolloschurchapp/src/ui/Onboarding/slides/AboutYou/AboutYou.tests.js
+++ packages/apolloschurchapp/src/ui/Onboarding/slides/AboutYou/AboutYou.tests.js
@@ -5,7 +5,7 @@ import Providers from 'apolloschurchapp/src/Providers';
 
 import AboutYou from '.';
 
-describe('The Onboarding SlideWrapper component', () => {
+describe('The Onboarding AboutYou component', () => {
   it('should render', () => {
     const tree = renderer.create(
       <Providers>
