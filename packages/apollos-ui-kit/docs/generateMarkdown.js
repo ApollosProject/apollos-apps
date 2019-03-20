81097fa6343e716504e94878598f1ee321dc843b
Michael Neeley <micneeley14@gmail.com>
2019-03-20 12:49:07 -0400

    es6 stuff

--- packages/apollos-ui-kit/docs/generateMarkdown.js
+++ packages/apollos-ui-kit/docs/generateMarkdown.js
@@ -16,48 +16,37 @@ function stringOfLength(string, length) {
 }
 
 function generateTitle(name) {
-  const title = '`' + name + '` (component)';
-  return title + '\n' + stringOfLength('=', title.length) + '\n';
+  const title = `\`${name}\` (component)`;
+  return `${title}\n${stringOfLength('=', title.length)}\n`;
 }
 
 function generateDesciption(description) {
-  return description + '\n';
+  return `${description}\n`;
 }
 
 function generatePropType(type) {
   let values;
   if (Array.isArray(type.value)) {
-    values =
-      '(' +
-      type.value
-        .map(function(typeValue) {
-          return typeValue.name || typeValue.value;
-        })
-        .join('|') +
-      ')';
+    values = `(${type.value
+      .map((typeValue) => typeValue.name || typeValue.value)
+      .join('|')})`;
   } else {
     values = type.value;
   }
 
-  return 'type: `' + type.name + (values ? values : '') + '`\n';
+  return `type: \`${type.name}${values || ''}\`\n`;
 }
 
 function generatePropDefaultValue(value) {
-  return 'defaultValue: `' + value.value + '`\n';
+  return `defaultValue: \`${value.value}\`\n`;
 }
 
 function generateProp(propName, prop) {
   return (
-    '### `' +
-    propName +
-    '`' +
-    (prop.required ? ' (required)' : '') +
-    '\n' +
-    '\n' +
-    (prop.description ? prop.description + '\n\n' : '') +
-    (prop.type ? generatePropType(prop.type) : '') +
-    (prop.defaultValue ? generatePropDefaultValue(prop.defaultValue) : '') +
-    '\n'
+    `### \`${propName}\`${prop.required ? ' (required)' : ''}\n` +
+    `\n${prop.description ? `${prop.description}\n\n` : ''}${
+      prop.type ? generatePropType(prop.type) : ''
+    }${prop.defaultValue ? generatePropDefaultValue(prop.defaultValue) : ''}\n`
   );
 }
 
@@ -65,27 +54,18 @@ function generateProps(props) {
   const title = 'Props';
 
   return (
-    title +
-    '\n' +
-    stringOfLength('-', title.length) +
-    '\n' +
-    '\n' +
-    Object.keys(props)
+    `${title}\n${stringOfLength('-', title.length)}\n` +
+    `\n${Object.keys(props)
       .sort()
-      .map(function(propName) {
-        return generateProp(propName, props[propName]);
-      })
-      .join('\n')
+      .map((propName) => generateProp(propName, props[propName]))
+      .join('\n')}`
   );
 }
 
 function generateMarkdown(name, reactAPI) {
-  const markdownString =
-    generateTitle(name) +
-    '\n' +
-    generateDesciption(reactAPI.description) +
-    '\n' +
-    generateProps(reactAPI.props);
+  const markdownString = `${generateTitle(name)}\n${generateDesciption(
+    reactAPI.description
+  )}\n${generateProps(reactAPI.props)}`;
 
   return markdownString;
 }
