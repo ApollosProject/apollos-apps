2d710d9153f8b1d0ff57c85d193cfc246cd41ea3
Michael Neeley <micneeley14@gmail.com>
2019-03-27 12:28:30 -0400

    fixed time format

--- packages/apollos-data-connector-rock/src/content-items/data-source.js
+++ packages/apollos-data-connector-rock/src/content-items/data-source.js
@@ -159,16 +159,11 @@ export default class ContentItem extends RockApolloDataSource {
 
   LIVE_CONTENT = () => {
     // get a date in the local timezone of the rock instance.
-    // Rock doesn't respect timezone information, hence the need to warp the date
+    // should output something like 2019-03-27T12:27:20-04:00 which means 12:27 in New York
     const date = moment()
-      .utc()
-      .add(
-        moment()
-          .tz(ROCK.TIMEZONE)
-          .utcOffset(),
-        'minutes'
-      );
-    return `((StartDateTime lt datetime'${date.toISOString()}') or (StartDateTime eq null)) and ((ExpireDateTime gt datetime'${date.toISOString()}') or (ExpireDateTime eq null)) `;
+      .tz(ROCK.TIMEZONE)
+      .format();
+    return `((StartDateTime lt datetime'${date}') or (StartDateTime eq null)) and ((ExpireDateTime gt datetime'${date}') or (ExpireDateTime eq null)) `;
   };
 
   expanded = true;
