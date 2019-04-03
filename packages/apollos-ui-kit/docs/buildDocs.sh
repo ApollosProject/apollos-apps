4fbce6f048768fef84234a6ac3cc34e511c5b917
Michael Neeley <micneeley14@gmail.com>
2019-04-03 11:12:42 -0400

    added generated docs folder

--- packages/apollos-ui-kit/docs/buildDocs.sh
+++ packages/apollos-ui-kit/docs/buildDocs.sh
@@ -32,7 +32,7 @@ function buildDocs(api) {
   for (const filepath in api) {
     const name = getComponentName(filepath);
     const markdown = generateMarkdown(name, api[filepath]);
-    //fs.mkdirSync(path.dirname(`docs/generated`), { recursive: true });
+    fs.mkdirSync(path.dirname(`docs/generated`), { recursive: true });
     fs.writeFileSync(`docs/generated/${name}.md`, markdown);
     process.stdout.write(`${filepath} -> ${name}.md\n`);
   }
