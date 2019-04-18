eeb74e263091b6e31e5f41e5a5506839bd2e68ad
Michael Neeley <micneeley14@gmail.com>
2019-04-18 09:26:44 -0400

    put the android stuff back

--- packages/apolloschurchapp/android/app/src/main/java/com/apolloschurchapp/MainActivity.java
+++ packages/apolloschurchapp/android/app/src/main/java/com/apolloschurchapp/MainActivity.java
@@ -1,9 +1,9 @@
 package com.apolloschurchapp;
 import android.os.Bundle;
-import com.facebook.react.ReactFragmentActivity;
+import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen;
 
-public class MainActivity extends ReactFragmentActivity {
+public class MainActivity extends ReactActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         SplashScreen.show(this, R.style.SplashScreenTheme);
