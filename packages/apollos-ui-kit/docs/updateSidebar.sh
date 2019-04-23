a8fa57880e0eec7c6bc2e8b0a44d679c86c2896b
Michael Neeley <micneeley14@gmail.com>
2019-04-23 11:30:09 -0400

    removed console logs

--- packages/apollos-ui-kit/docs/updateSidebar.sh
+++ packages/apollos-ui-kit/docs/updateSidebar.sh
@@ -3,12 +3,9 @@
 const fs = require('fs');
 
 function updateSidebars(sidebars) {
-  console.log(sidebars);
   const docs = JSON.parse(fs.readFileSync(`docs/generated/docs.json`));
-  console.log(docs);
   const newSideBars = sidebars;
   newSideBars.docs['UI-Kit'] = docs['UI-Kit'];
-  console.log(newSideBars);
   fs.writeFileSync(
     `docs/generated/sidebars.json`,
     JSON.stringify(sidebars, null, 2)
