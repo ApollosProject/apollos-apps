dfd564a1b8e9cff6a454fad8a44264d0923ba3fb
Michael Neeley <micneeley14@gmail.com>
2019-04-26 21:32:58 -0400

    adds script to debug travis jobs

---
+++ scripts/debugTravis.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+  curl -s -X POST \
+   -H "Content-Type: application/json" \
+   -H "Accept: application/json" \
+   -H "Travis-API-Version: 3" \
+   -H "Authorization: token NgwCFMqRYxaqurXAeNu50Q" \
+   -d '{ "quiet": true }' \
+   https://api.travis-ci.com/job/$"{TRAVIS_JOB_ID}"/debug
