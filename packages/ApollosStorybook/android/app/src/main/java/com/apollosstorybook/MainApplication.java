5597c0234f2a2538707b6b8db39e5b477678c5d1
Conrad VanLandingham <hi@convan.me>
2019-05-06 13:06:53 -0500

    initial storybook implementation

---
+++ packages/ApollosStorybook/android/app/src/main/java/com/apollosstorybook/MainApplication.java
@@ -0,0 +1,71 @@
+package com.apollosstorybook;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.webview.RNCWebViewPackage;
+import com.brentvatne.react.ReactVideoPackage;
+import com.horcrux.svg.SvgPackage;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.swmansion.rnscreens.RNScreensPackage;
+import com.mybdesign.RNPassKit.RNPassKitPackage;
+import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
+import com.tanguyantoine.react.MusicControl;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.imagepicker.ImagePickerPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new RNCWebViewPackage(),
+            new ReactVideoPackage(),
+            new SvgPackage(),
+            new SplashScreenReactPackage(),
+            new RNScreensPackage(),
+            new RNPassKitPackage(),
+            new ReactNativeOneSignalPackage(),
+            new MusicControl(),
+            new MapsPackage(),
+            new LinearGradientPackage(),
+            new ImagePickerPackage(),
+            new RNDeviceInfo(),
+            new ReactNativeConfigPackage()
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
