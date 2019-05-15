d5071139700f91b65f5aff1d7ec1e1edb5820d69
Vincent Wilson <vince@classyh.at>
2019-05-15 08:16:32 -0500

    Make church online URL configurable (#717)

--- packages/apollos-data-connector-church-online/src/data-source.js
+++ packages/apollos-data-connector-church-online/src/data-source.js
@@ -1,9 +1,13 @@
+/* eslint-disable class-methods-use-this */
 import { RESTDataSource } from 'apollo-datasource-rest';
+import ApollosConfig from '@apollosproject/config';
 
 export default class LiveStream extends RESTDataSource {
   resource = 'LiveStream';
 
-  baseURL = 'https://apollos.churchonline.org/api/v1/';
+  get baseURL() {
+    return ApollosConfig.CHURCH_ONLINE.URL;
+  }
 
   async getLiveStream() {
     return this.get('events/current');
