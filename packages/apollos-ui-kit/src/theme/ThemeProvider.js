0893fec2d3f073a3e72d7872f4cd21550de5467c
Burke Shartsis <burke@differential.com>
2019-05-21 14:35:18 -0400

    add iconInput prop to ThemeProvider.

--- packages/apollos-ui-kit/src/theme/ThemeProvider.js
+++ packages/apollos-ui-kit/src/theme/ThemeProvider.js
@@ -7,21 +7,25 @@ export default class ThemeProvider extends PureComponent {
   static propTypes = {
     children: PropTypes.node,
     themeInput: PropTypes.shape(THEME_PROPS),
+    iconInput: PropTypes.objectOf(PropTypes.element),
   };
 
   static childContextTypes = {
     theme: PropTypes.shape(THEME_PROPS),
     themeInput: PropTypes.shape(THEME_PROPS),
+    iconInput: PropTypes.objectOf(PropTypes.element),
   };
 
   static defaultProps = {
     children: null,
     themeInput: {},
+    iconInput: { Boom: 'What' },
   };
 
   getChildContext = () => ({
     theme: createTheme(this.props.themeInput),
     themeInput: this.props.themeInput,
+    iconInput: this.props.iconInput,
   });
 
   render() {
