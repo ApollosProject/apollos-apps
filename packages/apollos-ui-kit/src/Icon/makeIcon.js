786f99825a85fd89029b52b5b0e0d018d952fc02
Burke Shartsis <burke@differential.com>
2019-05-28 11:43:49 -0400

    fix withTheme import from makeIcon

--- packages/apollos-ui-kit/src/Icon/makeIcon.js
+++ packages/apollos-ui-kit/src/Icon/makeIcon.js
@@ -1,4 +1,4 @@
-import { withTheme } from '..';
+import { withTheme } from '../theme';
 
 // Currently used to inject default fill color to icons.
 // Eventually can be used for other shared functionality between icons
