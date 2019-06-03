11bb9a019a2b7da407b075b2822e1c316764f16c
Conrad VanLandingham <hi@convan.me>
2019-06-03 08:47:56 -0500

    add fetchMoreResolver and working pagination (#756)
    * add fetchMoreResolver and working pagination
    
    * update snaps
    
    * fix linter
    
    * Update pageInfo when fetching more
    
    * Refactor fetchMoreResolver for easier func signature and clarity
    
    * Undo snapshot changes

--- packages/apolloschurchapp/src/tabs/connect/getLikedContent.js
+++ packages/apolloschurchapp/src/tabs/connect/getLikedContent.js
@@ -4,8 +4,11 @@ import { largeCardFragment } from 'apolloschurchapp/src/ui/ContentCardConnected'
 import { contentItemFragment } from 'apolloschurchapp/src/content-single/getContentItem';
 
 export default gql`
-  query getAllLikedContent($first: Int) {
-    likedContent(first: $first) {
+  query getAllLikedContent($first: Int, $after: String) {
+    likedContent(first: $first, after: $after) {
+      pageInfo {
+        endCursor
+      }
       edges {
         node {
           ... on ContentItem {
