5e357f0cc7c5e1ee11eec0bb787a7151bac90902
Burke Shartsis <burke@differential.com>
2019-06-12 12:03:24 -0400

    add test over Component prop on FeaturesConnected

--- packages/apollos-ui-onboarding/src/slides/Features/FeaturesConnected.tests.js
+++ packages/apollos-ui-onboarding/src/slides/Features/FeaturesConnected.tests.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { Text } from 'react-native';
 import wait from 'waait';
 
-import { Providers } from '../../testUtils';
+import { renderWithApolloData, Providers } from '../../testUtils';
 
 import getUserFirstName from './getUserFirstName';
 import FeaturesConnected from './FeaturesConnected';
@@ -29,6 +30,38 @@ describe('The Onboarding FeaturesConnected component', () => {
       </Providers>
     );
     await wait(0); // wait for response from graphql
+    expect(tree).toMatchSnapshot();
+  });
+  it('should render a custom Component', async () => {
+    const mock = {
+      request: {
+        query: getUserFirstName,
+      },
+      result: {
+        data: {
+          currentUser: {
+            __typename: 'AuthenticatedUser',
+            id: 'AuthenticatedUser:123',
+            profile: {
+              __typename: 'Person',
+              id: 'Person:123',
+              firstName: 'Marty',
+            },
+          },
+        },
+      },
+    };
+
+    // eslint-disable-next-line react/prop-types
+    const CustomComponent = ({ firstName }) => (
+      <Text>{`${firstName} also thinks Skyline chili is the best.`}</Text>
+    );
+
+    const tree = await renderWithApolloData(
+      <Providers mocks={[mock]}>
+        <FeaturesConnected Component={CustomComponent} />
+      </Providers>
+    );
     expect(tree).toMatchSnapshot();
   });
 });
