dc5161cd6f58aa1217afaa89da6f462e1dc5bb74
Vincent Wilson <vince@classyh.at>
2019-06-27 09:50:25 -0400

    ActionList algorithm driven by a content channel

--- packages/apollos-data-connector-rock/src/features/resolver.js
+++ packages/apollos-data-connector-rock/src/features/resolver.js
@@ -11,12 +11,19 @@ export default {
     userFeedFeatures: async (root, args, context) => {
       const { Features } = context.dataSources;
       // Generate a feature to show the user on the home screen.
-      const feature = await Features.createActionListFeature({
+      const personaFeature = await Features.createActionListFeature({
         algorithms: ['PERSONA_FEED'],
         title: 'FOR YOU',
         subtitle: 'Explore what God calls you to today',
       });
-      return [feature];
+      const contentChannelFeature = await Features.createActionListFeature({
+        algorithms: [
+          { type: 'CONTENT_CHANNEL', arguments: { contentChannelId: 13 } },
+        ],
+        title: 'BULLETIN',
+        subtitle: "What's happening at apollos?",
+      });
+      return [personaFeature, contentChannelFeature];
     },
   },
 };
