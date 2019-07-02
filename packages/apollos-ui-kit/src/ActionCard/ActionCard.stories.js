76add398b3d28e036daae6565bf3ee42627b4ee8
Burke Shartsis <burke@differential.com>
2019-07-02 13:37:37 -0400

    add CenterView to story decorator. Change story to be more consistant with other stories

--- packages/apollos-ui-kit/src/ActionCard/ActionCard.stories.js
+++ packages/apollos-ui-kit/src/ActionCard/ActionCard.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@apollosproject/ui-storybook';
 
+import CenteredView from '../CenteredView';
 import BackgroundView from '../BackgroundView';
 
 import Chip from '../Chip';
@@ -8,8 +9,13 @@ import { BodyText } from '../typography';
 import ActionCard from '.';
 
 storiesOf('ui-kit/ActionCard', module)
-  .addDecorator((story) => <BackgroundView>{story()}</BackgroundView>)
-  .add('Examples', () => (
+  .addDecorator((story) => (
+    <BackgroundView>
+      {/* eslint-disable-next-line react-native/no-inline-styles */}
+      <CenteredView style={{ alignItems: 'stretch' }}>{story()}</CenteredView>
+    </BackgroundView>
+  ))
+  .add('example', () => (
     <ActionCard
       label="Key Idea"
       icon="text"
