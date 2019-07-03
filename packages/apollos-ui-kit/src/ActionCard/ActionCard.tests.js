c927d14c179a76f151c154b6787cf1511d6e2e33
Burke Shartsis <burke@differential.com>
2019-07-03 15:49:56 -0400

    add complete test coverage over ActionCard api

--- packages/apollos-ui-kit/src/ActionCard/ActionCard.tests.js
+++ packages/apollos-ui-kit/src/ActionCard/ActionCard.tests.js
@@ -8,14 +8,53 @@ import { BodyText } from '../typography';
 import ActionCard from '.';
 
 describe('the ActionCard component', () => {
-  it('should render', () => {
+  it('should render children', () => {
+    const tree = renderer.create(
+      <Providers>
+        <ActionCard>
+          <BodyText>
+            “Mauris non tempor quam, et lacinia sapien. Mauris accumsan eros
+            eget libero posuere vulputate. Etiam elit elitbibendum.”
+          </BodyText>
+        </ActionCard>
+      </Providers>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  it('should render an icon and label', () => {
+    const tree = renderer.create(
+      <Providers>
+        <ActionCard icon={'text'} label={'Key Idea'}>
+          <BodyText>
+            “Mauris non tempor quam, et lacinia sapien. Mauris accumsan eros
+            eget libero posuere vulputate. Etiam elit elitbibendum.”
+          </BodyText>
+        </ActionCard>
+      </Providers>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  it('should render an action', () => {
     const tree = renderer.create(
       <Providers>
         <ActionCard
-          label="Key Idea"
-          icon="text"
-          action={<Chip icon="share" title="Share" />}
+          icon={'text'}
+          label={'Key Idea'}
+          action={<Chip icon={'share'} title={'Share'} />}
         >
+          <BodyText>
+            “Mauris non tempor quam, et lacinia sapien. Mauris accumsan eros
+            eget libero posuere vulputate. Etiam elit elitbibendum.”
+          </BodyText>
+        </ActionCard>
+      </Providers>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  it('should accept additional Card props', () => {
+    const tree = renderer.create(
+      <Providers>
+        <ActionCard cardColor={'salmon'}>
           <BodyText>
             “Mauris non tempor quam, et lacinia sapien. Mauris accumsan eros
             eget libero posuere vulputate. Etiam elit elitbibendum.”
