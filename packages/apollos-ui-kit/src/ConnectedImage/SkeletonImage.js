317bf49fd444926eafd8e624a2fd80210b8d9100
Michael Neeley <micneeley14@gmail.com>
2019-07-09 16:35:47 -0400

    updated

--- packages/apollos-ui-kit/src/ConnectedImage/SkeletonImage.js
+++ packages/apollos-ui-kit/src/ConnectedImage/SkeletonImage.js
@@ -4,6 +4,7 @@ import Placeholder from 'rn-placeholder';
 import styled from '../styled';
 
 const SkeletonImage = styled(({ theme, forceRatio }) => ({
+  width: '100%',
   aspectRatio: forceRatio || 1,
   backgroundColor: theme.colors.background.inactive,
 }))(View);
