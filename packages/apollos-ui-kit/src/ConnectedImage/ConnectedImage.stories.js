c7083941a8efd8158e6e8473195902e2774afcc9
Burke Shartsis <burke@differential.com>
2019-07-10 17:36:48 -0400

    add stories for ConnectedImage

---
+++ packages/apollos-ui-kit/src/ConnectedImage/ConnectedImage.stories.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { storiesOf } from '@apollosproject/ui-storybook';
+
+import ConnectedImage from '.';
+
+storiesOf('ui-kit/ConnectedImage', module)
+  .add('default', () => (
+    <ConnectedImage
+      style={{ width: 300, height: 200 }} // eslint-disable-line react-native/no-inline-styles
+      source={{ uri: 'https://picsum.photos/600/400/?random' }}
+    />
+  ))
+  .add('maintainAspectRatio', () => (
+    <ConnectedImage
+      source={{ uri: 'https://picsum.photos/600/400/?random' }}
+      maintainAspectRatio
+    />
+  ));
