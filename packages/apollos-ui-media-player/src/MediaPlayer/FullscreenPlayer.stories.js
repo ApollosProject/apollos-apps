c6a7b4eeabdd7a3fad23df6d9bbb1ada8c05f161
Vincent Wilson <vince@classyh.at>
2019-07-12 11:48:19 -0400

    Adds "Open Livestream" Capability to the MediaControls (#857)
    * A content item will now report if it's live
    
    * Fix tests
    
    * Fix tests
    
    * Tests over new Sermon feature on ContentItem
    
    * Fix typo in church online datasource
    
    * Added support for media controls to open livestream
    
    * Rearrange ordering of variable assignment to lend clarity to importance
    
    * Add livestream test to storybook

--- packages/apollos-ui-media-player/src/MediaPlayer/FullscreenPlayer.stories.js
+++ packages/apollos-ui-media-player/src/MediaPlayer/FullscreenPlayer.stories.js
@@ -13,72 +13,124 @@ import MediaPlayerProvider from '../Provider';
 import { PLAY_VIDEO } from './mutations';
 import MediaPlayer from '.';
 
-storiesOf('ui-media-player/MediaPlayer', module).add('simple', () => (
-  <MediaPlayerProvider>
-    <BackgroundView>
-      <FlexedView>
-        <CenteredView>
-          <Mutation mutation={PLAY_VIDEO}>
-            {(play) => (
-              <ButtonLink
-                onPress={() =>
-                  play({
-                    variables: {
-                      mediaSource: {
-                        uri:
-                          'http://embed.wistia.com/deliveries/0e364f7e6f6604384ece8a35905a53a864386e9f.bin',
-                        __typename: 'VideoMediaSource',
-                      },
-                      posterSources: [
-                        {
+storiesOf('ui-media-player/MediaPlayer', module)
+  .add('Video', () => (
+    <MediaPlayerProvider>
+      <BackgroundView>
+        <FlexedView>
+          <CenteredView>
+            <Mutation mutation={PLAY_VIDEO}>
+              {(play) => (
+                <ButtonLink
+                  onPress={() =>
+                    play({
+                      variables: {
+                        mediaSource: {
                           uri:
-                            'https://apollosrock.newspring.cc/GetImage.ashx?guid=28ea9e81-09b4-411f-8ad5-741054837823',
-                          __typename: 'ImageMediaSource',
+                            'http://embed.wistia.com/deliveries/0e364f7e6f6604384ece8a35905a53a864386e9f.bin',
+                          __typename: 'VideoMediaSource',
                         },
-                      ],
-                      title: 'Holy Spirit',
-                      artist: 'Fuse Promos',
-                      isVideo: true,
-                    },
-                  })
-                }
-              >
-                Play video
-              </ButtonLink>
-            )}
-          </Mutation>
-          <Mutation mutation={PLAY_VIDEO}>
-            {(play) => (
-              <ButtonLink
-                onPress={() =>
-                  play({
-                    variables: {
-                      mediaSource: {
-                        uri:
-                          'https://s3.amazonaws.com/ns.downloads/newspring/audio/newspring_sermons/2017/180513_HolySpirit_06-Message-AUDIO.mp3',
-                        __typename: 'AudioMediaSource',
+                        posterSources: [
+                          {
+                            uri:
+                              'https://apollosrock.newspring.cc/GetImage.ashx?guid=28ea9e81-09b4-411f-8ad5-741054837823',
+                            __typename: 'ImageMediaSource',
+                          },
+                        ],
+                        title: 'Holy Spirit',
+                        artist: 'Fuse Promos',
+                        isVideo: true,
                       },
-                      posterSources: [
-                        {
+                    })
+                  }
+                >
+                  Play video
+                </ButtonLink>
+              )}
+            </Mutation>
+          </CenteredView>
+        </FlexedView>
+        <MediaPlayer />
+      </BackgroundView>
+    </MediaPlayerProvider>
+  ))
+  .add('Audio', () => (
+    <MediaPlayerProvider>
+      <BackgroundView>
+        <FlexedView>
+          <CenteredView>
+            <Mutation mutation={PLAY_VIDEO}>
+              {(play) => (
+                <ButtonLink
+                  onPress={() =>
+                    play({
+                      variables: {
+                        mediaSource: {
                           uri:
-                            'https://apollosrock.newspring.cc/GetImage.ashx?guid=28ea9e81-09b4-411f-8ad5-741054837823',
-                          __typename: 'ImageMediaSource',
+                            'https://s3.amazonaws.com/ns.downloads/newspring/audio/newspring_sermons/2017/180513_HolySpirit_06-Message-AUDIO.mp3',
+                          __typename: 'AudioMediaSource',
                         },
-                      ],
-                      title: 'The Greatest Evangelist',
-                      artist: 'Sermon',
-                      isVideo: false,
-                    },
-                  })
-                }
-              >
-                Play audio
-              </ButtonLink>
-            )}
-          </Mutation>
-        </CenteredView>
-      </FlexedView>
-      <MediaPlayer />
-    </BackgroundView>
-  </MediaPlayerProvider>
-));
+                        posterSources: [
+                          {
+                            uri:
+                              'https://apollosrock.newspring.cc/GetImage.ashx?guid=28ea9e81-09b4-411f-8ad5-741054837823',
+                            __typename: 'ImageMediaSource',
+                          },
+                        ],
+                        title: 'The Greatest Evangelist',
+                        artist: 'Sermon',
+                        isVideo: false,
+                      },
+                    })
+                  }
+                >
+                  Play audio
+                </ButtonLink>
+              )}
+            </Mutation>
+          </CenteredView>
+        </FlexedView>
+        <MediaPlayer />
+      </BackgroundView>
+    </MediaPlayerProvider>
+  ))
+  .add('Livestream', () => (
+    <MediaPlayerProvider>
+      <BackgroundView>
+        <FlexedView>
+          <CenteredView>
+            <Mutation mutation={PLAY_VIDEO}>
+              {(play) => (
+                <ButtonLink
+                  onPress={() =>
+                    play({
+                      variables: {
+                        mediaSource: {
+                          uri:
+                            'https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8',
+                          __typename: 'VideoMediaSource',
+                        },
+                        posterSources: [
+                          {
+                            uri:
+                              'https://apollosrock.newspring.cc/GetImage.ashx?guid=28ea9e81-09b4-411f-8ad5-741054837823',
+                            __typename: 'ImageMediaSource',
+                          },
+                        ],
+                        title: 'Live Stream',
+                        artist: 'Blender Foundation',
+                        isVideo: true,
+                      },
+                    })
+                  }
+                >
+                  Play livestream
+                </ButtonLink>
+              )}
+            </Mutation>
+          </CenteredView>
+        </FlexedView>
+        <MediaPlayer />
+      </BackgroundView>
+    </MediaPlayerProvider>
+  ));
--- packages/apolloschurchapp/src/content-single/MediaControls/index.js
+++ packages/apolloschurchapp/src/content-single/MediaControls/index.js
@@ -6,6 +6,7 @@ import { get } from 'lodash';
 
 import { PLAY_VIDEO } from '@apollosproject/ui-media-player';
 import { Icon, styled, Button } from '@apollosproject/ui-kit';
+import { WebBrowserConsumer } from 'apolloschurchapp/src/ui/WebBrowser';
 import GET_CONTENT_MEDIA from './getContentMedia';
 
 const buttonSizeDifferential = 4;
@@ -45,47 +46,109 @@ class MediaControls extends PureComponent {
     contentId: PropTypes.string,
   };
 
+  renderMedia = ({
+    videoSource,
+    coverImageSources,
+    title,
+    parentChannelName,
+  }) => (
+    <Mutation mutation={PLAY_VIDEO}>
+      {(play) => (
+        <Container>
+          <MediaButtonBorder>
+            <MediaButton
+              type="primary"
+              onPress={() =>
+                play({
+                  variables: {
+                    mediaSource: videoSource,
+                    posterSources: coverImageSources,
+                    title,
+                    isVideo: true,
+                    artist: parentChannelName,
+                  },
+                })
+              }
+              useForeground
+            >
+              <MediaIcon name="play" />
+            </MediaButton>
+          </MediaButtonBorder>
+        </Container>
+      )}
+    </Mutation>
+  );
+
+  renderWebView = ({ webViewUrl }) => (
+    <WebBrowserConsumer>
+      {(openUrl) => (
+        <Container>
+          <MediaButtonBorder>
+            <MediaButton
+              type="primary"
+              onPress={() => openUrl(webViewUrl)}
+              useForeground
+            >
+              <MediaIcon name="play" />
+            </MediaButton>
+          </MediaButtonBorder>
+        </Container>
+      )}
+    </WebBrowserConsumer>
+  );
+
   renderControls = ({
     loading,
     error,
     data: {
-      node: { videos, title, parentChannel = {}, coverImage = {} } = {},
+      node: {
+        videos,
+        title,
+        parentChannel = {},
+        coverImage = {},
+        liveStream = {},
+      } = {},
     } = {},
   }) => {
     if (loading || error) return null;
 
+    const isLive = get(liveStream, 'isLive', false);
+    const hasLiveStreamContent = !!(
+      get(liveStream, 'webViewUrl') || get(liveStream, 'media.sources[0]')
+    );
+
     const videoSource = get(videos, '[0].sources[0]', null);
+    const shouldRender = (isLive && hasLiveStreamContent) || videoSource;
+
+    if (!shouldRender) return null;
+
     const coverImageSources = (coverImage && coverImage.sources) || [];
 
-    return (
-      <Mutation mutation={PLAY_VIDEO}>
-        {(play) => (
-          <Container>
-            {videoSource ? (
-              <MediaButtonBorder>
-                <MediaButton
-                  type="primary"
-                  onPress={() =>
-                    play({
-                      variables: {
-                        mediaSource: videoSource,
-                        posterSources: coverImageSources,
-                        title,
-                        isVideo: true,
-                        artist: parentChannel.name,
-                      },
-                    })
-                  }
-                  useForeground
-                >
-                  <MediaIcon name="play" />
-                </MediaButton>
-              </MediaButtonBorder>
-            ) : null}
-          </Container>
-        )}
-      </Mutation>
-    );
+    // Content is live, and we have a livestream media
+    if (isLive && get(liveStream, 'media.sources[0].uri')) {
+      return this.renderMedia({
+        coverImageSources,
+        videoSource: liveStream.media.sources[0],
+        parentChannelName: parentChannel.name,
+        title,
+      });
+    }
+
+    // Content is live, and we don't have a livestream media
+    // but we do have a webview url
+    if (isLive && get(liveStream, 'webViewUrl')) {
+      return this.renderWebView({
+        webViewUrl: liveStream.webViewUrl,
+      });
+    }
+
+    // Default case, normal media.
+    return this.renderMedia({
+      coverImageSources,
+      videoSource,
+      parentChannelName: parentChannel.name,
+      title,
+    });
   };
 
   render() {
