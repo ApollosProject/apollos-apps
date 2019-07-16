15566fe407e3debd3d4886f1a115bfa1c9a5f312
Burke Shartsis <burke@differential.com>
2019-07-16 17:38:08 -0400

    add CardLabel component

---
+++ packages/apollos-ui-kit/src/Card/Label.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { get } from 'lodash';
+import PropTypes from 'prop-types';
+
+import styled from '../styled';
+import { ThemeMixin } from '../theme';
+import Chip from '../Chip';
+import Icon from '../Icon';
+
+const StyledChip = styled(
+  ({ theme, type }) => ({
+    marginBottom: theme.sizing.baseUnit,
+    // the overlay type is transparent and borders look funny. TODO: consider moving this change to `Chip` or `Button`
+    ...(type === 'overlay' ? { borderWidth: 0 } : {}),
+  }),
+  'ui-kit.CardLabel'
+)(Chip);
+
+// eslint-disable-next-line react/prop-types
+const Label = ({ title, icon, type, theme, ...props }) => (
+  <ThemeMixin
+    mixin={{
+      type: get(theme, 'type', 'light').toLowerCase(), // not sure why we need toLowerCase
+      colors: get(theme, 'colors', {}),
+    }}
+  >
+    <StyledChip title={title} icon={icon} type={type} {...props} />
+  </ThemeMixin>
+);
+
+Label.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: Icon.propTypes.name,
+  theme: PropTypes.shape({
+    type: PropTypes.string,
+    colors: PropTypes.shape({}),
+  }),
+  type: PropTypes.string,
+  ...Chip.propTypes,
+};
+
+Label.defaultProps = {
+  type: 'overlay',
+};
+
+Label.displayName = 'CardLabel';
+
+export default Label;
