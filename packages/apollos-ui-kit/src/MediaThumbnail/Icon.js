33db3a31137e34c13a7f7fcf1ea2a2f59d24e9cc
Burke Shartsis <burke@differential.com>
2019-07-23 17:05:27 -0400

    revert unwanted changes from cherrypick

--- packages/apollos-ui-kit/src/MediaThumbnail/Icon.js
+++
@@ -1,8 +0,0 @@
-import { withTheme } from '../theme';
-import Icon from '../Icon';
-
-const MediaThumbnailIcon = withTheme(({ theme }) => ({
-  size: theme.sizing.baseUnit,
-}))(Icon);
-
-export default MediaThumbnailIcon;
