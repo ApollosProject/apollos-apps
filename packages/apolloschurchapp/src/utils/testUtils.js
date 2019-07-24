73e5c46533559dccf1c34bf37e032978ddb4d760
Michael Neeley <micneeley14@gmail.com>
2019-07-24 14:58:05 -0400

    Updates the Test with Apollo Helper

--- packages/apolloschurchapp/src/utils/testUtils.js
+++ packages/apolloschurchapp/src/utils/testUtils.js
@@ -1,10 +1,34 @@
 /* eslint-disable import/prefer-default-export */
+import React from 'react';
 import renderer from 'react-test-renderer';
+import { MockedProvider } from 'react-apollo';
 import wait from 'waait';
 
-export const renderWithApolloData = async (component) => {
-  const tree = renderer.create(component);
-  await wait(1);
-  tree.update(component);
+export const renderWithApolloData = async (component, mocks = []) => {
+  /*
+   * mocks should be in the form:
+   *
+   *   mocks = [
+   *     {
+   *       request: {
+   *         query: GET_DOG_QUERY,
+   *         variables: {
+   *           name: 'Buck',
+   *         },
+   *       },
+   *       result: {
+   *         data: {
+   *           dog: { id: '1', name: 'Buck', breed: 'bulldog' },
+   *         },
+   *       },
+   *     },
+   *   ];
+   *
+   */
+
+  const tree = renderer.create(
+    <MockedProvider mocks={mocks}>{component}</MockedProvider>
+  );
+  await wait(0);
   return tree;
 };
