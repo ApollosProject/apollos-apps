f7f247ff178a6c155b4bba418d206fa1e2b2f69d
Michael Neeley <micneeley14@gmail.com>
2019-07-24 14:13:22 -0400

    con-ratified it

--- packages/apolloschurchapp/src/utils/content/__tests__/share.tests.js
+++ packages/apolloschurchapp/src/utils/content/__tests__/share.tests.js
@@ -6,30 +6,24 @@ jest.mock('Share');
 
 describe('the share function', () => {
   beforeAll(() => {
-    // we don't need to test the functionality of Share.share(),
-    // only that our args are being sent properly
-    Share.share = jest.fn((args) => {
-      throw new Error(JSON.stringify(args));
-    });
+    Share.share = jest.fn();
   });
   test('default arguments', () => {
     Platform.OS = 'ios';
-    expect(() => share({ title: 'title', url: 'http://url.com' })).toThrow(
-      JSON.stringify({
-        title: 'title',
-        message: 'title',
-        url: 'http://url.com',
-      })
-    );
+    share({ title: 'title', url: 'http://url.com' });
+    expect(Share.share).toBeCalledWith({
+      title: 'title',
+      message: 'title',
+      url: 'http://url.com',
+    });
   });
   test('android arguments', () => {
     Platform.OS = 'android';
-    expect(() => share({ title: 'title', url: 'http://url.com' })).toThrow(
-      JSON.stringify({
-        title: 'title',
-        message: 'title\nhttp://url.com',
-        url: 'http://url.com',
-      })
-    );
+    share({ title: 'title', url: 'http://url.com' });
+    expect(Share.share).toBeCalledWith({
+      title: 'title',
+      message: 'title\nhttp://url.com',
+      url: 'http://url.com',
+    });
   });
 });
