185e8e394937e1f17ee786abd58d6fc20bab4229
Conrad VanLandingham <hi@convan.me>
2019-07-24 14:36:12 -0500

    fix tests + lints

--- packages/apollos-ui-kit/src/OverlayBackgroundImage/OverlayBackgroundImage.stories.js
+++ packages/apollos-ui-kit/src/OverlayBackgroundImage/OverlayBackgroundImage.stories.js
@@ -16,7 +16,7 @@ storiesOf('OverlayBackgroundImage', module)
   .add('custom size', () => (
     <OverlayBackgroundImage
       overlayColor={null}
-      style={{ width: '100%', height: '75%', aspectRatio: undefined }}
+      style={{ width: '100%', height: '75%', aspectRatio: undefined }} // eslint-disable-line react-native/no-inline-styles
       source={{ uri: 'https://picsum.photos/600/600' }}
     />
   ));
