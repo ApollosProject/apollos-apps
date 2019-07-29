30428624aec79f7289b9c13f30cabcc45a0c182b
Michael Neeley <micneeley14@gmail.com>
2019-07-29 14:37:25 -0400

    added some comments for future suggestions

--- packages/apollos-ui-auth/src/buildErrorLink.js
+++ packages/apollos-ui-auth/src/buildErrorLink.js
@@ -8,13 +8,29 @@ export default (restore, navigate) =>
           console.warn(
             `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
           );
-          // wipe out all data and go to beginning of app
+          // wipe out all data and go somewhere
           if (code === 'UNAUTHENTICATED') {
             restore();
             navigate();
+            // TODO: re-auth
+            // modify the operation context with a new token
+            // const oldHeaders = operation.getContext().headers;
+            // operation.setContext({
+            // headers: {
+            // ...oldHeaders,
+            // authorization: getNewToken(),
+            // },
+            // });
+            // // retry the request, returning the new observable
+            // return forward(operation);
           }
           return null;
         }
       );
+
     if (networkError) console.warn(`[Network error]: ${networkError}`);
+    // TODO: retry
+    // if you would also like to retry automatically on
+    // network errors, we recommend that you use
+    // apollo-link-retry
   });
