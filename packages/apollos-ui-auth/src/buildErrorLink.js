cb5fac9fe6150027a46598756aa4e81a61a2413b
Vincent Wilson <vince@classyh.at>
2019-07-30 11:58:29 -0400

    Remove non-relevant comments

--- packages/apollos-ui-auth/src/buildErrorLink.js
+++ packages/apollos-ui-auth/src/buildErrorLink.js
@@ -1,32 +1,16 @@
 import { onError } from 'apollo-link-error';
 
-export default (restore, navigate) =>
+export default (onAuthError) =>
   onError(({ graphQLErrors, networkError }) => {
     if (graphQLErrors)
       graphQLErrors.map(({ extensions: { code } }) => {
         // wipe out all data and go somewhere
         if (code === 'UNAUTHENTICATED') {
-          restore();
-          navigate();
-          // TODO: re-auth
-          // modify the operation context with a new token
-          // const oldHeaders = operation.getContext().headers;
-          // operation.setContext({
-          // headers: {
-          // ...oldHeaders,
-          // authorization: getNewToken(),
-          // },
-          // });
-          // // retry the request, returning the new observable
-          // return forward(operation);
+          onAuthError();
         }
         return null;
       });
 
     if (networkError) return null;
-    // TODO: retry
-    // if you would also like to retry automatically on
-    // network errors, we recommend that you use
-    // apollo-link-retry
     return null;
   });
