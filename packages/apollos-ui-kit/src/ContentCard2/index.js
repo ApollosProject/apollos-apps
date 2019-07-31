38596b86befa0557990242e155a76d56a7bade6f
Burke Shartsis <burke@differential.com>
2019-07-31 10:19:34 -0400

    WIP

--- packages/apollos-ui-kit/src/ContentCard2/index.js
+++ packages/apollos-ui-kit/src/ContentCard2/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { View } from 'react-native';
-import { get } from 'lodash';
 import PropTypes from 'prop-types';
 
-import { withTheme, ThemeMixin } from '../theme';
+import { withTheme } from '../theme';
 import styled from '../styled';
 import Card, { CardImage, CardLabel, CardContent } from '../Card';
-import FlexedView from '../FlexedView';
 import { H3, BodyText } from '../typography';
 import Icon from '../Icon';
 import { withIsLoading } from '../isLoading';
@@ -28,15 +26,17 @@ const LikeIcon = withTheme(({ theme, isLiked }) => ({
 const Image = withTheme(({ theme }) => ({
   minAspectRatio: 1.2,
   maxAspectRatio: 1.5,
+  overlayType: 'gradient-top',
+  overlayColor: theme.colors.black,
 }))(CardImage);
 
-const Label = styled(({ theme, hasSummary }) => ({
+const withLabelStyles = styled(({ theme, hasSummary }) => ({
   ...(hasSummary ? { marginBottom: theme.sizing.baseUnit } : {}),
-}))(CardLabel);
+  // marginTop: -theme.sizing.baseUnit * 2,
+}));
 
 const Content = styled(({ theme }) => ({
   alignItems: 'flex-start', // needed to make `Label` display as an "inline" element
-  marginTop: -theme.sizing.baseUnit * 2,
   paddingHorizontal: theme.sizing.baseUnit * 1.5, // TODO: refactor CardContent to have this be the default
   paddingBottom: theme.sizing.baseUnit * 2, // TODO: refactor CardContent to have this be the default
 }))(CardContent);
@@ -45,7 +45,21 @@ const Summary = styled(({ theme }) => ({
   paddingTop: theme.sizing.baseUnit,
 }))(BodyText);
 
-const FeaturedCard = withIsLoading(
+const renderLabel = (LabelComponent, labelText, summary) => {
+  let ComponentToRender = null;
+
+  if (LabelComponent) {
+    ComponentToRender = LabelComponent;
+  } else if (labelText) {
+    ComponentToRender = (
+      <CardLabel hasSummary={summary} title={labelText} type={'secondary'} />
+    );
+  }
+
+  return ComponentToRender;
+};
+
+const ContentCard2 = withIsLoading(
   ({
     coverImage,
     title,
@@ -56,12 +70,10 @@ const FeaturedCard = withIsLoading(
     summary,
   }) => (
     <Card isLoading={isLoading}>
-      <Image source={coverImage} overlayType={'gradient-top'} />
+      <Image source={coverImage} />
 
       <Content>
-        {LabelComponent || (
-          <Label title={labelText} type={'secondary'} hasSummary={labelText} />
-        )}
+        {renderLabel(LabelComponent, labelText, summary)}
         {title ? <H3 numberOfLines={2}>{title}</H3> : null}
         {summary ? <Summary numberOfLines={2}>{summary}</Summary> : null}
       </Content>
@@ -72,7 +84,7 @@ const FeaturedCard = withIsLoading(
   )
 );
 
-FeaturedCard.propTypes = {
+ContentCard2.propTypes = {
   coverImage: PropTypes.oneOfType([
     PropTypes.arrayOf(ImageSourceType),
     ImageSourceType,
@@ -89,6 +101,6 @@ FeaturedCard.propTypes = {
   }),
 };
 
-FeaturedCard.displayName = 'FeaturedCard';
+ContentCard2.displayName = 'ContentCard2';
 
-export default FeaturedCard;
+export default ContentCard2;
