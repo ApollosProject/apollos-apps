d39fa2f574e7921efa7a88c068d680dc28a5409a
Burke Shartsis <burke@differential.com>
2019-08-01 14:52:25 -0400

    update ContentCardConnected
    - memoize it!
    - add Component prop
    - set default prop for Component prop to DefaultCard

--- packages/apolloschurchapp/src/ui/ContentCardConnected/index.js
+++ packages/apolloschurchapp/src/ui/ContentCardConnected/index.js
@@ -1,55 +1,57 @@
-import React from 'react';
+import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 import { Query } from 'react-apollo';
 import { get } from 'lodash';
 
-import { ContentCard, ErrorCard } from '@apollosproject/ui-kit';
+import { DefaultCard, ErrorCard } from '@apollosproject/ui-kit';
 import GET_CONTENT_CARD from './query';
 
 export { TILE_CARD_FRAGMENT, LARGE_CARD_FRAGMENT } from './query';
 
-const ContentCardConnected = ({
-  contentId,
-  isLoading,
-  tile,
-  ...otherProps
-}) => {
-  if (!contentId || isLoading)
-    return <ContentCard {...otherProps} isLoading tile={tile} />;
+const ContentCardConnected = memo(
+  ({ Component, contentId, isLoading, tile, ...otherProps }) => {
+    if (!contentId || isLoading)
+      return <Component {...otherProps} isLoading tile={tile} />;
 
-  return (
-    <Query query={GET_CONTENT_CARD} variables={{ contentId, tile: !!tile }}>
-      {({ data: { node = {} } = {}, loading, error }) => {
-        if (error) return <ErrorCard error={error} />;
+    return (
+      <Query query={GET_CONTENT_CARD} variables={{ contentId, tile: !!tile }}>
+        {({ data: { node = {} } = {}, loading, error }) => {
+          if (error) return <ErrorCard error={error} />;
 
-        const metrics = [
-          {
-            icon: node.isLiked ? 'like-solid' : 'like',
-            value: node.likedCount,
-          },
-        ];
+          const metrics = [
+            {
+              icon: node.isLiked ? 'like-solid' : 'like',
+              value: node.likedCount,
+            },
+          ];
 
-        const coverImage = get(node, 'coverImage.sources', undefined);
+          const coverImage = get(node, 'coverImage.sources', undefined);
 
-        return (
-          <ContentCard
-            {...node}
-            {...otherProps}
-            coverImage={coverImage}
-            metrics={metrics}
-            tile={tile}
-            isLoading={loading}
-          />
-        );
-      }}
-    </Query>
-  );
-};
+          return (
+            <Component
+              {...node}
+              {...otherProps}
+              coverImage={coverImage}
+              metrics={metrics}
+              tile={tile}
+              isLoading={loading}
+            />
+          );
+        }}
+      </Query>
+    );
+  }
+);
 
 ContentCardConnected.propTypes = {
-  isLoading: PropTypes.bool,
+  Component: PropTypes.element,
   contentId: PropTypes.string,
+  isLoading: PropTypes.bool,
   tile: PropTypes.bool,
+};
+
+ContentCardConnected.defaultProps = {
+  Component: DefaultCard,
 };
 
 export default ContentCardConnected;
