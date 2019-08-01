dbda2f5bdf32b9767a6262379665233ca8909ad0
Burke Shartsis <burke@differential.com>
2019-08-01 13:35:50 -0400

    Adds DefaultCard and deprecates ContentCard (#961)
    * alphabatize props 🤓
    
    * WIP
    
    * temp rename to something dumb
    
    * WIP
    
    * fix label positioning.
    
    * always render loading state for label even if we don't have one
    
    - we made this the default behavior because we think it's a better UX
    
    * fix aspect ratio and image overlay
    
    * fix all the tests and stories
    
    * rename `ContentCard2` to `DefaultCard`
    
    * more documentation
    
    * fix stories
    
    * add deprication warning to `ContentCard`
    
    * Update snaps

---
+++ packages/apollos-ui-kit/src/DefaultCard/DefaultCard.stories.js
@@ -0,0 +1,157 @@
+import React from 'react';
+import { storiesOf } from '@apollosproject/ui-storybook';
+import { ScrollView } from 'react-native';
+
+import BackgroundView from '../BackgroundView';
+import CenteredView from '../CenteredView';
+import { CardLabel } from '../Card';
+
+import DefaultCard from '.';
+
+storiesOf('ui-kit/DefaultCard', module)
+  .addDecorator((story) => (
+    <BackgroundView>
+      {/* eslint-disable-next-line react-native/no-inline-styles */}
+      <CenteredView style={{ alignItems: 'stretch' }}>{story()}</CenteredView>
+    </BackgroundView>
+  ))
+  .add('examples', () => (
+    <ScrollView>
+      <DefaultCard
+        title={
+          'Are you telling me that you built a time machine out of a DeLorean?'
+        }
+        coverImage={[
+          {
+            uri: 'https://picsum.photos/800/1600/?random',
+          },
+        ]}
+        summary={
+          'The way I see it, if you’re going to build a time machine into a car, why not do it with some style?'
+        }
+        labelText={'Quote'}
+      />
+      <DefaultCard
+        title={
+          'Are you telling me that you built a time machine out of a DeLorean?'
+        }
+        coverImage={[
+          {
+            uri: 'https://picsum.photos/1400/800/?random',
+          },
+        ]}
+        summary={
+          'The way I see it, if you’re going to build a time machine into a car, why not do it with some style?'
+        }
+        isLiked
+      />
+      <DefaultCard
+        title={
+          'Are you telling me that you built a time machine out of a DeLorean?'
+        }
+        coverImage={[
+          {
+            uri: 'https://picsum.photos/800/800/?random',
+          },
+        ]}
+      />
+    </ScrollView>
+  ))
+  .add('default', () => (
+    <DefaultCard
+      title={
+        'Are you telling me that you built a time machine out of a DeLorean?'
+      }
+      coverImage={[
+        {
+          uri: 'https://picsum.photos/1400/800/?random',
+        },
+      ]}
+    />
+  ))
+  .add('summary', () => (
+    <DefaultCard
+      title={
+        'Are you telling me that you built a time machine out of a DeLorean?'
+      }
+      coverImage={[
+        {
+          uri: 'https://picsum.photos/1400/800/?random',
+        },
+      ]}
+      summary={
+        'The way I see it, if you’re going to build a time machine into a car, why not do it with some style?'
+      }
+    />
+  ))
+  .add('isLiked', () => (
+    <DefaultCard
+      title={
+        'Are you telling me that you built a time machine out of a DeLorean?'
+      }
+      coverImage={[
+        {
+          uri: 'https://picsum.photos/1400/800/?random',
+        },
+      ]}
+      isLiked
+    />
+  ))
+  .add('labelText', () => (
+    <DefaultCard
+      title={
+        'Are you telling me that you built a time machine out of a DeLorean?'
+      }
+      coverImage={[
+        {
+          uri: 'https://picsum.photos/1400/800/?random',
+        },
+      ]}
+      labelText={'Quote'}
+    />
+  ))
+  .add('isLoading', () => (
+    <DefaultCard
+      title={
+        'Are you telling me that you built a time machine out of a DeLorean?'
+      }
+      coverImage={[
+        {
+          uri: 'https://picsum.photos/1400/800/?random',
+        },
+      ]}
+      summary={
+        'The way I see it, if you’re going to build a time machine into a car, why not do it with some style?'
+      }
+      labelText={'Quote'}
+      isLoading
+    />
+  ))
+  .add('LabelComponent', () => (
+    <DefaultCard
+      title={
+        'Are you telling me that you built a time machine out of a DeLorean?'
+      }
+      coverImage={[
+        {
+          uri: 'https://picsum.photos/1400/800/?random',
+        },
+      ]}
+      LabelComponent={
+        <CardLabel title={'Custom LabelComponent'} type={'primary'} />
+      }
+    />
+  ))
+  .add('labelText', () => (
+    <DefaultCard
+      title={
+        'Are you telling me that you built a time machine out of a DeLorean?'
+      }
+      coverImage={[
+        {
+          uri: 'https://picsum.photos/1400/800/?random',
+        },
+      ]}
+      labelText={'Quote'}
+    />
+  ));
