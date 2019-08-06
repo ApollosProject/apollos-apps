99278aa9bbf6d5026c8e9b76322b0aa56e28e361
Burke Shartsis <burke@differential.com>
2019-08-06 14:08:51 -0400

    Address PR feedback
    Co-Authored-By: @vinnyjth <vincent@differential.com>

--- packages/apolloschurchapp/src/ui/HorizontalContentCardConnected/horizontalContentCardComponentMapper.js
+++ packages/apolloschurchapp/src/ui/HorizontalContentCardConnected/horizontalContentCardComponentMapper.js
@@ -7,29 +7,16 @@ import {
 } from '@apollosproject/ui-kit';
 
 const horizontalContentCardComponentMapper = (props) => {
-  // check if we have a custom Component prop to use or we'll use the default Component prop.
-  let ComponentToRender;
-
   // map typename to the the card we want to render.
   switch (get(props, '__typename')) {
     case 'MediaContentItem':
-      ComponentToRender = HorizontalHighlightCard;
-      break;
     case 'WeekendContentItem':
-      ComponentToRender = HorizontalHighlightCard;
-      break;
     case 'ContentSeriesContentItem':
-      ComponentToRender = HorizontalHighlightCard;
-      break;
     case 'DevotionalContentItem':
-      ComponentToRender = HorizontalHighlightCard;
-      break;
+      return <HorizontalHighlightCard {...props} />;
     default:
-      ComponentToRender = HorizontalDefaultCard;
-      break;
+      return <HorizontalDefaultCard {...props} />;
   }
-
-  return <ComponentToRender {...props} />;
 };
 
 export default horizontalContentCardComponentMapper;
