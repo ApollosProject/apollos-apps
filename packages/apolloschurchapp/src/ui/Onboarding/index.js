a9de4174a556374fe2fbc65d6838107d684e78a9
Vincent Wilson <vince@classyh.at>
2019-08-12 12:07:11 -0400

    Adds grey background to tonboarding screens

--- packages/apolloschurchapp/src/ui/Onboarding/index.js
+++ packages/apolloschurchapp/src/ui/Onboarding/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
-import { GradientOverlayImage, styled } from '@apollosproject/ui-kit';
+import {
+  GradientOverlayImage,
+  styled,
+  BackgroundView,
+} from '@apollosproject/ui-kit';
 import { ApolloConsumer } from 'react-apollo';
 
 import {
@@ -14,64 +18,73 @@ import {
 
 import { requestPushPermissions } from '@apollosproject/ui-notifications';
 
+const FullscreenBackgroundView = styled({
+  position: 'absolute',
+  width: '100%',
+  height: '100%',
+})(BackgroundView);
+
 const StyledGradient = styled({
   maxHeight: '40%',
 })(GradientOverlayImage);
 
 function Onboarding({ navigation }) {
   return (
-    <OnboardingSwiper>
-      {({ swipeForward }) => (
-        <>
-          <AskNameConnected onPressPrimary={swipeForward} />
-          <FeaturesConnected
-            onPressPrimary={swipeForward}
-            BackgroundComponent={
-              <StyledGradient
-                source={'https://picsum.photos/640/640/?random'}
-              />
-            }
-          />
-          <AboutYouConnected
-            onPressPrimary={swipeForward}
-            BackgroundComponent={
-              <StyledGradient
-                source={'https://picsum.photos/640/640/?random'}
-              />
-            }
-          />
-          <LocationFinderConnected
-            onPressPrimary={swipeForward}
-            onNavigate={() => {
-              navigation.navigate('Location', {
-                onFinished: swipeForward,
-              });
-            }}
-            BackgroundComponent={
-              <StyledGradient
-                source={'https://picsum.photos/640/640/?random'}
-              />
-            }
-          />
-          <ApolloConsumer>
-            {(client) => (
-              <AskNotificationsConnected
-                onPressPrimary={() => navigation.replace('Tabs')}
-                onRequestPushPermissions={() =>
-                  requestPushPermissions({ client })
-                }
-                primaryNavText={'Finish'}
-                BackgroundComponent={
-                  <StyledGradient
-                    source={'https://picsum.photos/640/640/?random'}
-                  />
-                }
-              />
-            )}
-          </ApolloConsumer>
-        </>
-      )}
-    </OnboardingSwiper>
+    <>
+      <FullscreenBackgroundView />
+      <OnboardingSwiper>
+        {({ swipeForward }) => (
+          <>
+            <AskNameConnected onPressPrimary={swipeForward} />
+            <FeaturesConnected
+              onPressPrimary={swipeForward}
+              BackgroundComponent={
+                <StyledGradient
+                  source={'https://picsum.photos/640/640/?random'}
+                />
+              }
+            />
+            <AboutYouConnected
+              onPressPrimary={swipeForward}
+              BackgroundComponent={
+                <StyledGradient
+                  source={'https://picsum.photos/640/640/?random'}
+                />
+              }
+            />
+            <LocationFinderConnected
+              onPressPrimary={swipeForward}
+              onNavigate={() => {
+                navigation.navigate('Location', {
+                  onFinished: swipeForward,
+                });
+              }}
+              BackgroundComponent={
+                <StyledGradient
+                  source={'https://picsum.photos/640/640/?random'}
+                />
+              }
+            />
+            <ApolloConsumer>
+              {(client) => (
+                <AskNotificationsConnected
+                  onPressPrimary={() => navigation.replace('Tabs')}
+                  onRequestPushPermissions={() =>
+                    requestPushPermissions({ client })
+                  }
+                  primaryNavText={'Finish'}
+                  BackgroundComponent={
+                    <StyledGradient
+                      source={'https://picsum.photos/640/640/?random'}
+                    />
+                  }
+                />
+              )}
+            </ApolloConsumer>
+          </>
+        )}
+      </OnboardingSwiper>
+    </>
   );
 }
 
