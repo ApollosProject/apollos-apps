dee1f6826d42eeede9940df3ce95bbab37346450
Burke Shartsis <burke@differential.com>
2019-08-12 17:03:41 -0400

    add test coverage over loading state

--- packages/apollos-ui-kit/src/ContentTableCard/ContentTableCardItem/ContentTabelCardItem.tests.js
+++ packages/apollos-ui-kit/src/ContentTableCard/ContentTableCardItem/ContentTabelCardItem.tests.js
@@ -2,6 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 
 import Providers from '../../Providers';
+import { withIsLoading } from '../../isLoading';
 
 import ContentTableCardItem from '.';
 
@@ -29,6 +30,23 @@ describe('ContentTableCardItem', () => {
           label={'What'}
           id={'fakeID'}
         />
+      </Providers>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  it('should render a loading state', () => {
+    const SetIsLoading = withIsLoading(({ children }) => children);
+    const tree = renderer.create(
+      <Providers>
+        <SetIsLoading isLoading>
+          <ContentTableCardItem
+            onPress={() => {}}
+            imageSource={'https://picsum.photos/600/400/?random'}
+            title={'Boom'}
+            label={'What'}
+            id={'fakeID'}
+          />
+        </SetIsLoading>
       </Providers>
     );
     expect(tree).toMatchSnapshot();
