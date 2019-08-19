e4cfb55b74c0e8cd7283497b913e0323489ec0b8
Burke Shartsis <burke@differential.com>
2019-08-19 13:15:56 -0400

    center story because 💅

--- packages/apollos-ui-kit/src/inputs/Search/Search.stories.js
+++ packages/apollos-ui-kit/src/inputs/Search/Search.stories.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import { storiesOf } from '@apollosproject/ui-storybook';
 
+import PaddedView from '../../PaddedView';
+import CenteredView from '../../CenteredView';
+
 import Search from '.';
 
-storiesOf('ui-kit/Inputs', module).add('Search', () => <Search />);
+storiesOf('ui-kit/Inputs', module)
+  .addDecorator((story) => (
+    <CenteredView>
+      {/* eslint-disable-next-line react-native/no-inline-styles */}
+      <PaddedView style={{ alignSelf: 'stretch' }}>{story()}</PaddedView>
+    </CenteredView>
+  ))
+  .add('Search', () => <Search />);
