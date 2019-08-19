e650f5ab86448b38063a9b5b34abb9b6d2c5971a
Burke Shartsis <burke@differential.com>
2019-08-19 13:16:05 -0400

    WIP

--- packages/apollos-ui-kit/src/inputs/Search/index.js
+++ packages/apollos-ui-kit/src/inputs/Search/index.js
@@ -1,8 +1,87 @@
 import React from 'react';
+import { View, Animated, TextInput } from 'react-native';
+// import Color from 'color';
 import PropTypes from 'prop-types';
 
-import { H1 } from '../../typography';
+import styled from '../../styled';
+import { withTheme } from '../../theme';
+import Icon from '../../Icon';
 
-const Search = () => <H1>Hello World</H1>;
+// import InputWrapper from '../InputWrapper';
+// import TextInput from '../Text';
+import ErrorText from '../ErrorText';
+import InputAddon, { AddonRow } from '../InputAddon';
+// import { textStyle } from '../withInputControlStyles';
 
+/*
+ * search loop icon
+ * clear text icon
+ * cancel buttontext
+ *   text prop
+ *   onPress prop
+ *   animate cancel text in
+ * label text prop
+ * onSubmit prop
+ */
+
+const InputWrapper = styled(
+  ({ theme, disabled }) => ({
+    // marginVertical: theme.sizing.baseUnit,
+    borderRadius: theme.sizing.baseUnit,
+    backgroundColor: theme.colors.background.screen,
+    ...(disabled ? { opacity: 0.5 } : {}),
+  }),
+  'InputWrapper'
+)(View);
+
+const LoopIcon = withTheme(({ theme }) => ({
+  fill: theme.colors.text.tertiary,
+  size: theme.sizing.baseUnit,
+}))(Icon);
+
+const Input = withTheme(({ theme }) => ({
+  placeholderTextColor: theme.colors.text.tertiary,
+}));
+
+const Search = ({
+  label,
+  suffix,
+  value,
+  wrapperStyle,
+  error,
+  disabled = false,
+  theme,
+  inputRef,
+  underline,
+  ...textInputProps
+}) => {
+  const focusAnimation = new Animated.Value(1);
+
+  const animatedStyle = { opacity: focusAnimation, flex: 1 };
+
+  return (
+    <InputWrapper style={wrapperStyle} disabled={disabled}>
+      <View>
+        <AddonRow>
+          <InputAddon>
+            <LoopIcon name={'search'} />
+          </InputAddon>
+          <Animated.View style={animatedStyle}>
+            <Input
+              // style={textStyle({ theme })}
+              ref={inputRef}
+              editable={!disabled}
+              value={value}
+            />
+          </Animated.View>
+          <InputAddon>{suffix}</InputAddon>
+        </AddonRow>
+      </View>
+
+      {error && typeof error === 'string' ? (
+        <ErrorText>{error}</ErrorText>
+      ) : null}
+    </InputWrapper>
+  );
+};
 export default Search;
