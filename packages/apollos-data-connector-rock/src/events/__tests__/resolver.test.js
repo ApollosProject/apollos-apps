aca09fdac9a41c2686e061cf5d73c8e97342da42
Michael Neeley <micneeley14@gmail.com>
2019-08-27 16:54:55 -0400

    test stuff

--- packages/apollos-data-connector-rock/src/events/__tests__/resolver.test.js
+++ packages/apollos-data-connector-rock/src/events/__tests__/resolver.test.js
@@ -1,18 +1,18 @@
 import { graphql } from 'graphql';
 import { createTestHelpers } from '@apollosproject/server-core/lib/testUtils';
-import { fetch } from 'apollo-server-env';
-import ApollosConfig from '@apollosproject/config';
+// import { fetch } from 'apollo-server-env';
+// import ApollosConfig from '@apollosproject/config';
 
 import { campusSchema, peopleSchema } from '@apollosproject/data-schema';
 import * as Events from '../index';
 import { Campus } from '../../index';
 
-ApollosConfig.loadJs({
-  ROCK: {
-    API_URL: 'https://apollosrock.newspring.cc/api',
-    API_TOKEN: 'some-rock-token',
-  },
-});
+// ApollosConfig.loadJs({
+// ROCK: {
+// API_URL: 'https://apollosrock.newspring.cc/api',
+// API_TOKEN: 'some-rock-token',
+// },
+// });
 
 const { getSchema, getContext } = createTestHelpers({
   Events,
@@ -24,8 +24,8 @@ describe('Events resolver', () => {
   let context;
   let rootValue;
   beforeEach(() => {
-    fetch.resetMocks();
-    fetch.mockRockDataSourceAPI();
+    // fetch.resetMocks();
+    // fetch.mockRockDataSourceAPI();
     schema = getSchema([campusSchema, peopleSchema]);
     context = getContext();
     rootValue = {};
@@ -49,28 +49,38 @@ describe('Events resolver', () => {
     context.dataSources.Campus.getByLocation = jest.fn(() =>
       Promise.resolve([{ id: 1 }])
     );
-    context.dataSources.Events = {
-      ...context.dataSources.Events,
-      getByCampus: jest.fn(() =>
-        Promise.resolve([
-          { id: 1, campusId: 1, scheduleId: 1, location: '123 Main St' },
-        ])
-      ),
-      getName: jest.fn(() => Promise.resolve('Cookout')),
-      // for testing the getDateTime datasource function...
-      // first: jest.fn(() =>
-      // Promise.resolve({
-      // iCalendarContent:
-      // 'BEGIN:VCALENDAR\r\nBEGIN:VEVENT\r\nDTEND:20130501T190000\r\nDTSTART:20130501T180000\r\nRRULE:FREQ=WEEKLY;BYDAY=SA\r\nEND:VEVENT\r\nEND:VCALENDAR',
-      // })
-      // ),
-      // getDateTime: jest.fn(() =>
-      // Promise.resolve({
-      // start: '2019-08-26T17:00:00',
-      // end: '2019-08-26T19:00:00',
-      // })
-      // ),
-    };
+    context.dataSources.Events.getByCampus = jest.fn(() =>
+      Promise.resolve([
+        { id: 1, campusId: 1, scheduleId: 1, location: '123 Main St' },
+      ])
+    );
+    context.dataSources.Events.getName = jest.fn(() =>
+      Promise.resolve('Cookout')
+    );
+    // for testing the getDateTime datasource function...
+    context.dataSources.Events.request = () => ({
+      filter: () => ({
+        first: () =>
+          Promise.resolve({
+            iCalendarContent:
+              'BEGIN:VCALENDAR\r\nBEGIN:VEVENT\r\nDTEND:20130501T190000\r\nDTSTART:20130501T180000\r\nRRULE:FREQ=WEEKLY;BYDAY=SA\r\nEND:VEVENT\r\nEND:VCALENDAR',
+          }),
+      }),
+    });
+
+    // first: jest.fn(() =>
+    // Promise.resolve({
+    // iCalendarContent:
+    // 'BEGIN:VCALENDAR\r\nBEGIN:VEVENT\r\nDTEND:20130501T190000\r\nDTSTART:20130501T180000\r\nRRULE:FREQ=WEEKLY;BYDAY=SA\r\nEND:VEVENT\r\nEND:VCALENDAR',
+    // })
+    // ),
+    // getDateTime: jest.fn(() =>
+    // Promise.resolve({
+    // start: '2019-08-26T17:00:00',
+    // end: '2019-08-26T19:00:00',
+    // })
+    // ),
+
     const result = await graphql(schema, query, rootValue, context);
     expect(result).toMatchSnapshot();
   });
