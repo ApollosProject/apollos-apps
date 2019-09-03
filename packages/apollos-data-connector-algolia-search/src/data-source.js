27b5d09a1876c0e6d1838a75bff2ac2679371a5f
Conrad VanLandingham <hi@convan.me>
2019-09-03 12:24:10 -0500

    initial search query

--- packages/apollos-data-connector-algolia-search/src/data-source.js
+++ packages/apollos-data-connector-algolia-search/src/data-source.js
@@ -1,3 +1,15 @@
-class DataSource {}
+import ApollosConfig from '@apollosproject/config';
+import algoliasearch from 'algoliasearch';
 
-export default DataSource;
+const { ALGOLIA } = ApollosConfig;
+
+export default class Search {
+  client = algoliasearch(ALGOLIA.APPLICATION_ID, ALGOLIA.API_KEY);
+
+  index = this.client.initIndex(ALGOLIA.SEARCH_INDEX);
+
+  async search({ query, first, after }) {
+    const { hits } = await this.index.search({ query });
+    return hits;
+  }
+}
--- packages/apollos-data-connector-algolia-search/src/resolver.js
+++ packages/apollos-data-connector-algolia-search/src/resolver.js
@@ -1,3 +1,14 @@
-const resolver = {};
+const resolver = {
+  Query: {
+    search: async (root, { query, first, after }, { dataSources }) => {
+      const hits = await dataSources.Search.search({ query, first, after });
+      return {
+        edges: hits.map((hit) => ({
+          node: hit,
+        })),
+      };
+    },
+  },
+};
 
 export default resolver;
