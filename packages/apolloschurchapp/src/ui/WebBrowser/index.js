d44b0478cb2f95f8f0e2f56280a12759a6a4e87b
Michael Neeley <micneeley14@gmail.com>
2019-09-04 18:46:23 -0400

    android works

--- packages/apolloschurchapp/src/ui/WebBrowser/index.js
+++ packages/apolloschurchapp/src/ui/WebBrowser/index.js
@@ -1,16 +1,39 @@
 // Provider API for WebBrowser that injects theme values and defaults to the web browser:
 import { Platform } from 'react-native';
 import { createContext } from 'react';
+import gql from 'graphql-tag';
 
 import { withTheme } from '@apollosproject/ui-kit';
 
+import { client } from '../../client';
 import Browser from './Browser';
+
+export const GET_USER_COOKIE = gql`
+  query CurrentUserCookie {
+    currentUser {
+      id
+      rockToken
+    }
+  }
+`;
+
+export const getCookie = () => {
+  const {
+    data: { currentUser: { rockToken } = {} } = {},
+    loading,
+  } = client.readQuery({
+    query: GET_USER_COOKIE,
+  });
+  if (loading) return {};
+  return rockToken;
+};
 
 const { Provider: BaseProvider, Consumer } = createContext(Browser.open);
 
 const Provider = withTheme(({ theme }) => ({
-  value: (url, headers = {}, options = {}) =>
-    Browser.open(url, {
+  value: (url, headers = {}, options = {}) => {
+    const cookie = getCookie();
+    return Browser.open(url, {
       ...Platform.select({
         ios: {
           dismissButtonStyle: 'cancel',
@@ -31,9 +54,11 @@ const Provider = withTheme(({ theme }) => ({
           forceCloseOnRedirection: false,
         },
       }),
+      ...cookie,
       ...headers,
       ...options,
-    }),
+    });
+  },
 }))(BaseProvider);
 
 export default Browser;
