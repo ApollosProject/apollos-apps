fe543f8cb04b71b34fc9b3d071f010a005484e3e
Burke Shartsis <burke@differential.com>
2019-09-10 15:56:26 -0400

    remove loading spinner when clearing search

--- packages/apolloschurchapp/src/tabs/discover/DiscoverFeed/index.js
+++ packages/apolloschurchapp/src/tabs/discover/DiscoverFeed/index.js
@@ -37,7 +37,7 @@ const DiscoverFeed = memo(() => (
       <FeedView
         error={error}
         content={contentChannels}
-        isLoading={loading}
+        isLoading={loading && !contentChannels.length}
         refetch={refetch}
         renderItem={renderItem}
         loadingStateObject={feedItemLoadingState}
