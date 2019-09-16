79f5b6db8a1cdbe8e63df7225cfb70dc7ca8a477
Vincent Wilson <vince@classyh.at>
2019-09-16 17:16:01 -0400

    Tests for data source

--- packages/apollos-data-connector-rock/src/events/__tests__/data-source.test.js
+++ packages/apollos-data-connector-rock/src/events/__tests__/data-source.test.js
@@ -39,9 +39,44 @@ describe('Events', () => {
       })
     );
 
-    const result = await Events.getName(123);
+    const result = await Events.getName({ eventItemId: 123 });
     expect(result).toEqual('some event');
     expect(Events.get.mock.calls).toMatchSnapshot();
+  });
+
+  it('should return a name based on an EventItem', async () => {
+    const Events = new EventsDataSource();
+
+    Events.get = jest.fn(() =>
+      Promise.resolve({
+        name: 'some event',
+      })
+    );
+
+    const result = await Events.getName({ eventItemId: 123 });
+    expect(result).toEqual('some event');
+    expect(Events.get.mock.calls).toMatchSnapshot();
+  });
+
+  it('should return an image based on an EventItem', async () => {
+    const Events = new EventsDataSource();
+
+    Events.get = jest.fn(() =>
+      Promise.resolve({
+        name: 'some event',
+      })
+    );
+
+    const imageUrl = jest.fn(() => Promise.resolve('https://example.com'));
+
+    Events.context = {
+      dataSources: { BinaryFiles: { findOrReturnImageUrl: imageUrl } },
+    };
+
+    const result = await Events.getImage({ eventItemId: 123 });
+    expect(result).toMatchSnapshot();
+    expect(Events.get.mock.calls).toMatchSnapshot();
+    expect(imageUrl.mock.calls).toMatchSnapshot();
   });
 
   it('should get by Campus', async () => {
@@ -59,6 +94,46 @@ describe('Events', () => {
     const result = await Events.getByCampus(456);
     expect(result).toMatchSnapshot();
     expect(Events.get.mock.calls).toMatchSnapshot();
+  });
+
+  it('should get all recent events', async () => {
+    const Events = new EventsDataSource();
+
+    Events.get = jest.fn(() =>
+      Promise.resolve([
+        {
+          id: 123,
+          name: 'some event',
+        },
+      ])
+    );
+
+    const result = await Events.findRecent().get();
+    expect(result).toMatchSnapshot();
+    expect(Events.get.mock.calls).toMatchSnapshot();
+  });
+
+  it('should get all recent events using the plugin', async () => {
+    ApollosConfig.loadJs({
+      ROCK: { USE_PLUGIN: true },
+    });
+    const Events = new EventsDataSource();
+
+    Events.get = jest.fn(() =>
+      Promise.resolve([
+        {
+          id: 123,
+          name: 'some event',
+        },
+      ])
+    );
+
+    const result = await Events.findRecent().get();
+    expect(result).toMatchSnapshot();
+    expect(Events.get.mock.calls).toMatchSnapshot();
+    ApollosConfig.loadJs({
+      ROCK: { USE_PLUGIN: false },
+    });
   });
 
   it('should get by id', async () => {
