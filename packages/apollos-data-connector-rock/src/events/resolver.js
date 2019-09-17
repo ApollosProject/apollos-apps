8d02b404b1cf6e85e0924dd9ef8cb7d87ae8d792
Michael Neeley <micneeley14@gmail.com>
2019-09-17 15:48:38 -0400

    more refactor

--- packages/apollos-data-connector-rock/src/events/resolver.js
+++ packages/apollos-data-connector-rock/src/events/resolver.js
@@ -5,14 +5,10 @@ export default {
     id: ({ id }, args, context, { parentType }) =>
       createGlobalId(id, parentType.name),
     name: (root, args, { dataSources }) => dataSources.Events.getName(root),
-    start: ({ schedule }, args, { dataSources }) => {
-      const times = dataSources.Events.getDateTime(schedule);
-      return times.start;
-    },
-    end: ({ schedule }, args, { dataSources }) => {
-      const times = dataSources.Events.getDateTime(schedule);
-      return times.end;
-    },
+    start: ({ schedule }, args, { dataSources }) =>
+      dataSources.Events.getDateTime(schedule).start,
+    end: ({ schedule }, args, { dataSources }) =>
+      dataSources.Events.getDateTime(schedule).end,
   },
   Campus: {
     events: ({ id }, args, { dataSources }) =>
