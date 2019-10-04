e791e532aa38e46bdc42c81030a71b9972d6bbd3
Vincent Wilson <vince@classyh.at>
2019-10-04 15:32:18 -0400

    Replace custom safe area provider with native safe area layout provider

--- packages/apollos-ui-media-player/src/MediaPlayer/MediaPlayerSafeLayout.js
+++ packages/apollos-ui-media-player/src/MediaPlayer/MediaPlayerSafeLayout.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { View } from 'react-native';
-import { styled, LayoutConsumer } from '@apollosproject/ui-kit';
+import { styled } from '@apollosproject/ui-kit';
 
-const InnerView = styled(({ isFullscreen = false, theme, safeAreaInsets }) => ({
-  marginBottom: isFullscreen
-    ? 0
-    : Math.max(theme.sizing.baseUnit, safeAreaInsets.bottom * 0.75),
-}))(View);
+const InnerView = styled(
+  ({ isFullscreen = false, theme, safeAreaInsets }) =>
+    console.warn(String(safeAreaInsets.bottom)) || {
+      marginBottom: isFullscreen
+        ? 0
+        : Math.max(theme.sizing.baseUnit, safeAreaInsets.bottom * 0.75),
+    }
+)(View);
 
 const MediaPlayerSafeLayout = (props) => (
   <LayoutConsumer>
-    {({ safeAreaInsets }) => (
-      <InnerView safeAreaInsets={safeAreaInsets} {...props} />
+    {(insets) => (
+      <InnerView safeAreaInsets={insets} {...props} />
     )}
   </LayoutConsumer>
 );
