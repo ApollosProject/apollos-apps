8a503c814237d24189f78f6955c84eb11c2b1160
Vincent Wilson <vince@classyh.at>
2019-10-04 15:34:03 -0400

    Remove console.log

--- packages/apollos-ui-media-player/src/MediaPlayer/MediaPlayerSafeLayout.js
+++ packages/apollos-ui-media-player/src/MediaPlayer/MediaPlayerSafeLayout.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { View } from 'react-native';
-import { styled } from '@apollosproject/ui-kit';
+import { styled, LayoutConsumer } from '@apollosproject/ui-kit';
 
 const InnerView = styled(
-  ({ isFullscreen = false, theme, safeAreaInsets }) =>
-    console.warn(String(safeAreaInsets.bottom)) || {
+  ({ isFullscreen = false, theme, safeAreaInsets }) => ({
       marginBottom: isFullscreen
         ? 0
         : Math.max(theme.sizing.baseUnit, safeAreaInsets.bottom * 0.75),
