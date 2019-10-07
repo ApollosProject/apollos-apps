566b20ec3e3717f88f625d8f65506102d60e5eb6
Vincent Wilson <vince@classyh.at>
2019-10-07 07:05:28 -0700

    Fixes safe area layout provider (#1095)
    * Replace custom safe area provider with native safe area layout provider
    
    * Remove console.log
    
    * bad linter fix
    
    * Update snaps
    
    * Fix snaps
    
    * Fix linters

--- packages/apollos-ui-htmlview/jest.setup.js
+++ packages/apollos-ui-htmlview/jest.setup.js
@@ -6,11 +6,3 @@ jest.mock(
     SafeAreaProvider: ({ children }) => children,
   })
 );
-
-jest.mock('react-native-device-info', () => ({
-  getUniqueID: () => 'id-123',
-  getSystemVersion: () => 'sys-version-123',
-  getModel: () => 'ios',
-  getVersion: () => 'version-123',
-  getBuildNumber: () => 0,
-}));
--- packages/apollos-ui-kit/__mocks__/react-native-safe-area-context.js
+++ packages/apollos-ui-kit/__mocks__/react-native-safe-area-context.js
@@ -1,6 +1,4 @@
-jest.mock('rn-fetch-blob', () => 'Fetch');
-
-jest.mock('../apollos-ui-kit/node_modules/react-native-safe-area-context/', () => ({
+module.exports = {
   SafeAreaConsumer: ({ children }) => children({ top: 0, bottom: 0, left: 0, right: 0 }),
   SafeAreaProvider: ({ children }) => children,
-}));
+}
