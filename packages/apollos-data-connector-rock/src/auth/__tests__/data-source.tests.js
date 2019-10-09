e36ff7538a90ffec314b6bb65347136a7d540de7
Michael Neeley <micneeley14@gmail.com>
2019-10-09 17:00:18 -0400

    comment

--- packages/apollos-data-connector-rock/src/auth/__tests__/data-source.tests.js
+++ packages/apollos-data-connector-rock/src/auth/__tests__/data-source.tests.js
@@ -5,11 +5,6 @@ describe('Auth', () => {
 
   Date.now = jest.fn(() => 1482363367071);
 
-  // Return a fixed timestamp when moment().format() is called
-  // jest.mock('moment', () => () => ({
-  // add: () => ({ toISOString: () => '2018–01–30T12:34:56+00:00' }),
-  // }));
-
   class AuthWithContext extends AuthDataSource {
     context = {
       rockCookie: 'some cookie',
