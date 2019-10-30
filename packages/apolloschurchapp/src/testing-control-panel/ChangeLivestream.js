ad15c82a078e909c069833aebebe1a7a98e4a2ae
Vincent Wilson <vince@classyh.at>
2019-10-30 08:56:12 -0400

    Update testing control panel

--- packages/apolloschurchapp/src/testing-control-panel/ChangeLivestream.js
+++ packages/apolloschurchapp/src/testing-control-panel/ChangeLivestream.js
@@ -1,12 +1,11 @@
-// import React from 'react';
-//
+import React from 'react';
+
 // import { Query } from 'react-apollo';
 // import { get } from 'lodash';
-//
+
 // import { client } from 'apolloschurchapp/src/client';
-// import GET_LIVE_STREAM from 'apolloschurchapp/src/live/getLiveStream';
-// import TouchableCell from './TouchableCell';
-//
+import TouchableCell from './TouchableCell';
+
 // const changeLivestream = ({ isLive }) =>
 //   client.writeQuery({
 //     query: GET_LIVE_STREAM,
@@ -17,22 +16,24 @@
 //       },
 //     },
 //   });
-//
-// const ChangeLivestream = () => (
-//   <Query query={GET_LIVE_STREAM}>
-//     {({ data }) => {
-//       const isLive = get(data, 'liveStream.isLive', false);
-//       return (
-//         <TouchableCell
-//           handlePress={() => changeLivestream({ isLive: !isLive })}
-//           iconName={isLive ? 'pause' : 'play'}
-//           cellText={`${isLive ? 'End' : 'Start'} The Livestream`}
-//         />
-//       );
-//     }}
-//   </Query>
-// );
-//
-// export default ChangeLivestream;
 
-export default null;
+// **********************************************************************************************************
+// Currently disabled while we reconfigure this component to work with new ContentItem/LiveStream integration
+// **********************************************************************************************************
+
+const ChangeLivestream = () => (
+  // <Query query={GET_LIVE_STREAM}>
+  //   {({ data }) => {
+  //     const isLive = get(data, 'liveStream.isLive', false);
+  //     return (
+  <TouchableCell
+    handlePress={() => console.warn('Functionality currently not enabled')}
+    iconName={false ? 'pause' : 'play'}
+    cellText={`${false ? 'End' : 'Start'} The Livestream`}
+  />
+  //     );
+  //   }}
+  // </Query>
+);
+
+export default ChangeLivestream;
