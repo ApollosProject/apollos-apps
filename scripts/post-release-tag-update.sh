d9f3915f1a8f34980b4329a602863694a7b27593
Vincent Wilson <vince@classyh.at>
2019-10-31 14:46:55 -0400

    Release v1.2.0 (#1181)
    * v1.2.0
    
    * Fix tag script
    
    * Update changelog

--- scripts/post-release-tag-update.sh
+++ scripts/post-release-tag-update.sh
@@ -5,12 +5,12 @@ PACKAGE_VERSION=$(cat packages/apolloschurchapp/apollos.json \
   | grep \"version\" \
   | head -1 \
   | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g')
+  | sed 's/[", ]//g')
 
 echo "Updating tags for version $PACKAGE_VERSION"
 
-git tag -d v$PACKAGE_VERSION
+git tag -d "v$PACKAGE_VERSION"
 
-git tag v$PACKAGE_VERSION -m "Release v$PACKAGE_VERSION"
+git tag "v$PACKAGE_VERSION" -m "Release v$PACKAGE_VERSION"
 
 git push origin --tags
