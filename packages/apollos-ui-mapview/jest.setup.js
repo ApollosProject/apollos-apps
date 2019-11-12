4140c46627ad15f8253e22ea3038d33bc4252f57
Jordan Wade <jjwade31@gmail.com>
2019-11-12 10:35:24 -0600

    Clean-up tests and dependencies

--- packages/apollos-ui-mapview/jest.setup.js
+++ packages/apollos-ui-mapview/jest.setup.js
@@ -1,8 +1,3 @@
-jest.mock(
-  '../apollos-ui-kit/node_modules/react-native-safe-area-context/',
-  () => ({
-    SafeAreaConsumer: ({ children }) =>
-      children({ top: 0, bottom: 0, left: 0, right: 0 }),
-    SafeAreaProvider: ({ children }) => children,
-  })
-);
+jest.mock('@apollosproject/ui-media-player', () => ({
+  MediaPlayerSpacer: ({ children }) => children,
+}));
