bd2c2256f93c34f7a931cc1ca027afe052bf87f7
erikkgagewhitt <gage@differential.com>
2019-11-20 11:53:49 -0500

    Optimistic Response for Location Selector (#1236)
    * optimistic response
    
    * No await.

--- packages/apolloschurchapp/src/user-settings/Locations/index.js
+++ packages/apolloschurchapp/src/user-settings/Locations/index.js
@@ -90,10 +90,17 @@ class Location extends PureComponent {
                 initialRegion={this.props.initialRegion}
                 userLocation={this.state.userLocation}
                 currentCampus={get(currentUser, 'profile.campus')}
-                onLocationSelect={async ({ id }) => {
-                  await handlePress({
+                onLocationSelect={async (campus) => {
+                  handlePress({
                     variables: {
-                      campusId: id,
+                      campusId: campus.id,
+                    },
+                    optimisticResponse: {
+                      updateUserCampus: {
+                        __typename: 'Mutation',
+                        id: currentUser.id,
+                        campus,
+                      },
                     },
                   });
                   this.props.navigation.goBack();
