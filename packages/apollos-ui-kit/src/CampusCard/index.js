062979e8e9fcedd6a5cf925d9d3475e3ef302dbe
Jordan Wade <jjwade31@gmail.com>
2019-11-21 10:36:28 -0600

    Remove unused view import

--- packages/apollos-ui-kit/src/CampusCard/index.js
+++ packages/apollos-ui-kit/src/CampusCard/index.js
@@ -1,6 +1,5 @@
 import React, { memo } from 'react';
 import PropTypes from 'prop-types';
-import { View } from 'react-native';
 import isNull from 'lodash/isNull';
 
 import Card, { CardContent } from '../Card';
