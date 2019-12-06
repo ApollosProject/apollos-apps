277f0055a564de38abb2c64e98dbb427dac8f174
Burke Shartsis <burke@differential.com>
2019-12-06 15:53:21 -0500

    fix images not loading in CampusCard stories
    - required an array

--- packages/apollos-ui-kit/src/CampusCard/CampusCard.stories.js
+++ packages/apollos-ui-kit/src/CampusCard/CampusCard.stories.js
@@ -2,23 +2,30 @@
 import React from 'react';
 import { storiesOf } from '@apollosproject/ui-storybook';
 
+import CenteredView from '../CenteredView';
+
 import CampusCard from '.';
 
 storiesOf('ui-kit/CampusCard', module)
+  .addDecorator((story) => (
+    <CenteredView style={{ alignItems: 'stretch' }}>{story()}</CenteredView> // eslint-disable-next-line react-native/no-inline-styles
+  ))
   .add('default', () => (
     <CampusCard
-      title="FAKE_TITLE"
-      description="FAKE_DESCRIPTION"
-      images="https://www.placecage.com/c/250/250"
+      title={'Cameron Poe'}
+      description={'"Why couldn\'t you just put the Bunny back in the box?"'}
+      images={['https://www.placecage.com/c/250/250']}
       category=""
       distance={2037.6461577685534}
     />
   ))
   .add('isLoading', () => (
     <CampusCard
-      title="FAKE_TITLE"
-      description="FAKE_DESCRIPTION"
-      images="https://www.placecage.com/c/250/250"
+      title={'Stanley Goodspeed'}
+      description={
+        '"How… In the name of Zeus\'s BUTTHOLE, did you get out of your cell?"'
+      }
+      images={['https://www.placecage.com/c/250/250']}
       category=""
       distance={2037.6461577685534}
       isLoading
