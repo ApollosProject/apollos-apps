d2f139a61e2da20abf0fe200d7f6726458f82ada
Gage Whitt <gage@differential.com>
2019-12-16 14:19:57 -0500

    Remove permission utils which are no longer used.

--- packages/apollos-ui-notifications/src/index.js
+++ packages/apollos-ui-notifications/src/index.js
@@ -1,8 +1,6 @@
 export NotificationsProvider from './Provider';
 export {
   getPushPermissions,
-  requestPushPermissions,
-  GET_NOTIFICATIONS_ENABLED,
   GET_PUSH_ID,
 } from './permissionUtils';
 export updatePushId from './updatePushId';
--- packages/apollos-ui-notifications/src/permissionUtils.js
+++ packages/apollos-ui-notifications/src/permissionUtils.js
@@ -16,43 +16,10 @@ const getHasPrompted = async () =>
     )
   );
 
-const promptForPushNotificationsWithUserResponse = async () =>
-  new Promise((resolve) =>
-    OneSignal.promptForPushNotificationsWithUserResponse(resolve)
-  );
-
-const SET_NOTIFCATIONS_ENABLED = gql`
-  mutation updatePushPermissions($enabled: Boolean!) {
-    updatePushPermissions(enabled: $enabled) @client
-  }
-`;
-
-const GET_NOTIFICATIONS_ENABLED = gql`
-  query getPushPermissions {
-    notificationsEnabled @client(always: true)
-  }
-`;
-
 const GET_PUSH_ID = gql`
   query getPushId {
     pushId @client
   }
 `;
 
-const requestPushPermissions = async ({ client }) => {
-  const notificationsEnabled = await promptForPushNotificationsWithUserResponse();
-  await client.mutate({
-    mutation: SET_NOTIFCATIONS_ENABLED,
-    variables: { enabled: notificationsEnabled },
-  });
-
-  return notificationsEnabled;
-};
-
-export {
-  GET_NOTIFICATIONS_ENABLED,
-  GET_PUSH_ID,
-  getHasPrompted,
-  getPushPermissions,
-  requestPushPermissions,
-};
+export { GET_PUSH_ID, getHasPrompted, getPushPermissions };
