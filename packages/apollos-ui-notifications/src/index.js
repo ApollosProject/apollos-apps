7fc287bfef3097f34099b7c792a578762bd22273
Gage Whitt <gage@differential.com>
2019-12-16 14:31:42 -0500

    lint.

--- packages/apollos-ui-notifications/src/index.js
+++ packages/apollos-ui-notifications/src/index.js
@@ -1,7 +1,4 @@
 export NotificationsProvider from './Provider';
-export {
-  getPushPermissions,
-  GET_PUSH_ID,
-} from './permissionUtils';
+export { getPushPermissions, GET_PUSH_ID } from './permissionUtils';
 export updatePushId from './updatePushId';
 export PushProvider, { PushConsumer } from './pushProvider';
