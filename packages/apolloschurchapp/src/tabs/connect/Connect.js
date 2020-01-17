27879dd81e4cbde3496a0561c9f83af9f1d61890
Burke Shartsis <burke@differential.com>
2020-01-17 12:47:27 -0500

    update imports for RecentlyLikedTileFeed

--- packages/apolloschurchapp/src/tabs/connect/Connect.js
+++ packages/apolloschurchapp/src/tabs/connect/Connect.js
@@ -3,11 +3,11 @@ import { ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
 import PropTypes from 'prop-types';
 
+import { RecentlyLikedTileFeedConnected } from '@apollosproject/ui-connected';
 import { BackgroundView } from '@apollosproject/ui-kit';
 import ActionTable from './ActionTable';
 import ActionBar from './ActionBar';
 import UserAvatarHeader from './UserAvatarHeader';
-import { RecentlyLikedTileFeedConnected } from './RecentlyLikedTileFeed';
 
 class Connect extends PureComponent {
   static navigationOptions = () => ({
