0a1b57835ec13d9360c0d8b8c6e5fc04cd5fd62d
Burke Shartsis <burke@differential.com>
2020-01-17 12:47:12 -0500

    add export for RecentlyLikedTileFeed to ui-connected

--- packages/apollos-ui-connected/src/index.js
+++ packages/apollos-ui-connected/src/index.js
@@ -1,5 +1,9 @@
-export { LiveProvider, LiveConsumer } from './live';
 export LikedContentFeedConnected, {
   GET_LIKED_CONTENT,
 } from './LikedContentFeedConnected';
+export { LiveProvider, LiveConsumer } from './live';
+export {
+  RecentlyLikedTileFeed,
+  RecentlyLikedTileFeedConnected,
+} from './RecentlyLikedTileFeed';
 export { fetchMoreResolver } from './utils';
