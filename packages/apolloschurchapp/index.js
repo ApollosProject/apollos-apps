9be9d4c2092a49863c9cf339aef8ebdf4dca0e11
Conrad VanLandingham <conrad@differential.com>
2020-01-21 10:46:32 -0600

    Use default import in root index (#1292)

--- packages/apolloschurchapp/index.js
+++ packages/apolloschurchapp/index.js
@@ -7,7 +7,7 @@ import Storybook from './storybook';
 
 const useStorybook = ApollosConfig.STORYBOOK === 'true';
 
-const MainApp = require('./src');
+const MainApp = require('./src').default;
 
 let App = MainApp;
 if (useStorybook) {
