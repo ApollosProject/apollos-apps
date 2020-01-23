f0e7f566db14aaaa66eabe11699b7f2281fca605
Burke Shartsis <burke@differential.com>
2020-01-23 15:15:51 -0500

    refactor ShareContentButtonConnected to have a better seperation of concern
    - adds an onPress prop for flexibility

--- packages/apollos-ui-connected/src/ShareContentButtonConnected/ShareContentButton/index.js
+++ packages/apollos-ui-connected/src/ShareContentButtonConnected/ShareContentButton/index.js
@@ -1,45 +1,20 @@
 import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 import { Touchable, Icon, withTheme } from '@apollosproject/ui-kit';
-import { AnalyticsConsumer } from '@apollosproject/ui-analytics';
-import { share } from '../../utils';
 
 const ShareIcon = withTheme(({ theme }) => ({
   fill: theme.colors.secondary,
 }))(Icon);
 
-const ShareContentButton = memo(({ content, onPress }) => (
-  <AnalyticsConsumer>
-    {({ track }) => {
-      let handleOnPress = onPress;
-
-      if (!onPress) {
-        handleOnPress = () => {
-          share(content);
-          track({
-            eventName: 'Share',
-            properties: { id: content.id, title: content.title },
-          });
-        };
-      }
-
-      return (
-        <Touchable onPress={handleOnPress}>
-          <ShareIcon name={'share'} />
-        </Touchable>
-      );
-    }}
-  </AnalyticsConsumer>
+const ShareContentButton = memo(({ onPress }) => (
+  <Touchable onPress={onPress}>
+    <ShareIcon name={'share'} />
+  </Touchable>
 ));
 
 ShareContentButton.displayName = 'ShareContentButton';
 
 ShareContentButton.propTypes = {
-  content: PropTypes.shape({
-    title: PropTypes.string,
-    url: PropTypes.string,
-    id: PropTypes.string,
-  }).isRequired,
   onPress: PropTypes.func,
 };
 
--- packages/apollos-ui-connected/src/ShareContentButtonConnected/ShareContentButtonConnected.js
+++ packages/apollos-ui-connected/src/ShareContentButtonConnected/ShareContentButtonConnected.js
@@ -1,25 +1,50 @@
 import React from 'react';
-
 import PropTypes from 'prop-types';
 import { Query } from 'react-apollo';
 import { get } from 'lodash';
+import { AnalyticsConsumer } from '@apollosproject/ui-analytics';
+
+import { share } from '../utils';
 
 import ShareContentButton from './ShareContentButton';
 import GET_SHARE_CONTENT from './getShareContent';
 
-const ShareContentButtonConnected = ({ itemId, title, message, url }) => (
-  <Query query={GET_SHARE_CONTENT} variables={{ itemId }}>
-    {({ data }) => {
-      const sharing = get(data, 'node.sharing', {});
-      const content = {
-        id: itemId,
-        title: title || sharing.title,
-        message: message || sharing.message,
-        url: url || sharing.url,
-      };
-      return <ShareContentButton content={content} />;
-    }}
-  </Query>
+const ShareContentButtonConnected = ({
+  itemId,
+  message,
+  onPress,
+  title,
+  url,
+}) => (
+  <AnalyticsConsumer>
+    {({ track }) => (
+      <Query query={GET_SHARE_CONTENT} variables={{ itemId }}>
+        {({ data }) => {
+          const sharing = get(data, 'node.sharing', {});
+          const content = {
+            id: itemId,
+            title: title || sharing.title,
+            message: message || sharing.message,
+            url: url || sharing.url,
+          };
+          const handleOnPress = () => {
+            if (onPress) {
+              onPress(content);
+            } else {
+              share(content);
+            }
+
+            track({
+              eventName: 'Share',
+              properties: { id: itemId, title },
+            });
+          };
+
+          return <ShareContentButton onPress={handleOnPress} />;
+        }}
+      </Query>
+    )}
+  </AnalyticsConsumer>
 );
 
 ShareContentButtonConnected.propTypes = {
@@ -28,6 +53,8 @@ ShareContentButtonConnected.propTypes = {
   title: PropTypes.string,
   message: PropTypes.string,
   url: PropTypes.string,
+  // Pass your own function and do whatever you want with the data.
+  onPress: PropTypes.func,
 };
 
 export default ShareContentButtonConnected;
