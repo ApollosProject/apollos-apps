999c93b0793f26601d048230cacb9ead3597cabc
Burke Shartsis <burke@differential.com>
2020-01-28 15:04:25 -0500

    update Providers import for tests

--- packages/apollos-ui-connected/src/SearchCardConnected/SearchCardConnected.tests.js
+++ packages/apollos-ui-connected/src/SearchCardConnected/SearchCardConnected.tests.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import { Providers, renderWithApolloData } from '../utils/testUtils';
+import { Providers } from '../utils/testUtils';
 
 import SearchCardConnected from '.';
 
