6e8548805637dec01e37e25b3bf683cb9f4ffa02
Burke Shartsis <burke@differential.com>
2020-02-03 16:19:41 -0500

    Allows Mapview button text to be configurable (#1323)
    * add `buttonTitle` prop to MapView component
    
    * clean up
    
    * update 🍪for new defaul `buttonTitle`
    
    * update tests for buttonTitle prop
    
    * story for buttonTitle prop

--- packages/apollos-ui-mapview/src/MapView/index.js
+++ packages/apollos-ui-mapview/src/MapView/index.js
@@ -43,6 +43,7 @@ const StyledCampusCard = styled(
 
 class MapView extends Component {
   static propTypes = {
+    buttonTitle: PropTypes.string,
     campuses: PropTypes.arrayOf(
       PropTypes.shape({
         latitude: PropTypes.number.isRequired,
@@ -84,6 +85,7 @@ class MapView extends Component {
 
   static defaultProps = {
     Marker,
+    buttonTitle: 'Confirm Selection',
   };
 
   constructor(props) {
@@ -188,7 +190,6 @@ class MapView extends Component {
   };
 
   render() {
-    const { onLocationSelect, isLoading, isLoadingSelectedCampus } = this.props;
     const interpolations = this.sortedCampuses.map((marker, index) => {
       const inputRange = [
         (index - 1) * this.cardWidth,
@@ -255,7 +256,7 @@ class MapView extends Component {
             {this.sortedCampuses.map((campus) => (
               <Touchable
                 key={campus.id}
-                onPress={() => onLocationSelect(campus)}
+                onPress={() => this.props.onLocationSelect(campus)}
               >
                 <StyledCampusCard
                   distance={campus.distanceFromLocation}
@@ -263,7 +264,7 @@ class MapView extends Component {
                   description={this.getCampusAddress(campus)}
                   images={[campus.image]}
                   cardWidth={this.cardWidth}
-                  isLoading={isLoading}
+                  isLoading={this.props.isLoading}
                 />
               </Touchable>
             ))}
@@ -271,13 +272,15 @@ class MapView extends Component {
           <MediaPlayerSpacer>
             <PaddedView>
               <Button
-                title="Select Campus"
+                title={this.props.buttonTitle}
                 pill={false}
-                type="secondary"
+                type={'secondary'}
                 onPress={() =>
-                  onLocationSelect(this.currentCampus || this.sortedCampuses[0])
+                  this.props.onLocationSelect(
+                    this.currentCampus || this.sortedCampuses[0]
+                  )
                 }
-                loading={isLoadingSelectedCampus}
+                loading={this.props.isLoadingSelectedCampus}
               />
             </PaddedView>
           </MediaPlayerSpacer>
