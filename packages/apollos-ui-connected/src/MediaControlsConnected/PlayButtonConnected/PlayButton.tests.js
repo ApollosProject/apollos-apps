5bdad94307da83097c0b04d34c51581c136f37ca
Jordan Wade <jjwade31@gmail.com>
2020-02-11 15:34:47 -0600

    Add tests for custom title and custom icon

--- packages/apollos-ui-connected/src/MediaControlsConnected/PlayButtonConnected/PlayButton.tests.js
+++ packages/apollos-ui-connected/src/MediaControlsConnected/PlayButtonConnected/PlayButton.tests.js
@@ -24,4 +24,40 @@ describe('PlayButton', () => {
     );
     expect(tree).toMatchSnapshot();
   });
+  it('should render with custom title', () => {
+    const tree = renderer.create(
+      <Providers>
+        <PlayButton
+          onPress={jest.fn()}
+          coverImageSources={[
+            {
+              uri:
+                'https://res.cloudinary.com/apollos/image/fetch/c_limit,f_auto,q_auto:eco,w_1600/https://apollosrock.newspring.cc/GetImage.ashx%3Fguid%3Dfe44d493-b69f-4721-83f7-7942c4f99125',
+              __typename: 'ImageMediaSource',
+            },
+          ]}
+          title={'CUSTOM_TITLE'}
+        />
+      </Providers>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  it('should render with custom play icon', () => {
+    const tree = renderer.create(
+      <Providers>
+        <PlayButton
+          onPress={jest.fn()}
+          coverImageSources={[
+            {
+              uri:
+                'https://res.cloudinary.com/apollos/image/fetch/c_limit,f_auto,q_auto:eco,w_1600/https://apollosrock.newspring.cc/GetImage.ashx%3Fguid%3Dfe44d493-b69f-4721-83f7-7942c4f99125',
+              __typename: 'ImageMediaSource',
+            },
+          ]}
+          icon={'play-solid'}
+        />
+      </Providers>
+    );
+    expect(tree).toMatchSnapshot();
+  });
 });
