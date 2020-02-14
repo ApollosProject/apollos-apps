513e89a201ab100cec91148899be3431b316941f
Burke Shartsis <burke@differential.com>
2020-02-14 12:42:03 -0500

    fix default export statement

--- packages/apollos-ui-mapview/src/MapViewConnected/index.js
+++ packages/apollos-ui-mapview/src/MapViewConnected/index.js
@@ -3,4 +3,4 @@ import MapViewConnected from './MapViewConnected';
 export GET_CAMPUS_LOCATIONS from './getCampusLocations';
 export CAMPUS_CHANGE from './campusChange';
 
-export default MapViewConnected from './MapViewConnected';
+export default MapViewConnected;
