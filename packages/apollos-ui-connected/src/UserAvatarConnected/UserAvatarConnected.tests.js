e2cf18931ddc27cc129905598c39c85e529b3093
Jordan Wade <jjwade31@gmail.com>
2020-02-17 10:55:58 -0600

    Update UserAvatarConnected tests to use cloudinary

--- packages/apollos-ui-connected/src/UserAvatarConnected/UserAvatarConnected.tests.js
+++ packages/apollos-ui-connected/src/UserAvatarConnected/UserAvatarConnected.tests.js
@@ -20,7 +20,8 @@ describe('UserAvatarConnected component', () => {
               __typename: 'Person',
               id: 'Person:123',
               photo: {
-                uri: 'https://www.placecage.com/300/300',
+                uri:
+                  'https://res.cloudinary.com/apollos/image/fetch/c_limit,f_auto,w_1600/https://apollosrock.newspring.cc/GetImage.ashx%3Fguid%3D31af1a61-360c-4b1e-8e62-45517c06a9a2',
                 __typename: 'ImageMedia',
               },
             },
@@ -34,7 +35,6 @@ describe('UserAvatarConnected component', () => {
         <UserAvatarConnected navigation={navigation} />
       </Providers>
     );
-
     expect(tree).toMatchSnapshot();
   });
 });
