8338e32ce4af7662225d1e056defddd92b55c361
Jordan Wade <jjwade31@gmail.com>
2020-02-17 13:56:26 -0600

    Fix Features connected test data

--- packages/apollos-ui-connected/src/FeaturesConnected/FeaturesConnected.tests.js
+++ packages/apollos-ui-connected/src/FeaturesConnected/FeaturesConnected.tests.js
@@ -16,7 +16,7 @@ const fragmentMatcher = new IntrospectionFragmentMatcher({
 });
 
 describe('FeaturesConnected', () => {
-  it('should render', async () => {
+  it('should renderss', async () => {
     const mock = {
       request: {
         query: GET_CONTENT_ITEM_FEATURES,
@@ -30,7 +30,7 @@ describe('FeaturesConnected', () => {
             id: 'WeekendContentItem:1',
             features: [
               {
-                id: 'TextFeature:2a5b59e962544d9dcece1917a754a1df',
+                id: 'TextFeature:1',
                 body: 'this is another, text feature',
                 sharing: {
                   message: 'this is another, text feature',
@@ -38,11 +38,10 @@ describe('FeaturesConnected', () => {
                 },
                 scriptures: [
                   {
-                    id:
-                      'Scripture:64d68f218913cf16a8b4aab91753e774db83c13fcfc8f8f7916525e4abec61da7185c04db7fe4f4e7fcd804d983e54c3afe0945b61491c894397c846a91004af',
+                    id: 'Scripture:2',
                     html:
                       '<p class="p"><span data-number="16" class="v">16</span><span class="wj">¶ For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.</span> </p>',
-                    reference: 'John 3:16',
+                    reference: 'Matthew 4:10',
                     copyright:
                       'PUBLIC DOMAIN except in the United Kingdom, where a Crown Copyright applies to printing the KJV. See http://www.cambridge.org/about-us/who-we-are/queens-printers-patent',
                     version: 'KJV',
@@ -52,20 +51,19 @@ describe('FeaturesConnected', () => {
                 __typename: 'TextFeature',
               },
               {
-                id: 'ScriptureFeature:f690b5379eff177c5e34075ff824bd6b',
-                body: 'this is another, text feature',
+                id: 'ScriptureFeature:3',
+                body: 'this is another, scripture feature',
                 sharing: {
                   message:
-                    '16For God so loved the world, that he gave his only born Son, that whoever believes in him should not perish, but have eternal life.   John 3:16',
+                    '16For God so loved the world, that he gave his only born Son, that whoever believes in him should not perish, but have eternal life.   Luke 1:2',
                   __typename: 'SharableFeature',
                 },
                 scriptures: [
                   {
-                    id:
-                      'Scripture:64d68f218913cf16a8b4aab91753e774db83c13fcfc8f8f7916525e4abec61da7185c04db7fe4f4e7fcd804d983e54c3afe0945b61491c894397c846a91004af',
+                    id: 'Scripture:4',
                     html:
                       '<p class="p"><span data-number="16" class="v">16</span><span class="wj">¶ For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.</span> </p>',
-                    reference: 'John 3:16',
+                    reference: 'Mark 1:12',
                     copyright:
                       'PUBLIC DOMAIN except in the United Kingdom, where a Crown Copyright applies to printing the KJV. See http://www.cambridge.org/about-us/who-we-are/queens-printers-patent',
                     version: 'KJV',
@@ -75,8 +73,8 @@ describe('FeaturesConnected', () => {
                 __typename: 'ScriptureFeature',
               },
               {
-                id: 'ScriptureFeature:0e4e3fb6e031c94579b231db2dc10c85',
-                body: 'this is another, text feature',
+                id: 'ScriptureFeature:4',
+                body: 'this is another, scripture feature',
                 sharing: {
                   message:
                     '16For God so loved the world, that he gave his only born Son, that whoever believes in him should not perish, but have eternal life.   John 3:16',
@@ -84,10 +82,9 @@ describe('FeaturesConnected', () => {
                 },
                 scriptures: [
                   {
-                    id:
-                      'Scripture:64d68f218913cf16a8b4aab91753e7748c6989b388d0a31256790b6b814122da033b13868d59fe5facedf2e3c14bf45c87ec34dc5a657c8d1d3da39ee1bc80d8',
+                    id: 'Scripture:5',
                     html:
-                      '<p class="p"><span data-number="16" class="v">16</span><span class="wj">For God so loved the world, that he gave his only born</span> <span class="wj">Son, that whoever believes in him should not perish, but have eternal life. </span> </p>',
+                      '<p class="p"><span data-number="16" class="v">16</span><span class="wj">For God so loved the world, that he gave his only born</span> <span class="wj">Son, that whoever believes in him should not perish, but have eternal life, forever. </span> </p>',
                     reference: 'John 3:16',
                     copyright: 'PUBLIC DOMAIN',
                     version: 'WEB',
