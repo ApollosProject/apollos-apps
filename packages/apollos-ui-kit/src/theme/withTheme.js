f8a9a61392087fe1ae4b9a489ee829cf2d1b7669
Vincent Wilson <vince@classyh.at>
2020-02-25 11:11:53 -0500

    Adds override support to withTheme

--- packages/apollos-ui-kit/src/theme/withTheme.js
+++ packages/apollos-ui-kit/src/theme/withTheme.js
@@ -1,20 +1,33 @@
 import PropTypes from 'prop-types';
 import { compose, mapProps, getContext, hoistStatics } from 'recompose';
+import { get } from 'lodash';
 
 import { THEME_PROPS } from './createTheme';
 
 const DEFAULT_MAPPER_FN = ({ theme } = {}) => ({ theme });
 
-export default function(mapperFn = DEFAULT_MAPPER_FN) {
+export default function(mapperFn = DEFAULT_MAPPER_FN, fqn) {
   return hoistStatics(
     compose(
       getContext({
         theme: PropTypes.shape(THEME_PROPS),
       }),
-      mapProps(({ theme, ...otherProps }) => ({
-        ...otherProps,
-        ...mapperFn({ theme, ...otherProps }),
-      }))
+      mapProps(({ theme, ...otherProps }) => {
+        const themeOverridesValue = fqn
+          ? get(theme, `overrides['${fqn}']`, {})
+          : {};
+
+        const themeOverrides =
+          typeof themeOverridesValue === 'function'
+            ? themeOverridesValue(otherProps)
+            : themeOverridesValue;
+
+        return {
+          ...otherProps,
+          ...mapperFn({ theme, ...otherProps }),
+          ...themeOverrides,
+        };
+      })
     )
   );
 }
---
+++ packages/apollos-ui-kit/src/theme/withTheme.tests.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { View } from 'react-native';
+
+import Providers from '../Providers';
+
+import withTheme from './withTheme';
+
+describe('the styled HOC', () => {
+  it('provides a theme', () => {
+    const StyledView = withTheme(({ theme }) => ({
+      style: {
+        backgroundColor: theme.colors.primary,
+      },
+      clickable: false,
+    }))(View);
+    const tree = renderer.create(
+      <Providers>
+        <StyledView />
+      </Providers>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  it('allows a theme to supply overrides', () => {
+    const StyledView = withTheme(
+      ({ theme }) => ({
+        style: {
+          backgroundColor: theme.colors.primary,
+        },
+        clickable: false,
+      }),
+      'StyledView'
+    )(View);
+    const overrides = {
+      StyledView: { style: { backgroundColor: 'green' }, clickable: true },
+    };
+    const tree = renderer.create(
+      <Providers themeInput={{ overrides }}>
+        <StyledView />
+      </Providers>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  it('allows a theme to supply override functions', () => {
+    const StyledView = withTheme(
+      ({ theme }) => ({
+        style: {
+          backgroundColor: theme.colors.primary,
+        },
+        clickable: false,
+      }),
+      'StyledView'
+    )(View);
+    const overrides = {
+      StyledView: () => ({ active }) => ({
+        style: { backgroundColor: active ? 'active-color' : 'in-active-color' },
+        clickable: active,
+      }),
+    };
+    const tree = renderer.create(
+      <Providers themeInput={{ overrides }}>
+        <StyledView active />
+        <StyledView />
+      </Providers>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
