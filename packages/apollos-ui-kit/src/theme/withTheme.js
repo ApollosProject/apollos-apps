74adae9159899366bb535cd80c89306176cd3b69
Vincent Wilson <vince@classyh.at>
2020-02-27 15:03:01 -0500

    With theme works with overrides (#1360)
    * v1.3.0-alpha.8
    
    * Adds override support to withTheme
    
    * Revert "v1.3.0-alpha.8"
    
    This reverts commit b2ef3ad015157f6115fdce867e501d673cb9c229.
    
    * Ensure withTheme will cleanely merge style
    
    Co-authored-by: Burke Shartsis <burke@burkeshartsis.com>

--- packages/apollos-ui-kit/src/theme/withTheme.js
+++ packages/apollos-ui-kit/src/theme/withTheme.js
@@ -1,20 +1,47 @@
 import PropTypes from 'prop-types';
 import { compose, mapProps, getContext, hoistStatics } from 'recompose';
+import { get } from 'lodash';
 
+import mergeStyles from '../styled/mergeStyles';
 import { THEME_PROPS } from './createTheme';
 
 const DEFAULT_MAPPER_FN = ({ theme } = {}) => ({ theme });
 
-export default function(mapperFn = DEFAULT_MAPPER_FN) {
+export default function(mapperFn = DEFAULT_MAPPER_FN, fqn) {
   return hoistStatics(
     compose(
       getContext({
         theme: PropTypes.shape(THEME_PROPS),
       }),
-      mapProps(({ theme, ...otherProps }) => ({
-        ...otherProps,
-        ...mapperFn({ theme, ...otherProps }),
-      }))
+      mapProps(({ theme, ...otherProps }) => {
+        const themeOverridesValue = fqn
+          ? get(theme, `overrides['${fqn}']`, {})
+          : {};
+
+        const themeOverrides =
+          typeof themeOverridesValue === 'function'
+            ? themeOverridesValue(otherProps)
+            : themeOverridesValue;
+
+        const withThemeProps = mapperFn({ theme, ...otherProps });
+
+        let mergedOverides = {};
+        // We need to deep merge te style props. Other nested props are not currently merged
+        if (themeOverrides.style && withThemeProps.style) {
+          const mergedStyles = mergeStyles(
+            withThemeProps.style,
+            themeOverrides.style
+          );
+          mergedOverides = { style: mergedStyles };
+        }
+
+        return {
+          ...otherProps,
+          ...withThemeProps,
+          ...themeOverrides,
+          ...mergedOverides,
+        };
+      })
     )
   );
 }
---
+++ packages/apollos-ui-kit/src/theme/withTheme.tests.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { View } from 'react-native';
+
+import Providers from '../Providers';
+
+import withTheme from './withTheme';
+
+describe('the styled HOC', () => {
+  it('provides a theme', () => {
+    const StyledView = withTheme(({ theme }) => ({
+      style: {
+        backgroundColor: theme.colors.primary,
+      },
+      clickable: false,
+    }))(View);
+    const tree = renderer.create(
+      <Providers>
+        <StyledView />
+      </Providers>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  it('allows a theme to supply overrides', () => {
+    const StyledView = withTheme(
+      ({ theme }) => ({
+        style: {
+          backgroundColor: theme.colors.primary,
+        },
+        clickable: false,
+      }),
+      'StyledView'
+    )(View);
+    const overrides = {
+      StyledView: { style: { backgroundColor: 'green' }, clickable: true },
+    };
+    const tree = renderer.create(
+      <Providers themeInput={{ overrides }}>
+        <StyledView />
+      </Providers>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  it('allows a theme to supply override functions', () => {
+    const StyledView = withTheme(
+      ({ theme }) => ({
+        style: {
+          backgroundColor: theme.colors.primary,
+        },
+        clickable: false,
+      }),
+      'StyledView'
+    )(View);
+    const overrides = {
+      StyledView: () => ({ active }) => ({
+        style: { backgroundColor: active ? 'active-color' : 'in-active-color' },
+        clickable: active,
+      }),
+    };
+    const tree = renderer.create(
+      <Providers themeInput={{ overrides }}>
+        <StyledView active />
+        <StyledView />
+      </Providers>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  it('merges nested styles from withTheme and an overide', () => {
+    const StyledView = withTheme(
+      ({ theme }) => ({
+        style: {
+          backgroundColor: theme.colors.primary,
+          color: 'blue',
+        },
+        clickable: false,
+      }),
+      'StyledView'
+    )(View);
+    const overrides = {
+      StyledView: {
+        style: {
+          backgroundColor: 'active-color',
+          borderRadius: 7,
+        },
+        clickable: false,
+      },
+    };
+    const tree = renderer.create(
+      <Providers themeInput={{ overrides }}>
+        <StyledView />
+      </Providers>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
