945a24bd3a3d9c10c95ccf9ae465f6b6e94cf888
Burke Shartsis <burke@differential.com>
2020-02-28 14:00:31 -0500

    mostly finished ContentSingleFeatures component

--- packages/apollos-ui-connected/src/ContentSingleFeaturesConnected/ContentSingleFeatures.js
+++ packages/apollos-ui-connected/src/ContentSingleFeaturesConnected/ContentSingleFeatures.js
@@ -1,37 +1,53 @@
-import React from 'react';
+import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 import { H3, PaddedView } from '@apollosproject/ui-kit';
 import TextFeature from './TextFeature';
 import ScriptureFeature from './ScriptureFeature';
 
-const FEATURE_MAP = {
+const featureMap = {
   TextFeature,
   ScriptureFeature,
 };
 
-const ContentSingleFeatures = ({ contentId, isLoading }) => (
-  <PaddedView horizontal={false}>
-    <PaddedView vertical={false}>
-      <H3 padded>Engage</H3>
+const ContentSingleFeatures = memo(
+  ({ contentId, features, isLoading, title }) => (
+    <PaddedView horizontal={false}>
+      <PaddedView vertical={false}>
+        <H3 padded>{title}</H3>
+      </PaddedView>
+      {features.map(({ __typename, ...feature }) => {
+        const Feature = featureMap[__typename];
+        // if (!Feature) return null;
+        return (
+          (
+            <Feature
+              key={feature.id}
+              {...feature}
+              contentId={contentId}
+              isLoading={isLoading}
+            />
+          ) || null
+        );
+      })}
     </PaddedView>
-    {features.map(({ __typename, ...feature }) => {
-      const Feature = FEATURE_MAP[__typename];
-      if (!Feature) return null;
-      return (
-        <Feature
-          key={feature.id}
-          {...feature}
-          contentId={contentId}
-          isLoading={isLoading}
-        />
-      );
-    })}
-  </PaddedView>
+  )
 );
 
 ContentSingleFeatures.propTypes = {
   contentId: PropTypes.string,
+  features: PropTypes.shape({
+    id: PropTypes.string().isRequired,
+    order: PropTypes.string(),
+    __typename: PropTypes.string().isRequired,
+  }),
   isLoading: PropTypes.bool,
+  title: PropTypes.string,
 };
+
+ContentSingleFeatures.defaultProps = {
+  title: 'Engage',
+};
+
+ContentSingleFeatures.displayName = 'ContentSingleFeatures';
 
 export default ContentSingleFeatures;
