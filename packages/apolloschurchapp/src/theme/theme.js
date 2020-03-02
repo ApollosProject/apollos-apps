edc2e6df9a3819685df6de2971b822ad5a215f57
Burke Shartsis <burke@differential.com>
2020-03-02 15:21:32 -0500

    update overrides documentation in custom app theme file

--- packages/apolloschurchapp/src/theme/theme.js
+++ packages/apolloschurchapp/src/theme/theme.js
@@ -28,16 +28,22 @@
 /* Base overlays. These are used as configuration for LinearGradients across the app */
 // export const overlays = () => ({});
 
-/* Overrides allow you to override the styles of any component styled using the `styled` HOC.
- * For example, this component:
- * const SomeComponent = styled({ margin: 10, padding: 20 }, 'SomeComponent');
- * can have its styles overriden by including in overrides:
+/* Overrides allow you to override the styles of any component styled using the `styled` HOC. You
+ * can also override the props of any component using the `withTheme` HOC. See examples below:
+ * ```const StyledComponent = styled({ margin: 10, padding: 20 }, 'StyledComponent');
+ *    const PropsComponent = withTheme(({ theme }) => ({ fill: theme.colors.primary }), 'PropsComponent');
+ * ```
+ * These componnents can have their styles/props overriden by including the following overrides:
  * {
  *   overides: {
- *     SomeComponent: {
+ *     StyledComponent: {
  *       margin: 5,
  *       padding: 15,
  *     },
+ *     // #protip: you even have access 👇to component props! This applies to style overrides too 💥
+ *     PropsComponent: () => ({ theme, isActive }) => ({
+ *       fill: isActive ? theme.colors.secondary : theme.colors.primary,
+ *     }),
  *   },
  * }
  */
