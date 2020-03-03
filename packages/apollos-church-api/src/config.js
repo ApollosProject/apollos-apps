16886a615d72a4bac0620688ee0c646096aafc23
Vincent Wilson <vince@classyh.at>
2020-03-03 14:55:55 -0500

    Remove template packages

--- packages/apollos-church-api/src/config.js
+++
@@ -1,7 +0,0 @@
-import path from 'path';
-import dotenv from 'dotenv/config'; // eslint-disable-line
-import ApollosConfig from '@apollosproject/config';
-
-ApollosConfig.loadYaml({
-  configPath: path.join(__dirname, '..', 'config.yml'),
-});
