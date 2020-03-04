e9d0f150b2904fb9a662a6ff501a140f6dd5963d
Jordan Wade <jjwade31@gmail.com>
2020-03-04 09:22:24 -0600

    Move features to ui connected (#1345)
    * Initial move Features to ui-connected
    
    * Fix LiveConsumer PropType warning
    
    * Fix `simple (heuristic) fragment matcher` error
    
    * Fix Features connected test data
    
    * Change name to ContentSingleFeaturesConnected
    
    * WIP: create pure ContentSingleFeatures component
    
    * add isLoading prop to ContentSingleFeatures component
    
    * mostly finished ContentSingleFeatures component
    
    * refactored ContentSingleFeaturesConnected to use pure child.
    
    - moves view logic away from data concerns
    
    * clean up
    
    * a test that renders stuff
    
    * fix tests
    
    * initial 🍪
    
    * test over title prop
    
    * test over loading state
    
    * add comment over why we dont have loading states enabled
    
    * update all the imports so everything actually works!
    
    * Allow developers to pass down a custom feature map
    
    * Move fragements into fragments package
    
    * Fix linter error I created
    
    Co-authored-by: Vincent Wilson <vince@classyh.at>
    Co-authored-by: Burke Shartsis <burke@burkeshartsis.com>

---
+++ packages/apollos-ui-connected/src/ContentSingleFeaturesConnected/ContentSingleFeaturesConnected.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Query } from 'react-apollo';
+import { ErrorCard } from '@apollosproject/ui-kit';
+import { get } from 'lodash';
+
+import GET_CONTENT_ITEM_FEATURES from './getContentItemFeatures';
+import ContentSingleFeatures from './ContentSingleFeatures';
+
+const ContentSingleFeaturesConnected = ({ Component, contentId, ...props }) => {
+  if (!contentId) return null;
+
+  return (
+    <Query
+      query={GET_CONTENT_ITEM_FEATURES}
+      fetchPolicy="cache-and-network"
+      variables={{ contentId }}
+    >
+      {({ data: { node } = {}, loading, error }) => {
+        if (error) return <ErrorCard error={error} />;
+        // TODO: set an optimistic response for this query to return a visually appeallying empty query so we can enable loading states
+        if (loading) return null;
+
+        const features = get(node, 'features', []);
+        if (!features || !features.length) return null;
+
+        return (
+          <Component contentId={contentId} features={features} {...props} />
+        );
+      }}
+    </Query>
+  );
+};
+
+ContentSingleFeaturesConnected.propTypes = {
+  Component: PropTypes.node,
+  contentId: PropTypes.string,
+};
+
+ContentSingleFeaturesConnected.defaultProps = {
+  Component: ContentSingleFeatures,
+};
+
+export default ContentSingleFeaturesConnected;
---
+++ packages/apollos-ui-connected/src/ContentSingleFeaturesConnected/index.js
@@ -0,0 +1,9 @@
+import ContentSingleFeaturesConnected from './ContentSingleFeaturesConnected';
+
+export ContentSingleFeatures from './ContentSingleFeatures';
+export GET_CONTENT_ITEM_FEATURES from './getContentItemFeatures';
+
+export ScriptureFeature from './ScriptureFeature';
+export TextFeature from './TextFeature';
+
+export default ContentSingleFeaturesConnected;
