7ef21951976a4fe8be88b762de2a3df5d1a57b41
Michael Neeley <micneeley14@gmail.com>
2020-03-17 11:15:58 -0400

    HOF to Class

--- packages/apollos-ui-connected/src/HorizontalLikedContentFeedConnected/HorizontalLikedContentFeed/index.js
+++ packages/apollos-ui-connected/src/HorizontalLikedContentFeedConnected/HorizontalLikedContentFeed/index.js
@@ -58,7 +58,7 @@ class HorizontalLikedContentFeed extends Component {
     content: PropTypes.arrayOf(
       PropTypes.any // this component doesn't care about the shape of `node`, just that it exists
     ),
-    Component: PropTypes.node,
+    Component: PropTypes.element,
   };
 
   static defaultProps = {
@@ -71,17 +71,20 @@ class HorizontalLikedContentFeed extends Component {
     coverImage: [],
   };
 
-  titleImageItem = (TileComponent) => ({ item }) => (
-    <TouchableScale
-      onPress={() => {
-        this.props.navigation.push('ContentSingle', {
-          itemId: item.id,
-        });
-      }}
-    >
-      <TileComponent isLoading={item.isLoading} contentId={item.id} />
-    </TouchableScale>
-  );
+  titleImageItem = ({ item }) => {
+    const { Component: TileComponent } = this.props;
+    return (
+      <TouchableScale
+        onPress={() => {
+          this.props.navigation.push('ContentSingle', {
+            itemId: item.id,
+          });
+        }}
+      >
+        <TileComponent isLoading={item.isLoading} contentId={item.id} />
+      </TouchableScale>
+    );
+  };
 
   render() {
     const { isLoading, name, navigation, content = [] } = this.props;
