7fac0a14d1b2dca22b787cc04bd53a4e1df776ed
Burke Shartsis <burke@differential.com>
2020-04-13 16:45:03 -0400

    expose onPressActionItem and onPressCardActionButton props

--- packages/apollos-ui-connected/src/FeaturesFeedConnected/index.js
+++ packages/apollos-ui-connected/src/FeaturesFeedConnected/index.js
@@ -10,125 +10,89 @@ import VerticalCardListFeature from '../VerticalCardListFeature';
 
 import GET_FEED_FEATURES from './getFeedFeatures';
 
-const handleOnPressActionItem = ({ action, navigation, relatedNode }) => {
-  if (action === 'READ_CONTENT') {
-    navigation.navigate('ContentSingle', {
-      itemId: relatedNode.id,
-      transitionKey: 2,
-    });
-  }
-  if (action === 'READ_EVENT') {
-    navigation.navigate('Event', {
-      eventId: relatedNode.id,
-      transitionKey: 2,
-    });
-  }
-};
-
-// const handleOnPressCardActionButton = ({ id, navigation, title }) =>
-//   navigation.navigate('ContentFeed', {
-//     itemId: id,
-//     itemTitle: title,
-//   });
-
-const FeaturesFeedConnected = memo(({ navigation }) => (
-  <Query query={GET_FEED_FEATURES} fetchPolicy="cache-and-network">
-    {({ data: features, loading }) =>
-      get(features, 'userFeedFeatures', []).map(
-        ({
-          actions,
-          cards,
-          id,
-          isFeatured,
-          subtitle,
-          title,
-          __typename,
-          ...props
-        }) => {
-          switch (__typename) {
-            case 'ActionListFeature':
-              return (
-                <ActionListFeature
-                  // TODO: How can we better handle generating a loading state.
-                  actions={actions}
-                  isLoading={loading}
-                  onPressActionItem={({ action, relatedNode }) =>
-                    handleOnPressActionItem({
-                      action,
-                      navigation,
-                      relatedNode,
-                    })
-                  }
-                  // onPressActionListButton={() =>
-                  //   handleOnPressCardActionButton({
-                  //     id,
-                  //     navigation,
-                  //     title,
-                  //   })
-                  // }
-                  subtitle={subtitle}
-                  title={title}
-                  {...props}
-                />
-              );
-            case 'HorizontalCardListFeature':
-              return (
-                <HorizontalCardListFeature
-                  cards={cards.map(({ actionIcon, ...card }) => ({
-                    ...card,
-                    ...(actionIcon != null
-                      ? { actionIcon: card.actionIcon }
-                      : {}),
-                    coverImage: get(card, 'coverImage.sources', undefined),
-                    __typename: card.relatedNode.__typename,
-                    id: card.relatedNode.id,
-                  }))}
-                  isLoading={loading}
-                  listKey={id}
-                  onPressItem={({ action, relatedNode }) =>
-                    handleOnPressActionItem({ action, relatedNode, navigation })
-                  }
-                  subtitle={subtitle}
-                />
-              );
-            case 'VerticalCardListFeature': // eslint-disable-line no-case-declarations
-              const Component = isFeatured
-                ? CampaignItemListFeature
-                : VerticalCardListFeature;
-              return (
-                <Component
-                  cards={cards.map(({ actionIcon, ...card }) => ({
-                    ...card,
-                    ...(actionIcon != null
-                      ? { actionIcon: card.actionIcon }
-                      : {}),
-                    coverImage: get(card, 'coverImage.sources', undefined),
-                    __typename: card.relatedNode.__typename,
-                  }))}
-                  isLoading={loading}
-                  listKey={id}
-                  onPressItem={({ action, relatedNode }) =>
-                    handleOnPressActionItem({ action, relatedNode, navigation })
-                  }
-                  subtitle={subtitle}
-                  title={title}
-                />
-              );
-            default:
-              return null;
+const FeaturesFeedConnected = memo(
+  ({ onPressActionItem, onPressCardActionButton }) => (
+    <Query query={GET_FEED_FEATURES} fetchPolicy="cache-and-network">
+      {({ data: features, loading }) =>
+        get(features, 'userFeedFeatures', []).map(
+          ({
+            actions,
+            cards,
+            id,
+            isFeatured,
+            subtitle,
+            title,
+            __typename,
+            ...props
+          }) => {
+            switch (__typename) {
+              case 'ActionListFeature':
+                return (
+                  <ActionListFeature
+                    // TODO: How can we better handle generating a loading state.
+                    actions={actions}
+                    isLoading={loading}
+                    onPressActionItem={onPressActionItem}
+                    onPressActionListButton={onPressCardActionButton}
+                    subtitle={subtitle}
+                    title={title}
+                    {...props}
+                  />
+                );
+              case 'HorizontalCardListFeature':
+                return (
+                  <HorizontalCardListFeature
+                    cards={cards.map(({ actionIcon, ...card }) => ({
+                      ...card,
+                      ...(actionIcon != null
+                        ? { actionIcon: card.actionIcon }
+                        : {}),
+                      coverImage: get(card, 'coverImage.sources', undefined),
+                      __typename: card.relatedNode.__typename,
+                      id: card.relatedNode.id,
+                    }))}
+                    isLoading={loading}
+                    listKey={id}
+                    onPressItem={onPressActionItem}
+                    subtitle={subtitle}
+                  />
+                );
+              case 'VerticalCardListFeature': // eslint-disable-line no-case-declarations
+                const Component = isFeatured
+                  ? CampaignItemListFeature
+                  : VerticalCardListFeature;
+                return (
+                  <Component
+                    cards={cards.map(({ actionIcon, ...card }) => ({
+                      ...card,
+                      ...(actionIcon != null
+                        ? { actionIcon: card.actionIcon }
+                        : {}),
+                      coverImage: get(card, 'coverImage.sources', undefined),
+                      __typename: card.relatedNode.__typename,
+                    }))}
+                    isLoading={loading}
+                    listKey={id}
+                    onPressItem={onPressActionItem}
+                    subtitle={subtitle}
+                    title={title}
+                  />
+                );
+              default:
+                return null;
+            }
           }
-        }
-      )
-    }
-  </Query>
-));
+        )
+      }
+    </Query>
+  )
+);
 
 FeaturesFeedConnected.displayName = 'Features';
 
 FeaturesFeedConnected.proptypes = {
-  navigation: PropTypes.shape({
-    navigate: PropTypes.func,
-  }),
+  onPressActionItem: PropTypes.func,
+  onPressCardActionButton: PropTypes.func,
 };
 
 export default FeaturesFeedConnected;
