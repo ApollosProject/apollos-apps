3238f167c9a996d26907d51a605ec4780e518f8e
Burke Shartsis <burke@differential.com>
2020-04-13 17:08:38 -0400

    use FeaturesFeed component and expose Component prop

--- packages/apollos-ui-connected/src/FeaturesFeedConnected/index.js
+++ packages/apollos-ui-connected/src/FeaturesFeedConnected/index.js
@@ -1,89 +1,16 @@
 import React, { memo } from 'react';
 import { Query } from 'react-apollo';
-import { get } from 'lodash';
 import PropTypes from 'prop-types';
 
-import ActionListFeature from '../ActionListFeature';
-import CampaignItemListFeature from '../CampaignItemListFeature';
-import HorizontalCardListFeature from '../HorizontalCardListFeature';
-import VerticalCardListFeature from '../VerticalCardListFeature';
-
 import GET_FEED_FEATURES from './getFeedFeatures';
+import FeaturesFeed from './FeaturesFeed';
 
 const FeaturesFeedConnected = memo(
-  ({ onPressActionItem, onPressCardActionButton }) => (
+  ({ Component, onPressActionItem, onPressCardActionButton }) => (
     <Query query={GET_FEED_FEATURES} fetchPolicy="cache-and-network">
-      {({ data: features, loading }) =>
-        get(features, 'userFeedFeatures', []).map(
-          ({
-            actions,
-            cards,
-            id,
-            isFeatured,
-            subtitle,
-            title,
-            __typename,
-            ...props
-          }) => {
-            switch (__typename) {
-              case 'ActionListFeature':
-                return (
-                  <ActionListFeature
-                    // TODO: How can we better handle generating a loading state.
-                    actions={actions}
-                    isLoading={loading}
-                    onPressActionItem={onPressActionItem}
-                    onPressActionListButton={onPressCardActionButton}
-                    subtitle={subtitle}
-                    title={title}
-                    {...props}
-                  />
-                );
-              case 'HorizontalCardListFeature':
-                return (
-                  <HorizontalCardListFeature
-                    cards={cards.map(({ actionIcon, ...card }) => ({
-                      ...card,
-                      ...(actionIcon != null
-                        ? { actionIcon: card.actionIcon }
-                        : {}),
-                      coverImage: get(card, 'coverImage.sources', undefined),
-                      __typename: card.relatedNode.__typename,
-                      id: card.relatedNode.id,
-                    }))}
-                    isLoading={loading}
-                    listKey={id}
-                    onPressItem={onPressActionItem}
-                    subtitle={subtitle}
-                  />
-                );
-              case 'VerticalCardListFeature': // eslint-disable-line no-case-declarations
-                const Component = isFeatured
-                  ? CampaignItemListFeature
-                  : VerticalCardListFeature;
-                return (
-                  <Component
-                    cards={cards.map(({ actionIcon, ...card }) => ({
-                      ...card,
-                      ...(actionIcon != null
-                        ? { actionIcon: card.actionIcon }
-                        : {}),
-                      coverImage: get(card, 'coverImage.sources', undefined),
-                      __typename: card.relatedNode.__typename,
-                    }))}
-                    isLoading={loading}
-                    listKey={id}
-                    onPressItem={onPressActionItem}
-                    subtitle={subtitle}
-                    title={title}
-                  />
-                );
-              default:
-                return null;
-            }
-          }
-        )
-      }
+      {({ data: features, loading }) => (
+        <Component features={features} isLoading={loading} />
+      )}
     </Query>
   )
 );
@@ -91,8 +18,17 @@ const FeaturesFeedConnected = memo(
 FeaturesFeedConnected.displayName = 'Features';
 
 FeaturesFeedConnected.proptypes = {
+  Component: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.func,
+    PropTypes.object, // type check for React fragments
+  ]),
   onPressActionItem: PropTypes.func,
   onPressCardActionButton: PropTypes.func,
+};
+
+FeaturesFeedConnected.defaultProps = {
+  Component: FeaturesFeed,
 };
 
 export default FeaturesFeedConnected;
