0529e9a285a10a20516c0178c7fe3457732088d2
Burke Shartsis <burke@differential.com>
2020-04-14 12:32:08 -0400

    per PR feedback remove currently unsupported onPressActionListButton prop

--- packages/apollos-ui-connected/src/FeaturesFeedConnected/FeaturesFeedConnected.js
+++ packages/apollos-ui-connected/src/FeaturesFeedConnected/FeaturesFeedConnected.js
@@ -5,20 +5,17 @@ import PropTypes from 'prop-types';
 import GET_FEED_FEATURES from './getFeedFeatures';
 import FeaturesFeed from './FeaturesFeed';
 
-const FeaturesFeedConnected = memo(
-  ({ Component, onPressActionItem, onPressCardActionButton }) => (
-    <Query query={GET_FEED_FEATURES} fetchPolicy="cache-and-network">
-      {({ data: features, loading }) => (
-        <Component
-          features={features}
-          isLoading={loading}
-          onPressActionItem={onPressActionItem}
-          onPressCardActionButton={onPressCardActionButton}
-        />
-      )}
-    </Query>
-  )
-);
+const FeaturesFeedConnected = memo(({ Component, onPressActionItem }) => (
+  <Query query={GET_FEED_FEATURES} fetchPolicy="cache-and-network">
+    {({ data: features, loading }) => (
+      <Component
+        features={features}
+        isLoading={loading}
+        onPressActionItem={onPressActionItem}
+      />
+    )}
+  </Query>
+));
 
 FeaturesFeedConnected.proptypes = {
   Component: PropTypes.oneOfType([
@@ -27,7 +24,6 @@ FeaturesFeedConnected.proptypes = {
     PropTypes.object, // type check for React fragments
   ]),
   onPressActionItem: PropTypes.func,
-  onPressCardActionButton: PropTypes.func,
 };
 
 FeaturesFeedConnected.defaultProps = {
