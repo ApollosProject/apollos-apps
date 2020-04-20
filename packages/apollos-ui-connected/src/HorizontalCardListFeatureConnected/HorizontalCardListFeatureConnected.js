b6b5668e5144a80ba040a884220a9fee345fa209
Burke Shartsis <burke@differential.com>
2020-04-20 15:50:55 -0400

    use arrow functions 🤷‍♂️

--- packages/apollos-ui-connected/src/HorizontalCardListFeatureConnected/HorizontalCardListFeatureConnected.js
+++ packages/apollos-ui-connected/src/HorizontalCardListFeatureConnected/HorizontalCardListFeatureConnected.js
@@ -6,36 +6,34 @@ import PropTypes from 'prop-types';
 import HorizontalCardListFeature from './HorizontalCardListFeature';
 import GET_HORIZONTAL_CARD_LIST_FEATURE from './getHorizontalCardListFeature';
 
-function HorizontalCardListFeatureConnected({
-  featureId,
+const HorizontalCardListFeatureConnected = ({
   Component,
+  featureId,
   isLoading,
   ...props
-}) {
-  return (
-    <Query query={GET_HORIZONTAL_CARD_LIST_FEATURE} variables={{ featureId }}>
-      {({ data, loading }) => (
-        <Component
-          {...get(data, 'node')}
-          cards={get(data, 'node.cards', []).map(({ actionIcon, ...card }) => ({
-            ...card,
-            ...(actionIcon != null ? { actionIcon: card.actionIcon } : {}), // temp hack because ContentCard doesn't handle null action icon well
-            coverImage: get(card, 'coverImage.sources', undefined),
-            __typename: card.relatedNode.__typename,
-            id: card.relatedNode.id,
-          }))}
-          {...props}
-          isLoading={loading || isLoading}
-        />
-      )}
-    </Query>
-  );
-}
+}) => (
+  <Query query={GET_HORIZONTAL_CARD_LIST_FEATURE} variables={{ featureId }}>
+    {({ data, loading }) => (
+      <Component
+        {...get(data, 'node')}
+        cards={get(data, 'node.cards', []).map(({ actionIcon, ...card }) => ({
+          ...card,
+          ...(actionIcon != null ? { actionIcon: card.actionIcon } : {}), // temp hack because ContentCard doesn't handle null action icon well
+          coverImage: get(card, 'coverImage.sources', undefined),
+          __typename: card.relatedNode.__typename,
+          id: card.relatedNode.id,
+        }))}
+        {...props}
+        isLoading={loading || isLoading}
+      />
+    )}
+  </Query>
+);
 
 HorizontalCardListFeatureConnected.propTypes = {
   Component: PropTypes.node,
-  isLoading: PropTypes.bool,
   featureId: PropTypes.string.isRequired,
+  isLoading: PropTypes.bool,
 };
 
 HorizontalCardListFeatureConnected.defaultProps = {
--- packages/apollos-ui-connected/src/VerticalCardListFeatureConnected/VerticalCardListFeatureConnected.js
+++ packages/apollos-ui-connected/src/VerticalCardListFeatureConnected/VerticalCardListFeatureConnected.js
@@ -6,39 +6,35 @@ import CampaignItemListFeature from '../CampaignItemListFeature';
 import VerticalCardListFeature from './VerticalCardListFeature';
 import GET_VERTICAL_CARD_LIST_FEATURE from './getVerticalCardListFeature';
 
-function VerticalCardListFeatureConnected({
+const VerticalCardListFeatureConnected = ({
   featureId,
   Component,
   FeaturedComponent,
   isFeatured,
   isLoading,
   ...props
-}) {
-  return (
-    <Query query={GET_VERTICAL_CARD_LIST_FEATURE} variables={{ featureId }}>
-      {({ data, loading }) => {
-        const featured = get(data, 'node.isFeatured') || isFeatured;
-        const ComponentToRender = featured ? FeaturedComponent : Component;
-        return (
-          <ComponentToRender
-            {...get(data, 'node')}
-            cards={get(data, 'node.cards', []).map(
-              ({ actionIcon, ...card }) => ({
-                ...card,
-                ...(actionIcon != null ? { actionIcon: card.actionIcon } : {}), // temp hack because ContentCard doesn't handle null action icon well
-                coverImage: get(card, 'coverImage.sources', undefined),
-                __typename: card.relatedNode.__typename,
-                id: card.relatedNode.id,
-              })
-            )}
-            {...props}
-            isLoading={loading || isLoading}
-          />
-        );
-      }}
-    </Query>
-  );
-}
+}) => (
+  <Query query={GET_VERTICAL_CARD_LIST_FEATURE} variables={{ featureId }}>
+    {({ data, loading }) => {
+      const featured = get(data, 'node.isFeatured') || isFeatured;
+      const ComponentToRender = featured ? FeaturedComponent : Component;
+      return (
+        <ComponentToRender
+          {...get(data, 'node')}
+          cards={get(data, 'node.cards', []).map(({ actionIcon, ...card }) => ({
+            ...card,
+            ...(actionIcon != null ? { actionIcon: card.actionIcon } : {}), // temp hack because ContentCard doesn't handle null action icon well
+            coverImage: get(card, 'coverImage.sources', undefined),
+            __typename: card.relatedNode.__typename,
+            id: card.relatedNode.id,
+          }))}
+          {...props}
+          isLoading={loading || isLoading}
+        />
+      );
+    }}
+  </Query>
+);
 
 VerticalCardListFeatureConnected.propTypes = {
   Component: PropTypes.node,
@@ -53,5 +49,8 @@ VerticalCardListFeatureConnected.defaultProps = {
   FeaturedComponent: CampaignItemListFeature,
   isFeatured: false,
 };
+
+VerticalCardListFeatureConnected.displayName =
+  'VerticalCardListFeatureConnected';
 
 export default VerticalCardListFeatureConnected;
