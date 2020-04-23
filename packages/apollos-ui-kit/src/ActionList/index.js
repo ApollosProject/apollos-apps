2a3154b6aaa82051141d4f80e5383ea9d6a26985
Burke Shartsis <burke@differential.com>
2020-04-23 12:04:24 -0400

    remove comment eslint ignore and use destructuring.

--- packages/apollos-ui-kit/src/ActionList/index.js
+++ packages/apollos-ui-kit/src/ActionList/index.js
@@ -64,7 +64,7 @@ class ActionList extends PureComponent {
       header,
     } = this.props;
 
-    const RenderAsCard = this.RenderAsCard; // eslint-disable-line prefer-destructuring
+    const { RenderAsCard } = this.RenderAsCard;
 
     return (
       <RenderAsCard>
