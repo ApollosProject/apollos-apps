c23b4f9e678481c691a3cd25ded847a9d2a7f57f
Vincent Wilson <vince@classyh.at>
2020-04-26 21:04:44 -0400

    Fix linter

--- packages/apollos-ui-connected/src/safeOpenUrl/index.stories.js
+++ packages/apollos-ui-connected/src/safeOpenUrl/index.stories.js
@@ -1,4 +1,4 @@
-// eslint-disable react-native/no-inline-styles
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import { storiesOf } from '@apollosproject/ui-storybook';
 
