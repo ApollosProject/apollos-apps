20278a6771214585690d0a0b77e0a15954b55964
Burke Shartsis <burke@differential.com>
2020-04-29 17:03:34 -0400

    dulicate and rename ContentCardConnected to ListItemCard

---
+++ packages/apollos-ui-connected/src/ListItemCard/index.js
@@ -0,0 +1,6 @@
+import ListItemCard from './ListItemCard';
+
+export listItemCardMapper from './listItemCardMapper';
+export GET_CONTENT_CARD from './getContentCard';
+
+export default ListItemCard;
