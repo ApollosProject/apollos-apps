39ab74027dd64db8c5037d1ea04ff7d2bae76b70
Burke Shartsis <burke@differential.com>
2020-04-30 16:58:16 -0400

    clean up imports and exports

--- packages/apollos-ui-connected/src/ContentCardConnected/index.js
+++ packages/apollos-ui-connected/src/ContentCardConnected/index.js
@@ -1,5 +1,9 @@
 import ContentCardConnected from './ContentCardConnected';
 
+export {
+  listItemCardMapper as contentCardComponentMapper,
+} from '../ListItemCard';
+
 export GET_CONTENT_CARD from './getContentCard';
 
 export default ContentCardConnected;
--- packages/apollos-ui-connected/src/ListItemCard/index.js
+++ packages/apollos-ui-connected/src/ListItemCard/index.js
@@ -1,6 +1,5 @@
 import ListItemCard from './ListItemCard';
 
 export listItemCardMapper from './listItemCardMapper';
-export GET_CONTENT_CARD from './getContentCard';
 
 export default ListItemCard;
