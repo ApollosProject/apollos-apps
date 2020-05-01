ece9429866270a9a63ae76adc406c0e1b270516e
Burke Shartsis <burke@differential.com>
2020-05-01 12:12:00 -0400

    clean up/settle on approach

--- packages/apollos-ui-connected/src/ListItemCard/ListItemCard.js
+++ packages/apollos-ui-connected/src/ListItemCard/ListItemCard.js
@@ -1,24 +1,11 @@
 import React, { memo } from 'react';
-import { get } from 'lodash';
 import PropTypes from 'prop-types';
-
-import { DefaultCard, HighlightCard } from '@apollosproject/ui-kit';
 
 import listItemCardMapper from './listItemCardMapper';
 
-const ListItemCard = memo(({ Component, ...props }) => {
-  if (Component) return <Component {...props} />;
-
-  switch (get(props, '__typename')) {
-    case 'MediaContentItem':
-    case 'WeekendContentItem':
-    case 'ContentSeriesContentItem':
-    case 'DevotionalContentItem':
-      return <HighlightCard {...props} />;
-    default:
-      return <DefaultCard {...props} />;
-  }
-});
+const ListItemCard = memo(({ Component, ...props }) => (
+  <Component {...props} />
+));
 
 ListItemCard.propTypes = {
   Component: PropTypes.func,
