bebe3611849235ff9fbbb1f8b34d711f814c4726
Burke Shartsis <burke@differential.com>
2020-05-01 16:53:47 -0400

    move ContentCardComponentMapper back into ContentCardConnected
    it doesn't really have a better home right now so this works.

---
+++ packages/apollos-ui-connected/src/ContentCardConnected/ContentCardComponentMapper/ContentCardComponentMapper.tests.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import { DefaultCard } from '@apollosproject/ui-kit';
+
+import { Providers } from '../utils/testUtils';
+
+import ContentCardComponentMapper from '.';
+
+describe('The CampaignItemListFeature component', () => {
+  it('should render', () => {
+    const tree = renderer.create(
+      <Providers>{ContentCardComponentMapper()}</Providers>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  it('should render a Component prop', () => {
+    const tree = renderer.create(
+      <Providers>
+        <ContentCardComponentMapper Component={DefaultCard} />
+      </Providers>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
