eb5aca39fce0eb7a7f4d9196b6280e84065c1c09
Burke Shartsis <burke@differential.com>
2020-05-04 11:06:01 -0400

    add deprication warning

--- packages/apollos-ui-connected/src/ContentCardConnected/index.js
+++ packages/apollos-ui-connected/src/ContentCardConnected/index.js
@@ -2,9 +2,7 @@ import ContentCardConnected from './ContentCardConnected';
 
 export ContentCardComponentMapper from './ContentCardComponentMapper';
 // TODO: Update to `ContentCardComponentMapper` export below is for temporary backwards compatibility.
-export {
-  default as contentCardComponentMapper,
-} from './ContentCardComponentMapper/cardMapper';
+export contentCardComponentMapper from './ContentCardComponentMapper/contentCardComponentMapper';
 export GET_CONTENT_CARD from './getContentCard';
 
 export default ContentCardConnected;
