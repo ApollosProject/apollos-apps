36c5817ae0be92f061210bef0275e090c595e9e8
Jordan Wade <jjwade31@gmail.com>
2020-05-05 15:05:51 -0500

    update webview feature component

--- packages/apollos-ui-connected/src/ContentSingleFeaturesConnected/WebviewFeature.js
+++ packages/apollos-ui-connected/src/ContentSingleFeaturesConnected/WebviewFeature.js
@@ -1,48 +1,81 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { H3, styled, PaddedView } from '@apollosproject/ui-kit';
+import { SideBySideView, H3, UIText, styled } from '@apollosproject/ui-kit';
 import { WebView } from 'react-native-webview';
-import { Platform, View } from 'react-native';
+import { Platform, View, TouchableOpacity, Linking } from 'react-native';
 // import { SafeAreaView } from 'react-navigation';
 
-const FlexedActionCard = styled(({ theme, inHorizontalList = false }) => ({
-  backgroundColor: theme.colors.black,
-  height: 400,
-  borderRadius: theme.sizing.baseBorderRadius,
-  marginHorizontal: theme.sizing.baseUnit,
-  marginVertical: theme.sizing.baseUnit * 0.75,
-  overflow: 'hidden',
-  ...(inHorizontalList
-    ? {
-        // provides spacing between cards also fixes android shadow needing "space" to render into
-        margin: theme.sizing.baseUnit / 2,
-        marginBottom: theme.sizing.baseUnit * 0.75,
-      }
-    : {
-        marginHorizontal: theme.sizing.baseUnit,
-        marginVertical: theme.sizing.baseUnit * 0.75,
-      }),
-  ...Platform.select(theme.shadows.default),
-}))(View);
+const StyledCard = styled(
+  ({ theme, inHorizontalList = false }) => ({
+    backgroundColor: theme.colors.black,
+    height: 400,
+    borderRadius: theme.sizing.baseBorderRadius,
+    marginHorizontal: theme.sizing.baseUnit,
+    marginVertical: theme.sizing.baseUnit * 0.75,
+    overflow: 'hidden',
+    ...(inHorizontalList
+      ? {
+          // provides spacing between cards also fixes android shadow needing "space" to render into
+          margin: theme.sizing.baseUnit / 2,
+          marginBottom: theme.sizing.baseUnit * 0.75,
+        }
+      : {
+          marginHorizontal: theme.sizing.baseUnit,
+          marginVertical: theme.sizing.baseUnit * 0.75,
+        }),
+    ...Platform.select(theme.shadows.default),
+  }),
+  'ui-connected.WebviewFeature.StyledCard'
+)(View);
 
-const FlexedWebView = styled(() => ({
-  flex: 1,
-}))(WebView);
+const StyledWebView = styled(
+  () => ({
+    flex: 1,
+  }),
+  'ui-connected.WebviewFeature.StyledWebView'
+)(WebView);
 
-const StyledH3 = styled(({ theme }) => ({
-  paddingHorizontal: theme.sizing.baseUnit,
-  color: theme.colors.screen,
-}))(H3);
+const StyledH3 = styled(
+  ({ theme }) => ({
+    paddingHorizontal: theme.sizing.baseUnit,
+    color: theme.colors.screen,
+  }),
+  'ui-connected.WebviewFeature.StyledH3'
+)(H3);
 
-const WebviewFeature = ({ url }) => (
-  <FlexedActionCard>
-    <StyledH3 padded>{'Setlist'}</StyledH3>
-    <FlexedWebView source={{ uri: url }} />
-  </FlexedActionCard>
+const StyledText = styled(
+  ({ theme }) => ({
+    color: theme.colors.screen,
+    paddingHorizontal: theme.sizing.baseUnit,
+  }),
+  'ui-connected.WebviewFeature.StyledText'
+)(UIText);
+
+const StyledSideBySideView = styled(
+  () => ({
+    alignItems: 'center',
+  }),
+  'ui-connected.WebviewFeature.StyledSideBySideView'
+)(SideBySideView);
+
+const WebviewFeature = ({ url, title, linkText }) => (
+  <StyledCard>
+    {title && (
+      <StyledSideBySideView>
+        <StyledH3 padded>{title}</StyledH3>
+        <TouchableOpacity onPress={() => Linking.openURL(url)}>
+          <StyledText>{linkText}</StyledText>
+        </TouchableOpacity>
+      </StyledSideBySideView>
+    )}
+    <StyledWebView source={{ uri: url }} />
+  </StyledCard>
 );
 
 WebviewFeature.propTypes = {
   url: PropTypes.string.isRequired,
+  title: PropTypes.string,
+  linkText: PropTypes.string,
 };
 
 export default WebviewFeature;
