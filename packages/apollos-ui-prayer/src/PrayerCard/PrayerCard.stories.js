b979ac446b3e82d234225cce15dc32676a939508
Burke Shartsis <burke@differential.com>
2020-05-11 14:40:48 -0400

    add some props and clean up styles

--- packages/apollos-ui-prayer/src/PrayerCard/PrayerCard.stories.js
+++ packages/apollos-ui-prayer/src/PrayerCard/PrayerCard.stories.js
@@ -14,5 +14,9 @@ storiesOf('ui-prayer/PrayerCard', module)
       avatar={{
         uri: 'https://picsum.photos/400/400',
       }}
+      prayer={
+        'For my 15+ year old dog as she makes her journey from this life. she has been a joy and a blessing to us since we adopted her 12 years ago. Thank you Lord for giving us such a sweet and loving companion.'
+      }
+      title={'Pray for Peter'}
     />
   ));
--- packages/apollos-ui-prayer/src/PrayerCard/index.js
+++ packages/apollos-ui-prayer/src/PrayerCard/index.js
@@ -1,16 +1,42 @@
 import React from 'react';
+import { View } from 'react-native';
 import PropTypes from 'prop-types';
 
-import { Card, Avatar } from '@apollosproject/ui-kit';
+import {
+  Avatar,
+  BodyText,
+  Card,
+  CardContent,
+  H4,
+  styled,
+} from '@apollosproject/ui-kit';
 
-const PrayerCard = ({ avatar }) => (
+const AvatarWrapper = styled(({ theme }) => ({
+  alignItems: 'center',
+  paddingBottom: theme.sizing.baseUnit,
+}))(View);
+
+const Content = styled(({ theme }) => ({
+  paddingVertical: theme.sizing.baseUnit * 1.5, // TODO: move this style into `CardContent`
+  paddingHorizontal: theme.sizing.baseUnit * 1.5, // TODO: move this style into `CardContent`
+}))(CardContent);
+
+const PrayerCard = ({ avatar, prayer, title }) => (
   <Card>
-    <Avatar source={avatar} size={'medium'} />
+    <Content>
+      <AvatarWrapper>
+        <Avatar source={avatar} size={'medium'} />
+      </AvatarWrapper>
+      <H4 padded>{title}</H4>
+      <BodyText>{prayer}</BodyText>
+    </Content>
   </Card>
 );
 
 PrayerCard.propTypes = {
   avatar: PropTypes.shape({}),
+  title: PropTypes.string,
+  prayer: PropTypes.string,
 };
 
 export default PrayerCard;
