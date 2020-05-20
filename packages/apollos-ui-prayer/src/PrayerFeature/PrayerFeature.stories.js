f2fbc6c5f7310af537f1a9cd94fc17ed63ce8ce3
Burke Shartsis <burke@differential.com>
2020-05-20 18:04:29 -0400

    scaffold PrayerFeature component

---
+++ packages/apollos-ui-prayer/src/PrayerFeature/PrayerFeature.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { storiesOf } from '@apollosproject/ui-storybook';
+// import { CenteredView } from '@apollosproject/ui-kit';
+
+import PrayerFeature from '.';
+
+storiesOf('ui-prayer/PrayerFeature', module).add('example', () => (
+  <PrayerFeature />
+));
---
+++ packages/apollos-ui-prayer/src/PrayerFeature/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { H3 } from '@apollosproject/ui-kit';
+
+const PrayerFeature = () => <H3>Boom</H3>;
+
+export default PrayerFeature;
