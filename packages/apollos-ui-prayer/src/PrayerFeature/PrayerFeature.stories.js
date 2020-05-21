b3c3321f55e93b4f46ced18e44cc01a6c18a4f64
Burke Shartsis <burke@differential.com>
2020-05-21 09:43:17 -0400

    add PrayerFeature title and subtitles

--- packages/apollos-ui-prayer/src/PrayerFeature/PrayerFeature.stories.js
+++ packages/apollos-ui-prayer/src/PrayerFeature/PrayerFeature.stories.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
 import { storiesOf } from '@apollosproject/ui-storybook';
-// import { CenteredView } from '@apollosproject/ui-kit';
+import { CenteredView } from '@apollosproject/ui-kit';
 
 import PrayerFeature from '.';
 
-storiesOf('ui-prayer/PrayerFeature', module).add('example', () => (
-  <PrayerFeature />
-));
+storiesOf('ui-prayer/PrayerFeature', module)
+  .addDecorator((story) => (
+    <CenteredView style={{ alignItems: 'stretch' /* eslint-disable-line */ }}>
+      {story()}
+    </CenteredView>
+  ))
+  .add('example', () => (
+    <PrayerFeature title={'Example title'} subtitle={'Custom Subtitle'} />
+  ))
+  .add('default', () => <PrayerFeature />)
+  .add('title', () => <PrayerFeature title={'Example title'} />)
+  .add('subtitle', () => <PrayerFeature subtitle={'Custom Subtitle'} />);
--- packages/apollos-ui-prayer/src/PrayerFeature/index.js
+++ packages/apollos-ui-prayer/src/PrayerFeature/index.js
@@ -1,7 +1,45 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-import { H3 } from '@apollosproject/ui-kit';
+import { H3, H6, PaddedView, styled } from '@apollosproject/ui-kit';
 
-const PrayerFeature = () => <H3>Boom</H3>;
+const Header = styled(({ theme }) => ({
+  paddingTop: theme.sizing.baseUnit * 3,
+  paddingBottom: 0,
+}))(PaddedView);
+
+/* TODO: Change to H5 and add appropriate padding. We are using H6 here to be consistant with other
+ * "card titles" (`ActionListFeature`). */
+const Title = styled(
+  ({ theme }) => ({
+    color: theme.colors.text.tertiary,
+  }),
+  'ui-prayer.PrayerFeature.Title'
+)(H6);
+
+const Subtitle = styled({}, 'ui-prayer.PrayerFeature.Subtitle')(H3);
+
+const PrayerFeature = ({ isLoading, title, subtitle }) => (
+  <>
+    {isLoading || title || subtitle ? ( // only display the Header if we are loading or have a title/subtitle
+      <Header>
+        {isLoading || title ? ( // we check for isloading here so that they are included in the loading state
+          <Title numberOfLines={1}>{title}</Title>
+        ) : null}
+        {isLoading || subtitle ? <Subtitle>{subtitle}</Subtitle> : null}
+      </Header>
+    ) : null}
+  </>
+);
+
+PrayerFeature.propTypes = {
+  isLoading: PropTypes.bool,
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+};
+
+PrayerFeature.defaultProps = {
+  subtitle: 'Prayer',
+};
 
 export default PrayerFeature;
