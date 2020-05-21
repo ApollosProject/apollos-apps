a16d949c13b51d3e7118bd7d52a1761326259091
Burke Shartsis <burke@differential.com>
2020-05-21 10:41:43 -0400

    add test coverage for what we have so far

---
+++ packages/apollos-ui-prayer/src/PrayerFeature/PrayerFeature.tests.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import { Providers } from '../testUtils';
+
+import PrayerFeature from '.';
+
+describe('The PrayerFeature component', () => {
+  it('should render', () => {
+    const tree = renderer.create(
+      <Providers>
+        <PrayerFeature />
+      </Providers>
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+  it('should render without a `Card` border', () => {
+    const tree = renderer.create(
+      <Providers>
+        <PrayerFeature isCard={false} />
+      </Providers>
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+  it('should render with a title', () => {
+    const tree = renderer.create(
+      <Providers>
+        <PrayerFeature title={'Example title'} />
+      </Providers>
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+  it('should render with a custom subtitle', () => {
+    const tree = renderer.create(
+      <Providers>
+        <PrayerFeature subtitle={'Custom subtitle'} />
+      </Providers>
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+});
