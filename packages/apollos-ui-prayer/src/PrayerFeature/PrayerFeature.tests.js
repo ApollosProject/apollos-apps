b7e4da4628b88861805499bc8a74bfdddd5493c2
Burke Shartsis <burke@differential.com>
2020-05-26 16:39:45 -0400

    updated `PrayerFeature` tests

--- packages/apollos-ui-prayer/src/PrayerFeature/PrayerFeature.tests.js
+++ packages/apollos-ui-prayer/src/PrayerFeature/PrayerFeature.tests.js
@@ -24,6 +24,51 @@ describe('The PrayerFeature component', () => {
 
     expect(tree).toMatchSnapshot();
   });
+  it('should render a loading state', () => {
+    const tree = renderer.create(
+      <Providers>
+        <PrayerFeature isLoading title={'Example title'} />
+      </Providers>
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+  it('should render a loading state without a `Card` border', () => {
+    const tree = renderer.create(
+      <Providers>
+        <PrayerFeature isCard={false} isLoading title={'Example title'} />
+      </Providers>
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+  it('accept an onPressAdd function', () => {
+    const tree = renderer.create(
+      <Providers>
+        <PrayerFeature onPressAdd={jest.fn()} />
+      </Providers>
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+  it('should accept an onPressAdd function', () => {
+    const tree = renderer.create(
+      <Providers>
+        <PrayerFeature onPressAdd={jest.fn()} />
+      </Providers>
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+  it('should accept an onPressAvatar function', () => {
+    const tree = renderer.create(
+      <Providers>
+        <PrayerFeature onPressAvatar={jest.fn()} />
+      </Providers>
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
   it('should render with a title', () => {
     const tree = renderer.create(
       <Providers>
@@ -37,24 +82,6 @@ describe('The PrayerFeature component', () => {
     const tree = renderer.create(
       <Providers>
         <PrayerFeature subtitle={'Custom subtitle'} />
-      </Providers>
-    );
-
-    expect(tree).toMatchSnapshot();
-  });
-  it('should render a loading state', () => {
-    const tree = renderer.create(
-      <Providers>
-        <PrayerFeature isLoading title={'Example title'} />
-      </Providers>
-    );
-
-    expect(tree).toMatchSnapshot();
-  });
-  it('should render a loading state without a `Card` border', () => {
-    const tree = renderer.create(
-      <Providers>
-        <PrayerFeature isLoading title={'Example title'} isCard={false} />
       </Providers>
     );
 
