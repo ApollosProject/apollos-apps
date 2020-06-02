4f37151792b1cfe83842cd58013b9b78b0b927db
Burke Shartsis <burke@differential.com>
2020-06-01 22:14:58 -0400

    correct story name for AvatarCloud

--- packages/apollos-ui-kit/src/Avatar/AvatarCloud/AvatarCloud.stories.js
+++ packages/apollos-ui-kit/src/Avatar/AvatarCloud/AvatarCloud.stories.js
@@ -6,7 +6,7 @@ import CenteredView from '../../CenteredView';
 
 import AvatarCloud from '.';
 
-storiesOf('ui-kit/Avatar/AvatarList', module)
+storiesOf('ui-kit/Avatar/AvatarCloud', module)
   .addDecorator((story) => (
     <CenteredView style={{ alignItems: 'stretch' /* eslint-disable-line */ }}>
       {story()}
