e65399575cc2a0f1cea49248a93acdc92b34f624
Burke Shartsis <burke@differential.com>
2020-06-02 18:07:30 -0400

    tests

--- packages/apollos-ui-kit/src/Avatar/AvatarCloud/AvatarCloud.tests.js
+++ packages/apollos-ui-kit/src/Avatar/AvatarCloud/AvatarCloud.tests.js
@@ -5,11 +5,47 @@ import Providers from '../../Providers';
 
 import AvatarCloud from '.';
 
+const avatars = [
+  'https://picsum.photos/200',
+  'https://picsum.photos/200',
+  'https://picsum.photos/200',
+];
+
 describe('The AvatarCloud component', () => {
   it('should render', () => {
     const tree = renderer.create(
       <Providers>
-        <AvatarCloud />
+        <AvatarCloud avatars={avatars} />
+      </Providers>
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+  it('should accept a maxAvatarSize', () => {
+    const tree = renderer.create(
+      <Providers>
+        <AvatarCloud avatars={avatars} maxAvatarSize={10} />
+      </Providers>
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+  it('should accept a minAvatarSize', () => {
+    const tree = renderer.create(
+      <Providers>
+        <AvatarCloud avatars={avatars} maxAvatarSize={100} />
+      </Providers>
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+  it('should accept a primaryAvatar', () => {
+    const tree = renderer.create(
+      <Providers>
+        <AvatarCloud
+          avatars={avatars}
+          primaryAvatar={'https://picsum.photos/200'}
+        />
       </Providers>
     );
 
