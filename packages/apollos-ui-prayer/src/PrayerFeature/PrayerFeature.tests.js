68be671067d7c89a9817ca8b0dcac6bfbb18b593
Burke Shartsis <burke@differential.com>
2020-06-01 22:02:53 -0400

    squashed fork of #1471 move-AvatarList-to-ui-kit

---
+++ packages/apollos-ui-prayer/src/PrayerFeature/PrayerFeature.tests.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import { Providers } from '../testUtils';
+
+import PrayerFeature from '.';
+
+const avatars = [
+  'https://picsum.photos/200',
+  'https://picsum.photos/200',
+  'https://picsum.photos/200',
+  'https://picsum.photos/200',
+  'https://picsum.photos/200',
+  'https://picsum.photos/200',
+  'https://picsum.photos/200',
+  'https://picsum.photos/200',
+];
+
+describe('The PrayerFeature component', () => {
+  it('should render', () => {
+    const tree = renderer.create(
+      <Providers>
+        <PrayerFeature avatars={avatars} />
+      </Providers>
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+  it('should render without a `Card` border', () => {
+    const tree = renderer.create(
+      <Providers>
+        <PrayerFeature avatars={avatars} isCard={false} />
+      </Providers>
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+  it('should render a loading state', () => {
+    const tree = renderer.create(
+      <Providers>
+        <PrayerFeature avatars={avatars} isLoading title={'Example title'} />
+      </Providers>
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+  it('should render a loading state without a `Card` border', () => {
+    const tree = renderer.create(
+      <Providers>
+        <PrayerFeature
+          avatars={avatars}
+          isCard={false}
+          isLoading
+          title={'Example title'}
+        />
+      </Providers>
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+  it('should accept an onPressAdd function', () => {
+    const tree = renderer.create(
+      <Providers>
+        <PrayerFeature avatars={avatars} onPressAdd={jest.fn()} />
+      </Providers>
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+  it('should accept an onPressAvatar function', () => {
+    const tree = renderer.create(
+      <Providers>
+        <PrayerFeature avatars={avatars} onPressAvatar={jest.fn()} />
+      </Providers>
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+  it('should render with a title', () => {
+    const tree = renderer.create(
+      <Providers>
+        <PrayerFeature avatars={avatars} title={'Example title'} />
+      </Providers>
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+  it('should render with a custom subtitle', () => {
+    const tree = renderer.create(
+      <Providers>
+        <PrayerFeature avatars={avatars} subtitle={'Custom subtitle'} />
+      </Providers>
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+});
