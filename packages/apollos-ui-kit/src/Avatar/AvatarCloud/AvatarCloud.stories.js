1262511d83671e5095317695694b271bc1ba283b
Burke Shartsis <burke@differential.com>
2020-06-02 12:47:43 -0400

    randomly size and positon avatars with users avatar centered

--- packages/apollos-ui-kit/src/Avatar/AvatarCloud/AvatarCloud.stories.js
+++ packages/apollos-ui-kit/src/Avatar/AvatarCloud/AvatarCloud.stories.js
@@ -7,9 +7,9 @@ import CenteredView from '../../CenteredView';
 import AvatarCloud from '.';
 
 storiesOf('ui-kit/Avatar/AvatarCloud', module)
-  .addDecorator((story) => (
-    <CenteredView style={{ alignItems: 'stretch' /* eslint-disable-line */ }}>
-      {story()}
-    </CenteredView>
-  ))
+  // .addDecorator((story) => (
+  //   <CenteredView style={{ alignItems: 'stretch' /* eslint-disable-line */ }}>
+  //     {story()}
+  //   </CenteredView>
+  // ))
   .add('default', () => <AvatarCloud />);
--- packages/apollos-ui-kit/src/Avatar/AvatarCloud/index.js
+++ packages/apollos-ui-kit/src/Avatar/AvatarCloud/index.js
@@ -1,8 +1,66 @@
 import React from 'react';
+import { Dimensions, View } from 'react-native';
+import { sample } from 'lodash';
 // import PropTypes from 'prop-types';
 
-import { H1 } from '../../typography';
+import CenteredView from '../../CenteredView';
+import ConnectedImage from '../../ConnectedImage';
+import styled from '../../styled';
 
-const AvatarCloud = () => <H1>Hello World</H1>;
+const getRandomHeightWidth = ({ avatarSizes }) => {
+  const sizesWithoutLarge = Object.keys(avatarSizes).filter(
+    (size) => size !== 'large'
+  );
+
+  const randomSize = sample(sizesWithoutLarge);
+
+  return {
+    borderRadius: avatarSizes[randomSize] / 2,
+    width: avatarSizes[randomSize],
+    height: avatarSizes[randomSize],
+  };
+};
+
+const getRandomXYPosition = ({ imageHeight, imageWidth }) => {
+  const positionX = Dimensions.get('window').width - imageWidth;
+  const positionY = Dimensions.get('window').height - imageHeight;
+
+  return {
+    left: Math.round(Math.random() * positionX),
+    top: Math.round(Math.random() * positionY),
+  };
+};
+
+const RandomAvatar = styled(
+  ({ theme }) => ({
+    position: 'absolute',
+    ...getRandomHeightWidth({ avatarSizes: theme.sizing.avatar }),
+    ...getRandomXYPosition({
+      imageHeight: theme.sizing.avatar.large,
+      imageWidth: theme.sizing.avatar.large,
+    }),
+  }),
+  'ui-kit.AvatarList.RandomAvatar'
+)(ConnectedImage);
+
+const UserAvatar = styled(
+  ({ theme }) => ({
+    borderRadius: theme.sizing.avatar.large / 2,
+    height: theme.sizing.avatar.large,
+    width: theme.sizing.avatar.large,
+    zIndex: 100,
+  }),
+  'ui-kit.AvatarList.UserAvatar'
+)(ConnectedImage);
+
+const AvatarCloud = () => (
+  <CenteredView>
+    <UserAvatar source={'https://picsum.photos/200'} />
+    <RandomAvatar source={'https://picsum.photos/200'} />
+    <RandomAvatar source={'https://picsum.photos/200'} />
+    <RandomAvatar source={'https://picsum.photos/200'} />
+    <RandomAvatar source={'https://picsum.photos/200'} />
+  </CenteredView>
+);
 
 export default AvatarCloud;
