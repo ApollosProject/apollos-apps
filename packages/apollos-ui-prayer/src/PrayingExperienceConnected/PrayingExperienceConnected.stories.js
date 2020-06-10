d4ee0e6bf672c8a29825d7e01d862d46ab43db85
Conrad VanLandingham <conrad@differential.com>
2020-06-10 15:53:41 -0500

    export prayer components and add PrayerExperienceConnected boilerplate

---
+++ packages/apollos-ui-prayer/src/PrayingExperienceConnected/PrayingExperienceConnected.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { storiesOf } from '@apollosproject/ui-storybook';
+import { CenteredView } from '@apollosproject/ui-kit';
+
+import PrayerExperienceConnected from '.';
+
+storiesOf('ui-prayer/PrayerExperienceConnected', module).add('example', () => (
+  <PrayerExperienceConnected />
+));
---
+++ packages/apollos-ui-prayer/src/PrayingExperienceConnected/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const PrayingExperienceConnected = () => null;
+
+export default PrayingExperienceConnected;
