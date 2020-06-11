8287c4cba716d908c5084f16741f1f73ee0334c0
Conrad VanLandingham <conrad@differential.com>
2020-06-10 22:52:00 -0500

    quickly wire up paryerswiper

--- packages/apollos-ui-prayer/src/PrayingExperienceConnected/PrayingExperienceConnected.stories.js
+++ packages/apollos-ui-prayer/src/PrayingExperienceConnected/PrayingExperienceConnected.stories.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import { storiesOf } from '@apollosproject/ui-storybook';
-import { CenteredView } from '@apollosproject/ui-kit';
 
 import PrayerExperienceConnected from '.';
 
+// note: if you want these stories to run, make sure to log into the app first!
+// open the app store, login, then come back to this story.
+// TODO: where do we put this instruction to be more easily seen?
+
 storiesOf('ui-prayer/PrayerExperienceConnected', module).add('example', () => (
-  <PrayerExperienceConnected />
+  <PrayerExperienceConnected id="PrayerListFeature:9724110384a5109532f35a4980ea9b77f2d199f4cb456bb8c8ea55a925cec739815c742a0c3ce42c726d35af507ce66e2e2a631c3b28a8ca52d3892b63fef178083cb2d10b100b68607aa5362263418b297450b10882570edf492f07a3037b58" />
 ));
--- packages/apollos-ui-prayer/src/PrayingExperienceConnected/index.js
+++ packages/apollos-ui-prayer/src/PrayingExperienceConnected/index.js
@@ -1,5 +1,64 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
 
-const PrayingExperienceConnected = () => null;
+import PrayerCard from '../PrayerCard';
+import PrayerScreen from '../PrayerScreen';
+import PrayerSwiper from '../PrayerSwiper';
+
+const GET_PRAYER_FEATURE = gql`
+  query($id: ID!) {
+    node(id: $id) {
+      # todo - this needs to be broken up into fragments
+      ... on PrayerListFeature {
+        prayers {
+          id
+          text
+          requestor {
+            nickName
+            firstName
+            photo {
+              uri
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+// todo: does this component belong here or ui-connected??
+const PrayingExperienceConnected = ({ id }) => (
+  <Query
+    query={GET_PRAYER_FEATURE}
+    variables={{ id }}
+    fetchPolicy={'cache-and-network'}
+  >
+    {({ data = {} }) => {
+      const { prayers = [] } = data?.node || {};
+      return (
+        <PrayerSwiper>
+          {() =>
+            prayers.map((prayer) => (
+              <PrayerScreen key={prayer.id}>
+                <PrayerCard
+                  prayer={prayer.text}
+                  avatar={prayer.requestor?.photo?.uri || null}
+                  title={`Pray for ${prayer.requestor?.nickName ||
+                    prayer.requestor?.firstName}`}
+                />
+              </PrayerScreen>
+            ))
+          }
+        </PrayerSwiper>
+      );
+    }}
+  </Query>
+);
+
+PrayingExperienceConnected.propTypes = {
+  id: PropTypes.string.isRequired,
+};
 
 export default PrayingExperienceConnected;
