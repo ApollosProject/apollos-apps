aa317ce608e522edb514df90ea772baff130fde8
Conrad VanLandingham <conrad@differential.com>
2020-06-11 22:01:09 -0500

    add dark mode!

--- packages/apollos-ui-prayer/src/PrayingExperienceConnected/PrayingExperienceConnected.stories.js
+++ packages/apollos-ui-prayer/src/PrayingExperienceConnected/PrayingExperienceConnected.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { storiesOf } from '@apollosproject/ui-storybook';
+import { ThemeMixin } from '@apollosproject/ui-kit';
 
 import PrayerExperienceConnected from '.';
 
@@ -8,6 +9,12 @@ import PrayerExperienceConnected from '.';
 // open the app store, login, then come back to this story.
 // TODO: where do we put this instruction to be more easily seen?
 
-storiesOf('ui-prayer/PrayerExperienceConnected', module).add('example', () => (
-  <PrayerExperienceConnected id="PrayerListFeature:9724110384a5109532f35a4980ea9b77f2d199f4cb456bb8c8ea55a925cec739815c742a0c3ce42c726d35af507ce66e2e2a631c3b28a8ca52d3892b63fef178083cb2d10b100b68607aa5362263418b297450b10882570edf492f07a3037b58" />
-));
+storiesOf('ui-prayer/PrayerExperienceConnected', module)
+  .add('example', () => (
+    <PrayerExperienceConnected id="PrayerListFeature:9724110384a5109532f35a4980ea9b77f2d199f4cb456bb8c8ea55a925cec739815c742a0c3ce42c726d35af507ce66e2e2a631c3b28a8ca52d3892b63fef178083cb2d10b100b68607aa5362263418b297450b10882570edf492f07a3037b58" />
+  ))
+  .add('example - dark', () => (
+    <ThemeMixin mixin={{ type: 'dark' }}>
+      <PrayerExperienceConnected id="PrayerListFeature:9724110384a5109532f35a4980ea9b77f2d199f4cb456bb8c8ea55a925cec739815c742a0c3ce42c726d35af507ce66e2e2a631c3b28a8ca52d3892b63fef178083cb2d10b100b68607aa5362263418b297450b10882570edf492f07a3037b58" />
+    </ThemeMixin>
+  ));
