59a28818a27261f028b1b70e04ab47425361f16f
Vincent Wilson <vince@classyh.at>
2020-06-15 16:06:33 -0400

    Reduces Algolia calls (#1484)
    * Move algolia init logic out of datasource constructor
    
    * Fixed tests
    
    * Fix Mr. Linter

--- packages/apollos-data-connector-algolia-search/__mocks__/@apollosproject/config.js
+++ packages/apollos-data-connector-algolia-search/__mocks__/@apollosproject/config.js
@@ -4,6 +4,11 @@ ApolloServer.loadJs({
   ROCK: {
     TIMEZONE: 'America/New_York',
   },
+  ALGOLIA: {
+    APPLICATION_ID: 'some-application-id',
+    API_KEY: 'some-api-key',
+    CONFIGURATION: {},
+  },
 });
 
 export default ApolloServer;
