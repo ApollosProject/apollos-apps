cda4cf86ce71cca8d8ba15ab9a98a94490526a2f
Marcelo Reyna <lepozepo@gmail.com>
2020-06-18 13:05:12 -0700

    Unzip and stage

--- packages/create-apollos-app/src/index.js
+++ packages/create-apollos-app/src/index.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
+const cp = require('child_process');
 const { Command, flags: Flags } = require('@oclif/command');
-const { cli } = require('cli-ux');
+// const { cli } = require('cli-ux');
 const fetch = require('node-fetch');
+const Zip = require('adm-zip');
 
 module.exports = class CreateApollosAppCommand extends Command {
   static description = 'Create an apollos app';
@@ -33,42 +35,55 @@ module.exports = class CreateApollosAppCommand extends Command {
   };
 
   async run() {
-    const { flags, args } = this.parse(CreateApollosAppCommand);
-    console.log({ flags, args });
+    try {
+      const { flags, args } = this.parse(CreateApollosAppCommand);
 
-    // eslint-disable-next-line
-    cli.action.start(`Creating apollos app at ${flags.dir}/${args.name} (Version: ${flags.release})`);
+      const projectPath = `${flags.dir}/${args.name}`;
+      if (fs.existsSync(projectPath)) {
+        throw new Error('Project already exists!');
+      }
 
-    // Identify version
-    let url = '';
-    if (flags.release === 'edge') {
       // eslint-disable-next-line
-      url = 'https://github.com/ApollosProject/apollos-templates/archive/master.zip';
-    } else if (flags.release === 'latest') {
-      // NOTE: Not too sure why releases was returning a blank array, maybe we don't have any releases?
-      // eslint-disable-next-line
-      const releases = await fetch('https://api.github.com/repos/ApollosProject/apollos-templates/tags?per_page=1').then((r) => r.json());
-      const latestRelease = releases[0];
+      this.log(`Creating apollos app at ${flags.dir}/${args.name} (Version: ${flags.release})`);
 
-      url = latestRelease.zipball_url;
-    } else {
-      // eslint-disable-next-line
-      url = `https://api.github.com/repos/ApollosProject/apollos-templates/zipball/${flags.release}`;
-    }
+      // Identify version
+      let url = '';
+      if (flags.release === 'edge') {
+        // eslint-disable-next-line
+        url = 'https://github.com/ApollosProject/apollos-templates/archive/master.zip';
+      } else if (flags.release === 'latest') {
+        // NOTE: Not too sure why releases was returning a blank array, maybe we don't have any releases?
+        // eslint-disable-next-line
+        const releases = await fetch('https://api.github.com/repos/ApollosProject/apollos-templates/tags?per_page=1').then((r) => r.json());
+        const latestRelease = releases[0];
 
-    // Download zip
-    const zipPath = `${flags.dir}/${args.name}.zip`;
-    if (fs.existsSync(zipPath)) {
+        url = latestRelease.zipball_url;
+      } else {
+        // eslint-disable-next-line
+        url = `https://api.github.com/repos/ApollosProject/apollos-templates/zipball/${flags.release}`;
+      }
+
+      // Download zip
+      const zipPath = `${flags.dir}/${args.name}.zip`;
+      if (fs.existsSync(zipPath)) {
+        fs.unlinkSync(zipPath);
+      }
+      const zipFile = await fetch(url);
+      const file = fs.createWriteStream(zipPath);
+      await new Promise((resolve) => {
+        zipFile.body.pipe(file).on('finish', resolve);
+      });
+
+      // Extract zip
+      const zip = new Zip(zipPath);
+      const zipRootDirectory = zip.getEntries()[0].entryName;
+      zip.extractAllTo(`${flags.dir}/.tmp`, true);
       fs.unlinkSync(zipPath);
+      // eslint-disable-next-line
+      fs.renameSync(`${flags.dir}/.tmp/${zipRootDirectory}`, projectPath);
+      cp.execSync(`rm -rf ${flags.dir}/.tmp`);
+    } catch (err) {
+      this.error(err.message);
     }
-    const zipFile = await fetch(url);
-    const file = fs.createWriteStream(zipPath);
-    await new Promise((resolve) => {
-      zipFile.body.pipe(file).on('finish', resolve);
-    });
-
-    // Extract zip
-
-    cli.action.stop('done');
   }
 };
