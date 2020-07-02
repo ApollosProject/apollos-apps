fb4b6cf118164fe23e91628dd5ed769547ac2691
Conrad VanLandingham <conrad@differential.com>
2020-07-02 11:45:45 -0500

    Fix issue where re-renders aren't picked up in StretchyView (#1499)

--- packages/apollos-ui-kit/src/StretchyView/StretchyView.stories.js
+++ packages/apollos-ui-kit/src/StretchyView/StretchyView.stories.js
@@ -1,7 +1,7 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
+import React, { useState } from 'react';
 import { storiesOf } from '@apollosproject/ui-storybook';
-import { StyleSheet, ScrollView, View } from 'react-native';
+import { StyleSheet, Animated, View } from 'react-native';
 import {
   ConnectedImage,
   GradientOverlayImage,
@@ -9,11 +9,54 @@ import {
   H1,
   H2,
   H3,
+  Button,
 } from '@apollosproject/ui-kit';
 
 import StretchyView from '.';
 
+const { ScrollView } = Animated;
+
 const imageStyle = { width: '100%', aspectRatio: 1 };
+
+const HeaderStory = () => {
+  const [i, setSource] = useState(342);
+  const uri = `https://picsum.photos/id/${i}/600/600`;
+  return (
+    <StretchyView>
+      {({ Stretchy, ...scrollViewProps }) => (
+        <ScrollView {...scrollViewProps}>
+          <View>
+            <Stretchy>
+              <ConnectedImage style={imageStyle} source={{ uri }} />
+            </Stretchy>
+            <View
+              style={{
+                ...StyleSheet.absoluteFillObject,
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <H2>Text on top of the stretchy!</H2>
+              <Button
+                onPress={() => setSource(i + 1)}
+                title="Change header image"
+              />
+            </View>
+          </View>
+          <PaddedView>
+            <H1 padded>Well hello there!</H1>
+            <H2 padded>
+              Hello there!
+              {'\n'}
+            </H2>
+            <H3 padded>Hello there!</H3>
+            <H1 padded>Try to scroll me around.</H1>
+          </PaddedView>
+        </ScrollView>
+      )}
+    </StretchyView>
+  );
+};
 
 storiesOf('StretchyView', module)
   .add('stretchy as a background', () => (
@@ -41,40 +84,7 @@ storiesOf('StretchyView', module)
       )}
     </StretchyView>
   ))
-  .add('stretchy as a header', () => (
-    <StretchyView>
-      {({ Stretchy, ...scrollViewProps }) => (
-        <ScrollView {...scrollViewProps}>
-          <View>
-            <Stretchy>
-              <ConnectedImage
-                style={imageStyle}
-                source={{ uri: 'https://picsum.photos/id/342/600/600' }}
-              />
-            </Stretchy>
-            <View
-              style={{
-                ...StyleSheet.absoluteFillObject,
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}
-            >
-              <H2>Text on top of the stretchy!</H2>
-            </View>
-          </View>
-          <PaddedView>
-            <H1 padded>Well hello there!</H1>
-            <H2 padded>
-              Hello there!
-              {'\n'}
-            </H2>
-            <H3 padded>Hello there!</H3>
-            <H1 padded>Try to scroll me around.</H1>
-          </PaddedView>
-        </ScrollView>
-      )}
-    </StretchyView>
-  ))
+  .add('stretchy as a header', () => <HeaderStory />)
   .add('stretchy as a footer', () => (
     <StretchyView>
       {({ Stretchy, ...scrollViewProps }) => (
