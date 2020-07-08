c910ec5f5b8dcdf8dafbd4496fbb4957353fe007
Christina Megura <cmegura@gmail.com>
2020-07-08 13:50:20 -0400

    Actually fix import

--- packages/apollos-ui-onboarding/src/onboardingStatus.js
+++ packages/apollos-ui-onboarding/src/onboardingStatus.js
@@ -1,6 +1,5 @@
 import AsyncStorage from '@react-native-community/async-storage';
 import gql from 'graphql-tag';
-import { resetAction } from '@apollosproject/ui-kit/src/NavigationService';
 
 export const WITH_USER_ID = gql`
   query currentUserId {
@@ -23,46 +22,22 @@ export const checkOnboardingStatusAndNavigate = async ({
   client,
   navigation,
 }) => {
-  console.log('checkOnboardingStatusAndNavigate');
   const { data } = await client.query({ query: WITH_USER_ID });
-  console.log({ data });
   let hasOnboarded = false;
   if (data.currentUser.id) {
-    console.log('outside try');
     try {
       const jsonValue = await AsyncStorage.getItem(
         `@onboarding-status/${data.currentUser.id}`
       );
-      console.log({ jsonValue });
       hasOnboarded = jsonValue != null ? JSON.parse(jsonValue) : false;
-      console.log({ hasOnboarded });
     } catch (e) {
       // error reading value
     }
   }
-  console.log({ hasOnboarded });
-
-  // at this point, we can that resetAction is not a function
-  console.log({ resetAction });
-  // 👆 this is undefined, b/c of how resetAction is imported.
-  /*
-    Two options to fix:
-
-    # First, you could import this way:
-        import NavigationService from '@apollosproject/ui-kit/src/NavigationService';
-      And then call resetAction below like this:
-        NavigationService.resetAction({ ... })
-
-    # Second option:
-        Since `navigation` is already a prop on this function, you don't need to import at all again.
-        Just reference it below:
-
-        navigation.resetAction({ ... })
-  */
 
   if (hasOnboarded) {
     navigation.dispatch(
-      resetAction({
+      navigation.resetAction({
         navigatorName: 'Tabs',
         routeName: 'Home',
       })
