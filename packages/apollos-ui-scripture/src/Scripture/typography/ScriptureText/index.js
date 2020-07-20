866124f7bb4068aec91c15c5112d1336cc729a21
Christina Megura <cmegura@gmail.com>
2020-07-20 18:02:05 -0400

    Quick fixes

--- packages/apollos-ui-scripture/src/Scripture/typography/ScriptureText/index.js
+++ packages/apollos-ui-scripture/src/Scripture/typography/ScriptureText/index.js
@@ -23,7 +23,7 @@ const styles = styled(({ theme, bold, italic }) => {
     fontFamily: fontStack,
     color: theme.colors.text.primary,
   };
-}, 'ui-scripture.Scripture.ScriptureText.styles');
+});
 
 const ScriptureText = compose(
   setDisplayName('ScriptureText'),
