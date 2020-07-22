d24b38a2e4f4a30537a93e328a6cf33374261743
Vincent Wilson <vincent@differential.com>
2020-07-22 09:17:27 -0400

    Ported over bugsnag code from Grace and updated to Bugsnag 7

---
+++ packages/apollos-bugsnag/src/__tests__/js-load.tests.js
@@ -0,0 +1,26 @@
+import ApollosConfig from '../index';
+
+describe('the yaml loader', () => {
+  it('must load variables from a js object', () => {
+    ApollosConfig.loadJs({
+      ROCK: {
+        API_URL: 'https://some-url.com',
+      },
+      BIBLE_API: {
+        API_TOKEN: 'foobar',
+      },
+      VALUE_NOT_IN_DEFAULT_CONFIG: {
+        FOO: 'bar',
+      },
+    });
+    expect(ApollosConfig.config).toMatchSnapshot();
+  });
+  it('must load empty config', () => {
+    ApollosConfig.loadJs({});
+    expect(ApollosConfig.config).toMatchSnapshot();
+  });
+  it('must load empty config with no arguments', () => {
+    ApollosConfig.loadJs();
+    expect(ApollosConfig.config).toMatchSnapshot();
+  });
+});
