b0c9955d3c14579d16d147a80a1090c06a9a6644
Vincent Wilson <vince@classyh.at>
2020-07-28 15:59:17 -0400

    Fix tests

--- packages/apollos-data-connector-rock/src/template/data-source.tests.js
+++ packages/apollos-data-connector-rock/src/template/data-source.tests.js
@@ -4,10 +4,15 @@ import { Auth, Person } from '../index';
 
 import Template from './data-source';
 
+class Cache {
+  get = jest.fn();
+}
+
 const { getContext } = createTestHelpers({
   Person,
   Auth,
   Template: { dataSource: Template },
+  Cache: { dataSource: Cache },
 });
 
 describe('Template (Lava) Data Source', () => {
