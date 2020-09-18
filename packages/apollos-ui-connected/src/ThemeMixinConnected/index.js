f392d835f0728e4a3ade5e60123bc8e400819794
Vincent Wilson <vincent@differential.com>
2020-09-18 12:32:05 -0400

    feat: ConnectedThemeMixin

---
+++ packages/apollos-ui-connected/src/ThemeMixinConnected/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { get } from 'lodash';
+import { ThemeMixin } from '@apollosproject/ui-kit';
+import { Query } from 'react-apollo';
+import GET_NODE_THEME from './getNodeTheme';
+
+const ThemeMixinConnected = ({ nodeId, ...props }) => (
+  <Query query={GET_NODE_THEME} variables={{ nodeId }}>
+    {({ data, loading }) => (
+      <ThemeMixin
+        mixin={{
+          ...get(data, 'node.theme'),
+        }}
+        {...props}
+      />
+    )}
+  </Query>
+);
+
+ThemeMixinConnected.propTypes = {
+  nodeId: PropTypes.string,
+  children: PropTypes.oneOf([PropTypes.func, PropTypes.node]),
+  defaultMixin: PropTypes.shape({}),
+};
+
+export { ThemeMixinConnected as default, GET_NODE_THEME };
