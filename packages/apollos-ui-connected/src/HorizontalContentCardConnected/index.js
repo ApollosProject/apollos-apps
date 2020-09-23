38f21ff3432138ff29340a7952dfdeb9bd0313c4
Vincent Wilson <vincent@differential.com>
2020-09-23 09:17:37 -0400

    Added stories demonstrating working theme overrides

--- packages/apollos-ui-connected/src/HorizontalContentCardConnected/index.js
+++ packages/apollos-ui-connected/src/HorizontalContentCardConnected/index.js
@@ -2,6 +2,6 @@ import HorizontalContentCardConnected from './HorizontalContentCardConnected';
 
 export HorizontalContentCardComponentMapper from './HorizontalContentCardComponentMapper';
 // TODO: Update to `HorizontalContentCardComponentMapper` export below is for temporary backwards compatibility.
-export horizontalContentCardComponentMapper from './HorizontalCardComponentMapper/horizontalContentCardComponentMapper';
+export horizontalContentCardComponentMapper from './HorizontalContentCardComponentMapper/horizontalContentCardComponentMapper';
 
 export default HorizontalContentCardConnected;
