de4a1702a58a1af574bc01f9978dd30998161524
Vincent Wilson <vince@classyh.at>
2020-09-29 12:50:49 -0400

    Finalize code, cleanup code, add tests

--- packages/apollos-ui-kit/src/Avatar/AvatarCloud/AvatarCloud.stories.js
+++ packages/apollos-ui-kit/src/Avatar/AvatarCloud/AvatarCloud.stories.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import { View } from 'react-native';
 import { storiesOf } from '@apollosproject/ui-storybook';
+import { times } from 'lodash';
 
+import { Button } from '../..';
 import AvatarCloud from '.';
 
 const avatars = [
@@ -10,14 +13,35 @@ const avatars = [
   'https://picsum.photos/200?3',
 ];
 
+const generateAvatars = (count = 10) => {
+  const seed = Math.abs(Math.random() * 100);
+  return times(count, (i) => `https://picsum.photos/200?${i + seed}`);
+};
+
+function RadialWithRefresh(props) {
+  const [avatars, setAvatars] = useState(generateAvatars());
+  const [seed, setSeed] = useState(Math.random());
+  return (
+    <>
+      <AvatarCloud radial avatars={avatars} seed={seed} {...props} />
+      <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+        <Button title="Three" onPress={() => setAvatars(generateAvatars(3))} />
+        <Button title="Ten" onPress={() => setAvatars(generateAvatars(10))} />
+        <Button
+          title="One Hundred"
+          onPress={() => setAvatars(generateAvatars(100))}
+        />
+      </View>
+      <Button title="Refresh" style={{ marginBottom: 30 }} onPress={() => setSeed(Math.random())} />
+    </>
+  );
+}
+
 storiesOf('ui-kit/Avatar/AvatarCloud', module)
   .add('default', () => <AvatarCloud avatars={avatars} />)
-  .add('radial', () => (
-    <AvatarCloud
-      radial
-      primaryAvatar={'https://picsum.photos/200'}
-      avatars={avatars}
-    />
+  .add('radial', () => <RadialWithRefresh />)
+  .add('radial w/ primary avatar', () => (
+    <RadialWithRefresh primaryAvatar={'https://picsum.photos/200'} />
   ))
   .add('blur (false)', () => <AvatarCloud avatars={avatars} blur={false} />)
   .add('isLoading', () => (
