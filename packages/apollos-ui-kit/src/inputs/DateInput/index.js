215d417cd4fa96582f70b483e6de18f536cf0ff8
Vincent Wilson <vince@classyh.at>
2020-09-30 10:19:03 -0400

    fix: Date picker defaults (#1591)
    * Included a permissions lib
    
    * Better date picker default props
    
    * Add more helpful date comments
    
    * Fix color prop
    
    * Bump version of datepicker module to support ios 14
    
    * Update storybook to latest RN datepicker
    
    * Fix ugly styles on iOS
    
    * Forgot platform
    
    * Fixing typos v3
    
    Co-authored-by: christinamegura <52924611+christinamegura@users.noreply.github.com>
    Co-authored-by: Michael Neeley <micneeley14@gmail.com>

--- packages/apollos-ui-kit/src/inputs/DateInput/index.js
+++ packages/apollos-ui-kit/src/inputs/DateInput/index.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
-import { Animated } from 'react-native';
+import { Animated, Platform } from 'react-native';
 import DateTimePicker from 'react-native-modal-datetime-picker';
 
 import Chip from '../../Chip';
@@ -37,6 +37,7 @@ class DateInput extends PureComponent {
     onChangeText: PropTypes.func,
     onBlur: PropTypes.func,
     error: PropTypes.any, // eslint-disable-line
+    minimumDate: PropTypes.any, // eslint-disable-line
     maximumDate: PropTypes.any, // eslint-disable-line
   };
 
@@ -74,16 +75,22 @@ class DateInput extends PureComponent {
           date={
             this.props.value
               ? moment(this.props.value).toDate()
-              : new Date(Date.now())
+              : moment(Date.now())
+                  .subtract(18, 'years')
+                  .toDate() // 18 years in the past, to ensure you don't have to change the year first on iOS
           } // Using Date.now so we have something to mock in the tests
           datePickerModeAndroid={'spinner'}
           isVisible={this.state.isVisible}
           maximumDate={
             this.props.maximumDate
               ? moment(this.props.maximumDate).toDate()
-              : new Date(Date.now())
+              : moment(Date.now())
+                  .subtract(16, 'years')
+                  .toDate() // sixteen year in the past to limit signups for 16 > year olds
           } // Using Date.now so we have something to mock in the tests
+          minimumDate={this.props.minimumDate}
           mode={'date'}
+          display={Platform.OS === 'android' ? 'calendar' : 'spinner'}
           onConfirm={this.handleConfirm}
           onCancel={this.handleClose}
         />
@@ -100,4 +107,4 @@ class DateInput extends PureComponent {
   }
 }
 
-export default DateInput;
+export default withTheme(() => ({}), 'ui-kit.inputs.DateInput')(DateInput);
