62fdbd695f9ce3f34ac343eb177537e8e0666c56
Vincent Wilson <vince@classyh.at>
2020-09-30 08:53:55 -0400

    fix: AvatarCloud improvements (#1595)
    * Futzing with a more radially oriented avatar cloud
    
    * Finalize code, cleanup code, add tests
    
    * Fix linter
    
    * Really fix linter

--- packages/apollos-ui-kit/src/Avatar/AvatarCloud/AvatarCloud.stories.js
+++ packages/apollos-ui-kit/src/Avatar/AvatarCloud/AvatarCloud.stories.js
@@ -1,30 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import { View } from 'react-native';
 import { storiesOf } from '@apollosproject/ui-storybook';
+import { times } from 'lodash';
 
+import { Button } from '../..';
 import AvatarCloud from '.';
 
-const avatars = [
-  'https://picsum.photos/200?1',
-  'https://picsum.photos/200?2',
-  'https://picsum.photos/200?3',
-];
+const generateAvatars = (count = 10) => {
+  const seed = Math.abs(Math.random() * 100);
+  return times(count, (i) => `https://picsum.photos/200?${i + seed}`);
+};
+
+function RadialWithRefresh(props) {
+  const [avatars, setAvatars] = useState(generateAvatars());
+  const [seed, setSeed] = useState(Math.random());
+  return (
+    <>
+      <AvatarCloud radial avatars={avatars} seed={seed} {...props} />
+      {/* eslint-disable-next-line react-native/no-inline-styles */}
+      <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+        <Button title="Three" onPress={() => setAvatars(generateAvatars(3))} />
+        <Button title="Ten" onPress={() => setAvatars(generateAvatars(10))} />
+        <Button
+          title="One Hundred"
+          onPress={() => setAvatars(generateAvatars(100))}
+        />
+      </View>
+      <Button
+        title="Refresh"
+        style={{ marginBottom: 30 }} // eslint-disable-line react-native/no-inline-styles
+        onPress={() => setSeed(Math.random())}
+      />
+    </>
+  );
+}
 
 storiesOf('ui-kit/Avatar/AvatarCloud', module)
-  .add('default', () => <AvatarCloud avatars={avatars} />)
-  .add('blur (false)', () => <AvatarCloud avatars={avatars} blur={false} />)
+  .add('default', () => <AvatarCloud avatars={generateAvatars()} />)
+  .add('radial', () => <RadialWithRefresh />)
+  .add('radial w/ primary avatar', () => (
+    <RadialWithRefresh primaryAvatar={'https://picsum.photos/200'} />
+  ))
+  .add('blur (false)', () => (
+    <AvatarCloud avatars={generateAvatars()} blur={false} />
+  ))
   .add('isLoading', () => (
     <AvatarCloud avatars={['', '', '']} primaryAvatar={''} isLoading />
   ))
   .add('maxAvatarWidth', () => (
-    <AvatarCloud avatars={avatars} maxAvatarWidth={0.1} />
+    <AvatarCloud avatars={generateAvatars()} maxAvatarWidth={0.1} />
   ))
   .add('minAvatarWidth', () => (
-    <AvatarCloud avatars={avatars} minAvatarWidth={1} />
+    <AvatarCloud avatars={generateAvatars()} minAvatarWidth={1} />
   ))
   .add('primaryAvatar', () => (
     <AvatarCloud
-      avatars={avatars}
+      avatars={generateAvatars()}
       primaryAvatar={'https://picsum.photos/200'}
     />
   ));
