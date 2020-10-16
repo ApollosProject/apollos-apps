e63874fe6e07b58c39899220cc7ed05032e8fdff
Vincent Wilson <vince@classyh.at>
2020-10-16 13:06:10 -0400

    feat: SearchFeed connected components (#1619)
    * SearchFeed connected components
    
    * Add snaps

--- packages/apollos-ui-fragments/src/index.js
+++ packages/apollos-ui-fragments/src/index.js
@@ -6,6 +6,7 @@ import * as pass from './pass';
 import * as scripture from './scripture';
 import * as user from './user';
 import * as interfaces from './interfaces';
+import * as searchResults from './searchResults';
 
 const FRAGMENTS = {
   ...campus,
@@ -16,6 +17,7 @@ const FRAGMENTS = {
   ...pass,
   ...user,
   ...interfaces,
+  ...searchResults,
 };
 
 export default FRAGMENTS;
