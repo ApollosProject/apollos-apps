6f68804d222d595b59166231b50619eba431412f
Vincent Wilson <vince@classyh.at>
2020-10-28 12:55:19 -0400

    fix: Custom icon prop type warning (#1637)
    * Fix custom icon prop type warning
    
    * Update packages/apollos-ui-kit/src/Icon/Icon.js
    
    * Update packages/apollos-ui-kit/src/Icon/Icon.js
    
    * Fix linter

--- packages/apollos-ui-kit/src/Button/ButtonIcon/ButtonIcon.stories.js
+++ packages/apollos-ui-kit/src/Button/ButtonIcon/ButtonIcon.stories.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { storiesOf } from '@apollosproject/ui-storybook';
 
+import CenteredView from '../../CenteredView';
 import ButtonIcon from '.';
 
 storiesOf('ui-kit/Buttons/Icon', module)
+  .addDecorator((story) => (
+    <CenteredView style={{ alignItems: 'stretch' }}>{story()}</CenteredView> // eslint-disable-line
+  ))
   .add('default', () => <ButtonIcon name={'umbrella'} />)
+  .add('custom icon', () => <ButtonIcon name={'custom'} />)
   .add('iconPadding', () => <ButtonIcon name={'umbrella'} iconPadding={50} />)
   .add('disabled', () => <ButtonIcon name={'umbrella'} disabled />)
   .add('isLoading', () => <ButtonIcon name={'umbrella'} disabled isLoading />)
