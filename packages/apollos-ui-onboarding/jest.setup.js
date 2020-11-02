5bc358841fad26c53d7e97bc0de574e7fbc75106
Michael Neeley <micneeley14@gmail.com>
2020-11-02 14:26:28 -0500

    onboarding tests

--- packages/apollos-ui-onboarding/jest.setup.js
+++ packages/apollos-ui-onboarding/jest.setup.js
@@ -5,12 +5,6 @@ import FRAGMENTS from '@apollosproject/ui-fragments';
 
 ApollosConfig.loadJs({ FRAGMENTS });
 
-jest.mock('react-native-safe-area-context', () => ({
-  SafeAreaConsumer: ({ children }) =>
-    children({ top: 0, bottom: 0, left: 0, right: 0 }),
-  SafeAreaProvider: ({ children }) => children,
-}));
-
 jest.mock('@apollosproject/ui-analytics', () => ({
   track: () => '',
   AnalyticsProvider: ({ children }) => children,
