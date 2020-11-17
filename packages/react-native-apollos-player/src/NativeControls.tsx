53b7426a2b54f25f75f14f0c7174f464a0bbb05c
Michael Neeley <micneeley14@gmail.com>
2020-11-17 11:15:06 -0500

    optimized a bit

--- packages/react-native-apollos-player/src/NativeControls.tsx
+++ packages/react-native-apollos-player/src/NativeControls.tsx
@@ -3,13 +3,37 @@ import MusicControl from 'react-native-music-control';
 import { NowPlayingContext, InternalPlayerContext } from './context';
 
 export default () => {
-  const { nowPlaying, setIsPlaying, skip } = useContext(NowPlayingContext);
+  const { nowPlaying, setIsPlaying, skip, isPlaying } = useContext(
+    NowPlayingContext
+  );
   const { playheadRef } = useContext(InternalPlayerContext);
 
+  // set metadata when media changes
   useEffect(() => {
     MusicControl.enableBackgroundMode(true);
     MusicControl.handleAudioInterruptions(true);
+    MusicControl.setNowPlaying({
+      title: nowPlaying?.presentationProps?.title,
+      // URL or RN's image require()
+      artwork: Array.isArray(nowPlaying?.coverImage)
+        ? nowPlaying?.coverImage?.[0]?.uri
+        : nowPlaying?.coverImage?.uri,
+      //artist: 'Church',
+      //album: 'Sermons - Series',
+      //genre: 'Post-disco, Rhythm and Blues, Funk, Dance-pop',
+      duration: playheadRef?.current?.playableDuration, // (Seconds)
+      description: nowPlaying?.presentationProps?.description, // Android Only
+      color: 0xffffff, // Android Only - Notification Color
+      colorized: true, // Android 8+ Only - Notification Color extracted from the artwork. Set to false to use the color property instead
+      //date: '1983-01-02T00:00:00Z', // Release Date (RFC 3339) - Android Only
+      //rating: 84, // Android Only (Boolean or Number depending on the type)
+      //notificationIcon: 'my_custom_icon', // Android Only (String), Android Drawable resource name for a custom notification icon
+      //isLiveStream: true, // iOS Only (Boolean), Show or hide Live Indicator instead of seekbar on lock screen for live streams. Default value is false.
+    });
+  }, [nowPlaying, playheadRef]);
 
+  // configure listeners
+  useEffect(() => {
     MusicControl.enableControl('play', true);
     // @ts-ignore
     MusicControl.on('play', () => {
@@ -33,30 +57,20 @@ export default () => {
       skip(30);
     });
 
-    MusicControl.setNowPlaying({
-      title: nowPlaying?.presentationProps?.title,
-      // URL or RN's image require()
-      artwork: Array.isArray(nowPlaying?.coverImage)
-        ? nowPlaying?.coverImage?.[0]?.uri
-        : nowPlaying?.coverImage?.uri,
-      //artist: 'Church',
-      //album: 'Sermons - Series',
-      //genre: 'Post-disco, Rhythm and Blues, Funk, Dance-pop',
-      duration: playheadRef?.current?.playableDuration, // (Seconds)
-      description: nowPlaying?.presentationProps?.description, // Android Only
-      color: 0xffffff, // Android Only - Notification Color
-      colorized: true, // Android 8+ Only - Notification Color extracted from the artwork. Set to false to use the color property instead
-      //date: '1983-01-02T00:00:00Z', // Release Date (RFC 3339) - Android Only
-      //rating: 84, // Android Only (Boolean or Number depending on the type)
-      //notificationIcon: 'my_custom_icon', // Android Only (String), Android Drawable resource name for a custom notification icon
-      //isLiveStream: true, // iOS Only (Boolean), Show or hide Live Indicator instead of seekbar on lock screen for live streams. Default value is false.
-    });
+    // Remote (headphones) play/pause
+    MusicControl.enableControl('togglePlayPause', true);
+    // @ts-ignore
+    MusicControl.on('togglePlayPause', () =>
+      isPlaying ? setIsPlaying(false) : setIsPlaying(true)
+    );
+  }, [setIsPlaying, skip, isPlaying]);
 
-    // unmount
+  // unmount
+  useEffect(() => {
     return () => {
       MusicControl.stopControl();
     };
-  }, [nowPlaying, playheadRef, setIsPlaying, skip]);
+  }, []);
 
   return null;
 };
