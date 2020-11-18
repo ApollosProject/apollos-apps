420042b5396518cd64b9fd0bc93bccae5ce50d2c
Michael Neeley <micneeley14@gmail.com>
2020-11-18 13:39:09 -0500

    sets up test stuff

--- packages/apollos-ui-media-player/src/__tests__/index.test.tsx
+++ packages/apollos-ui-media-player/src/__tests__/index.test.tsx
@@ -1,1 +1,10 @@
-it.todo('write a test');
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { ApollosPlayerContainer } from '../index';
+
+describe('The media player', () => {
+  it('renders', () => {
+    const tree = renderer.create(<ApollosPlayerContainer />);
+    expect(tree).toMatchSnapshot();
+  });
+});
