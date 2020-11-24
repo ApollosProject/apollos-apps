6fee44db436f289db3e57f346fd5b28cf8d7070f
Vincent Wilson <vincent@differential.com>
2020-11-24 13:31:39 -0500

    Turn failing test back on

--- packages/apollos-ui-media-player/src/__tests__/index.test.tsx
+++ packages/apollos-ui-media-player/src/__tests__/index.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import ShallowRenderer from 'react-test-renderer/shallow';
+// import ShallowRenderer from 'react-test-renderer/shallow';
+import renderer from 'react-test-renderer';
 import { Text } from 'react-native';
 import { ApollosPlayerContainer } from '../index';
 import { Providers } from '@apollosproject/ui-test-utils';
@@ -8,8 +9,25 @@ jest.useFakeTimers();
 
 describe('The media player', () => {
   it('renders', () => {
-    const renderer = new ShallowRenderer();
-    const tree = renderer.render(<Providers><ApollosPlayerContainer
+  // SHALLOW RENDER - only use if we can't get a normal ren der to work
+  //   const renderer = new ShallowRenderer();
+  //   const tree = renderer.render(<Providers><ApollosPlayerContainer
+  //   source={{
+  //     uri:
+  //       'http://embed.wistia.com/deliveries/0e364f7e6f6604384ece8a35905a53a864386e9f.bin',
+  //   }}
+  //   presentationProps={{
+  //     title: 'Video Title',
+  //     description: 'Video Description',
+  //   }}
+  //   coverImage={{
+  //     uri: `https://picsum.photos/seed/${Math.random()}/100/100`,
+  //   }}
+  // >
+  //   <Text>---------</Text>
+  // </ApollosPlayerContainer></Providers>);
+
+    const tree = renderer.create(<Providers><ApollosPlayerContainer
     source={{
       uri:
         'http://embed.wistia.com/deliveries/0e364f7e6f6604384ece8a35905a53a864386e9f.bin',
@@ -19,11 +37,11 @@ describe('The media player', () => {
       description: 'Video Description',
     }}
     coverImage={{
-      uri: `https://picsum.photos/seed/${Math.random()}/100/100`,
+      uri: `https://picsum.photos/seed/123/100/100`,
     }}
   >
     <Text>---------</Text>
-  </ApollosPlayerContainer></Providers>);
+  </ApollosPlayerContainer></Providers>);  
     expect(tree).toMatchSnapshot();
   });
 });
