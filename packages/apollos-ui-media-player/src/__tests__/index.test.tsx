5dcdaddcda58b0bae2c94741440cbf34c3369870
Conrad VanLandingham <conrad@differential.com>
2020-11-24 12:37:33 -0600

    add jest types

--- packages/apollos-ui-media-player/src/__tests__/index.test.tsx
+++ packages/apollos-ui-media-player/src/__tests__/index.test.tsx
@@ -9,39 +9,43 @@ jest.useFakeTimers();
 
 describe('The media player', () => {
   it('renders', () => {
-  // SHALLOW RENDER - only use if we can't get a normal ren der to work
-  //   const renderer = new ShallowRenderer();
-  //   const tree = renderer.render(<Providers><ApollosPlayerContainer
-  //   source={{
-  //     uri:
-  //       'http://embed.wistia.com/deliveries/0e364f7e6f6604384ece8a35905a53a864386e9f.bin',
-  //   }}
-  //   presentationProps={{
-  //     title: 'Video Title',
-  //     description: 'Video Description',
-  //   }}
-  //   coverImage={{
-  //     uri: `https://picsum.photos/seed/${Math.random()}/100/100`,
-  //   }}
-  // >
-  //   <Text>---------</Text>
-  // </ApollosPlayerContainer></Providers>);
+    // SHALLOW RENDER - only use if we can't get a normal ren der to work
+    //   const renderer = new ShallowRenderer();
+    //   const tree = renderer.render(<Providers><ApollosPlayerContainer
+    //   source={{
+    //     uri:
+    //       'http://embed.wistia.com/deliveries/0e364f7e6f6604384ece8a35905a53a864386e9f.bin',
+    //   }}
+    //   presentationProps={{
+    //     title: 'Video Title',
+    //     description: 'Video Description',
+    //   }}
+    //   coverImage={{
+    //     uri: `https://picsum.photos/seed/${Math.random()}/100/100`,
+    //   }}
+    // >
+    //   <Text>---------</Text>
+    // </ApollosPlayerContainer></Providers>);
 
-    const tree = renderer.create(<Providers><ApollosPlayerContainer
-    source={{
-      uri:
-        'http://embed.wistia.com/deliveries/0e364f7e6f6604384ece8a35905a53a864386e9f.bin',
-    }}
-    presentationProps={{
-      title: 'Video Title',
-      description: 'Video Description',
-    }}
-    coverImage={{
-      uri: `https://picsum.photos/seed/123/100/100`,
-    }}
-  >
-    <Text>---------</Text>
-  </ApollosPlayerContainer></Providers>);  
+    const tree = renderer.create(
+      <Providers>
+        <ApollosPlayerContainer
+          source={{
+            uri:
+              'http://embed.wistia.com/deliveries/0e364f7e6f6604384ece8a35905a53a864386e9f.bin',
+          }}
+          presentationProps={{
+            title: 'Video Title',
+            description: 'Video Description',
+          }}
+          coverImage={{
+            uri: `https://picsum.photos/seed/123/100/100`,
+          }}
+        >
+          <Text>---------</Text>
+        </ApollosPlayerContainer>
+      </Providers>
+    );
     expect(tree).toMatchSnapshot();
   });
 });
