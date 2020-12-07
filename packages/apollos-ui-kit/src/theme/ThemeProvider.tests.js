947a156894866d388f3ece620d59f0dec94768b6
Christina Megura <cmegura@gmail.com>
2020-12-07 14:34:56 -0500

    Working on tests

--- packages/apollos-ui-kit/src/theme/ThemeProvider.tests.js
+++ packages/apollos-ui-kit/src/theme/ThemeProvider.tests.js
@@ -1,27 +1,55 @@
 
 import React from 'react';
+import { useColorScheme } from 'react-native-appearance';
 import renderer from 'react-test-renderer';
+
+import FlexedView from '../FlexedView';
+import { H3 } from '../typography';
 
 import { ThemeProvider } from './';
 
+const themeExample = () => {
+  const colorScheme = useColorScheme();
+  const isDarkMode = colorScheme === 'dark';
+  const themeColor = isDarkMode ?
+    themeInput.type === 'dark' :
+    themeInput.type === 'light';
+  return (
+    <FlexedView>
+      <H3 style={themeColor}>Hello there</H3>/>
+    </FlexedView>
+  )
+}
+
+const mockColorScheme = jest.fn()
+jest.mock('react-native-appearance', () => ({
+  useColorScheme: mockColorScheme,
+}))
+
+// recreate complex behavior such that multiple function calls produce different results
 describe('ThemeProvider', () => {
   it('defaults to user settings', () => {
     const tree = renderer.create(
       <ThemeProvider>
+        <themeExample />
       </ThemeProvider>
     );
     expect(tree).toMatchSnapshot();
   });
-  it('switches to light theme', () => {
+  it('renders in light theme', () => {
+    mockColorScheme.mockImplementationOnce(() => 'light');
     const tree = renderer.create(
-      <ThemeProvider themeInput={{ type: 'light' }}>
+      <ThemeProvider>
+        <themeExample />
       </ThemeProvider>
     );
     expect(tree).toMatchSnapshot();
   });
-  it('switches to dark theme', () => {
+  it('renders in dark theme', () => {
+    mockColorScheme.mockImplementationOnce(() => 'dark');
     const tree = renderer.create(
-      <ThemeProvider themeInput={{ type: 'dark' }}>
+      <ThemeProvider>
+        <themeExample />
       </ThemeProvider>
     );
     expect(tree).toMatchSnapshot();
