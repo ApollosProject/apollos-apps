ef3d47019b63a3fc1dd6cb0ea905ab82b40b1850
Vincent Wilson <vincent@differential.com>
2021-01-20 10:17:10 -0500

    Remove runInBand by using different test databases for different works

--- packages/apollos-data-connector-postgres/__mocks__/@apollosproject/config.js
+++ packages/apollos-data-connector-postgres/__mocks__/@apollosproject/config.js
@@ -2,7 +2,7 @@ const ApolloServer = require.requireActual('@apollosproject/config').default;
 
 ApolloServer.loadJs({
   DATABASE: {
-    URL: `sqlite:${process.env.PWD}/testing.db`,
+    URL: `sqlite:${process.env.PWD}/testing-${process.env.JEST_WORKER_ID}.db`,
   },
 });
 
