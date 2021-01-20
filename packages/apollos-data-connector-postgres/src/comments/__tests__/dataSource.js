04fd40967c05ef0467d08dfae9331e96f03c90b1
Jimmy Hogoboom <jimmy@differential.com>
2021-01-20 09:39:54 -0600

    Add test for duplicate comment prevention

--- packages/apollos-data-connector-postgres/src/comments/__tests__/dataSource.js
+++ packages/apollos-data-connector-postgres/src/comments/__tests__/dataSource.js
@@ -23,6 +23,7 @@ describe('Apollos Postgres Comments DatSource', () => {
   afterEach(async () => {
     await sequelize.dropAllSchemas();
   });
+
   it('should support creating new comments', async () => {
     const commentDataSource = new CommentDataSource();
     commentDataSource.initialize({ context });
@@ -36,11 +37,40 @@ describe('Apollos Postgres Comments DatSource', () => {
     expect(comment.apollosId).toBe(createGlobalId(comment.id, 'Comment'));
   });
 
+  it('should prevent creating duplicate comments', async () => {
+    const commentDataSource = new CommentDataSource();
+    commentDataSource.initialize({ context });
+
+    const commentArgs = {
+      text: 'I am a fun comment!',
+      parentId: createGlobalId(123, 'UniversalContentItem'),
+    };
+
+    await commentDataSource.addComment(commentArgs);
+
+    // Make sure the error assertion is made by expecting all the assertions below
+    expect.assertions(2);
+
+    try {
+      await commentDataSource.addComment(commentArgs);
+    } catch (e) {
+      expect(e).toBeTruthy();
+    }
+
+    const comments = await commentDataSource.getForNode({
+      nodeId: 123,
+      nodeType: 'UniversalContentItem',
+    });
+    expect(comments.length).toBe(1);
+  });
+
   it('should return comments for a given node', async () => {
     const commentDataSource = new CommentDataSource();
     commentDataSource.initialize({ context });
 
+    // eslint-disable-next-line no-restricted-syntax
     for (const index of range(10)) {
+      // eslint-disable-next-line no-await-in-loop
       await commentDataSource.addComment({
         text: `I am a fun comment #${index}!`,
         parentId: createGlobalId(123, 'UniversalContentItem'),
