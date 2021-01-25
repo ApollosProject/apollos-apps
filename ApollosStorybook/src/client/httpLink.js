1bfd240e4cf9b8de4072fe2d6f29f8a85c714c96
Vincent Wilson <vincent@differential.com>
2021-01-25 12:51:42 -0500

    Get storybook running w/ Android

--- ApollosStorybook/src/client/httpLink.js
+++ ApollosStorybook/src/client/httpLink.js
@@ -6,10 +6,10 @@ import { createHttpLink } from 'apollo-link-http';
 import { getMainDefinition } from 'apollo-utilities';
 
 let uri = ApollosConfig.APP_DATA_URL;
-const androidUri = ApollosConfig.ANDROID_URL || '10.0.2.2';
+// const androidUri = ApollosConfig.ANDROID_URL || '10.0.2.2';
 
 // Android's emulator requires localhost network traffic to go through 10.0.2.2
-if (Platform.OS === 'android') uri = uri.replace('localhost', androidUri);
+// if (Platform.OS === 'android') uri = uri.replace('localhost', androidUri);
 
 export default split(
   ({ query }) => {
