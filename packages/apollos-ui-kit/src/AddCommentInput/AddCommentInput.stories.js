7bb9b41e1a181b5e3590b18c412a588bcab212cc
Vincent Wilson <vince@classyh.at>
2021-01-29 07:23:40 -0800

    feat: Add scripture comment component (#1694)
    * Adds scripture comment component
    
    * Progress towards add comment input
    
    * Better storybook story
    
    * Trying to make that keyboard move :/
    
    * Modal aproach being investigated
    
    * Removed modal implementation for now and added profile prop
    
    * Removed references to react-native-root-modal
    
    * Ensure profile.image is optional
    
    * Add tests
    
    * Pick up packagejson changes
    
    * Snaps
    
    * Remove testing dbs
    
    Co-authored-by: Jimmy Hogoboom <jimmy@Differential.com>

---
+++ packages/apollos-ui-kit/src/AddCommentInput/AddCommentInput.stories.js
@@ -0,0 +1,61 @@
+/* eslint-disable react-native/no-inline-styles */
+
+import React from 'react';
+import { storiesOf } from '@apollosproject/ui-storybook';
+import {
+  View,
+  FlatList,
+  Text,
+  ScrollView,
+  KeyboardAvoidingView,
+} from 'react-native';
+import { times } from 'lodash';
+
+import ScriptureComment from '../ScriptureComment';
+import AddCommentInput from './AddCommentInput';
+
+const fakeData = [
+  { __typename: 'AddCommentInput' },
+  ...times(10, (i) => ({
+    __typename: 'Comment',
+    profile: {
+      image: { uri: `https://picsum.photos/seed/${i}/200` },
+      nickName: 'Albert Flores',
+    },
+    subtitle: 'Anderson Campus',
+    onPressLike: () => ({}),
+    onPressActionMenu: () => ({}),
+    commentText:
+      'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+  })),
+];
+
+storiesOf('ui-kit/AddCommentInput', module)
+  .add('default', () => (
+    <View style={{ justifyContent: 'flex-end', flex: 1 }}>
+      <AddCommentInput
+        profile={{
+          image: { uri: 'https://picsum.photos/200' },
+        }}
+      />
+    </View>
+  ))
+  .add('with content and comments', () => (
+    <KeyboardAvoidingView behavior={'padding'} style={{ flex: 1 }}>
+      <ScrollView>
+        {times(30, () => (
+          <Text>{'----------------'}</Text>
+        ))}
+        <FlatList
+          renderItem={({ item }) =>
+            item.__typename === 'Comment' ? (
+              <ScriptureComment {...item} />
+            ) : (
+              <AddCommentInput />
+            )
+          }
+          data={fakeData}
+        />
+      </ScrollView>
+    </KeyboardAvoidingView>
+  ));
