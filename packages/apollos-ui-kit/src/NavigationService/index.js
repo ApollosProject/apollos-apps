e53412fbbacf920a3bb6541e72da71cb11ea568e
Vincent Wilson <vince@classyh.at>
2021-02-02 06:00:20 -0800

    fix: Adds support for @react-navigation onReady callback (#1708)

--- packages/apollos-ui-kit/src/NavigationService/index.js
+++ packages/apollos-ui-kit/src/NavigationService/index.js
@@ -1,12 +1,13 @@
 import { CommonActions } from '@react-navigation/native';
 
 let _navigator;
+let _ready = false;
 let _pendingActions = [];
 
 const getNavigator = () => _navigator;
 
 const performWhenReady = (func) => (...args) => {
-  if (_navigator) {
+  if (_navigator && _ready) {
     func(...args);
   } else {
     _pendingActions.push(() => func(...args));
@@ -15,10 +16,18 @@ const performWhenReady = (func) => (...args) => {
 
 const setTopLevelNavigator = (navigatorRef) => {
   _navigator = navigatorRef;
-  if (_pendingActions.length > 0) {
+  if (_pendingActions.length > 0 && _ready) {
     _pendingActions.forEach((action) => action());
+    _pendingActions = [];
   }
-  _pendingActions = [];
+};
+
+const setIsReady = () => {
+  _ready = true;
+  if (_pendingActions.length > 0 && _navigator) {
+    _pendingActions.forEach((action) => action());
+    _pendingActions = [];
+  }
 };
 
 const navigate = performWhenReady((...args) => {
@@ -61,4 +70,5 @@ export default {
   resetToAuth,
   resetAction,
   getNavigator,
+  setIsReady,
 };
