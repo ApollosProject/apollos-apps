1af362ea152dee035848ef0f7a992fa13c49017d
Vincent Wilson <vince@classyh.at>
2021-02-03 11:06:01 -0800

    chore: Get storybook running w/ Android (#1697)
    * Get storybook running w/ Android
    
    * Update httpLink.js
    
    * podfile
    
    Co-authored-by: Michael Neeley <micneeley14@gmail.com>

--- ApollosStorybook/android/app/src/main/java/com/apollosstorybook/MainApplication.java
+++ ApollosStorybook/android/app/src/main/java/com/apollosstorybook/MainApplication.java
@@ -11,6 +11,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import androidx.multidex.MultiDexApplication;
 
 public class MainApplication extends MultiDexApplication implements ReactApplication {
 
