695e8cb4df9331aba19fc1666e5566d9f2545e2f
Vincent Wilson <vince@classyh.at>
2021-02-15 11:29:22 -0800

    feat: Add NodeSingleConnected live stream support. (#1741)
    * Fix missing cast
    
    * Add livestream support to node single
    
    * Apply suggestions from code review
    
    thanks
    
    Co-authored-by: Michael Neeley <micneeley14@gmail.com>
    
    * Update packages/apollos-ui-connected/src/NodeSingleConnected/index.js
    
    Co-authored-by: Michael Neeley <micneeley14@gmail.com>
    
    * Update packages/apollos-ui-connected/src/NodeSingleConnected/index.js
    
    * Fix linter
    
    Co-authored-by: Michael Neeley <micneeley14@gmail.com>

--- ApollosStorybook/loadConfig.js
+++ ApollosStorybook/loadConfig.js
@@ -1,4 +1,16 @@
 import ApollosConfig from '@apollosproject/config';
 import FRAGMENTS from '@apollosproject/ui-fragments';
+import { fragmentTypes } from '@apollosproject/ui-test-utils';
 
-ApollosConfig.loadJs({ FRAGMENTS });
+const TYPEMAP = fragmentTypes.__schema.types.reduce((acc, curr) => {
+  const { name } = curr;
+  const types = Object.fromEntries(
+    curr.possibleTypes.map((type) => [type.name, name])
+  );
+  Object.keys(types).forEach((key) => {
+    acc[key] = acc[key] ? [...acc[key], types[key]] : [types[key]];
+  });
+  return acc;
+}, {});
+
+ApollosConfig.loadJs({ FRAGMENTS, TYPEMAP });
