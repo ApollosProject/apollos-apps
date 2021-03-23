844e7f260085e1c3b285b73da2d25c8ac672c4c5
Vincent Wilson <vince@classyh.at>
2021-03-23 17:23:03 -0400

    Onboard only certain slides

--- packages/apollos-ui-onboarding/src/onboardingStatus.js
+++ packages/apollos-ui-onboarding/src/onboardingStatus.js
@@ -9,33 +9,45 @@ export const WITH_USER_ID = gql`
   }
 `;
 
-export const onboardingComplete = async ({ userId }) => {
+const makeOnboardingKey = ({ userId }) => `@onboarding-status/${userId}`;
+
+export const onboardingComplete = async ({ userId, version }) => {
   try {
-    const jsonValue = JSON.stringify(true);
-    await AsyncStorage.setItem(`@onboarding-status/${userId}`, jsonValue);
+    const jsonValue = JSON.stringify(version);
+    const key = makeOnboardingKey({ userId });
+    await AsyncStorage.setItem(key, jsonValue);
   } catch (e) {
     // saving error
   }
 };
 
+const safeGetOnboardingStatus = async ({ userId }) => {
+  try {
+    const key = makeOnboardingKey({ userId });
+    const jsonValue = await AsyncStorage.getItem(key);
+    return JSON.parse(jsonValue);
+  } catch (e) {
+    // error reading value
+  }
+  return null;
+};
+
 export const checkOnboardingStatusAndNavigate = async ({
   client,
   navigation,
+  latestOnboardingVersion = 1,
 }) => {
   const { data } = await client.query({ query: WITH_USER_ID });
-  let hasOnboarded = false;
+  let onboardingVersion;
   if (data.currentUser.id) {
-    try {
-      const jsonValue = await AsyncStorage.getItem(
-        `@onboarding-status/${data.currentUser.id}`
-      );
-      hasOnboarded = jsonValue != null ? JSON.parse(jsonValue) : false;
-    } catch (e) {
-      // error reading value
+    onboardingVersion = safeGetOnboardingStatus();
+    if (onboardingVersion === true) {
+      onboardingVersion = 1; // if we have onboarded before, we've seen version 0
     }
   }
 
-  if (hasOnboarded) {
+  // If the user has onboarded before, and they have seen the current version of onboarding.
+  if (onboardingVersion && onboardingVersion >= latestOnboardingVersion) {
     navigation.dispatch(
       navigation.resetAction({
         navigatorName: 'Tabs',
@@ -43,6 +55,17 @@ export const checkOnboardingStatusAndNavigate = async ({
       })
     );
   } else {
-    navigation.navigate('Onboarding');
+    navigation.navigate('Onboarding', { userVersion: onboardingVersion });
   }
+};
+
+export const hideIfSeen = (Component) => ({
+  version = 1,
+  userVersion = 0,
+  ...props
+}) => {
+  if (version > userVersion) {
+    return <Component {...props} />;
+  }
+  return null;
 };
