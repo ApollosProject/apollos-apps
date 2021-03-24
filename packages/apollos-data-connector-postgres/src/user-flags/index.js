81c7c4423b25579f2c8b12223503e560d0743863
Jimmy Hogoboom <jimmy@differential.com>
2021-03-24 16:39:46 -0500

    Migrate user-flags

--- packages/apollos-data-connector-postgres/src/user-flags/index.js
+++ packages/apollos-data-connector-postgres/src/user-flags/index.js
@@ -1,3 +1,4 @@
 export * as models from './model';
 export dataSource from './dataSource';
+export migrations from './migrations';
 export { commentSchema as schema } from '@apollosproject/data-schema';
