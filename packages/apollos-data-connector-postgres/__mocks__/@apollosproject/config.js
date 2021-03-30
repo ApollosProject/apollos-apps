567f0cb479f3ed11c85435c8d53f277953d198a4
Vincent Wilson <vincent@differential.com>
2021-03-30 12:37:09 -0400

    Follower push notification opens to connect tab

--- packages/apollos-data-connector-postgres/__mocks__/@apollosproject/config.js
+++ packages/apollos-data-connector-postgres/__mocks__/@apollosproject/config.js
@@ -1,6 +1,9 @@
 const ApolloServer = require.requireActual('@apollosproject/config').default;
 
 ApolloServer.loadJs({
+  APP: {
+    DEEP_LINK_HOST: 'apolloschurchapp',
+  },
 });
 
 export default ApolloServer;
