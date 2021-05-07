bb9737e7efea8a7fcda5d2d487a5d3f94f631a8c
Michael Neeley <micneeley14@gmail.com>
2021-05-07 14:44:31 -0400

    fix: Fix login for the first time
    This will also surface the error we're getting from Rock better

--- packages/apollos-data-connector-rock/src/auth/data-source.js
+++ packages/apollos-data-connector-rock/src/auth/data-source.js
@@ -23,48 +23,15 @@ export default class AuthDataSource extends RockApolloDataSource {
     }
 
     if (userCookie) {
-      try {
-        const request = await this.request('People/GetCurrentPerson').get({
-          options: {
-            headers: { cookie: userCookie, 'Authorization-Token': null },
-          },
-        });
-        this.context.currentPerson = request;
-        return request;
-      } catch (e) {
-        const person = await this.lookupUserFromCache({ userCookie });
-
-        if (!person) {
-          throw new AuthenticationError('Invalid user cookie; no person found');
-        }
-        // TODO: Send over a new cookie to be stored in the `set-cookie` header.
-        return person;
-      }
+      const request = await this.request('People/GetCurrentPerson').get({
+        options: {
+          headers: { cookie: userCookie, 'Authorization-Token': null },
+        },
+      });
+      this.context.currentPerson = request;
+      return request;
     }
     throw new AuthenticationError('Must be logged in');
-  };
-
-  lookupUserFromCache = async ({ userCookie }) => {
-    const { Cache } = this.context.dataSources;
-    const cachedUserName = await Cache.get({
-      key: `:userLogins:${crypto
-        .createHash('sha1')
-        .update(userCookie)
-        .digest('hex')}`,
-    });
-    if (!cachedUserName) {
-      throw new AuthenticationError(
-        'Invalid user cookie; no eligble user login found'
-      );
-    }
-    const login = await this.request('/UserLogins')
-      .filter(`UserName eq '${cachedUserName}'`)
-      .first();
-
-    if (!login || !login.personId) {
-      throw new AuthenticationError('Invalid user cookie; no user login found');
-    }
-    return this.request('/People').filter(`Id eq ${login.personId}`).first();
   };
 
   fetchUserCookie = async (Username, Password) => {
@@ -100,29 +67,15 @@ export default class AuthDataSource extends RockApolloDataSource {
   };
 
   authenticate = async ({ identity, password }) => {
-    const { Cache } = this.context.dataSources;
-    try {
-      const cookie = await this.fetchUserCookie(identity, password);
-      const sessionId = await this.createSession({ cookie });
-      const token = generateToken({ cookie, sessionId });
-      this.context.rockCookie = cookie;
-      this.context.userToken = token;
-      this.context.sessionId = sessionId;
-      Cache.set({
-        key: `:userLogins:${crypto
-          .createHash('sha1')
-          .update(cookie)
-          .digest('hex')}`,
-        data: identity,
-        expiresIn: 31556952, // one year
-      });
-      return { token, rockCookie: cookie };
-    } catch (e) {
-      if (e instanceof AuthenticationError) {
-        throw new UserInputError('Username or Password incorrect');
-      }
-      throw e;
-    }
+    const cookie = await this.fetchUserCookie(identity, password);
+    const sessionId = await this.createSession({ cookie });
+    const token = generateToken({ cookie, sessionId });
+
+    this.context.rockCookie = cookie;
+    this.context.userToken = token;
+    this.context.sessionId = sessionId;
+
+    return { token, rockCookie: cookie };
   };
 
   personExists = async ({ identity }) => {
