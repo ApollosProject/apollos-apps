caa97da8594c7495009a7e6a1d15714d3c08b995
Vincent Wilson <vince@classyh.at>
2021-05-12 09:13:58 -0400

    chore: Added simple / inline api setup (#1799)
    * Added simple / inline setup
    
    * Update scripts/setup-api.sh
    
    Co-authored-by: Michael Neeley <micneeley14@gmail.com>
    
    * Update scripts/setup-api.sh
    
    Co-authored-by: Michael Neeley <micneeley14@gmail.com>
    
    Co-authored-by: Michael Neeley <micneeley14@gmail.com>

---
+++ scripts/setup-api.sh
@@ -0,0 +1,35 @@
+rm -rf ApollosApi
+
+
+mkdir -p ApollosApi
+mkdir -p tmp
+
+echo $2
+BRANCH=$2
+
+cd tmp
+git clone git@github.com:ApollosProject/apollos-templates.git
+
+cd apollos-templates
+git checkout ${BRANCH:-master}
+tput setaf 1
+echo "Please enter your .env password (typically stored in 1Password)"
+tput sgr0
+
+read SECRET
+./scripts/secrets.sh -d $SECRET
+mv ./apollos-church-api/.env.shared ./apollos-church-api/.env
+cd ..
+
+
+
+shopt -s dotglob
+mv apollos-templates/apollos-church-api/* ../ApollosApi
+
+
+
+cd ..
+
+node scripts/swap-package-json-to-links.js ./ApollosApi
+
+rm -rf tmp
