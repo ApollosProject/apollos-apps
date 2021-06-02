21082104410c8af6d7a2338cb47b69b04f96befa
Vincent Wilson <vincent@differential.com>
2021-06-02 09:19:28 -0400

    include exports

--- packages/apollos-data-connector-postgres/src/notification-preferences/index.js
+++ packages/apollos-data-connector-postgres/src/notification-preferences/index.js
@@ -1,4 +1,4 @@
 export * as models from './model';
-// export dataSource from './dataSource';
-// export resolver from './resolver';
+export dataSource from './dataSource';
+export resolver from './resolver';
 export migrations from './migrations';
