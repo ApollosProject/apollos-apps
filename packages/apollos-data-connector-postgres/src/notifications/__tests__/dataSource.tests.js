58f64b916ea489fd44ec191fa266fd7bcf6f4fe3
Vincent Wilson <vince@classyh.at>
2021-06-07 08:44:53 -0400

    feat: Send pushes from Notification Model (#1972)
    * Added notifications model
    
    * Cleans up model and adds tests
    
    * Fix notifications model
    
    * Rename test
    
    * move towards a notification preference model
    
    * Cleaned up notification preferences
    
    * Added resolver / tests
    
    * include exports
    
    * Wrap up close to final implementation
    
    * fix linter
    
    * Integrate push with notififications model

--- packages/apollos-data-connector-postgres/src/notifications/__tests__/dataSource.tests.js
+++ packages/apollos-data-connector-postgres/src/notifications/__tests__/dataSource.tests.js
@@ -1,6 +1,8 @@
 import { sequelize, sync } from '../../postgres/index';
 import { createModel, setupModel } from '../model';
 import { createModel as createPeopleModel } from '../../people/model';
+import * as notificationPreferenceModel from '../../notification-preferences/model';
+import NotificationPreferenceDataSource from '../../notification-preferences/dataSource';
 import NotificationsDataSource from '../dataSource';
 
 let person1;
@@ -11,13 +13,28 @@ describe('Apollos Postgres Notifications DataSource', () => {
   beforeEach(async () => {
     await createPeopleModel();
     await createModel();
+    await notificationPreferenceModel.createModel();
     await setupModel();
+    await notificationPreferenceModel.setupModel();
     await sync();
 
     person1 = await sequelize.models.people.create({
       originId: '1',
       originType: 'rock',
     });
+
+    await sequelize.models.notificationPreferences.create({
+      personId: person1.id,
+      notificationProviderId: '111-bbb-ccc',
+      notificationProviderType: 'one_signal',
+      enabled: true,
+    });
+
+    const notificationPreferenceDataSource = new NotificationPreferenceDataSource();
+    notificationPreferenceDataSource.initialize({ context });
+    context.dataSources = {
+      NotificationPreference: notificationPreferenceDataSource,
+    };
   });
 
   afterEach(async () => {
@@ -29,7 +46,7 @@ describe('Apollos Postgres Notifications DataSource', () => {
     notificationDataSource.initialize({ context });
 
     notificationDataSource.DELIVERY_METHODS.one_signal = jest.fn(async () => ({
-      notificationId: '123-123-123',
+      id: '123-123-123',
     }));
 
     const notification = await notificationDataSource.createAndSend({
@@ -48,6 +65,49 @@ describe('Apollos Postgres Notifications DataSource', () => {
     expect(
       notificationDataSource.DELIVERY_METHODS.one_signal.mock.calls.length
     ).toBe(1);
+  });
+  it('should not create a notifcation without a preference', async () => {
+    const notificationDataSource = new NotificationsDataSource();
+    notificationDataSource.initialize({ context });
+
+    await sequelize.models.notificationPreferences.destroy({
+      where: {
+        personId: person1.id,
+      },
+    });
+
+    const notification = await notificationDataSource.createAndSend({
+      title: 'Some title',
+      subtitle: 'Some subtitle',
+      body: 'Some body',
+      personId: person1.id,
+      type: notificationDataSource.NOTIFICATION_TYPES.PRAYER,
+    });
+
+    expect(notification).toBe(null);
+  });
+  it('should not create a notifcation with a disabled preference', async () => {
+    const notificationDataSource = new NotificationsDataSource();
+    notificationDataSource.initialize({ context });
+
+    await sequelize.models.notificationPreferences.update(
+      { enabled: false },
+      {
+        where: {
+          personId: person1.id,
+        },
+      }
+    );
+
+    const notification = await notificationDataSource.createAndSend({
+      title: 'Some title',
+      subtitle: 'Some subtitle',
+      body: 'Some body',
+      personId: person1.id,
+      type: notificationDataSource.NOTIFICATION_TYPES.PRAYER,
+    });
+
+    expect(notification).toBe(null);
   });
   it('throws an error without personId', async () => {
     const notificationDataSource = new NotificationsDataSource();
