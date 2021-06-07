2d6044ccc2540d6515918ccd706a4f4cb26f3a7b
Michael Neeley <micneeley14@gmail.com>
2021-06-07 11:50:24 -0400

    chore: makes app developement faster (#1974)
    * chore: makes app developement faster
    
    uses the default URL for the API so there's no need to type in the encryption password
    
    * made it optional
    
    Co-authored-by: Vincent Wilson <vince@classyh.at>

--- scripts/setup-app.sh
+++ scripts/setup-app.sh
@@ -3,24 +3,27 @@ rm -rf ApollosApp
 mkdir -p ApollosApp
 mkdir -p tmp
 
-cd tmp
+cd tmp || exit
 git clone git@github.com:ApollosProject/apollos-templates.git
 
-cd apollos-templates
-git checkout ${BRANCH:-master}
-tput setaf 1
-echo "Please enter your .env password (typically stored in 1Password)"
-tput sgr0
+cd apollos-templates || exit
+git checkout "${BRANCH:-master}"
+echo "Enter password to decrypt secrets (press enter to skip):"
+read -r SECRET
+if [ "$SECRET" = "" ]; then
+    echo "APP_DATA_URL=https://apollos-church-api.herokuapp.com" >>./apolloschurchapp/.env
+else
+    ./scripts/secrets.sh -d "$SECRET"
+    mv ./apolloschurchapp/.env.shared ./apolloschurchapp/.env
+fi
 
-read SECRET
-./scripts/secrets.sh -d $SECRET
-mv ./apolloschurchapp/.env.shared ./apolloschurchapp/.env
-cd ..
+cd .. || exit
 
 shopt -s dotglob
 mv apollos-templates/apolloschurchapp/* ../ApollosApp
+cp apollos-templates/yarn.lock ../ApollosApp
 
-cd ..
+cd .. || exit
 
 node scripts/swap-package-json-to-links.js ./ApollosApp
 
