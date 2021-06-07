4bfac0eed47b5ba923ad0275fde7ab288ef35ab4
Michael Neeley <micneeley14@gmail.com>
2021-06-07 10:15:48 -0400

    made it optional

--- scripts/setup-app.sh
+++ scripts/setup-app.sh
@@ -3,18 +3,27 @@ rm -rf ApollosApp
 mkdir -p ApollosApp
 mkdir -p tmp
 
-cd tmp
+cd tmp || exit
 git clone git@github.com:ApollosProject/apollos-templates.git
 
-cd apollos-templates
-git checkout ${BRANCH:-master}
-echo "APP_DATA_URL=https://apollos-church-api.herokuapp.com" >> ./apolloschurchapp/.env
-cd ..
+cd apollos-templates || exit
+git checkout "${BRANCH:-master}"
+echo "Enter password to decrypt secrets (press enter to skip):"
+read -r SECRET
+if [ "$SECRET" = "" ]; then
+    echo "APP_DATA_URL=https://apollos-church-api.herokuapp.com" >>./apolloschurchapp/.env
+else
+    ./scripts/secrets.sh -d "$SECRET"
+    mv ./apolloschurchapp/.env.shared ./apolloschurchapp/.env
+fi
+
+cd .. || exit
 
 shopt -s dotglob
 mv apollos-templates/apolloschurchapp/* ../ApollosApp
+cp apollos-templates/yarn.lock ../ApollosApp
 
-cd ..
+cd .. || exit
 
 node scripts/swap-package-json-to-links.js ./ApollosApp
 
