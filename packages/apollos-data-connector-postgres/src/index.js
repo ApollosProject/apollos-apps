e2d7bc2e7bd4e65994279609eec3d353a81a2d11
Vincent Wilson <vincent@differential.com>
2021-06-23 13:32:27 -0400

    Fix linter

--- packages/apollos-data-connector-postgres/src/index.js
+++ packages/apollos-data-connector-postgres/src/index.js
@@ -14,6 +14,6 @@ export * as Person from './people';
 export * as Campus from './campus';
 export * as Notification from './notifications';
 export * as NotificationPreference from './notification-preferences';
-export * as Media from './media'
+export * as Media from './media';
 
 export createMigrationRunner from './postgres/performMigrations';
