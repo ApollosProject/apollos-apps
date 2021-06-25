52395ef75941c5fa260fd9bd860deeead2e3be5d
Tom Thornton <tom@differential.com>
2021-06-25 15:03:22 -0400

    chore: Update Postgres media model with orginId and originType (#2021)
    * Update 001_create_media.js
    
    * Add relationships
    
    * add migration for primary keys
    
    * Update second migration order
    
    * Update packages/apollos-data-connector-postgres/src/media/migrations/002_update_media_primary_keys.js
    
    * Fix lint error
    
    Co-authored-by: Vincent Wilson <vince@classyh.at>

--- packages/apollos-data-connector-postgres/src/media/migrations/001_create_media.js
+++ packages/apollos-data-connector-postgres/src/media/migrations/001_create_media.js
@@ -1,11 +1,20 @@
-import { DataTypes } from 'sequelize';
+import { DataTypes, Sequelize } from 'sequelize';
 
 async function up({ context: queryInterface }) {
   await queryInterface.createTable('media', {
+    originId: {
+      primaryKey: true,
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    originType: { type: Sequelize.STRING, allowNull: false },
     type: DataTypes.ENUM(['IMAGE', 'VIDEO', 'AUDIO']),
     url: DataTypes.STRING,
     nodeId: DataTypes.UUID,
     nodeType: DataTypes.STRING,
+  });
+  await queryInterface.addIndex('media', ['originId', 'originType'], {
+    unique: true,
   });
 }
 
@@ -15,4 +24,4 @@ async function down({ context: queryInterface }) {
 
 const name = '001-create-media';
 
-module.exports = { up, down, name, order: 5 };
+module.exports = { up, down, name, order: 4 };
