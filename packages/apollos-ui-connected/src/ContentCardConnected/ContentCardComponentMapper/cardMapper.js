da910be66936f35a8b66e1c706fe75e08556a7c3
Conrad VanLandingham <conrad@differential.com>
2021-06-30 16:54:23 -0500

    fully deprecate contentcardcomponentmapper

--- packages/apollos-ui-connected/src/ContentCardConnected/ContentCardComponentMapper/cardMapper.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { DefaultCard } from '@apollosproject/ui-kit';
-
-const cardMapper = (props) => {
-  return <DefaultCard {...props} />;
-};
-
-export default cardMapper;
