3fcd0cbcc5332d999c757430b99b7d0a50874e26
Conrad VanLandingham <conrad@differential.com>
2021-07-02 08:12:37 -0500

    Update packages/apollos-ui-kit/src/Card/Content.js
    Co-authored-by: Michael Neeley <micneeley14@gmail.com>

--- packages/apollos-ui-kit/src/Card/Content.js
+++ packages/apollos-ui-kit/src/Card/Content.js
@@ -1,6 +1,6 @@
 import PaddedView from '../PaddedView';
 import styled from '../styled';
 
-const Content = styled({}, 'ui-kit.Card.Content.Content')(PaddedView);
+const Content = named('ui-kit.Card.Content.Content')(PaddedView);
 
 export default Content;
