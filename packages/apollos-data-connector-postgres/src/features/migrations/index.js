a8460f796688439132067131b149aea4c02d12a0
Vincent Wilson <vince@classyh.at>
2021-07-06 14:16:38 -0400

    Feature unique index migration

--- packages/apollos-data-connector-postgres/src/features/migrations/index.js
+++ packages/apollos-data-connector-postgres/src/features/migrations/index.js
@@ -1,5 +1,6 @@
 import CreateFeatures001 from './001_create_features';
+import UniqueIndexForFeatures002 from './002_unique_index_for_features';
 
-const migrations = [CreateFeatures001];
+const migrations = [CreateFeatures001, UniqueIndexForFeatures002];
 
 export default migrations;
