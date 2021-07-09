821910be0183dd32eafca800ad3e984cf6ac039c
Tom Thornton <tom@differential.com>
2021-07-09 12:05:03 -0400

    chore: Add metadata column to Postgres media table (#2056)
    * Add media metadata column to Postgres
    
    * Change order of migration
    
    Co-authored-by: Vincent Wilson <vince@classyh.at>
    
    Co-authored-by: Vincent Wilson <vince@classyh.at>

--- packages/apollos-data-connector-postgres/src/media/migrations/index.js
+++ packages/apollos-data-connector-postgres/src/media/migrations/index.js
@@ -1,7 +1,13 @@
 import CreateMedia001 from './001_create_media';
 import UpdateMediaPrimaryKeys002 from './002_update_media_primary_keys';
 import AddIdToMedia003 from './003_add_id_to_media';
+import AddMetadataColumn004 from './004_add_metadata_column';
 
-const migrations = [CreateMedia001, UpdateMediaPrimaryKeys002, AddIdToMedia003];
+const migrations = [
+  CreateMedia001,
+  UpdateMediaPrimaryKeys002,
+  AddIdToMedia003,
+  AddMetadataColumn004,
+];
 
 export default migrations;
