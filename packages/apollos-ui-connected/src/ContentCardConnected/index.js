e8d69642b6e83c35ce949f1c51335e497f76e217
Michael Neeley <micneeley14@gmail.com>
2021-07-09 20:51:05 -0400

    chore: warn on content card mapper deprecation

--- packages/apollos-ui-connected/src/ContentCardConnected/index.js
+++ packages/apollos-ui-connected/src/ContentCardConnected/index.js
@@ -1,8 +1,12 @@
+import React from 'react';
 import { DefaultCard } from '@apollosproject/ui-kit';
 import ContentCardConnected from './ContentCardConnected';
 
-export const ContentCardComponentMapper = DefaultCard;
+export const ContentCardComponentMapper = (props) => {
+  console.warn('ContentCardComponentMapper deprecated');
+  return <DefaultCard {...props} />;
+};
 
-export GET_CONTENT_CARD from './getContentCard';
+export { default as GET_CONTENT_CARD } from './getContentCard';
 
 export default ContentCardConnected;
