4da4a95bb97ca9f8ce05327d58e300bc0df24352
Michael Neeley <micneeley14@gmail.com>
2021-07-09 21:22:13 -0400

    chore: converted ProtectedRoute to functional component

--- packages/apollos-ui-auth/src/ProtectedRoute.js
+++ packages/apollos-ui-auth/src/ProtectedRoute.js
@@ -1,65 +1,27 @@
-import React, { PureComponent } from 'react';
-import { Query } from '@apollo/client/react/components';
+import React, { useCallback, useEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { useQuery } from '@apollo/client';
 import { ActivityIndicator } from 'react-native';
 import { CenteredView } from '@apollosproject/ui-kit';
 import PropTypes from 'prop-types';
 import GET_LOGIN_STATE_WITH_CACHE_LOADED from './getLoginStateWithCacheLoaded';
 
-/* This function can be included as the default route in your navigator
-to direct the user to a different route depending on if the user is logged in or not.
+const ProtectedRoute = ({
+  loggedInRouteName,
+  loggedOutRouteName,
+  LoadingIndicator,
+  onRouteChange,
+}) => {
+  const navigation = useNavigation();
+  const {
+    data: { isLoggedIn = false, cacheLoaded = false } = {},
+    loading = true,
+  } = useQuery(GET_LOGIN_STATE_WITH_CACHE_LOADED, {
+    fetchPolicy: 'network-only',
+  });
+  const isLoading = loading || !cacheLoaded;
 
-Usage:
-```
-const AuthLoading = (props) => <AuthLoadingSwitch loggedInRouteName="Home" loggedOutRouteName="Auth" LoadingIndicator={LoadingComponent}
-
-const AppNavigator = createStackNavigator(
-  {
-    AuthLoading,
-    Home,
-    Auth,
-  },
-  {
-    initialRouteName: 'AuthLoading',
-  }
-);
-```
-*/
-
-class ProtectedRoute extends PureComponent {
-  static propTypes = {
-    navigation: PropTypes.shape({ replace: PropTypes.func.isRequired })
-      .isRequired,
-    isLoading: PropTypes.bool.isRequired,
-    isLoggedIn: PropTypes.bool.isRequired,
-    loggedInRouteName: PropTypes.string,
-    loggedOutRouteName: PropTypes.string,
-    LoadingIndicator: PropTypes.node,
-    onRouteChange: PropTypes.func,
-  };
-
-  static defaultProps = {
-    loggedOutRouteName: 'LandingScreen',
-    loggedInRouteName: 'Tabs',
-  };
-
-  componentDidMount() {
-    this.handleLoginDataChanged();
-  }
-
-  componentDidUpdate() {
-    this.handleLoginDataChanged();
-  }
-
-  handleLoginDataChanged = () => {
-    const {
-      isLoading,
-      isLoggedIn,
-      onRouteChange,
-      navigation,
-      loggedInRouteName,
-      loggedOutRouteName,
-    } = this.props;
-
+  const handleLoginDataChanged = useCallback(() => {
     const shouldNavigate = !isLoading;
     // console.warn(shouldNavigate, isLoading);
     if (shouldNavigate && onRouteChange) onRouteChange({ isLoggedIn });
@@ -68,32 +30,35 @@ class ProtectedRoute extends PureComponent {
     } else if (shouldNavigate) {
       navigation.replace(loggedOutRouteName);
     }
-  };
+  }, [
+    navigation,
+    isLoading,
+    onRouteChange,
+    isLoggedIn,
+    loggedInRouteName,
+    loggedOutRouteName,
+  ]);
+  useEffect(() => {
+    handleLoginDataChanged();
+  }, [handleLoginDataChanged]);
 
-  render() {
-    const { LoadingIndicator } = this.props;
-    if (LoadingIndicator) return LoadingIndicator;
-    return (
-      <CenteredView>
-        <ActivityIndicator />
-      </CenteredView>
-    );
-  }
-}
+  if (LoadingIndicator) return LoadingIndicator;
+  return (
+    <CenteredView>
+      <ActivityIndicator />
+    </CenteredView>
+  );
+};
 
-const ProtectedRouteWithData = (props) => (
-  <Query fetchPolicy={'network-only'} query={GET_LOGIN_STATE_WITH_CACHE_LOADED}>
-    {({
-      data: { isLoggedIn = false, cacheLoaded = false } = {},
-      loading = true,
-    }) => (
-      <ProtectedRoute
-        {...props}
-        isLoggedIn={isLoggedIn}
-        isLoading={loading || !cacheLoaded}
-      />
-    )}
-  </Query>
-);
+ProtectedRoute.propTypes = {
+  loggedInRouteName: PropTypes.string,
+  loggedOutRouteName: PropTypes.string,
+  LoadingIndicator: PropTypes.node,
+  onRouteChange: PropTypes.func,
+};
 
-export default ProtectedRouteWithData;
+ProtectedRoute.defaultProps = {
+  loggedOutRouteName: 'LandingScreen',
+  loggedInRouteName: 'Tabs',
+};
+export default ProtectedRoute;
