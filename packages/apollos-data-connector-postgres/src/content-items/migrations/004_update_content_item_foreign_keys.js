de14e3d4ad62255c0b7a6c9a057fc336a7fb592d
Tom Thornton <tom@differential.com>
2021-07-19 15:12:10 -0400

    chore: Update parentId and coverImageId foreign keys to set null (#2068)
    * Update parentId and coverImageId foreign keys to set null
    
    * lint fix

---
+++ packages/apollos-data-connector-postgres/src/content-items/migrations/004_update_content_item_foreign_keys.js
@@ -0,0 +1,45 @@
+async function up({ context: queryInterface }) {
+  await queryInterface.removeConstraint(
+    'contentItems',
+    'contentItems_parentId_fkey'
+  );
+
+  await queryInterface.removeConstraint(
+    'contentItems',
+    'contentItems_coverImageId_fkey'
+  );
+  await queryInterface.addConstraint('contentItems', {
+    fields: ['parentId'],
+    type: 'foreign key',
+    references: {
+      table: 'contentItems',
+      field: 'id',
+    },
+    onDelete: 'set null',
+  });
+  await queryInterface.addConstraint('contentItems', {
+    fields: ['coverImageId'],
+    type: 'foreign key',
+    references: {
+      table: 'media',
+      field: 'id',
+    },
+    onDelete: 'set null',
+  });
+}
+
+async function down({ context: queryInterface }) {
+  await queryInterface.removeConstraint(
+    'contentItems',
+    'contentItems_coverImageId_media_fk'
+  );
+
+  await queryInterface.removeConstraint(
+    'contentItems',
+    'contentItems_parentId_contentItems_fk'
+  );
+}
+
+const name = '004-update-content-item-foreign-keys';
+
+module.exports = { up, down, name, order: 7 };
--- packages/apollos-data-connector-postgres/src/content-items/migrations/index.js
+++ packages/apollos-data-connector-postgres/src/content-items/migrations/index.js
@@ -1,7 +1,13 @@
 import CreateContentItems001 from './001_create_content_items';
 import AddCoverImage002 from './002_add_cover_image';
 import AddParent003 from './003_add_parent';
+import UpdateContentItemForeignKeys from './004_update_content_item_foreign_keys';
 
-const migrations = [CreateContentItems001, AddCoverImage002, AddParent003];
+const migrations = [
+  CreateContentItems001,
+  AddCoverImage002,
+  AddParent003,
+  UpdateContentItemForeignKeys,
+];
 
 export default migrations;
