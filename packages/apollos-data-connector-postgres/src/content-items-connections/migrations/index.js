8f82add5b071865ff8006faf0c8edc65f8cd4320
Tom Thornton <tom@differential.com>
2021-07-20 10:09:11 -0400

    chore: Update foreign key on content item associations to delete when content items are deleted (#2066)
    * Update foreign key to delete when content items are deleted
    
    * Update content_tag foreign key constraints
    
    * Fix linting errors and update tag migration name
    
    Co-authored-by: Vincent Wilson <vince@classyh.at>

--- packages/apollos-data-connector-postgres/src/content-items-connections/migrations/index.js
+++ packages/apollos-data-connector-postgres/src/content-items-connections/migrations/index.js
@@ -1,5 +1,9 @@
 import CreateContentItemsConnection001 from './001_create_content_item_connections';
+import UpdateForeignKeyConstraints002 from './002_update_foreign_key_constraints';
 
-const migrations = [CreateContentItemsConnection001];
+const migrations = [
+  CreateContentItemsConnection001,
+  UpdateForeignKeyConstraints002,
+];
 
 export default migrations;
