d4fe1504772b89f0db2b34c7ea5ef1cdc154b64f
Nathan Lewis <nathan@differential.com>
2021-08-06 17:36:38 -0500

    fix: adds padding to the last comment item (#2098)
    * adds padding to the last comment item
    
    * changed to check number of comments

--- packages/apollos-ui-connected/src/CommentListFeatureConnected/CommentListFeature/index.js
+++ packages/apollos-ui-connected/src/CommentListFeatureConnected/CommentListFeature/index.js
@@ -1,24 +1,31 @@
-import { Comment } from '@apollosproject/ui-kit';
+import { Comment, useTheme } from '@apollosproject/ui-kit';
 import React from 'react';
 import { FlatList } from 'react-native';
 import PropTypes from 'prop-types';
 
-const CommentListFeature = ({ comments, onPressLike, onPressActionMenu }) => (
-  <FlatList
-    renderItem={({ item }) => (
-      <Comment
-        onPressLike={onPressLike && (() => onPressLike(item))}
-        onPressActionMenu={onPressActionMenu && (() => onPressActionMenu(item))}
-        subtitle={item.person?.campus?.name}
-        profile={item.person}
-        key={item.id}
-        isLiked={item.isLiked}
-        commentText={item.text}
-      />
-    )}
-    data={comments}
-  />
-);
+const CommentListFeature = ({ comments, onPressLike, onPressActionMenu }) => {
+  const theme = useTheme();
+  return (
+    <FlatList
+      renderItem={({ item }) => (
+        <Comment
+          onPressLike={onPressLike && (() => onPressLike(item))}
+          onPressActionMenu={
+            onPressActionMenu && (() => onPressActionMenu(item))
+          }
+          subtitle={item.person?.campus?.name}
+          profile={item.person}
+          key={item.id}
+          isLiked={item.isLiked}
+          commentText={item.text}
+        />
+      )}
+      data={comments}
+      // Adds a bit of padding to the last item of the list of comments
+      paddingBottom={comments?.length ? theme.sizing.baseUnit : 0}
+    />
+  );
+};
 
 CommentListFeature.propTypes = {
   onPressActionMenu: PropTypes.func,
