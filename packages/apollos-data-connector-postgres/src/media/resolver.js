37eab715d3d6d80cf68d10004df7dcdee4c12de9
Michael Neeley <micneeley14@gmail.com>
2021-08-12 18:37:39 -0400

    chore: safety check on media urls (#2117)

--- packages/apollos-data-connector-postgres/src/media/resolver.js
+++ packages/apollos-data-connector-postgres/src/media/resolver.js
@@ -1,7 +1,10 @@
 // If we have a sources array, return that.
 // Otherwise, return the URL or URI field. Add in the typename provided to the source.
-const addTypenameToSources = (__typename) => ({ sources, url, uri }) =>
-  sources || [{ __typename, uri: url || uri }];
+const addTypenameToSources = (__typename) => ({ sources, url, uri }) => {
+  const dirtyURI = url || uri;
+  const cleanURI = dirtyURI.startswith('http') ? dirtyURI : '';
+  return sources || [{ __typename, uri: cleanURI }];
+};
 
 export default {
   Media: {
