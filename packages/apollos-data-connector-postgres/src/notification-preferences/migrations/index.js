9108145640229ec74f7628efb5fc7b3b7b5c8fca
Tom Thornton <tom@differential.com>
2021-09-24 12:44:04 -0400

    Update notification preference foreign key constraint with people so that deleting people will delete the corresponding notification preferences (#2192)

--- packages/apollos-data-connector-postgres/src/notification-preferences/migrations/index.js
+++ packages/apollos-data-connector-postgres/src/notification-preferences/migrations/index.js
@@ -1,9 +1,11 @@
 import CreateNotificationPreferences001 from './001_create_notification_preferences';
 import RenameNotificationPreferences002 from './002_rename_notification_preference_fields';
+import UpdateNotificationPreferencesForeignKeyConstraint from './003_update_notification_preferences_foreign_key_constraint';
 
 const migrations = [
   CreateNotificationPreferences001,
   RenameNotificationPreferences002,
+  UpdateNotificationPreferencesForeignKeyConstraint,
 ];
 
 export default migrations;
