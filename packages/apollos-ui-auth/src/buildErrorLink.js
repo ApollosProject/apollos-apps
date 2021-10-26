0cfb9e3c30d186b49d8bf87c6cc44328b07018aa
Michael Neeley <micneeley14@gmail.com>
2021-10-26 12:29:09 -0400

    chore: await token removal on error (#2235)
    Co-authored-by: Nathan Lewis <nathan@differential.com>

--- packages/apollos-ui-auth/src/buildErrorLink.js
+++ packages/apollos-ui-auth/src/buildErrorLink.js
@@ -2,17 +2,13 @@ import { onError } from '@apollo/client/link/error';
 import AsyncStorage from '@react-native-community/async-storage';
 
 export default (onAuthError) =>
-  onError(({ graphQLErrors, networkError }) => {
+  onError(({ graphQLErrors }) => {
     if (graphQLErrors)
-      graphQLErrors.map(({ extensions: { code } }) => {
+      graphQLErrors.map(async ({ extensions: { code } }) => {
         // wipe out all data and go somewhere
         if (code === 'UNAUTHENTICATED') {
-          AsyncStorage.removeItem('authToken');
+          await AsyncStorage.removeItem('authToken');
           onAuthError();
         }
-        return null;
       });
-
-    if (networkError) return null;
-    return null;
   });
