43b0e69050e93a37837a55b639df87d013b539f0
Vincent Wilson <vince@classyh.at>
2022-01-13 20:32:04 -0500

    Update mutaiton

--- packages/apollos-data-connector-postgres/src/multi-tenant/migrations/007_add_row_level_security_to_authentication_tables.js
+++ packages/apollos-data-connector-postgres/src/multi-tenant/migrations/007_add_row_level_security_to_authentication_tables.js
@@ -1,10 +1,6 @@
 import { Sequelize } from 'sequelize';
 
-const tables = [
-  'otp',
-  'open_id_identity',
-  'refresh_token'
-];
+const tables = ['otp', 'open_id_identity', 'refresh_token'];
 
 async function up({ context: queryInterface }) {
   await Promise.all(
@@ -25,36 +21,38 @@ async function up({ context: queryInterface }) {
       );
       await queryInterface.sequelize.query(
         `ALTER TABLE ${t} ALTER COLUMN church_slug SET DEFAULT current_user;`
-      );      
+      );
     })
-  );  
+  );
 
-  await queryInterface.removeConstraint('otp', 'otp_code_key', {})
+  await queryInterface.removeConstraint('otp', 'otp_code_key', {});
   await queryInterface.removeIndex('otp', 'otp_code_key');
-  await queryInterface.removeConstraint('otp', 'otp_identity_key', {})
+  await queryInterface.removeConstraint('otp', 'otp_identity_key', {});
   await queryInterface.removeIndex('otp', 'otp_identity_key');
 
   await queryInterface.addIndex('otp', {
     fields: ['church_slug', 'code'],
     unique: true,
-  });  
+  });
 
   await queryInterface.addIndex('otp', {
     fields: ['church_slug', 'identity'],
     unique: true,
-  });    
+  });
 }
 
 async function down({ context: queryInterface }) {
-  await Promise.all(tables.map(async (t) => {
-    await queryInterface.sequelize.query(
-      `DROP POLICY church_tenant_${t} ON ${t}`
-    );
-    await queryInterface.removeColumn(
-      t, // table name
-      'church_slug' // new field name
-    );
-  }));
+  await Promise.all(
+    tables.map(async (t) => {
+      await queryInterface.sequelize.query(
+        `DROP POLICY church_tenant_${t} ON ${t}`
+      );
+      await queryInterface.removeColumn(
+        t, // table name
+        'church_slug' // new field name
+      );
+    })
+  );
 }
 
 const name = '007-add-row-level-security-to-authentication-table';
