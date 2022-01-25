5fbb1cb03aedcf5f1c59284d47197d238383aa12
Ryan Davidson <ryanrkd2112@gmail.com>
2022-01-24 23:46:56 -0500

    Add enum value LINK_CODE migration

--- packages/apollos-data-connector-postgres/src/otp/migrations/index.js
+++ packages/apollos-data-connector-postgres/src/otp/migrations/index.js
@@ -1,5 +1,6 @@
 import AddOTPTable001 from './001_add_otp_table';
+import AddOTPTypeEnumValueLinkCode002 from './002_add_otp_type_enum_value_link_code';
 
-const migrations = [AddOTPTable001];
+const migrations = [AddOTPTable001, AddOTPTypeEnumValueLinkCode002];
 
 export default migrations;
