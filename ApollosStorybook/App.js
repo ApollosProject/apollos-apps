5b949881f64ba3cf2e576136f9cf2be4a80e3d30
Conrad VanLandingham <conrad@differential.com>
2022-01-31 09:39:57 -0600

    chore: upgrade and rebuild ApollosStorybook (#2368)

--- ApollosStorybook/App.js
+++ ApollosStorybook/App.js
@@ -1,26 +1,2 @@
-import { LogBox } from 'react-native';
-
-import StorybookUI, {
-  loadApollosStories,
-  addApollosProviderDecorator,
-  addDecorator,
-  configure,
-} from '@apollosproject/ui-storybook';
-import decorator from './src/decorator';
-
-import { loadStories } from './storybook/storyLoader';
-import { enableScreens } from 'react-native-screens';
-enableScreens();
-
-// Storybook issue. Might be solved by updating Core to RN 63
-LogBox.ignoreLogs(['null is not an object (evaluating \'story.id\')', 'Story with id', 'No permission handler detected.']);
-
-addDecorator(decorator);
-
-configure(() => {
-  require('./src/Welcome');
-  loadStories();
-  loadApollosStories();
-}, module);
-
-export default StorybookUI;
+export { default } from './storybook';
+// export default () => null;
--- ApollosStorybook/metro.config.js
+++ ApollosStorybook/metro.config.js
@@ -1,49 +1,26 @@
-// const blacklist = require('metro-config/src/defaults/blacklist');
-//
-// module.exports = {
-//   resolver: {
-//     blacklistRE: blacklist([
-//       /node_modules\/.*\/node_modules\/react-native\/.*/,
-//       /node_modules\/.*\/node_modules\/react-native-linear-gradient\/.*/,
-//       /node_modules\/.*\/node_modules\/react-native-svg\/.*/,
-//     ])
-//   },
-//   transformer: {
-//     getTransformOptions: async () => ({
-//       transform: {
-//         experimentalImportSupport: false,
-//         inlineRequires: false,
-//       },
-//     }),
-//   },
-// };
-
 const {
-    applyConfigForLinkedDependencies,
+  applyConfigForLinkedDependencies,
 } = require('@carimus/metro-symlinked-deps');
 
 module.exports = applyConfigForLinkedDependencies(
-    {
-        // transformer: {
-        //     getTransformOptions: async () => ({
-        //         transform: {
-        //             experimentalImportSupport: false,
-        //             inlineRequires: false,
-        //         },
-        //     }),
-        // },
+  {
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: false,
+        },
+      }),
     },
-    {
-        projectRoot: __dirname,
-        blacklistLinkedModules: [
-            'react',
-            'react-native',
-            'react-native-linear-gradient',
-            'react-native-svg',
-            'react-native-safe-area-context',
-            'react-native-gesture-handler',
-            '@storybook'
-        ],
-        resolveNodeModulesAtRoot: true,
-    },
+  },
+  {
+    projectRoot: __dirname,
+    blacklistLinkedModules: [
+      'react-native',
+      'react-native-linear-gradient',
+      'react-native-svg',
+      '@storybook',
+    ],
+    resolveNodeModulesAtRoot: true,
+  }
 );
