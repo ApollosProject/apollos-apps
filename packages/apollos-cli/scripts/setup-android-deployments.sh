fccd5a9dd62e0341dd7dc622621bc065b3250605
Michael Neeley <michael@neeley.dev>
2022-03-28 19:18:43 -0400

    chore: fixes some android deployment setup bugs (#2444)

--- packages/apollos-cli/scripts/setup-android-deployments.sh
+++ packages/apollos-cli/scripts/setup-android-deployments.sh
@@ -4,24 +4,26 @@ KEY=$1
 #echo "Creating metadata folders..."
 cp "$KEY" fastlane/google-api-key.json
 
-echo "Generating keystore..."
-STORE_PW=$(openssl rand 16 | base64 | sed "s/=//g")
-KEY_PW=$(openssl rand 16 | base64 | sed "s/=//g")
-keytool -genkey -v \
-  -keystore android/app/apollos.keystore \
-  -storepass "$STORE_PW" \
-  -keypass "$KEY_PW" \
-  -alias apollos \
-  -keyalg RSA \
-  -keysize 2048 \
-  -validity 10000
+if [ ! -f android/app/apollos.keystore ]; then
+  echo "Generating keystore..."
+  STORE_PW=$(openssl rand 16 | base64 | sed "s/[\/+=]//g")
+  KEY_PW=$(openssl rand 16 | base64 | sed "s/[\/+=]//g")
+  keytool -genkey -v \
+    -keystore android/app/apollos.keystore \
+    -storepass "$STORE_PW" \
+    -keypass "$KEY_PW" \
+    -alias apollos \
+    -keyalg RSA \
+    -keysize 2048 \
+    -validity 10000
 
-echo "
-KEYSTORE_FILE=apollos.keystore
-KEYSTORE_PASSWORD=$STORE_PW
-KEY_ALIAS=apollos
-KEY_PASSWORD=$KEY_PW" >>.env
-echo "Keystore generated at android/app/apollos.keystore! You may want to store externally as this is the only one you can ever use with this app."
+  echo "
+  KEYSTORE_FILE=apollos.keystore
+  KEYSTORE_PASSWORD=$STORE_PW
+  KEY_ALIAS=apollos
+  KEY_PASSWORD=$KEY_PW" >>.env
+  echo "Keystore generated at android/app/apollos.keystore! You may want to store externally as this is the only one you can ever use with this app."
+fi
 
 echo "Building Android bundle..."
 fastlane run gradle task:bundle build_type:Release project_dir:android >/dev/null 2>&1
