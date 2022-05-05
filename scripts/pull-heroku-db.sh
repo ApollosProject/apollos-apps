8f0d4bdb34a070aab3484a85506b8ab5b58e027a
Michael Neeley <michael@neeley.dev>
2022-05-05 16:03:12 -0400

    chore: clean up old server scripts and commands (#2488)
    * chore: clean up old server scripts and commands
    
    * put back builder

--- scripts/pull-heroku-db.sh
+++
@@ -1,4 +0,0 @@
-heroku pg:backups:capture --app apollos-church-api
-heroku pg:backups:download --app apollos-church-api
-pg_restore --verbose --clean --no-acl --no-owner -h localhost -d postgres latest.dump
-rm latest.dump
