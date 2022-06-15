c15e9533b8b27d95f2b2239546a2eecfbeaf4ff8
Nathan Lewis <nathan@differential.com>
2022-06-15 12:28:21 -0500

    chore: change styled to withTheme for better overrides (#2534)

--- packages/apollos-ui-kit/src/FollowList/index.js
+++ packages/apollos-ui-kit/src/FollowList/index.js
@@ -1,8 +1,8 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-import { View } from 'react-native';
-import styled from '../styled';
+import { View, StyleSheet } from 'react-native';
+import { named, useTheme } from '../theme';
 import { ImageSourceType } from '../ConnectedImage';
 import Card, { CardContent } from '../Card';
 import { withIsLoading } from '../isLoading';
@@ -10,15 +10,120 @@ import Button from '../Button';
 
 import FollowListItem from './FollowListItem';
 
-const HeaderView = styled(
-  ({ theme }) => ({
+const HeaderView = named('ui-kit.FollowList.Header')(View);
+
+const Content = named('ui-kit.FollowList.Content')(CardContent);
+
+const FullWidthButton = named('ui-kit.FollowList.FullWidthButton')(Button);
+
+function FollowList(props) {
+  const {
+    onPressFollowListButton,
+    onFollow,
+    onHide,
+    onConfirm,
+    followListButtonTitle,
+    followers,
+    header,
+    isLoading,
+    isCard,
+  } = props;
+
+  const RenderAsCard = ({ children }) =>
+    isCard ? <Card isLoading={isLoading}>{children}</Card> : children;
+
+  const showButton = onPressFollowListButton && followListButtonTitle;
+  const theme = useTheme();
+
+  if (followers?.length === 0 && !isLoading && !showButton) {
+    return null;
+  }
+
+  return (
+    <RenderAsCard>
+      <Content style={styles.headerView(theme, isCard)}>
+        <HeaderView style={styles.headerView(theme)}>
+          {header || null}
+        </HeaderView>
+        {followers.map((person) => {
+          const isFollowingState = person?.currentUserFollowing?.state;
+          const isFollowed = person?.followingCurrentUser?.state;
+          return (
+            <FollowListItem
+              key={person.id}
+              id={person.id}
+              requestingFollow={isFollowingState === 'REQUESTED'}
+              followRequested={isFollowed === 'REQUESTED'}
+              confirmedFollowing={isFollowingState === 'ACCEPTED'}
+              confirmedFollower={isFollowed === 'ACCEPTED'}
+              name={
+                [person.firstName, person.lastName]
+                  .filter((name) => Boolean(name))
+                  .join(' ') || ''
+              }
+              profile={person}
+              onFollow={() => onFollow?.({ personId: person.id })}
+              onHide={() =>
+                onHide?.({
+                  personId: person.id,
+                  requestId: person?.followingCurrentUser?.id,
+                })
+              }
+              onConfirm={() =>
+                onConfirm?.({
+                  personId: person.id,
+                  requestId: person?.followingCurrentUser?.id,
+                })
+              }
+            />
+          );
+        })}
+        {showButton ? (
+          <FullWidthButton
+            title={followListButtonTitle}
+            type={'default'}
+            pill={false}
+            bordered
+            onPress={onPressFollowListButton}
+            style={styles.fullWidthButton(theme)}
+          />
+        ) : null}
+      </Content>
+    </RenderAsCard>
+  );
+}
+
+FollowList.propTypes = {
+  followers: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      firstName: PropTypes.string,
+      lastName: PropTypes.string,
+      image: ImageSourceType,
+      request: PropTypes.bool,
+      requested: PropTypes.bool,
+      confirmed: PropTypes.bool,
+    })
+  ),
+  followListButtonTitle: PropTypes.string,
+  isCard: PropTypes.bool,
+  header: PropTypes.element,
+  isLoading: PropTypes.bool,
+  onPressFollowListButton: PropTypes.func,
+  onFollow: PropTypes.func,
+  onHide: PropTypes.func,
+  onConfirm: PropTypes.func,
+};
+
+FollowList.defaultProps = {
+  followers: [],
+};
+
+const styles = StyleSheet.create({
+  headerView: (theme) => ({
     marginBottom: theme.sizing.baseUnit,
   }),
-  'ui-kit.FollowList.Header'
-)(View);
-
-const Content = styled(
-  ({ theme, cardPadding }) => ({
+  content: (theme, cardPadding) => ({
     paddingHorizontal: cardPadding
       ? theme.sizing.baseUnit * 1.5
       : theme.sizing.baseUnit,
@@ -26,122 +131,11 @@ const Content = styled(
       ? theme.sizing.baseUnit * 1.5
       : theme.sizing.baseUnit,
   }),
-  'ui-kit.FollowList.Content'
-)(CardContent);
-
-const FullWidthButton = styled(
-  ({ theme }) => ({
+  fullWidthButton: (theme) => ({
     width: '100%', // fixes loading state not showing up at 100% width
     marginTop: theme.sizing.baseUnit * 0.5,
   }),
-  'ui-kit.FollowList.FullWidthButton'
-)(Button);
-
-class FollowList extends PureComponent {
-  static propTypes = {
-    followers: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.string,
-        firstName: PropTypes.string,
-        lastName: PropTypes.string,
-        image: ImageSourceType,
-        request: PropTypes.bool,
-        requested: PropTypes.bool,
-        confirmed: PropTypes.bool,
-      })
-    ),
-    followListButtonTitle: PropTypes.string,
-    isCard: PropTypes.bool,
-    header: PropTypes.element,
-    isLoading: PropTypes.bool,
-    onPressFollowListButton: PropTypes.func,
-    onFollow: PropTypes.func,
-    onHide: PropTypes.func,
-    onConfirm: PropTypes.func,
-  };
-
-  static defaultProps = {
-    followers: [],
-  };
-
-  RenderAsCard = ({ children }) =>
-    this.props.isCard ? (
-      <Card isLoading={this.props.isLoading}>{children}</Card>
-    ) : (
-      children
-    );
-
-  render() {
-    const {
-      onPressFollowListButton,
-      onFollow,
-      onHide,
-      onConfirm,
-      followListButtonTitle,
-      followers,
-      header,
-      isLoading,
-    } = this.props;
-
-    const { RenderAsCard } = this;
-
-    const showButton = onPressFollowListButton && followListButtonTitle;
-
-    if (followers?.length === 0 && !isLoading && !showButton) {
-      return null;
-    }
-
-    return (
-      <RenderAsCard>
-        <Content cardPadding={this.props.isCard}>
-          <HeaderView>{header || null}</HeaderView>
-          {followers.map((person) => {
-            const isFollowingState = person?.currentUserFollowing?.state;
-            const isFollowed = person?.followingCurrentUser?.state;
-            return (
-              <FollowListItem
-                key={person.id}
-                id={person.id}
-                requestingFollow={isFollowingState === 'REQUESTED'}
-                followRequested={isFollowed === 'REQUESTED'}
-                confirmedFollowing={isFollowingState === 'ACCEPTED'}
-                confirmedFollower={isFollowed === 'ACCEPTED'}
-                name={
-                  [person.firstName, person.lastName]
-                    .filter((name) => Boolean(name))
-                    .join(' ') || ''
-                }
-                profile={person}
-                onFollow={() => onFollow?.({ personId: person.id })}
-                onHide={() =>
-                  onHide?.({
-                    personId: person.id,
-                    requestId: person?.followingCurrentUser?.id,
-                  })
-                }
-                onConfirm={() =>
-                  onConfirm?.({
-                    personId: person.id,
-                    requestId: person?.followingCurrentUser?.id,
-                  })
-                }
-              />
-            );
-          })}
-          {showButton ? (
-            <FullWidthButton
-              title={followListButtonTitle}
-              type={'default'}
-              pill={false}
-              bordered
-              onPress={onPressFollowListButton}
-            />
-          ) : null}
-        </Content>
-      </RenderAsCard>
-    );
-  }
-}
+});
 
 export { FollowListItem };
 export default withIsLoading(FollowList);
