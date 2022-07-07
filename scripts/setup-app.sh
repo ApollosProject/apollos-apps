45e845fee77d4599ed607ade9e91f890ed8d8be1
Vincent Wilson <vince@classyh.at>
2022-07-07 10:59:36 -0400

    Sub in apple team id

--- scripts/setup-app.sh
+++ scripts/setup-app.sh
@@ -10,11 +10,13 @@ ICON_BG_COLOR=$(curl -s --fail -H "x-api-key: $APOLLOS_API_KEY" -H "accept: text
 LOGO=$(curl -s --fail -H "x-api-key: $APOLLOS_API_KEY" -H "accept: text/plain" "$API_URL/config/$CHURCH/APP.LOGO_URL")
 WORDMARK=$(curl -s --fail -H "x-api-key: $APOLLOS_API_KEY" -H "accept: text/plain" "$API_URL/config/$CHURCH/APP.WORDMARK_URL")
 IOS_BUNDLE=$(curl -s --fail -H "x-api-key: $APOLLOS_API_KEY" -H "accept: text/plain" "$API_URL/config/$CHURCH/APP.APPLE_BUNDLE_ID")
+APPLE_TEAM_ID=$(curl -s --fail -H "x-api-key: $APOLLOS_API_KEY" -H "accept: text/plain" "$API_URL/config/$CHURCH/APP.APPLE_TEAM_ID")
 ANDROID_BUNDLE=$(curl -s --fail -H "x-api-key: $APOLLOS_API_KEY" -H "accept: text/plain" "$API_URL/config/$CHURCH/APP.ANDROID_PKG_ID")
 SERVER_URL=$(curl -s --fail -H "x-api-key: $APOLLOS_API_KEY" -H "accept: text/plain" "$API_URL/config/$CHURCH/APP.SERVER_URL")
 GOOGLE_MAPS_KEY=$(curl -s --fail -H "x-api-key: $APOLLOS_API_KEY" -H "accept: text/plain" "$API_URL/config/$CHURCH/APP_GOOGLE_MAPS_API_KEY")
 THEME=$(curl -s --fail -H "x-api-key: $APOLLOS_API_KEY" -H "accept: text/plain" "$API_URL/config/$CHURCH/APP.THEME")
-if [ -z "$APP" ] || [ -z "$ICON" ] || [ -z "$ICON_BG_COLOR" ] || [ -z "$LOGO" ] || [ -z "$WORDMARK" ] || [ -z "$IOS_BUNDLE" ] || [ -z "$ANDROID_BUNDLE" ] || [ -z "$SERVER_URL" ] || [ -z "$GOOGLE_MAPS_KEY" ] || [ -z "$THEME" ]; then
+
+if [ -z "$APP" ] || [ -z "$ICON" ] || [ -z "$ICON_BG_COLOR" ] || [ -z "$LOGO" ] || [ -z "$WORDMARK" ] || [ -z "$IOS_BUNDLE" ] || [ -z "$ANDROID_BUNDLE" ] || [ -z "$SERVER_URL" ] || [ -z "$GOOGLE_MAPS_KEY" ] || [ -z "$THEME" ] || [ -z "$APPLE_TEAM_ID" ]; then
     echo "Missing some variables:
     APP.NAME=$APP
     APP.ICON_URL=$ICON
@@ -25,6 +27,7 @@ if [ -z "$APP" ] || [ -z "$ICON" ] || [ -z "$ICON_BG_COLOR" ] || [ -z "$LOGO" ] 
     APP.ANDROID_PKG_ID=$ANDROID_BUNDLE
     APP.SERVER_URL=$SERVER_URL
     APP_GOOGLE_MAPS_API_KEY=$GOOGLE_MAPS_KEY
+    APP.APPLE_TEAM_ID=$APPLE_TEAM_ID
     APP.THEME=$THEME"
     exit 1
 fi
@@ -51,8 +54,8 @@ sed -i "" -E "s/com\.differential\.apollos/$IOS_BUNDLE/g" "ios/$CLEAN_APP/$CLEAN
 sed -i "" -E "s/apollos-demo/$SUBDOMAIN/g" "ios/$CLEAN_APP/$CLEAN_APP.entitlements"
 sed -i "" -E "s/com\.differential\.apollos/$IOS_BUNDLE/g" ios/OneSignalNotificationServiceExtension/OneSignalNotificationServiceExtension.entitlements
 sed -i "" -E "s/com\.differential\.apollos/$IOS_BUNDLE/g" "ios/$CLEAN_APP.xcodeproj/project.pbxproj"
-sed -i "" -E "s/DEVELOPMENT_TEAM = [[:alnum:]]+;/DEVELOPMENT_TEAM = \"\";/g" "ios/$CLEAN_APP.xcodeproj/project.pbxproj"
-sed -i "" -E "s/DevelopmentTeam = [[:alnum:]]+;/DevelopmentTeam = \"\";/g" "ios/$CLEAN_APP.xcodeproj/project.pbxproj"
+sed -i "" -E "s/DEVELOPMENT_TEAM = [[:alnum:]]+;/DEVELOPMENT_TEAM = $APPLE_TEAM_ID;/g" "ios/$CLEAN_APP.xcodeproj/project.pbxproj"
+sed -i "" -E "s/DevelopmentTeam = [[:alnum:]]+;/DevelopmentTeam = $APPLE_TEAM_ID;/g" "ios/$CLEAN_APP.xcodeproj/project.pbxproj"
 
 rm "ios/$CLEAN_APP.xcworkspace/xcshareddata/xcschemes/apolloschurchapp.xcscheme"
 
