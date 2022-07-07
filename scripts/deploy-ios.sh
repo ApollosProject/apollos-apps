e59ad7fb0c3e54410f1954e9d324bbe85c07d53d
Vincent Wilson <vince@classyh.at>
2022-07-07 15:27:14 -0400

    Bump minor version number

--- scripts/deploy-ios.sh
+++ scripts/deploy-ios.sh
@@ -1,15 +1,15 @@
 TRACK=$2
 
 if [ -z "$CHURCH" ] || [ -z "$APOLLOS_API_KEY" ]; then
-  echo "CHURCH and APOLLOS_API_KEY required!"
-  exit 1
+    echo "CHURCH and APOLLOS_API_KEY required!"
+    exit 1
 fi
 
-if [ -n "$(git status --porcelain)" ]; then
-  echo "Git status not clean!"
-  git status --porcelain
-  exit 1
-fi
+# if [ -n "$(git status --porcelain)" ]; then
+#     echo "Git status not clean!"
+#     git status --porcelain
+#     exit 1
+# fi
 
 cd deploys/mobile || exit 1
 
@@ -24,20 +24,24 @@ APPLE_API_KEY=$(curl -s --fail -H "accept: text/plain" -H "x-api-key: $APOLLOS_A
 printf "%s" "$APPLE_API_KEY" >fastlane/apple-api-key.json
 
 APP_ID=$(grep "PRODUCT_BUNDLE_IDENTIFIER.*One" ios/*.xcodeproj/project.pbxproj |
-  sed -n 1p |
-  sed -E "s/.*PRODUCT_BUNDLE_IDENTIFIER = \"?(.*).One.*/\1/")
+    sed -n 1p |
+sed -E "s/.*PRODUCT_BUNDLE_IDENTIFIER = \"?(.*).One.*/\1/")
 
 TARGET=$(grep "target" ios/Podfile | sed -n 1p | sed -E "s/target '(.*)'.*/\1/")
 BUILD_NUMBER=$(git rev-list --count HEAD)
-VERSION=$(NO_COLOR=1 fastlane run get_version_number xcodeproj:ios target:"$TARGET" |
-  grep Result |
-  sed -E "s/.*Result: ([\d\.]*)/\1/")
+
+
+CURRENT_VERSION=$(curl http://itunes.apple.com/lookup\?bundleId\=$APP_ID | grep "version" | sed 's/.*"version":"\([0-9\.]*\)".*/\1/')
+
+# Bumps patch numer
+# From https://stackoverflow.com/questions/8653126/how-to-increment-version-number-in-a-shell-script
+VERSION=$(echo ${CURRENT_VERSION} | awk -F. -v OFS=. '{$NF += 1 ; print}')
 
 # use default keychain for local deployments
 LOCAL=$(fastlane run setup_ci | grep "skipping CI")
 if [ -z "$LOCAL" ]; then
-  export MATCH_KEYCHAIN_NAME="fastlane_tmp_keychain"
-  export MATCH_KEYCHAIN_PASSWORD=""
+    export MATCH_KEYCHAIN_NAME="fastlane_tmp_keychain"
+    export MATCH_KEYCHAIN_PASSWORD=""
 fi
 
 export MATCH_GIT_BASIC_AUTHORIZATION
@@ -47,20 +51,21 @@ export FASTLANE_ITC_TEAM_NAME
 export FASTLANE_TEAM_ID
 
 if [ "$TRACK" = "internal" ] || [ -z "$TRACK" ]; then
-  fastlane match appstore -a "$APP_ID,$APP_ID.OneSignalNotificationServiceExtension" --git-branch "$CHURCH" --readonly
-  fastlane run increment_build_number build_number:"$BUILD_NUMBER" xcodeproj:"ios/$TARGET.xcodeproj"
-  fastlane run build_app scheme:"$TARGET" workspace:"ios/$TARGET.xcworkspace"
-  fastlane run testflight api_key_path:fastlane/apple-api-key.json
-elif [ "$TRACK" = "beta" ]; then
-  fastlane run testflight api_key_path:fastlane/apple-api-key.json \
+    fastlane match appstore -a "$APP_ID,$APP_ID.OneSignalNotificationServiceExtension" --git-branch "$CHURCH" --readonly
+    fastlane run increment_build_number build_number:"$BUILD_NUMBER" xcodeproj:"ios/$TARGET.xcodeproj"
+    fastlane run increment_version_number version_number:"$VERSION" xcodeproj:"ios/$TARGET.xcodeproj"
+    fastlane run build_app scheme:"$TARGET" workspace:"ios/$TARGET.xcworkspace"
+    fastlane run testflight api_key_path:fastlane/apple-api-key.json
+    elif [ "$TRACK" = "beta" ]; then
+    fastlane run testflight api_key_path:fastlane/apple-api-key.json \
     app_identifier:"$APP_ID" \
     distribute_only:true \
     distribute_external:true \
     groups:"Beta Testers" \
     app_platform:ios \
     build_number:"$BUILD_NUMBER"
-elif [ "$TRACK" = "production" ]; then
-  fastlane deliver --api_key_path fastlane/apple-api-key.json \
+    elif [ "$TRACK" = "production" ]; then
+    fastlane deliver --api_key_path fastlane/apple-api-key.json \
     --skip_binary_upload true \
     --overwrite_screenshots false \
     --submit_for_review true \
